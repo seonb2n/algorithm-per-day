721e3ea48380a039cb25e441b5f0c884979b8f98
seonb2n <626ksb@gmail.com>
2022-01-03 21:20:47 +0900

    백준 알고리즘 4673번

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int caseNumber = Integer.parseInt(br.readLine());
-        String[] caseStrings = new String[caseNumber];
+    public static void main(String[] args) {
 
-        for(int i=0; i<caseStrings.length; i++) {
-            int[] studentNumber = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int result = 0;
-            int count = 0;
-            for(int k = 0; k < studentNumber[0]; k++) {
-                result += studentNumber[k+1];
-            }
-            int average = result / (studentNumber.length-1);
-            for(int k = 0; k < studentNumber[0]; k++) {
-                count = (studentNumber[k+1] > average) ? ++count : count;
-            }
+        HashSet<Integer> set = new HashSet<>();
 
-            caseStrings[i] = String.format("%.3f", ((float)count / studentNumber[0]) * 100);
+        for (int i = 0; i < 9999; i++) {
+            set.add(i+1);
         }
 
-        for (String caseString : caseStrings) {
-            System.out.println(caseString + "%");
+        for (int i = 0; i < 10000; i++) {
+            set.remove(getNumber(i));
+        }
+
+        for (Integer integer : set) {
+            System.out.println(integer);
         }
 
     }
+
+    public static int getNumber(int a) {
+        int[] numbers = Arrays.stream(String.valueOf(a).split("")).mapToInt(Integer::parseInt).toArray();
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        result += a;
+        return result;
+    }
+
 }
