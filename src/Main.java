501939744fad08272aae61e1cabd308decad153e
seonb2n <626ksb@gmail.com>
2022-01-05 21:45:12 +0900

    백준 알고리즘 2992번

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String word = br.readLine();
-        int[] numbers = Arrays.stream(word.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int inputNumber = Integer.parseInt(word);
 
-        int price = numbers[0];
-        int fixedCost = numbers[1];
-        int variableCost = numbers[2];
-
-        int stockNumber;
-
-        if(variableCost == fixedCost) {
-            System.out.println(-1);
-        } else {
-            stockNumber = (int) Math.floor(price / (variableCost - fixedCost)) + 1;
-            if(stockNumber > 0 ) {
-                System.out.println(stockNumber);
-            } else {
-                System.out.println(-1);
-            }
+        int result = 0;
+        while(result * (result + 1) * 3 + 2 < inputNumber) {
+            result++;
         }
+
+        if(result * (result + 1) * 3 + 2 == inputNumber) {
+            System.out.println(result+2);
+        } else {
+            System.out.println(result+1);
+        }
+
     }
 }
