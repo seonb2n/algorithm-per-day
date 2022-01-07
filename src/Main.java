aec9e5a9419d54fe6d72176da676687e1823c3c1
seonb2n <626ksb@gmail.com>
2022-01-07 21:03:15 +0900

    백준 알고리즘 2869번

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String word = br.readLine();
-        int inputNumber = Integer.parseInt(word);
 
-        int n = 1;
-        int x = 0;
-        int y = 0;
-        int distance = 0;
+        long[] numbers = Arrays.stream(word.split(" ")).mapToLong(Long::parseLong).toArray();
 
-        while(calcaulateStartN(n) <= inputNumber) {
-            n++;
-        }
+        long dayHeight = numbers[0];
+        long nightHeight = numbers[1];
+        long treeHeight = numbers[2];
 
-        n = n - 1;
+        long a = treeHeight - nightHeight;
+        long b= dayHeight - nightHeight;
 
-        if(n % 2 == 0) {
-            x = n - 1;
-            y = 0;
-            distance = inputNumber - calcaulateStartN(n);
-            x = x-distance;
-            y = y + distance;
+        long num = a /b;
+        if(a % b == 0) {
+            System.out.println(num);
         } else {
-            x = 0;
-            y = n -1;
-            distance = inputNumber - calcaulateStartN(n);
-            x = x + distance;
-            y = y - distance;
+            System.out.println(num + 1);
         }
-
-        System.out.println((y+1) + "/" + (x+1));
-
-
-    }
-
-    public static int calcaulateStartN(int n) {
-        return (n * n - n + 2) / 2;
     }
 }
