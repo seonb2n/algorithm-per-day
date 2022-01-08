b1a8f098e8f9e6060097230200e2197e4a6934be
seonb2n <626ksb@gmail.com>
2022-01-08 17:31:11 +0900

    백준 알고리즘 11729번

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCaseNumber = Integer.parseInt(br.readLine());
+        List<String> plates = new ArrayList<>();
 
-        makePattern(testCaseNumber);
+        for (int i = 0; i < testCaseNumber; i++) {
+            plates.add(String.valueOf(i + 1));
+        }
 
+        movePlates(testCaseNumber, "1", "3");
+
+        System.out.println(count);
         System.out.print(sb);
-
     }
 
-    public static void makePattern(int totalNumber) {
-        for (int i = 1; i <= totalNumber; i++) {
-            drawLine(totalNumber, i);
+    public static void movePlates(int platesNumber, String fromTop, String toTop) {
+        if (platesNumber == 1) {
+            sb.append(fromTop + " " + toTop);
             sb.append("\n");
-        }
-    }
-
-    //주어진 n 의 값에 해당하는 줄 그리기
-    public static void drawLine(int totalNUmber, int currentNumber) {
-
-        if(totalNUmber == 1) {
-            sb.append("*");
+            count++;
         } else {
-            totalNUmber = totalNUmber / 3;
-            //만약 지금 줄이 공백이 포함된 줄이면
-            if(currentNumber >= totalNUmber + 1 && currentNumber <= totalNUmber * 2) {
-                //3번 호출하는 것이 아니라, 1번 띄고 1번 호출
-                currentNumber = currentNumber % totalNUmber;
-                drawLine(totalNUmber, currentNumber);
-                drawBlank(totalNUmber);
-                drawLine(totalNUmber, currentNumber);
-            } else {
-                //공백이 포함되지 않은 줄이면, 하위 패턴의 줄 그리기
-                currentNumber = currentNumber % totalNUmber;
-                drawLine(totalNUmber, currentNumber);
-                drawLine(totalNUmber, currentNumber);
-                drawLine(totalNUmber, currentNumber);
-            }
+
+            String middleTop = getMiddleTop(fromTop, toTop);
+            //n - 1번째 까지의 원판을 fromTop -> middleTop 으로 옮긴다.
+            movePlates((platesNumber - 1), fromTop, middleTop);
+
+            //n 번째 원판을 fromTop > toTop 으로 옮긴다.
+            sb.append(fromTop + " " + toTop);
+            sb.append("\n");
+            count++;
+
+            //n - 1번째 까지의 원판을 middleTop -> toTop으로 옮긴다.
+            movePlates((platesNumber - 1), middleTop, toTop);
+
         }
     }
 
-    public static void drawBlank(int totalNumber) {
-        for (int i = 0; i < totalNumber; i++) {
-            sb.append(" ");
-        }
+    public static String getMiddleTop(String toTop, String fromTop) {
+        return String.valueOf(6 - Integer.parseInt(toTop) - Integer.parseInt(fromTop));
     }
-
 }
