2ab39ec1af90e7a6975cbfc5b8e3f6fd3b659462
seonb2n <626ksb@gmail.com>
2022-01-09 13:56:29 +0900

    백준 알고리즘 2108번

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -9,22 +11,60 @@ public class Main {
         int testCaseNumber = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int[] numbers = new int[10001];
+        int[] numbers = new int[8001];
+        List<Integer> sortedNumber = new ArrayList<>();
+
+        double mean = 0;
+        int median;
+        int mode = 0;
+        int range;
+
+        //가장 많이 존재하는 값
+        int max = 0;
+        //겹칠 때, 두번째임을 확인
+        boolean isSecond = false;
 
         for (int i = 0; i < testCaseNumber; i++) {
-            int result = Integer.parseInt(br.readLine());
-            numbers[result]++;
+            int number = Integer.parseInt(br.readLine());
+            numbers[number + 4000]++;
+            mean += number;
         }
+
+        //평균 구하기
+        mean =Math.round(mean / testCaseNumber);
+        sb.append((int) mean + "\n");
 
         for (int i = 0; i < numbers.length; i++) {
             if(numbers[i] != 0) {
-                for (int k = 0; k < numbers[i]; k++) {
-                    sb.append(i + "\n");
+
+                if(numbers[i] > max) {
+                    max = numbers[i];
+                    mode = i-4000;
+                    isSecond = false;
+                } else if(numbers[i] == max) {
+                    if(!isSecond) {
+                        max = numbers[i];
+                        mode = i-4000;
+                        isSecond = true;
+                    }
+                }
+
+                for (int j = 0; j < numbers[i]; j++) {
+                    sortedNumber.add(i - 4000);
                 }
             }
         }
+        //중앙값 구하기
+        median = sortedNumber.get(testCaseNumber / 2);
+        sb.append(median + "\n");
 
-        System.out.println(sb);
+        sb.append(mode + "\n");
+
+
+        //범위 구하기
+        range = sortedNumber.get(sortedNumber.size() - 1) - sortedNumber.get(0);
+        sb.append(range);
+
+        System.out.println(sb + "\n");
     }
-
 }
