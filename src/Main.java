41cd61810d5af0a0ec83e2cbaf2492255c378349
seonb2n <626ksb@gmail.com>
2022-01-09 12:53:43 +0900

    백준 알고리즘 2751번

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
-
-
-    static List<Person> personList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCaseNumber = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        boolean[] booleans = new boolean[2000001];
+
         for (int i = 0; i < testCaseNumber; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
-            personList.add(
-                    new Person(Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()))
-            );
+            int number = Integer.parseInt(br.readLine());
+                booleans[number+1000000] = true;
         }
 
-        for (Person person : personList) {
-            person.rank = getRank(person);
-            sb.append(person.rank + " ");
-        }
-
-        System.out.println(sb);
-
-
-
-    }
-
-    private static int getRank(Person person) {
-        int result = 1;
-
-        for (int i = 0; i < personList.size(); i++) {
-            if(person.height < personList.get(i).height && person.weight < personList.get(i).weight) {
-                result++;
+        for (int i = 0; i < booleans.length; i++) {
+            if (booleans[i]) {
+                sb.append(i - 1000000 + "\n");
             }
         }
 
-        return result;
+        System.out.println(sb);
     }
-}
-
-class Person {
-
-    public Person(int weight, int height) {
-        this.weight = weight;
-        this.height = height;
-        rank = 0;
-    }
-
-    int weight;
-    int height;
-    int rank;
-
 }
