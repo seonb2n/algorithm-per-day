2a447d516ec9d8828b0ca621f8955ca131ae3069
seonb2n <626ksb@gmail.com>
2022-01-09 13:16:58 +0900

    백준 알고리즘 10989번

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,22 @@ public class Main {
         int testCaseNumber = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        boolean[] booleans = new boolean[2000001];
+        int[] numbers = new int[10001];
 
         for (int i = 0; i < testCaseNumber; i++) {
-            int number = Integer.parseInt(br.readLine());
-                booleans[number+1000000] = true;
+            int result = Integer.parseInt(br.readLine());
+            numbers[result]++;
         }
 
-        for (int i = 0; i < booleans.length; i++) {
-            if (booleans[i]) {
-                sb.append(i - 1000000 + "\n");
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] != 0) {
+                for (int k = 0; k < numbers[i]; k++) {
+                    sb.append(i + "\n");
+                }
             }
         }
 
         System.out.println(sb);
     }
+
 }
