b9d90dbfbdd9af789175709e77b87334959a4af4
seonb2n <626ksb@gmail.com>
2022-01-10 22:43:24 +0900

    백준 알고리즘 15649번 - 백트래킹 사용

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
+    static int[] numbers;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] numbers = new int[2];
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
 
         numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         //1부터 numbers[0] 까지의 자연수 중에서
         //numbers[1] 개로 구성된 수열을 출력
 
-        List<Integer> values = new ArrayList<>();
-        for (int i = 1; i <= numbers[0]; i++) {
-            values.add(i);
-        }
+        int[] numberList  = new int[numbers[1]];
 
-        String result = "";
-        addNumber(values, sb, numbers[1], result);
+        back_tracking(0, numberList);
 
         System.out.println(sb);
 
     }
 
+    public static void back_tracking(int numberCount, int[] numberList) {
 
-    //숫자를 추가해주는 메서드
-    public static void addNumber(List<Integer> numArr , StringBuilder sb, int numberCount, String result) {
-        if (numberCount == 0) {
-            sb.append(result);
+        if(numberCount == numbers[1]) {
+            //숫자의 개수가 numbers[1]개와 동일하다면
+            //앞서 있던 모든 조건을 만족한다는 뜻이므로 답을 출력
+            for (int i : numberList) {
+                sb.append(i);
+                sb.append(" ");
+            }
             sb.append("\n");
-        }
 
-        if (numberCount != 0) {
-            for (Integer integer : numArr) {
-                result += integer + " ";
-                List<Integer> numArr2 = new ArrayList<>(numArr);
-                numArr2.remove(integer);
-                addNumber(numArr2, sb, numberCount-1, result);
-                result = result.substring(0, result.length()-2);
+        } else {
+            //현재 상황 전까지는 조건대로 되어 있고,
+            // 현재 상황인 numberCount 에 숫자를 추가하는 상황
+            for(int i = 1; i<=numbers[0]; i++) {
+                //1부터 numbers[0] 까지의 자연수 모두를 따져본다.
+                int finalI = i;
+                if(!Arrays.stream(numberList).filter(s -> s == finalI).findAny().isPresent()) {
+                    //numberList 에 숫자가 없다면
+                    numberList[numberCount] = i;
+                    //지금의 numberList 상태를 저장하기 위해 clone 을 해줌
+                    int[] numberList2 = numberList.clone();
+                    //다음 차례로 진입
+                    back_tracking(numberCount+1, numberList2);
+                }
             }
         }
     }
-
 }
