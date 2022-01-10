f42d9c2f719cdbf6feb058e4dcb9d15d553f73b0
seonb2n <626ksb@gmail.com>
2022-01-10 23:20:22 +0900

    백준 알고리즘 15650번

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     static int[] numbers;
     static StringBuilder sb;
+    static int[] numberList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,20 +15,16 @@ public class Main {
 
         numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        //1부터 numbers[0] 까지의 자연수 중에서
-        //numbers[1] 개로 구성된 수열을 출력
+        numberList = new int[numbers[1]];
 
-        int[] numberList  = new int[numbers[1]];
-
-        back_tracking(0, numberList);
-
+        dfs(1, 0);
         System.out.println(sb);
 
     }
 
-    public static void back_tracking(int numberCount, int[] numberList) {
+    public static void dfs(int startNumber, int numberCount) {
 
-        if(numberCount == numbers[1]) {
+        if (numberCount == numbers[1]) {
             //숫자의 개수가 numbers[1]개와 동일하다면
             //앞서 있던 모든 조건을 만족한다는 뜻이므로 답을 출력
             for (int i : numberList) {
@@ -35,22 +32,15 @@ public class Main {
                 sb.append(" ");
             }
             sb.append("\n");
+            return;
+        }
 
-        } else {
-            //현재 상황 전까지는 조건대로 되어 있고,
-            // 현재 상황인 numberCount 에 숫자를 추가하는 상황
-            for(int i = 1; i<=numbers[0]; i++) {
-                //1부터 numbers[0] 까지의 자연수 모두를 따져본다.
-                int finalI = i;
-                if(!Arrays.stream(numberList).filter(s -> s == finalI).findAny().isPresent()) {
-                    //numberList 에 숫자가 없다면
-                    numberList[numberCount] = i;
-                    //지금의 numberList 상태를 저장하기 위해 clone 을 해줌
-                    int[] numberList2 = numberList.clone();
-                    //다음 차례로 진입
-                    back_tracking(numberCount+1, numberList2);
-                }
-            }
+        //현재 상황 전까지는 조건대로 되어 있고,
+        // 현재 상황인 numberCount 에 숫자를 추가하는 상황
+        for (int i = startNumber; i <= numbers[0]; i++) {
+            //전의 경우보다 큰 숫자부터 탐색하며 된다.
+            numberList[numberCount] = i;
+            dfs(i + 1, numberCount + 1);
         }
     }
 }
