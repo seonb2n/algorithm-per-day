8c70c3534302f06149364b205d61d69624d03104
seonb2n <626ksb@gmail.com>
2022-01-10 21:02:21 +0900

    백준 알고리즘 15649번

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCaseNumber = Integer.parseInt(br.readLine());
+        int[] numbers = new int[2];
         StringBuilder sb = new StringBuilder();
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
-        int[] values = new int[testCaseNumber];
 
-        //값 부여
-        for (int i = 0; i < testCaseNumber; i++) {
-            int number = Integer.parseInt(stringTokenizer.nextToken());
-            values[i] = number;
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        //1부터 numbers[0] 까지의 자연수 중에서
+        //numbers[1] 개로 구성된 수열을 출력
+
+        List<Integer> values = new ArrayList<>();
+        for (int i = 1; i <= numbers[0]; i++) {
+            values.add(i);
         }
 
-        
-        int[] sortedValues = values.clone();
-        Arrays.sort(sortedValues);
-
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        int rank = 0;
-
-        hashMap.put(sortedValues[0], rank);
-        for (int i = 1; i < sortedValues.length; i++) {
-            if(sortedValues[i] != sortedValues[i-1]) {
-                rank++;
-            }
-            hashMap.put(sortedValues[i], rank);
-        }
-
-        for (int i = 0; i < values.length; i++) {
-            sb.append(hashMap.get(values[i]) + " ");
-        }
+        String result = "";
+        addNumber(values, sb, numbers[1], result);
 
         System.out.println(sb);
+
+    }
+
+
+    //숫자를 추가해주는 메서드
+    public static void addNumber(List<Integer> numArr , StringBuilder sb, int numberCount, String result) {
+        if (numberCount == 0) {
+            sb.append(result);
+            sb.append("\n");
+        }
+
+        if (numberCount != 0) {
+            for (Integer integer : numArr) {
+                result += integer + " ";
+                List<Integer> numArr2 = new ArrayList<>(numArr);
+                numArr2.remove(integer);
+                addNumber(numArr2, sb, numberCount-1, result);
+                result = result.substring(0, result.length()-2);
+            }
+        }
     }
 
 }
