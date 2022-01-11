f4040f253c043b6554f6fc9d62bbe1ae6c78e0a8
seonb2n <626ksb@gmail.com>
2022-01-11 22:01:06 +0900

    백준 알고리즘 15651번

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,12 @@ public class Main {
 
         numberList = new int[numbers[1]];
 
-        dfs(1, 0);
+        dfs(0);
         System.out.println(sb);
 
     }
 
-    public static void dfs(int startNumber, int numberCount) {
+    public static void dfs(int numberCount) {
 
         if (numberCount == numbers[1]) {
             //숫자의 개수가 numbers[1]개와 동일하다면
@@ -37,10 +37,9 @@ public class Main {
 
         //현재 상황 전까지는 조건대로 되어 있고,
         // 현재 상황인 numberCount 에 숫자를 추가하는 상황
-        for (int i = startNumber; i <= numbers[0]; i++) {
-            //전의 경우보다 큰 숫자부터 탐색하며 된다.
+        for (int i = 1; i <= numbers[0]; i++) {
             numberList[numberCount] = i;
-            dfs(i + 1, numberCount + 1);
+            dfs(numberCount + 1);
         }
     }
 }
