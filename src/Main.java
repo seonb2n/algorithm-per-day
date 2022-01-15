3ae5091767e8325437ee54269c2701005e5ba497
seonb2n <626ksb@gmail.com>
2022-01-15 18:45:35 +0900

    백준 알고리즘 1149번

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     static StringBuilder sb;
-    static int[] tiles;
+    static int houseNumber;
+    static int[][] costs;
+    static int[] chosenColor;
+    static int[] chosenCosts;
+    static int[] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        tiles = new int[1000001];
+        houseNumber = Integer.parseInt(br.readLine());
+        costs = new int[houseNumber+1][3];
+        chosenColor = new int[houseNumber+1];
+        chosenColor[0] = -999;
+        chosenCosts = new int[houseNumber+1];
+        chosenCosts[0] = -999;
+        result = new int[houseNumber+1];
 
-        tiles[1] = 1;
-        tiles[2] = 2;
-        tiles[3] = 3;
-        tiles[4] = 5;
 
-        int number = Integer.parseInt(br.readLine());
-
-        sb.append(getNumber(number));
+        for (int i = 0; i < houseNumber; i++) {
+            costs[i+1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        sb.append(getMinCost(houseNumber));
 
         System.out.println(sb);
     }
 
-    private static int getNumber(int tileNumber) {
-        if(tiles[tileNumber] != 0) {
-            return tiles[tileNumber];
+    //어떤 호수의 집 까지의 최소값은 n-2 번째 집까지의 최소값 + n-1, n 호의 최소값
+    private static int getMinCost(int nowHouse) {
+        //집의 개수가 3개 이상인 경우
+        if (nowHouse > 2) {
+            result[nowHouse] = getMinCost(nowHouse-2) + getMinCostOf(nowHouse - 1, nowHouse);
+            return result[nowHouse];
+        } else if(nowHouse == 2){
+            int min = getMinCostOf(1, 2);
+            result[2] = min;
+            return result[2];
         } else {
-            tiles[tileNumber] = ((getNumber(tileNumber-2) + getNumber(tileNumber - 1)) % 15746);
-            return tiles[tileNumber];
+            int min = 1001;
+            for (int i = 0; i < 3; i++) {
+                if(costs[1][i] < min) {
+                    min = costs[1][i];
+                    chosenColor[1] = i;
+                }
+            }
+            result[1] = min;
+            return min;
         }
+    }
+
+    //이전까지의 results 에다가 두개의 집을 더했을 때 최소값을 추가하는 메서드
+    private static int getMinCostOf(int lastBeforeHouse, int lastHouse) {
+        int min = 3001;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(i != j && i != chosenColor[lastBeforeHouse-1]) {
+                    if (min >= (costs[lastBeforeHouse][i] + costs[lastHouse][j])) {
+                        min = costs[lastBeforeHouse][i] + costs[lastHouse][j];
+                        chosenColor[lastBeforeHouse] = i;
+                        chosenColor[lastHouse] = j;
+                    }
+                }
+            }
+        }
+        return min;
     }
 }
