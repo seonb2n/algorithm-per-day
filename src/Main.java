27e4fff910efc5d81faa4b31871332a68b89f2e1
seonb2n <626ksb@gmail.com>
2022-01-15 20:32:56 +0900

    백준 알고리즘 1149번 - 최적화

--- src/Main.java
+++ src/Main.java
@@ -8,67 +8,72 @@ public class Main {
     static StringBuilder sb;
     static int houseNumber;
     static int[][] costs;
-    static int[] chosenColor;
-    static int[] chosenCosts;
-    static int[] result;
+    static int[][] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
         houseNumber = Integer.parseInt(br.readLine());
-        costs = new int[houseNumber+1][3];
-        chosenColor = new int[houseNumber+1];
-        chosenColor[0] = -999;
-        chosenCosts = new int[houseNumber+1];
-        chosenCosts[0] = -999;
-        result = new int[houseNumber+1];
+        costs = new int[houseNumber + 1][3];
+        result = new int[houseNumber + 1][3];
 
 
         for (int i = 0; i < houseNumber; i++) {
-            costs[i+1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            costs[i + 1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-        sb.append(getMinCost(houseNumber));
+        for (int i = 1; i <= houseNumber; i++) {
+            for (int j = 0; j < 3; j++) {
+                getMinCostIf(i,j);
+            }
+        }
+
+        sb.append(getMinThreeNumber(result[houseNumber][0], result[houseNumber][1], result[houseNumber][2]));
 
         System.out.println(sb);
     }
 
-    //어떤 호수의 집 까지의 최소값은 n-2 번째 집까지의 최소값 + n-1, n 호의 최소값
-    private static int getMinCost(int nowHouse) {
-        //집의 개수가 3개 이상인 경우
-        if (nowHouse > 2) {
-            result[nowHouse] = getMinCost(nowHouse-2) + getMinCostOf(nowHouse - 1, nowHouse);
-            return result[nowHouse];
-        } else if(nowHouse == 2){
-            int min = getMinCostOf(1, 2);
-            result[2] = min;
-            return result[2];
-        } else {
-            int min = 1001;
-            for (int i = 0; i < 3; i++) {
-                if(costs[1][i] < min) {
-                    min = costs[1][i];
-                    chosenColor[1] = i;
-                }
-            }
-            result[1] = min;
-            return min;
-        }
+    static int getMinTwoNumber(int a, int b) {
+        return Math.min(a, b);
     }
 
-    //이전까지의 results 에다가 두개의 집을 더했을 때 최소값을 추가하는 메서드
-    private static int getMinCostOf(int lastBeforeHouse, int lastHouse) {
-        int min = 3001;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if(i != j && i != chosenColor[lastBeforeHouse-1]) {
-                    if (min >= (costs[lastBeforeHouse][i] + costs[lastHouse][j])) {
-                        min = costs[lastBeforeHouse][i] + costs[lastHouse][j];
-                        chosenColor[lastBeforeHouse] = i;
-                        chosenColor[lastHouse] = j;
-                    }
-                }
+    static int getMinThreeNumber(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+
+    //n 호까지의 색깔은, (n-1,0) + n 1또는 2, (n-1,1) + n 0 또는 2, (n-1,2) + n 0 또는 1 중에 최솟값이다.
+    static void getMinCostIf(int houseNumber, int nowHouseColor) {
+
+        if (houseNumber > 1) {
+            switch (nowHouseColor) {
+                case 0:
+                    result[houseNumber][0]
+                            = getMinTwoNumber(result[houseNumber-1][1], result[houseNumber-1][2]) + costs[houseNumber][nowHouseColor];
+                    break;
+                case 1:
+                    result[houseNumber][1]
+                            = getMinTwoNumber(result[houseNumber-1][0], result[houseNumber-1][2]) + costs[houseNumber][nowHouseColor];
+                    break;
+                case 2:
+                    result[houseNumber][2]
+                            = getMinTwoNumber(result[houseNumber-1][0], result[houseNumber-1][1]) + costs[houseNumber][nowHouseColor];
+                    break;
+            }
+        } else {
+            //첫번째 집일 때
+            switch (nowHouseColor) {
+                case 0:
+                    result[houseNumber][0]
+                            = costs[houseNumber][nowHouseColor];
+                    break;
+                case 1:
+                    result[houseNumber][1]
+                            = costs[houseNumber][nowHouseColor];
+                    break;
+                case 2:
+                    result[houseNumber][2]
+                            = costs[houseNumber][nowHouseColor];
+                    break;
             }
         }
-        return min;
     }
 }
