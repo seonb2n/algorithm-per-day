7b847673eab5be761447bc48f4dc7e93817f07f7
seonb2n <626ksb@gmail.com>
2022-01-17 20:46:21 +0900

    백준 알고리즘 10844번

--- src/Main.java
+++ src/Main.java
@@ -5,41 +5,58 @@ import java.io.InputStreamReader;
 public class Main {
 
     static StringBuilder sb;
-    static int[] stairs;
-    static int[] results;
+    static long[][] numbers;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
         int caseNumber = Integer.parseInt(br.readLine());
-        stairs = new int[caseNumber+1];
-        results = new int[caseNumber+1];
+        numbers = new long[caseNumber + 1][10];
 
         for (int i = 1; i <= caseNumber; i++) {
-            stairs[i] = Integer.parseInt(br.readLine());
+            for (int j = 0; j <= 9; j++) {
+                numbers[i][j] = (getNumber(i, j)) % 1000000000;
+            }
         }
 
-        for (int i = 1; i <= caseNumber; i++) {
-            results[i] = getMax(i);
+        long result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result += numbers[caseNumber][i];
         }
-
-        sb.append(results[caseNumber]);
-
+        sb.append(result % 1000000000);
         System.out.println(sb);
     }
 
-    private static int getMax(int nowStair) {
-        if(nowStair == 1) {
-            return stairs[nowStair];
+    private static long getNumber(int i, int j) {
+        //i자리수 이면서 j로 끝나는 숫자의 개수
+        if (i == 1 && j == 0) {
+            return 0;
+        } else if (i == 1) {
+            return 1;
         }
-        if(nowStair == 2) {
-            return results[1] + stairs[nowStair];
+        int k = i - 1;
+        switch (j) {
+            case 0:
+                return numbers[k][1];
+            case 1:
+                return numbers[k][0] + numbers[k][2];
+            case 2:
+                return numbers[k][1] + numbers[k][3];
+            case 3:
+                return numbers[k][2] + numbers[k][4];
+            case 4:
+                return numbers[k][3] + numbers[k][5];
+            case 5:
+                return numbers[k][4] + numbers[k][6];
+            case 6:
+                return numbers[k][5] + numbers[k][7];
+            case 7:
+                return numbers[k][6] + numbers[k][8];
+            case 8:
+                return numbers[k][7] + numbers[k][9];
+            case 9:
+                return numbers[k][8];
         }
-        if(nowStair == 3) {
-            return Math.max(results[1] + stairs[nowStair], stairs[2] + stairs[3]);
-        }
-
-        return Math.max(results[nowStair-2] + stairs[nowStair], results[nowStair-3] + stairs[nowStair-1] + stairs[nowStair]);
-
+        return 0;
     }
 }
