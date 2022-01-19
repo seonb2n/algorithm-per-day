fa35974371d9e5c7b7f7fc9eadb4abc80495a347
seonb2n <626ksb@gmail.com>
2022-01-19 22:33:56 +0900

    백준 알고리즘 11053번

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     static StringBuilder sb;
-    static long[][] numbers;
+    static int[] numbers;
+    static int[] results;
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
         int caseNumber = Integer.parseInt(br.readLine());
-        numbers = new long[caseNumber + 1][10];
 
-        for (int i = 1; i <= caseNumber; i++) {
-            for (int j = 0; j <= 9; j++) {
-                numbers[i][j] = (getNumber(i, j)) % 1000000000;
+        numbers = new int[caseNumber+1];
+        results = new int[caseNumber+1];
+
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < caseNumber; i++) {
+            results[i] = getNumber(i);
+            if(max < results[i]) {
+                max = results[i];
             }
         }
 
-        long result = 0;
-        for (int i = 0; i <= 9; i++) {
-            result += numbers[caseNumber][i];
-        }
-        sb.append(result % 1000000000);
-        System.out.println(sb);
+        System.out.println(max);
     }
 
-    private static long getNumber(int i, int j) {
-        //i자리수 이면서 j로 끝나는 숫자의 개수
-        if (i == 1 && j == 0) {
-            return 0;
-        } else if (i == 1) {
+    private static int getNumber(int i) {
+        if(i == 0) {
             return 1;
+        } else {
+            int minMax = 0;
+            boolean valueChanged = false;
+            for (int j = 0; j < i; j++) {
+                //j 의 숫자가 i보다 작다면
+                if (numbers[j] < numbers[i]) {
+                    //그 중에 results[j] 값이 가장 큰 놈을 찾는다.
+                    if (minMax < results[j]) {
+                        minMax = results[j];
+                        valueChanged = true;
+                    }
+                }
+            }
+            //i 보다 작은 최댓값이 없는 경우에는 1을 반환한다.
+            if (!valueChanged) {
+                return 1;
+            }
+
+            //i 보다 작은 최댓값의 result + 1을 반환한다.
+            return minMax + 1;
         }
-        int k = i - 1;
-        switch (j) {
-            case 0:
-                return numbers[k][1];
-            case 1:
-                return numbers[k][0] + numbers[k][2];
-            case 2:
-                return numbers[k][1] + numbers[k][3];
-            case 3:
-                return numbers[k][2] + numbers[k][4];
-            case 4:
-                return numbers[k][3] + numbers[k][5];
-            case 5:
-                return numbers[k][4] + numbers[k][6];
-            case 6:
-                return numbers[k][5] + numbers[k][7];
-            case 7:
-                return numbers[k][6] + numbers[k][8];
-            case 8:
-                return numbers[k][7] + numbers[k][9];
-            case 9:
-                return numbers[k][8];
-        }
-        return 0;
     }
 }
