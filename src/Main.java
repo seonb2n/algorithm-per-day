7a86e83b3f10b3e274fb890e846f9455db4d5dae
seonb2n <626ksb@gmail.com>
2022-01-20 00:20:15 +0900

    백준 알고리즘 11054번

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
     static StringBuilder sb;
     static int[] numbers;
-    static int[] results;
+    static int[] dpForUp;
+    static int[] dpForDown;
     static int max = 0;
 
     public static void main(String[] args) throws IOException {
@@ -16,43 +17,74 @@ public class Main {
         int caseNumber = Integer.parseInt(br.readLine());
 
         numbers = new int[caseNumber+1];
-        results = new int[caseNumber+1];
+        dpForUp = new int[caseNumber+1];
+        dpForDown = new int[caseNumber+1];
 
         numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         for (int i = 0; i < caseNumber; i++) {
-            results[i] = getNumber(i);
-            if(max < results[i]) {
-                max = results[i];
-            }
+            getNumber(i);
+            max = Math.max(max, Math.max(dpForUp[i], dpForDown[i]));
         }
 
         System.out.println(max);
     }
 
-    private static int getNumber(int i) {
+    private static void getNumber(int i) {
         if(i == 0) {
-            return 1;
-        } else {
-            int minMax = 0;
-            boolean valueChanged = false;
-            for (int j = 0; j < i; j++) {
-                //j 의 숫자가 i보다 작다면
-                if (numbers[j] < numbers[i]) {
-                    //그 중에 results[j] 값이 가장 큰 놈을 찾는다.
-                    if (minMax < results[j]) {
-                        minMax = results[j];
-                        valueChanged = true;
-                    }
+            dpForUp[0] = 1;
+            dpForDown[0] = 1;
+            return;
+        } else if(i == 1) {
+            if(numbers[1] > numbers[0]) {
+                dpForUp[1] = 2;
+                dpForDown[1] = 1;
+            } else if (numbers[1] < numbers[0]){
+                dpForUp[1] = 1;
+                dpForDown[1] = 2;
+            } else {
+                dpForUp[1] = 1;
+                dpForDown[1] = 1;
+            }
+            return;
+        }
+
+        int max1 = 0;
+        boolean valueChanged = false;
+        //점점 증가하는 형태
+        for (int j = 0; j < i; j++) {
+            //j 의 숫자가 i보다 작다면
+            if (numbers[j] < numbers[i]) {
+                //그 중에 results[j] 값이 가장 큰 놈을 찾는다.
+                if (max1 < dpForUp[j]) {
+                    max1 = dpForUp[j];
+                    valueChanged = true;
                 }
             }
-            //i 보다 작은 최댓값이 없는 경우에는 1을 반환한다.
-            if (!valueChanged) {
-                return 1;
-            }
+        }
+        if(valueChanged) {
+            dpForUp[i] = max1 + 1;
+        } else {
+            dpForUp[i] = 1;
+        }
 
-            //i 보다 작은 최댓값의 result + 1을 반환한다.
-            return minMax + 1;
+        valueChanged = false;
+        int max2 = 0;
+        //내려오는 형태
+        for (int j = 0; j < i; j++) {
+            //j 의 숫자가 i 보다 큰 값 중에 dp 값이 제일 큰 값을 찾는다.
+            if (numbers[j] > numbers[i]){
+                if (max2 < Math.max(dpForDown[j], dpForUp[j])) {
+                    max2 = Math.max(dpForDown[j], dpForUp[j]);
+                    valueChanged = true;
+                }
+            }
+        }
+
+        if(valueChanged) {
+            dpForDown[i] = max2 + 1;
+        } else {
+            dpForDown[i] = 1;
         }
     }
 }
