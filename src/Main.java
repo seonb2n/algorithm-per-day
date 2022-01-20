986f7d9804ef95e2d1ace2b52159e34130aa5d62
seonb2n <626ksb@gmail.com>
2022-01-20 19:47:25 +0900

    백준 알고리즘 2565번

--- src/Main.java
+++ src/Main.java
@@ -2,89 +2,64 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 
     static StringBuilder sb;
-    static int[] numbers;
-    static int[] dpForUp;
-    static int[] dpForDown;
-    static int max = 0;
+    static int[][] lines;
+    static int caseNumber;
+    static int[] dp;
+    static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        int caseNumber = Integer.parseInt(br.readLine());
+        caseNumber = Integer.parseInt(br.readLine());
 
-        numbers = new int[caseNumber+1];
-        dpForUp = new int[caseNumber+1];
-        dpForDown = new int[caseNumber+1];
-
-        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        lines = new int[caseNumber][2];
+        dp = new int[caseNumber];
+        max = 0;
 
         for (int i = 0; i < caseNumber; i++) {
-            getNumber(i);
-            max = Math.max(max, Math.max(dpForUp[i], dpForDown[i]));
+            lines[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        System.out.println(max);
-    }
-
-    private static void getNumber(int i) {
-        if(i == 0) {
-            dpForUp[0] = 1;
-            dpForDown[0] = 1;
-            return;
-        } else if(i == 1) {
-            if(numbers[1] > numbers[0]) {
-                dpForUp[1] = 2;
-                dpForDown[1] = 1;
-            } else if (numbers[1] < numbers[0]){
-                dpForUp[1] = 1;
-                dpForDown[1] = 2;
-            } else {
-                dpForUp[1] = 1;
-                dpForDown[1] = 1;
+        //정렬 필요
+        Arrays.sort(lines, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
             }
-            return;
-        }
+        });
 
-        int max1 = 0;
-        boolean valueChanged = false;
-        //점점 증가하는 형태
-        for (int j = 0; j < i; j++) {
-            //j 의 숫자가 i보다 작다면
-            if (numbers[j] < numbers[i]) {
-                //그 중에 results[j] 값이 가장 큰 놈을 찾는다.
-                if (max1 < dpForUp[j]) {
-                    max1 = dpForUp[j];
-                    valueChanged = true;
-                }
-            }
-        }
-        if(valueChanged) {
-            dpForUp[i] = max1 + 1;
-        } else {
-            dpForUp[i] = 1;
-        }
+        for (int i = 0; i < caseNumber; i++) {
+            //1개는 최소한 연결 가능하기 때문
+            dp[i] = 1;
 
-        valueChanged = false;
-        int max2 = 0;
-        //내려오는 형태
-        for (int j = 0; j < i; j++) {
-            //j 의 숫자가 i 보다 큰 값 중에 dp 값이 제일 큰 값을 찾는다.
-            if (numbers[j] > numbers[i]){
-                if (max2 < Math.max(dpForDown[j], dpForUp[j])) {
-                    max2 = Math.max(dpForDown[j], dpForUp[j]);
-                    valueChanged = true;
+            for (int j = 0; j < i; j++) {
+                //자기 이전의 전선을 확인
+
+                //B 전봇대에 연결된 점이 더 위에 있다면 서로 겹치지 않는다.
+                if(lines[i][1] > lines[j][1]) {
+                    //이전에 탐색한 j 에서 출발한 점에다 1을 더한 값이 된다.
+                    //조건에 부합하는 갑 중 최댓값이 dp[i] 가 된다.
+                    if(dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                    }
                 }
             }
         }
 
-        if(valueChanged) {
-            dpForDown[i] = max2 + 1;
-        } else {
-            dpForDown[i] = 1;
+        //최대로 많이 연결된 경우를 찾는다.
+        for (int i = 0; i <caseNumber; i++) {
+            if(dp[i] > max) {
+                max = dp[i];
+            }
         }
+
+        System.out.println(caseNumber - max);
+
     }
 }
+
