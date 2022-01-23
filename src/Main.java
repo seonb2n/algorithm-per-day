2198dbe1086a98bd893e47090ccbfadd73e77e0a
seonb2n <626ksb@gmail.com>
2022-01-23 21:05:46 +0900

    백준 알고리즘 9461번

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
     static StringBuilder sb;
-    static int[] caseNumber;
-    static int[][] items;
-    static Integer[][] dp;
-    static int max;
+    static int caseNumber;
+    static long[] dp;
+    static int[] numbers;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        caseNumber = new int[2];
+        caseNumber = Integer.parseInt(br.readLine());
 
-        caseNumber = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        dp = new long[101];
+        numbers = new int[caseNumber];
 
-        items = new int[caseNumber[0]][2];
-        dp = new Integer[caseNumber[1] + 1][caseNumber[0] + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
 
-        for (int i = 0; i < caseNumber[0]; i++) {
-            items[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < caseNumber; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
         }
 
-        //0부터 최대 무게 까지 확인하면서 dp 를 채워나간다.
-        //dp 를 마지막까지 채웠을 때 최대 무게가 존재할 것이므로
-        System.out.println(knapsack(caseNumber[1], caseNumber[0]-1));
+        for (int i = 0; i < caseNumber; i++) {
+            System.out.println(getNumber(numbers[i]));
+        }
     }
 
-    //무게가 i이고, j번째 물건을 담을려고 할 떄 가장 가치가 높은 값을 반환하는 함수
-    private static int knapsack(int i, int j) {
-        if(j < 0)
+
+    public static long getNumber(int i) {
+        if(i <= 0) {
             return 0;
-
-        //아직 채워지지 않은 부분이라면
-        if(dp[i][j] == null) {
-            //값을 0 으로 초기화
-            dp[i][j] = 0;
-            //j 번째 물건이 들어갈 수 있다면
-            if(i >= items[j][0]) {
-                //j번째 물건을 넣기전의 가치와 j 번째 물건과, 무게 i - j 번째 아이템의 무게일 때의 최댓값 + 지금 아이템의 무게
-                dp[i][j] = Math.max(knapsack(i, j-1), knapsack(i-items[j][0], j-1) + items[j][1]);
-            }
-            //j 번째 물건을 넣지 못하는 경우에는
-            else {
-                //이전에 넣었던 물건의 최대 가치를 갖는다.
-                dp[i][j] = knapsack(i, j-1);
-            }
-
         } else {
-            return dp[i][j];
+            if(dp[i] != 0) {
+                return dp[i];
+            }
+            dp[i] = getNumber(i - 2) + getNumber(i - 3);
+            return dp[i];
         }
-        return dp[i][j];
     }
 }
 
