f6cbc22852f2b7857017d7249e619d5c18052a1e
seonb2n <626ksb@gmail.com>
2022-01-25 16:16:59 +0900

    백준 알고리즘 1931번

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static StringBuilder sb;
-    static int coinType;
-    static int total;
-    static int[] coins;
-    static int min;
+    static BufferedReader br;
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        sb = new StringBuilder();
 
-        coinType = Integer.parseInt(st.nextToken());
-        total = Integer.parseInt(st.nextToken());
-        coins = new int[coinType + 1];
-        min = 0;
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNumber = Integer.parseInt(br.readLine());
+        int[][] timeTable = new int[caseNumber][2];
 
-        for (int i = 1; i <= coinType; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < caseNumber; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            timeTable[i][0] = Integer.parseInt(st.nextToken());	// 시작시간
+            timeTable[i][1] = Integer.parseInt(st.nextToken());	// 종료시간
         }
 
-        for (int i = coinType; i >= 1; i--) {
-            getCoinNumber(coins[i]);
+        Arrays.sort(timeTable, (o1, o2) -> {
+            // 종료시간이 같을 경우 시작시간이 빠른순으로 정렬
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            // 종료시간이 빠른 순으로 정렬
+            return o1[1] - o2[1];
+        });
+
+        int max = 0;
+        int nowEndTime = 0;
+
+        for(int i = 0; i < caseNumber; i++) {
+            if(nowEndTime <= timeTable[i][0]) {
+                nowEndTime = timeTable[i][1];
+                max++;
+            }
         }
 
-        System.out.println(min);
+        System.out.println(max);
     }
 
-    private static void getCoinNumber(int value) {
-        //동전의 가치가 합보다 작다면
-        if(value <= total) {
-            int temp = total / value;
-            min += temp;
-            total = (total-(temp * value));
-        }
-    }
 }
 
