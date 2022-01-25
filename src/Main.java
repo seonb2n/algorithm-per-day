0f12fe08348dc6c477c35e42d6a1426e025e3e23
seonb2n <626ksb@gmail.com>
2022-01-25 15:06:37 +0900

    백준 알고리즘 1912번

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     static StringBuilder sb;
     static int number;
+    static int[] numbers;
     static long[] dp;
+    static long max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
         number = Integer.parseInt(br.readLine());
+        dp = new long[100001];
+        numbers = new int[number + 1];
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        dp = new long[1000001];
+        dp[0] = numbers[0];
+        max = dp[0];
 
-        dp[1] = 0;
-        dp[2] = 1;
-        dp[3] = 1;
-        dp[4] = 2;
-        dp[5] = 3;
-        dp[6] = 2;
-        dp[7] = 3;
-        dp[8] = 3;
-        dp[9] = 2;
-        System.out.println(getNumber(number));
+        for (int i = 1; i < number; i++) {
+            dp[i] = getMax(i);
+            if(max < dp[i]) {
+                max = dp[i];
+            }
+        }
+
+        System.out.println(max);
     }
 
-    //주어진 값이 i 일때 횟수의 최솟값을 구하는 함수
-    public static long getNumber(int i) {
-        if(dp[i] != 0) {
-            return dp[i];
-        } else if(i <= 1) {
-            return 0;
-        } else {
-            if(i % 2 == 0 && i % 3 == 0) {
-                dp[i] = Math.min(Math.min(getNumber(i/2), getNumber(i/3)), getNumber(i-1)) + 1;
-            } else if(i % 2 == 0) {
-                dp[i] = Math.min(getNumber(i/2), getNumber(i-1)) + 1;
-            } else if(i % 3 == 0) {
-                dp[i] = Math.min(getNumber(i/3), getNumber(i-1)) + 1;
-            } else {
-                dp[i] = getNumber(i-1) + 1;
-            }
-            return dp[i];
-        }
+    //i 번째에 대해서 i를 포함한 최댓값을 dp[i] 에 할당한다.
+    private static long getMax(int i) {
+        return Math.max(numbers[i], dp[i-1] + numbers[i]);
     }
 }
 
