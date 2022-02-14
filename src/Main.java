3b4645f3dc7743687bd26624606c8c5b46a3ea8c
seonb2n <626ksb@gmail.com>
2022-02-14 22:34:59 +0900

    백준 알고리즘 2110번

--- src/Main.java
+++ src/Main.java
@@ -39,61 +39,54 @@ public class Main {
 
     //min 부터 max 사이의 범위 중에 C개의 공유기를 설치할 때, 공유기 사이의 범위가 최대가 되도록 해야 한다.
     private static long findMaxLength() {
-        min = wifiArr[0];
-        max = wifiArr[N-1];
-        long maxMinGap = max - min;
+        //거리가 가질 수 있는 최소 범위
+        min = 1;
+
+        //거리가 가질 수 있는 최대 범위
+        max = wifiArr[N-1] - wifiArr[0];
 
         if(C == 2) {
-            return maxMinGap;
+            return max;
         }
 
         //와이파이 사이의 길이를 비교하며 길이 이상일 때만 설치한다.
-        long mid = 1;
-        while(mid < maxMinGap) {
+        while(min < max) {
             //설치된 개수가 C 의 개수인 길이 중 최대의 값을 찾는다.
-            lastAddNumber = wifiArr[0];
-            mid = (max + min) / 2;
-            int count = 1;
-            for (int i = 1; i < N; i++) {
-                if((wifiArr[i] - lastAddNumber) >= mid) {
-                    count++;
-                    lastAddNumber = wifiArr[i];
-                }
+            long mid = (max + min) / 2;
 
-                if(count > C) {
-                    break;
-                }
-            }
+            int count = countNumber(mid);
 
-            //count 가 C보다 많다면 mid 가 너무 작다는 뜻
-            if(count > C) {
+            //count 가 C보다 많거나 같다면 mid 를 키우면 된다.
+            if(count >= C) {
+                //최솟값이 mid 이상일 수 있다.
                 min = mid + 1;
+                //min = mid+1 인 경우가 딱 최대일 수도 있다.
             }
             //count 가 C 보다 작다면 mid 가 너무 크다는 뜻
-            else if (count < C){
-                max = mid - 1;
-            }
-
-            //count 가 C 와 같다면 max 를 검증해야 한다.
             else {
-                lastAddNumber = wifiArr[0];
-                count = 1;
-                for (int i = 1; i < N; i++) {
-                    if((wifiArr[i] - lastAddNumber) >= max) {
-                        count++;
-                        lastAddNumber = wifiArr[i];
-                    }
-                }
-
-                if(count == C) {
-                    return max;
-                } else {
-                    max--;
-                    min = mid;
-                }
+                max = mid;
             }
         }
 
-        return max;
+        return min-1;
+    }
+
+    //주어진 길이에 대해서 몇개의 공유기가 설치 가능한지 구하는 함수
+    private static int countNumber(long distance) {
+        int count = 1;
+        lastAddNumber = wifiArr[0];
+
+        for (int i = 1; i < N; i++) {
+            if((wifiArr[i] - lastAddNumber) >= distance) {
+                count++;
+                lastAddNumber = wifiArr[i];
+            }
+
+            if(count > C) {
+                return count;
+            }
+        }
+
+        return count;
     }
 }
