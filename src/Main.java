679738d574aef5551786bcbb8e0bfe2b98763d46
seonb2n <626ksb@gmail.com>
2022-02-19 11:51:18 +0900

    백준 알고리즘 11279번

--- src/Main.java
+++ src/Main.java
@@ -6,66 +6,99 @@ import java.util.*;
 public class Main {
 
     static BufferedReader br;
-    static StringTokenizer st;
     static StringBuilder sb;
     static int N;
-    static int[] A;
+    static Map<Integer, Integer> numberList;
+    static int nodeNumber;
 
     public static void main(String[] args) throws IOException {
 
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
+        numberList = new HashMap<>();
 
         N = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine(), " ");
-
-        A = new int[N];
-
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-
-        System.out.println(getLCS());
-
-    }
-
-    //해당 범위 내 증가하는 수열의 길이를 반환하는 함수
-    public static int getLCS() {
-        int[] result;
-        result = new int[N];
-        int lastAddPoint = 0;
-
-        result[0] = A[0];
-
-        for (int i = 1; i < N; i++) {
-            //만약 수열에 추가하고자 하는 수가 result 의 마지막 수보다 크다면 그대로 값을 추가해준다.
-            if(result[lastAddPoint] < A[i]) {
-                lastAddPoint++;
-                result[lastAddPoint] = A[i];
-            }
-
-            //수열에 추가하고자 하는 수가 마지막 수보다 크지 않다면, 새로운 수가 들어갈 적절한 위치를 찾아줘야 한다.
-            else {
-                //새로운 수는 자기보다 작은 result 의 수 바로 앞의 수를 대체한다.
-                //upper Bound 로 추가하고자 하는 수를 초과한 값이 처음 나오는 위치를 찾아야 한다.
-                int min = 0;
-                int max = lastAddPoint;
-                int temp = A[i];
-                while(min < max) {
-
-                    int mid = (min + max) / 2;
-                    if(result[mid] < temp) {
-                        min = mid+1;
-                    } else {
-                        max = Math.max(0, mid);
-                    }
-
+            int temp =  Integer.parseInt(br.readLine());
+            if (temp == 0) {
+                if(nodeNumber == 0) {
+                    sb.append(0);
+                    sb.append("\n");
+                } else {
+                    sb.append(popNumber());
+                    sb.append("\n");
                 }
-                result[min] = temp;
+            }
+
+            else {
+                addNumber(temp);
             }
         }
 
-        return lastAddPoint+1;
+        System.out.println(sb);
+
     }
+
+    //루트 노드를 제거한 후에 배열을 재정렬하는 메서드
+    private static int popNumber() {
+        int result = numberList.get(1);
+        numberList.replace(1, 0);
+        swap(1, nodeNumber);
+        nodeNumber--;
+        if(nodeNumber > 1) {
+            int cursor = 1;
+            //자신의 자식 노드와 값을 비교해가며, 자식이 자신보다 크면 위치를 바꾼다.
+            while (true) {
+                //왼쪽 자식과 오른쪽 자식을 비교해서 더 큰 녀석과 자리를 바꿔야 한다.
+                if(numberList.containsKey(cursor * 2) && numberList.get(cursor * 2) > numberList.get(cursor)) {
+                    if(numberList.containsKey(cursor * 2 +1) && numberList.get(cursor * 2 + 1) > numberList.get(cursor * 2)) {
+                        swap(cursor * 2 + 1, cursor);
+                        cursor = cursor * 2 + 1;
+                    } else {
+                        swap(cursor * 2, cursor);
+                        cursor = cursor * 2;
+                    }
+                }
+                else if(numberList.containsKey(cursor * 2 + 1) && numberList.get(cursor * 2 + 1) > numberList.get(cursor)) {
+                    swap(cursor * 2 + 1, cursor);
+                    cursor = cursor * 2 + 1;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    //주어진 숫자를 numberList 를 최대 힙의 형태로 정렬해서 추가하는 메서드
+    public static void addNumber(int number) {
+        if(nodeNumber == 0) {
+            nodeNumber = 1;
+        } else {
+            nodeNumber++;
+        }
+        numberList.put(nodeNumber, number);
+
+        //부모 노드와 비교해서 자신이 부모보다 크면 부모 노드와 swap
+        //이 과정을 자신이 루트 노드에 다다를 때까지 반복
+        int cursor = nodeNumber;
+        while (cursor != 1) {
+            if(numberList.get(cursor / 2) < numberList.get(cursor)) {
+                swap(cursor / 2, cursor);
+                cursor = cursor / 2;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void swap(int a, int b) {
+        int temp = numberList.get(a);
+        numberList.replace(a, numberList.get(b));
+        numberList.replace(b, temp);
+    }
+
 }
