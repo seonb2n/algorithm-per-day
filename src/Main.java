956f0bd452dcbf78a4c3ec87c9dfa819b757d4ba
seonb2n <626ksb@gmail.com>
2022-03-02 22:40:59 +0900

    백준 알고리즘 3197번 - 호수의 얼음에 대해서 몇 턴 뒤에 녹는지 모두 구했으나 시간 초과

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,6 @@ public class Main {
     static int swanBX;
     static int swanBY;
 
-    static int day = 0;
-
     public static void main(String[] args) throws IOException {
 
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -92,10 +90,38 @@ public class Main {
             }
         }
 
-        System.out.println(map);
+        //자기 주변 얼음을 확인해서 해당 값의 최솟값 + 1 로 값을 바꾼다.
+        for (int i = 0; i < mapHeight; i++) {
+            for (int j = 0; j <mapWidth; j++) {
+                if (map[i][j] != 0) {
+                    map[i][j] = getNearMinValue(i, j)+1;
+                }
+            }
+        }
 
         System.out.println(getDateToMeet());
 
+    }
+
+    private static int getNearMinValue(int i, int j) {
+        int top = 1501;
+        int bottom = 1501;
+        int left = 1501;
+        int right = 1501;
+        if(i+1 < mapHeight) {
+            top = map[i+1][j];
+        }
+        if(i-1 >= 0) {
+            bottom = map[i-1][j];
+        }
+        if(j+1 < mapWidth) {
+            right = map[i][j+1];
+        }
+        if(j-1 >= 0) {
+            left = map[i][j-1];
+        }
+
+       return Math.min(Math.min(top, bottom), Math.min(left, right));
     }
 
     //i, j 에서 밑으로 가까운 0까지의 거리를 구하는 함수
@@ -152,55 +178,18 @@ public class Main {
     }
 
     //해당 좌표에서 밑에쪽의 0까지 거리를 구하는 함수
-
-
-    // 두 백조가 만나는 날을 계산하는 프로그램
+    // 두 백조가 만나는 날을 계산
     private static int getDateToMeet() {
-
-        if(isSwanCanMeet()) {
-            return 0;
+        int i = 0;
+        while (!isSwanCanMeet(i)) {
+            i++;
         }
 
-        while (!isSwanCanMeet()) {
-            changeDay();
-        }
-
-        return day;
-    }
-
-    //시간이 지나서 얼음이 녹은 형태로 구현
-    private static void changeDay() {
-        day++;
-        int[][] tempMap = new int[mapHeight][mapWidth];
-        for (int i = 0; i < mapHeight; i++) {
-            for (int j = 0; j < mapWidth; j++) {
-                tempMap[i][j] = map[i][j];
-            }
-        }
-
-        for (int i = 0; i < mapHeight; i++) {
-            for (int j = 0; j < mapWidth; j++) {
-                //해당 칸이 얼음일 때, 주변에 물이 하나라도 있으면 물로 바꾼다.
-                if(tempMap[i][j] == 1) {
-                    if(i+1<mapHeight && tempMap[i+1][j] == 0) {
-                        map[i][j] = 0;
-                    }
-                    else if(i-1 >= 0 && tempMap[i-1][j] == 0) {
-                        map[i][j] = 0;
-                    }
-                    else if(j+1<mapWidth && tempMap[i][j+1] == 0) {
-                        map[i][j] = 0;
-                    }
-                    else if(j-1 >= 0 && tempMap[i][j-1] == 0) {
-                        map[i][j] = 0;
-                    }
-                }
-            }
-        }
+        return i;
     }
 
     //백조가 서로 만날 수 있는지를 탐색하는 알고리즘
-    private static boolean isSwanCanMeet() {
+    private static boolean isSwanCanMeet(int day) {
         //1인 경우는 물이기 때문에 통행이 가능하다.
         for (int i = 0; i < mapHeight; i++) {
             for (int j = 0; j < mapWidth; j++) {
@@ -208,36 +197,36 @@ public class Main {
             }
         }
 
-        return isWay(swanAX, swanAY);
+        return isWay(swanAX, swanAY, day);
     }
 
     //현재 위치에서 목표 지점인 swanBX, swanBY 까지 가는 경로가 있는지 탐색하는 알고리즘
     //지나온 길을 다시 지나서는 안된다.
-    private static boolean isWay(int nowX, int nowY) {
+    private static boolean isWay(int nowX, int nowY, int day) {
         if(nowX == swanBX && nowY == swanBY) {
             return true;
         }
 
-        tempWayMap[nowY][nowX] = -1;
+        tempWayMap[nowY][nowX] = 9999;
 
         //오른쪽 칸 탐색
-        if(nowX + 1 < mapWidth && tempWayMap[nowY][nowX+1] == 0) {
-            return isWay(nowX+1, nowY);
+        if(nowX + 1 < mapWidth && tempWayMap[nowY][nowX+1] <= day) {
+            return isWay(nowX+1, nowY, day);
         }
 
         //왼쪽 칸 탐색
-        if(nowX - 1 >= 0 && tempWayMap[nowY][nowX-1] == 0) {
-            return isWay(nowX-1, nowY);
+        if(nowX - 1 >= 0 && tempWayMap[nowY][nowX-1] <= day) {
+            return isWay(nowX-1, nowY, day);
         }
 
         //아래쪽 칸 탐색
-        if(nowY + 1 < mapHeight && tempWayMap[nowY+1][nowX] == 0) {
-            return isWay(nowX, nowY+1);
+        if(nowY + 1 < mapHeight && tempWayMap[nowY+1][nowX] <= day) {
+            return isWay(nowX, nowY+1, day);
         }
 
         //위쪽 칸 탐색
-        if(nowY - 1 >= 0 && tempWayMap[nowY-1][nowX] == 0) {
-            return isWay(nowX, nowY-1);
+        if(nowY - 1 >= 0 && tempWayMap[nowY-1][nowX] <= day) {
+            return isWay(nowX, nowY-1, day);
         }
 
         return false;
