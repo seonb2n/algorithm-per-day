afb080ea596b0e4f28cbefda3a141a0583a44d0e
seonb2n <626ksb@gmail.com>
2022-03-05 13:52:21 +0900

    백준 알고리즘 3197번 - 주석 추가

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,13 @@ public class Main {
     static int mapHeight;
     static int[][] map;
     static int[][] countMeltMap;
-    static PriorityQueue<Point> countMeltQueue;
+    static PriorityQueue<Node> countMeltQueue;
     static boolean[][] countMeltVisited;
 
-    private static int[] x_move = {1, 0, -1, 0};
-    private static int[] y_move = {0, 1, 0, -1};
+    //노드 탐색을 할 때, 4방향으로 움직일 수 있기 때문에 움직임 가능한 쌍을 배열로 넣어둔다.
+    private static final int[] x_move = {1, 0, -1, 0};
+    private static final int[] y_move = {0, 1, 0, -1};
+
     static int swanAX;
     static int swanAY;
     static int swanBX;
@@ -47,7 +49,7 @@ public class Main {
                 if(temp.equals(".")) {
                     map[i][j] = 0;
                     countMeltMap[i][j] = 0;
-                    countMeltQueue.offer(new Point(j, i, 0));
+                    countMeltQueue.offer(new Node(j, i, 0));
                 }
                 //얼음은 1
                 else if(temp.equals("X")) {
@@ -65,7 +67,7 @@ public class Main {
                    }
                    map[i][j] = 0;
                    countMeltMap[i][j] = 0;
-                   countMeltQueue.offer(new Point(j, i, 0));
+                   countMeltQueue.offer(new Node(j, i, 0));
                 }
             }
         }
@@ -79,67 +81,66 @@ public class Main {
     private static void countMeltDay() {
         //큐에는 물만 들어가 있다. 즉 물로부터 얼음에 대해서 노드 탐색을 진행한다.
         while (!countMeltQueue.isEmpty()) {
-            Point point = countMeltQueue.poll();
+            Node node = countMeltQueue.poll();
             for (int i = 0; i < 4; i++) {
-                int new_x = point.x + x_move[i];
-                int new_y = point.y + y_move[i];
+                int new_x = node.x + x_move[i];
+                int new_y = node.y + y_move[i];
 
                 //방문하지 않은 노드이면서, 얼음인 부분 탐색
                 if(inArea(new_x, new_y) && !countMeltVisited[new_y][new_x] && map[new_y][new_x] == 1) {
                     //PriorityQueue 이므로 물에 가까운 얼음들부터 노드 탐색이 된다.
                     //아직 탐색이 되지 않았으므로 MAX VALUE 를 갖고 있을 것이다.
-                    if(countMeltMap[new_y][new_x] > point.count+1) {
+                    if(countMeltMap[new_y][new_x] > node.count+1) {
                         countMeltVisited[new_y][new_x] = true;
-                        countMeltMap[new_y][new_x] = point.count+1;
-                        countMeltQueue.offer(new Point(new_x, new_y, point.count+1));
+                        countMeltMap[new_y][new_x] = node.count+1;
+                        countMeltQueue.offer(new Node(new_x, new_y, node.count+1));
                     }
                 }
             }
         }
     }
 
-
-    //해당 좌표에서 밑에쪽의 0까지 거리를 구하는 함수
     // 두 백조가 만나는 날을 계산
     private static int getDateToMeet() {
-        Point swanA = new Point(swanAX, swanAY);
-        Point swanB = new Point(swanBX, swanBY);
+        Node swanA = new Node(swanAX, swanAY);
+        Node swanB = new Node(swanBX, swanBY);
 
         return isWay(swanA, swanB);
     }
 
-    //point1 에서 point2 까지 지정된 날 안에 방문이 가능한지를 확인하는 함수
+    //node1 에서 node2 까지 방문이 가능한지를 확인하는 함수
     //BFS 사용
-    private static int isWay(Point point1, Point point2) {
+    private static int isWay(Node node1, Node node2) {
         boolean[][] visited = new boolean[mapHeight][mapWidth];
 
-        PriorityQueue<Point> queue = new PriorityQueue<Point>();
-        queue.offer(point1);
-        visited[point1.y][point1.x] = true;
+        PriorityQueue<Node> queue = new PriorityQueue<Node>();
+        queue.offer(node1);
+        visited[node1.y][node1.x] = true;
 
         while (!queue.isEmpty()) {
-            Point point = queue.poll();
+            Node node = queue.poll();
 
-            //해당 지점이 point2라면 결과를 반환한다.
-            if(point.y == point2.y && point.x == point2.x) {
-                return point.count;
+            //해당 지점이 두 번째 백조의 위치라면 해당 노드까지 오는데 걸린 최대 얼음의 결과를 반환한다.
+            //count 의 오름차순으로 있는 우선 순위 큐를 사용하기 때문에, 가장 먼저 도착한 경우가 count 를 가장 덜 쓴 경우이다.
+            if(node.y == node2.y && node.x == node2.x) {
+                return node.count;
             }
 
             for (int k = 0; k < 4; k++) {
-                //지정된 point 주위 4칸에 대해서 탐색한다.
-                int new_x = point.x + x_move[k];
-                int new_y = point.y + y_move[k];
+                //지정된 node 주위 4칸에 대해서 탐색한다.
+                int new_x = node.x + x_move[k];
+                int new_y = node.y + y_move[k];
 
                 //범위에도 벗어나지 않고 방문을 안한 경우라면
                 if (inArea(new_x, new_y) && !visited[new_y][new_x]) {
                     visited[new_y][new_x] = true;
 
                     //새로 방문하는 지점의 녹는 점이 더 높다면
-                    if(countMeltMap[new_y][new_x] > point.count) {
-                        queue.offer(new Point(new_x, new_y, countMeltMap[new_y][new_x]));
+                    if(countMeltMap[new_y][new_x] > node.count) {
+                        queue.offer(new Node(new_x, new_y, countMeltMap[new_y][new_x]));
                     }
                     else {
-                        queue.offer(new Point(new_x, new_y, point.count));
+                        queue.offer(new Node(new_x, new_y, node.count));
                     }
                 }
             }
@@ -151,17 +152,17 @@ public class Main {
         return x >= 0 && y >= 0 && x < mapWidth && y < mapHeight;
     }
 
-    static class Point implements Comparable<Point>{
+    static class Node implements Comparable<Node>{
         int x;
         int y;
         int count;
 
-        public Point(int x, int y) {
+        public Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
 
-        public Point(int x, int y, int count) {
+        public Node(int x, int y, int count) {
             this.x = x;
             this.y = y;
             this.count = count;
@@ -169,7 +170,7 @@ public class Main {
 
         //얼음이 녹는데 걸리는 시간이 적은 객체가 우선순위 큐의 제일 앞에 위치한다.
         @Override
-        public int compareTo(Point o) {
+        public int compareTo(Node o) {
             if(this.count < o.count) {
                 return -1;
             }
