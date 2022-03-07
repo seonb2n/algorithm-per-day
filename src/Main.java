f388942a1bcb76f105fb823e8f8094ad5fb1b049
seonb2n <626ksb@gmail.com>
2022-03-07 22:03:04 +0900

    백준 알고리즘 2933번

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,14 @@ public class Main {
     static StringTokenizer st;
     static int mapWidth;
     static int mapHeight;
-    static int[][] map;
-    static int[][] countMeltMap;
-    static PriorityQueue<Node> countMeltQueue;
-    static boolean[][] countMeltVisited;
+    static char[][] map;
+    static PriorityQueue<Node> findMineralQueue;
+    static PriorityQueue<Node> tempQueue;
+    static int N;
 
     //노드 탐색을 할 때, 4방향으로 움직일 수 있기 때문에 움직임 가능한 쌍을 배열로 넣어둔다.
     private static final int[] x_move = {1, 0, -1, 0};
     private static final int[] y_move = {0, 1, 0, -1};
-
-    static int swanAX;
-    static int swanAY;
-    static int swanBX;
-    static int swanBY;
 
     public static void main(String[] args) throws IOException {
 
@@ -32,150 +27,170 @@ public class Main {
         mapHeight = Integer.parseInt(st.nextToken());
         mapWidth = Integer.parseInt(st.nextToken());
 
-        map = new int[mapHeight][mapWidth];
-        countMeltMap = new int[mapHeight][mapWidth];
-        countMeltQueue = new PriorityQueue<>();
-        countMeltVisited = new boolean[mapHeight][mapWidth];
-
-        boolean isFirst = false;
+        map = new char[mapHeight][mapWidth];
+        findMineralQueue = new PriorityQueue<>();
+        tempQueue = new PriorityQueue<>();
 
         for (int i = 0; i < mapHeight; i++) {
-            Arrays.fill(countMeltMap[i], Integer.MAX_VALUE);
-
-            String[] str = br.readLine().split("");
-            for (int j = 0; j < str.length; j++) {
-                String temp = str[j];
-                //물은 0
-                if(temp.equals(".")) {
-                    map[i][j] = 0;
-                    countMeltMap[i][j] = 0;
-                    countMeltQueue.offer(new Node(j, i, 0));
-                }
-                //얼음은 1
-                else if(temp.equals("X")) {
-                    map[i][j] = 1;
-                }
-                else {
-                   if(!isFirst) {
-                       swanAY = i;
-                       swanAX = j;
-                       isFirst = true;
-                   }
-                   else {
-                       swanBY = i;
-                       swanBX = j;
-                   }
-                   map[i][j] = 0;
-                   countMeltMap[i][j] = 0;
-                   countMeltQueue.offer(new Node(j, i, 0));
-                }
+            String str = br.readLine();
+            for (int j = 0; j < mapWidth; j++) {
+                map[i][j] = str.charAt(j);
             }
         }
 
-        //BFS 로 해당 지점의 얼음이 며칠 뒤에 녹을지 계산해야 한다.
-        countMeltDay();
-        System.out.println(getDateToMeet());
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            if(i % 2 == 0) {
+                shootFromLeft(Integer.parseInt(st.nextToken()));
+            }
+            else {
+                shootFromRight(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < mapHeight; i++) {
+            for (int j = 0; j < mapWidth; j++) {
+                sb.append(map[i][j]);
+            }
+            if(i != mapHeight-1) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
 
     }
 
-    private static void countMeltDay() {
-        //큐에는 물만 들어가 있다. 즉 물로부터 얼음에 대해서 노드 탐색을 진행한다.
-        while (!countMeltQueue.isEmpty()) {
-            Node node = countMeltQueue.poll();
+    private static void shootFromLeft(int height) {
+        int heightY = mapHeight - height;
+        for (int i = 0; i < mapWidth; i++) {
+            if(map[heightY][i] == 'x') {
+                map[heightY][i] = '.';
+                checkMap(heightY, i);
+                break;
+            }
+        }
+    }
+
+    private static void shootFromRight(int height) {
+        int heightY = mapHeight - height;
+        for (int i = mapWidth-1; i >= 0; i--) {
+            if(map[heightY][i] == 'x') {
+                map[heightY][i] = '.';
+                checkMap(heightY, i);
+                break;
+            }
+        }
+    }
+
+
+    private static void checkMap(int firstY, int firstX) {
+        PriorityQueue<Node> tempQueue = new PriorityQueue<>();
+        //부서진 노드의 밑의 노드를 탐색한다.
+
+        int new_x;
+        int new_y;
+        for (int i = 0; i < 4; i++) {
+            new_x = firstX + x_move[i];
+            new_y = firstY + y_move[i];
+            if(inArea(new_x, new_y) && map[new_y][new_x] == 'x') {
+                findNode(new_y, new_x, tempQueue);
+            }
+            tempQueue.clear();
+        }
+    }
+
+    static void findNode(int new_y, int new_x, PriorityQueue<Node> tempQueue) {
+        boolean[][] tempVisited = new boolean[mapHeight][mapWidth];
+        Node new_Node = new Node(new_x, new_y);
+        findMineralQueue.offer(new_Node);
+        tempQueue.offer(new_Node);
+        tempVisited[new_y][new_x] = true;
+        //비어 있지 않거나, peek 값이 바닥이 아니면 계속해서 탐색
+        while (!findMineralQueue.isEmpty()) {
+            Node n = findMineralQueue.poll();
+            //연결된 노드를 탐색
+            //꺼낸 노드가 바닥에 닿아있다면, 더이상 탐색할 필요가 없다.
+            if(n.y == mapHeight - 1) {
+                break;
+            }
             for (int i = 0; i < 4; i++) {
-                int new_x = node.x + x_move[i];
-                int new_y = node.y + y_move[i];
-
-                //방문하지 않은 노드이면서, 얼음인 부분 탐색
-                if(inArea(new_x, new_y) && !countMeltVisited[new_y][new_x] && map[new_y][new_x] == 1) {
-                    //PriorityQueue 이므로 물에 가까운 얼음들부터 노드 탐색이 된다.
-                    //아직 탐색이 되지 않았으므로 MAX VALUE 를 갖고 있을 것이다.
-                    if(countMeltMap[new_y][new_x] > node.count+1) {
-                        countMeltVisited[new_y][new_x] = true;
-                        countMeltMap[new_y][new_x] = node.count+1;
-                        countMeltQueue.offer(new Node(new_x, new_y, node.count+1));
-                    }
+                new_x = n.x + x_move[i];
+                new_y = n.y + y_move[i];
+                if(inArea(new_x, new_y) && map[new_y][new_x] == 'x' && !tempVisited[new_y][new_x]) {
+                    new_Node = new Node(new_x, new_y);
+                    findMineralQueue.offer(new_Node);
+                    tempQueue.offer(new_Node);
+                    tempVisited[new_y][new_x] = true;
                 }
             }
         }
+
+        if(!tempQueue.isEmpty() && tempQueue.peek().y != mapHeight-1) {
+            //tempQueue 에 존재하는 Node 들의 y 값을 내려야 한다.
+            downNode(tempQueue);
+        }
+
+        tempQueue.clear();
+        findMineralQueue.clear();
     }
 
-    // 두 백조가 만나는 날을 계산
-    private static int getDateToMeet() {
-        Node swanA = new Node(swanAX, swanAY);
-        Node swanB = new Node(swanBX, swanBY);
-
-        return isWay(swanA, swanB);
+    static void downNode(PriorityQueue<Node> downNodeQueue) {
+        //밑에 미네랄에 붙는 경우를 구해야 한다.
+        int downHeight = findMinimumHeight(downNodeQueue);
+        //땅에 붙는 경우
+        while (!tempQueue.isEmpty()) {
+            Node downNode = tempQueue.poll();
+            map[downNode.y + downHeight][downNode.x] = 'x';
+            map[downNode.y][downNode.x] = '.';
+        }
     }
 
-    //node1 에서 node2 까지 방문이 가능한지를 확인하는 함수
-    //BFS 사용
-    private static int isWay(Node node1, Node node2) {
-        boolean[][] visited = new boolean[mapHeight][mapWidth];
-
-        PriorityQueue<Node> queue = new PriorityQueue<Node>();
-        queue.offer(node1);
-        visited[node1.y][node1.x] = true;
-
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-
-            //해당 지점이 두 번째 백조의 위치라면 해당 노드까지 오는데 걸린 최대 얼음의 결과를 반환한다.
-            //count 의 오름차순으로 있는 우선 순위 큐를 사용하기 때문에, 가장 먼저 도착한 경우가 count 를 가장 덜 쓴 경우이다.
-            if(node.y == node2.y && node.x == node2.x) {
-                return node.count;
-            }
-
-            for (int k = 0; k < 4; k++) {
-                //지정된 node 주위 4칸에 대해서 탐색한다.
-                int new_x = node.x + x_move[k];
-                int new_y = node.y + y_move[k];
-
-                //범위에도 벗어나지 않고 방문을 안한 경우라면
-                if (inArea(new_x, new_y) && !visited[new_y][new_x]) {
-                    visited[new_y][new_x] = true;
-
-                    //새로 방문하는 지점의 녹는 점이 더 높다면
-                    if(countMeltMap[new_y][new_x] > node.count) {
-                        queue.offer(new Node(new_x, new_y, countMeltMap[new_y][new_x]));
+    //해당 큐에서 땅이나 미네랄까지 가장 가까운 거리를 찾는 메서드
+    private static int findMinimumHeight(PriorityQueue<Node> downNodeQueue) {
+        int min = mapHeight - downNodeQueue.peek().y - 1;
+        boolean[] isXVisited = new boolean[100];
+        while (!downNodeQueue.isEmpty()) {
+            Node n = downNodeQueue.poll();
+            tempQueue.offer(n);
+            if(!isXVisited[n.x]) {
+                for (int i = n.y+1; i < mapHeight; i++) {
+                    if(map[i][n.x] == 'x') {
+                        min = Math.min(min, (i - n.y)-1);
                     }
-                    else {
-                        queue.offer(new Node(new_x, new_y, node.count));
+                    if(i == mapHeight - 1 && map[i][n.x] == '.') {
+                        min = Math.min(min, (i - n.y));
                     }
                 }
+                isXVisited[n.x] = true;
             }
+
         }
-        return Integer.MAX_VALUE;
+        return min;
     }
 
-    private static boolean inArea(int x, int y) {
-        return x >= 0 && y >= 0 && x < mapWidth && y < mapHeight;
+    static boolean inArea(int x, int y) {
+        return x >= 0 && x < mapWidth && y >= 0 && y < mapHeight;
     }
 
     static class Node implements Comparable<Node>{
         int x;
         int y;
-        int count;
 
         public Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
 
-        public Node(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-
-        //얼음이 녹는데 걸리는 시간이 적은 객체가 우선순위 큐의 제일 앞에 위치한다.
+        //높이가 가장 낮은 객체가 우선순위 큐의 제일 앞에 위치한다.
         @Override
         public int compareTo(Node o) {
-            if(this.count < o.count) {
-                return -1;
+            if(this.y < o.y) {
+                return 1;
             }
             else {
-                return 1;
+                return -1;
             }
         }
     }
