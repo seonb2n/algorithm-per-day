e8de6ce6c84cafab1fc728548601ba7efbd59f4a
seonb2n <626ksb@gmail.com>
2022-03-15 11:58:22 +0900

    백준 알고리즘 7579번

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ class Main {
     static int needMemory;
     static App[] apps;
     static int minCost;
-    static int[] dp;
+    static int[][] dp;
 
     public static void main(String[]args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,60 +23,60 @@ class Main {
 
         apps = new App[appNumber];
 
-        //특정 메모리를 채울 수 있는 가장 효율적인 cost
-        dp = new int[needMemory];
-
+        int maxCost = 0;
         st = new StringTokenizer(br.readLine(), " ");
         st2 = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < appNumber; i++) {
             apps[i] = new App(Integer.parseInt(st2.nextToken()), Integer.parseInt(st.nextToken()));
+            maxCost+= apps[i].appCost;
         }
 
-        Arrays.sort(apps);
+        // i cost 일 경우, j 번째 app 까지 탐색할 때, 가질 수 있는 최대 메모리
+        dp = new int[maxCost+1][appNumber];
 
-        minCost = Integer.MAX_VALUE;
-        boolean[] searchAppArr = new boolean[appNumber];
-        for (int i = 0; i < appNumber; i++) {
-            boolean[] temp = searchAppArr.clone();
-            temp[i] = true;
-            findMinCost(temp, apps[i].appCost, apps[i].appSize);
-        }
-
-        System.out.println(minCost);
-    }
-
-    private static void findMinCost(boolean[] searchApp, int nowCost, int nowMemory) {
-        if(nowMemory >= needMemory) {
-            minCost = Math.min(minCost, nowCost);
-        } else {
-            for (int i = 0; i < appNumber; i++) {
-                if(!searchApp[i]) {
-                    boolean[] temp = searchApp.clone();
-                    temp[i] = true;
-                    findMinCost(temp, nowCost+apps[i].appCost, nowMemory+apps[i].appSize);
+        for (int i = 0; i < maxCost+1; i++) {
+            for (int j = 0; j < appNumber; j++) {
+                dp[i][j] = findMaxMemory(i, j);
+                if(dp[i][j] >= needMemory) {
+                    System.out.println(i);
+                    return;
                 }
             }
         }
     }
 
-    public static class App implements Comparable<App>{
+    //maxCost 일 때, searchApp 까지 탐색한 경우 가질 수 있는 최대 메모리를 반환한다.
+    private static int findMaxMemory(int maxCost, int searchAppNode) {
+        App searchApp = apps[searchAppNode];
+        if(searchApp.appCost > maxCost) {
+            if(maxCost == 0 && searchAppNode == 0) {
+                return 0;
+            }
+            else if(searchAppNode == 0) {
+                return dp[maxCost-1][searchAppNode];
+            }
+            else {
+                return dp[maxCost][searchAppNode-1];
+            }
+        }
+        //해당 cost 에서 지금 노드를 포함할 수 있다면
+        else {
+           if(searchAppNode == 0) {
+               return searchApp.appSize;
+           }
+           else {
+               return Math.max(dp[maxCost][searchAppNode-1], dp[maxCost-searchApp.appCost][searchAppNode-1] + searchApp.appSize);
+           }
+        }
+    }
+
+    public static class App {
         int appCost;
         int appSize;
-        int eff;
 
         public App(int appCost, int appSize) {
             this.appCost = appCost;
             this.appSize = appSize;
-            if(appCost == 0) {
-                eff = Integer.MAX_VALUE;
-            } else {
-                eff = appSize / appCost;
-            }
-        }
-
-        @Override
-        public int compareTo(App o) {
-            return this.eff >= o.eff ? -1 : 1;
         }
     }
 }
