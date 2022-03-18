f0eb08bb81d29aed10383efb172f5e0e157d1690
seonb2n <626ksb@gmail.com>
2022-03-18 12:02:00 +0900

    백준 알고리즘 - 9370번 (DFS 메모리 초과)

--- src/Main.java
+++ src/Main.java
@@ -7,80 +7,99 @@ class Main {
     static BufferedReader br;
     static StringBuilder sb;
     static StringTokenizer st;
-    static int maxResult;
-    static int N;
-    static int K;
-    static int M;
-    static int[] numbers;
-    static int[] maxNumbers;
-
+    static int testCaseNumber;
+    static int interNumber;
+    static int roadNumber;
+    static int destinationNumber;
+    static int startPoint;
+    static int crossedPoint1;
+    static int crossedPoint2;
+    static int[][] map;
+    static int[] targetPoint;
+    static boolean[] isCrossedToMin;
+    static int minDistance;
 
     public static void main(String[]args) throws IOException {
         sb = new StringBuilder();
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine(), " ");
-        String tempN = st.nextToken();
-        N = Integer.parseInt(tempN);
-        K = Integer.parseInt(st.nextToken());
-        M = tempN.split("").length;
 
-        maxResult = 0;
+        testCaseNumber = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCaseNumber; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            interNumber = Integer.parseInt(st.nextToken());
+            roadNumber = Integer.parseInt(st.nextToken());
+            destinationNumber = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            startPoint = Integer.parseInt(st.nextToken());
+            crossedPoint1 = Integer.parseInt(st.nextToken());
+            crossedPoint2 = Integer.parseInt(st.nextToken());
+            map = new int[interNumber+1][interNumber+1];
 
-        List<Integer> list = new ArrayList<>();
-        list.add(N);
-
-        BFS(0, list);
-
-        if(maxResult == 0) {
-            System.out.println(-1);
-        } else {
-            System.out.println(maxResult);
-        }
-    }
-
-    //BFS 로 K 단계에 해당하는 만큼 진행
-    static void BFS(int nowLevel, List<Integer> numbersList) {
-        int size = numbersList.size();
-        if(nowLevel == K) {
-            for (int i = 0; i < size; i++) {
-                maxResult = Math.max(numbersList.get(i), maxResult);
+            for (int j = 0; j < roadNumber; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int point1 = Integer.parseInt(st.nextToken());
+                int point2 = Integer.parseInt(st.nextToken());
+                int distance = Integer.parseInt(st.nextToken());
+                map[point1][point2] = distance;
+                map[point2][point1] = distance;
             }
-        }
 
-        else {
-            List<Integer> tempList = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                int nowValue = numbersList.get(i);
-                for (int j = 0; j < M-1; j++) {
-                    for (int k = j+1; k < M; k++) {
-                        int[] newValue = numberToArr(nowValue);
-                        newValue = swap(j, k, newValue);
-                        if(!tempList.contains(arrToNumber(newValue)) && newValue[0] != 0) {
-                            tempList.add(arrToNumber(newValue));
-                        }
-                    }
+            targetPoint = new int[destinationNumber];
+            for (int j = 0; j < destinationNumber; j++) {
+                targetPoint[j] = Integer.parseInt(br.readLine());
+            }
+            Arrays.sort(targetPoint);
+
+            isCrossedToMin = new boolean[interNumber+1];
+            //startPoint 에서 targetPoint 까지 가는 경로의 최솟값을 구한다.
+            for (int j = 0; j < destinationNumber; j++) {
+                minDistance = Integer.MAX_VALUE;
+                boolean[][] isVisited = new boolean[interNumber+1][interNumber+1];
+                DFS(targetPoint[j], startPoint, 0, false, isVisited);
+                if(isCrossedToMin[targetPoint[j]]) {
+                    sb.append(targetPoint[j]);
+                    sb.append(" ");
                 }
             }
-            BFS(nowLevel+1, tempList);
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    static int[] swap(int a, int b, int[] targetArr) {
-        int temp = targetArr[a];
-        targetArr[a] = targetArr[b];
-        targetArr[b] = temp;
-        return targetArr;
-    }
-
-    static int[] numberToArr(int number) {
-        return Arrays.stream(String.valueOf(number).split("")).mapToInt(Integer::new).toArray();
-    }
-
-    static int arrToNumber(int[] numberArr) {
-        String str = "";
-        for (int i = 0; i < numberArr.length; i++) {
-            str += numberArr[i];
+    //startPoint 에서 targetPoint 의 각 지점까지 가는 최소 경로를 구한다.
+    //해당 경로에 지나간 경로가 포함되는지를 탐색한다. 포함되어 있는 경우에만 minDistance 를 업데이트한다.
+    private static void DFS(int targetPoint, int nowPoint, int nowDistance, boolean isCrossed, boolean[][] isVisited) {
+        if(nowPoint == targetPoint) {
+            if(minDistance > nowDistance) {
+                if(isCrossed) {
+                    minDistance = nowDistance;
+                    isCrossedToMin[targetPoint] = true;
+                }
+                else {
+                    minDistance = nowDistance;
+                    isCrossedToMin[targetPoint] = false;
+                }
+            }
         }
-        return Integer.parseInt(str);
+        else {
+            //탐색 실시
+            boolean[][] tempVisited = new boolean[interNumber+1][interNumber+1];
+            for (int i = 0; i < isVisited.length; i++) {
+                tempVisited[i] = isVisited[i].clone();
+            }
+            for (int i = 1; i <= interNumber; i++) {
+                if(map[nowPoint][i] != 0 && !tempVisited[nowPoint][i]) {
+                    tempVisited[nowPoint][i] = true;
+                    tempVisited[i][nowPoint] = true;
+                    boolean tempCrossed = isCrossed;
+                    if((crossedPoint1 == nowPoint && crossedPoint2 == i) || (crossedPoint1 == i && crossedPoint2 == nowPoint)){
+                        tempCrossed = true;
+                    }
+                    DFS(targetPoint, i, nowDistance+map[nowPoint][i], tempCrossed, tempVisited);
+                }
+            }
+        }
     }
 }
+
