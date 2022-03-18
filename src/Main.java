9a7176c71530cd37c2160bccab69556793360743
seonb2n <626ksb@gmail.com>
2022-03-18 14:46:04 +0900

    9370 번 변수명 수정

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 public class Main {
 
-    static int N; // 노드의 수(2,000)
-    static int M; // 간선의 수(50,000)
-    static int T; // 목적지 후보 수(100)
-    static int S; // 출발지
-    static int G, H; // 지나간 경로
+    static int N;
+    static int M;
+    static int T;
+    static int S;
+    static int G, H;
 
     static ArrayList<Node>[] adjList;
-    static int[] dist;
-    static PriorityQueue<Node> PQ;
+    static int[] dp;
+    static PriorityQueue<Node> priorityQueue;
     static ArrayList<Integer> candi;
     static final int MAX_VAL = Integer.MAX_VALUE / 2 * 2;
 
@@ -20,9 +20,9 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        int TC = Integer.parseInt(br.readLine());
+        int testCaseNumber = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= TC; tc++) {
+        for (int tc = 0; tc < testCaseNumber; tc++) {
             st = new StringTokenizer(br.readLine());
 
             N = Integer.parseInt(st.nextToken());
@@ -30,14 +30,14 @@ public class Main {
             T = Integer.parseInt(st.nextToken());
 
             adjList = new ArrayList[N + 1];
-            dist = new int[N + 1];
+            dp = new int[N + 1];
             candi = new ArrayList<Integer>();
 
             for (int i = 1; i <= N; i++) {
                 adjList[i] = new ArrayList<Node>();
             }
 
-            Arrays.fill(dist, MAX_VAL);
+            Arrays.fill(dp, MAX_VAL);
 
             st = new StringTokenizer(br.readLine());
 
@@ -70,7 +70,7 @@ public class Main {
             dijkstra(S);
 
             for (int i : candi) {
-                if (dist[i] % 2 == 1) {
+                if (dp[i] % 2 == 1) {
                     sb.append(i);
                     sb.append(" ");
                 }
@@ -97,22 +97,22 @@ public class Main {
     }
 
     public static void dijkstra(int start) {
-        PQ = new PriorityQueue<>();
+        priorityQueue = new PriorityQueue<>();
 
-        dist[start] = 0;
-        PQ.add(new Node(start, 0));
+        dp[start] = 0;
+        priorityQueue.add(new Node(start, 0));
 
-        while (!PQ.isEmpty()) {
+        while (!priorityQueue.isEmpty()) {
 
-            Node here = PQ.poll();
+            Node here = priorityQueue.poll();
 
-            if (dist[here.to] < here.cost)
+            if (dp[here.to] < here.cost)
                 continue;
 
             for (Node next : adjList[here.to]) {
-                if (dist[next.to] > dist[here.to] + next.cost) {
-                    dist[next.to] = dist[here.to] + next.cost;
-                    PQ.add(new Node(next.to, dist[next.to]));
+                if (dp[next.to] > dp[here.to] + next.cost) {
+                    dp[next.to] = dp[here.to] + next.cost;
+                    priorityQueue.add(new Node(next.to, dp[next.to]));
                 }
             }
         }
