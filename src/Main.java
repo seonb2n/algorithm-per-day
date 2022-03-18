2402ff98eccf0d95010aadbc2bbbfbd7b0e4c2d3
seonb2n <626ksb@gmail.com>
2022-03-18 14:38:13 +0900

    백준 알고리즘 - 9370번(다익스트라)

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,77 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-class Main {
-    static BufferedReader br;
-    static StringBuilder sb;
-    static StringTokenizer st;
-    static int testCaseNumber;
-    static int interNumber;
-    static int roadNumber;
-    static int destinationNumber;
-    static int startPoint;
-    static int crossedPoint1;
-    static int crossedPoint2;
-    static int[][] map;
-    static int[] targetPoint;
-    static boolean[] isCrossedToMin;
-    static int minDistance;
+public class Main {
 
-    public static void main(String[]args) throws IOException {
-        sb = new StringBuilder();
-        br = new BufferedReader(new InputStreamReader(System.in));
+    static int N; // 노드의 수(2,000)
+    static int M; // 간선의 수(50,000)
+    static int T; // 목적지 후보 수(100)
+    static int S; // 출발지
+    static int G, H; // 지나간 경로
 
-        testCaseNumber = Integer.parseInt(br.readLine());
-        for (int i = 0; i < testCaseNumber; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            interNumber = Integer.parseInt(st.nextToken());
-            roadNumber = Integer.parseInt(st.nextToken());
-            destinationNumber = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine(), " ");
-            startPoint = Integer.parseInt(st.nextToken());
-            crossedPoint1 = Integer.parseInt(st.nextToken());
-            crossedPoint2 = Integer.parseInt(st.nextToken());
-            map = new int[interNumber+1][interNumber+1];
+    static ArrayList<Node>[] adjList;
+    static int[] dist;
+    static PriorityQueue<Node> PQ;
+    static ArrayList<Integer> candi;
+    static final int MAX_VAL = Integer.MAX_VALUE / 2 * 2;
 
-            for (int j = 0; j < roadNumber; j++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int point1 = Integer.parseInt(st.nextToken());
-                int point2 = Integer.parseInt(st.nextToken());
-                int distance = Integer.parseInt(st.nextToken());
-                map[point1][point2] = distance;
-                map[point2][point1] = distance;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int TC = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= TC; tc++) {
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            T = Integer.parseInt(st.nextToken());
+
+            adjList = new ArrayList[N + 1];
+            dist = new int[N + 1];
+            candi = new ArrayList<Integer>();
+
+            for (int i = 1; i <= N; i++) {
+                adjList[i] = new ArrayList<Node>();
             }
 
-            targetPoint = new int[destinationNumber];
-            for (int j = 0; j < destinationNumber; j++) {
-                targetPoint[j] = Integer.parseInt(br.readLine());
-            }
-            Arrays.sort(targetPoint);
+            Arrays.fill(dist, MAX_VAL);
 
-            isCrossedToMin = new boolean[interNumber+1];
-            //startPoint 에서 targetPoint 까지 가는 경로의 최솟값을 구한다.
-            for (int j = 0; j < destinationNumber; j++) {
-                minDistance = Integer.MAX_VALUE;
-                boolean[][] isVisited = new boolean[interNumber+1][interNumber+1];
-                DFS(targetPoint[j], startPoint, 0, false, isVisited);
-                if(isCrossedToMin[targetPoint[j]]) {
-                    sb.append(targetPoint[j]);
+            st = new StringTokenizer(br.readLine());
+
+            S = Integer.parseInt(st.nextToken());
+            G = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+
+            for (int i = 1; i <= M; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                int cost = Integer.parseInt(st.nextToken());
+
+                if ((from == G || from == H) && (to == G || to == H)) {
+                    adjList[from].add(new Node(to, cost * 2 - 1));
+                    adjList[to].add(new Node(from, cost * 2 - 1));
+                } else {
+                    adjList[from].add(new Node(to, cost * 2));
+                    adjList[to].add(new Node(from, cost * 2));
+                }
+
+            }
+
+            for (int i = 1; i <= T; i++) {
+                candi.add(Integer.parseInt(br.readLine()));
+            }
+            Collections.sort(candi);
+
+            dijkstra(S);
+
+            for (int i : candi) {
+                if (dist[i] % 2 == 1) {
+                    sb.append(i);
                     sb.append(" ");
                 }
             }
@@ -67,39 +81,40 @@ class Main {
         System.out.println(sb);
     }
 
-    //startPoint 에서 targetPoint 의 각 지점까지 가는 최소 경로를 구한다.
-    //해당 경로에 지나간 경로가 포함되는지를 탐색한다. 포함되어 있는 경우에만 minDistance 를 업데이트한다.
-    private static void DFS(int targetPoint, int nowPoint, int nowDistance, boolean isCrossed, boolean[][] isVisited) {
-        if(nowPoint == targetPoint) {
-            if(minDistance > nowDistance) {
-                if(isCrossed) {
-                    minDistance = nowDistance;
-                    isCrossedToMin[targetPoint] = true;
-                }
-                else {
-                    minDistance = nowDistance;
-                    isCrossedToMin[targetPoint] = false;
-                }
-            }
+    public static class Node implements Comparable<Node> {
+        int to;
+        int cost;
+
+        public Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
         }
-        else {
-            //탐색 실시
-            boolean[][] tempVisited = new boolean[interNumber+1][interNumber+1];
-            for (int i = 0; i < isVisited.length; i++) {
-                tempVisited[i] = isVisited[i].clone();
-            }
-            for (int i = 1; i <= interNumber; i++) {
-                if(map[nowPoint][i] != 0 && !tempVisited[nowPoint][i]) {
-                    tempVisited[nowPoint][i] = true;
-                    tempVisited[i][nowPoint] = true;
-                    boolean tempCrossed = isCrossed;
-                    if((crossedPoint1 == nowPoint && crossedPoint2 == i) || (crossedPoint1 == i && crossedPoint2 == nowPoint)){
-                        tempCrossed = true;
-                    }
-                    DFS(targetPoint, i, nowDistance+map[nowPoint][i], tempCrossed, tempVisited);
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+
+    public static void dijkstra(int start) {
+        PQ = new PriorityQueue<>();
+
+        dist[start] = 0;
+        PQ.add(new Node(start, 0));
+
+        while (!PQ.isEmpty()) {
+
+            Node here = PQ.poll();
+
+            if (dist[here.to] < here.cost)
+                continue;
+
+            for (Node next : adjList[here.to]) {
+                if (dist[next.to] > dist[here.to] + next.cost) {
+                    dist[next.to] = dist[here.to] + next.cost;
+                    PQ.add(new Node(next.to, dist[next.to]));
                 }
             }
         }
     }
 }
-
