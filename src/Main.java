2faadced783d7d6d3e78ce41beae0767c6650e1b
seonb2n <626ksb@gmail.com>
2022-03-19 09:28:52 +0900

    프로그래머스 - 시험장 나누기

--- src/Main.java
+++ src/Main.java
@@ -1,120 +1,85 @@
-import java.io.*;
 import java.util.*;
 
-public class Main {
+class Solution {
+    int n, root, cnt;
+    int[] parent, left, right, people;
 
-    static int N;
-    static int M;
-    static int T;
-    static int S;
-    static int G, H;
+    public int solution(int k, int[] num, int[][] links) {
+        n = num.length;
+        parent = new int[n];
+        left = new int[n];
+        right = new int[n];
+        people = num;
+        int lo = 0, hi = (int) 1e9;
 
-    static ArrayList<Node>[] adjList;
-    static int[] dp;
-    static PriorityQueue<Node> priorityQueue;
-    static ArrayList<Integer> candi;
-    static final int MAX_VAL = Integer.MAX_VALUE / 2 * 2;
+        Arrays.fill(parent, -1);
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        //각 i 별로 왼쪽 노드와 오른쪽 노드, 부모 노드를 채운다.
+        for(int i = 0; i < links.length; i++) {
+            left[i] = links[i][0];
+            right[i] = links[i][1];
 
-        int testCaseNumber = Integer.parseInt(br.readLine());
+            if(left[i] != -1)
+                parent[left[i]] = i;
+            if(right[i] != -1)
+                parent[right[i]] = i;
 
-        for (int tc = 0; tc < testCaseNumber; tc++) {
-            st = new StringTokenizer(br.readLine());
-
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            T = Integer.parseInt(st.nextToken());
-
-            adjList = new ArrayList[N + 1];
-            dp = new int[N + 1];
-            candi = new ArrayList<Integer>();
-
-            for (int i = 1; i <= N; i++) {
-                adjList[i] = new ArrayList<Node>();
-            }
-
-            Arrays.fill(dp, MAX_VAL);
-
-            st = new StringTokenizer(br.readLine());
-
-            S = Integer.parseInt(st.nextToken());
-            G = Integer.parseInt(st.nextToken());
-            H = Integer.parseInt(st.nextToken());
-
-            for (int i = 1; i <= M; i++) {
-                st = new StringTokenizer(br.readLine());
-
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                int cost = Integer.parseInt(st.nextToken());
-
-                if ((from == G || from == H) && (to == G || to == H)) {
-                    adjList[from].add(new Node(to, cost * 2 - 1));
-                    adjList[to].add(new Node(from, cost * 2 - 1));
-                } else {
-                    adjList[from].add(new Node(to, cost * 2));
-                    adjList[to].add(new Node(from, cost * 2));
-                }
-
-            }
-
-            for (int i = 1; i <= T; i++) {
-                candi.add(Integer.parseInt(br.readLine()));
-            }
-            Collections.sort(candi);
-
-            dijkstra(S);
-
-            for (int i : candi) {
-                if (dp[i] % 2 == 1) {
-                    sb.append(i);
-                    sb.append(" ");
-                }
-            }
-            sb.append("\n");
+            lo = Math.max(lo, people[i]);
         }
 
-        System.out.println(sb);
+        //루트 노드를 찾는다.
+        for(int i = 0; i < n; i++)
+            if(parent[i] == -1) {
+                root = i;
+                break;
+            }
+
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if(getGroup(mid) <= k) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return hi + 1;
     }
 
-    public static class Node implements Comparable<Node> {
-        int to;
-        int cost;
-
-        public Node(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.cost, o.cost);
-        }
+    //limit 이하의 사람 수를 가진 그룹이 몇 개가 나오는가.
+    public int getGroup(int limit) {
+        //현재 그룹 수
+        cnt = 0;
+        dfs(root, limit);
+        return cnt + 1;
     }
 
-    public static void dijkstra(int start) {
-        priorityQueue = new PriorityQueue<>();
+    //지금 노드에서 탐색할 때, limit 수가 넘으면 그룹을 나눈다.
+    public int dfs(int currentNode, int limit) {
+        int leftStudentNumber = 0;
+        int rightStudentNumber = 0;
 
-        dp[start] = 0;
-        priorityQueue.add(new Node(start, 0));
+        //왼쪽 하위 노드로 탐색을 한다.
+        if(left[currentNode] != -1)
+            leftStudentNumber = dfs(left[currentNode], limit);
 
-        while (!priorityQueue.isEmpty()) {
+        //오른쪽 하위 노드로 탐색을 한다.
+        if(right[currentNode] != -1)
+            rightStudentNumber = dfs(right[currentNode], limit);
 
-            Node here = priorityQueue.poll();
-
-            if (dp[here.to] < here.cost)
-                continue;
-
-            for (Node next : adjList[here.to]) {
-                if (dp[next.to] > dp[here.to] + next.cost) {
-                    dp[next.to] = dp[here.to] + next.cost;
-                    priorityQueue.add(new Node(next.to, dp[next.to]));
-                }
-            }
+        //숫자가 작으면 지금 그룹을 유지해도 된다.
+        if(people[currentNode] + leftStudentNumber + rightStudentNumber <= limit) {
+            return people[currentNode] + leftStudentNumber + rightStudentNumber;
         }
+
+        //숫자가 크지만, 둘 중 왼쪽과 오른쪽 중 작은쪽만 사용해도 된다면 그룹을 쪼갠다.
+        if(people[currentNode] + Math.min(leftStudentNumber, rightStudentNumber) <= limit) {
+            cnt++;
+            return people[currentNode] + Math.min(leftStudentNumber, rightStudentNumber);
+        }
+
+        //루트, 왼쪽 하위, 오른쪽 하위 3개로 그룹을 쪼개야 한다.
+        cnt += 2;
+        return people[currentNode];
     }
 }
