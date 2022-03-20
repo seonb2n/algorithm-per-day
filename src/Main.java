e9f9bb8f5e26a972c3903edb7fa5672b7ca73d06
seonb2n <626ksb@gmail.com>
2022-03-20 10:55:28 +0900

    프로그래머스 N 으로 표현

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,55 @@
 import java.util.*;
 
 class Solution {
-    HashMap<Integer, Integer> timeMap = new HashMap<>();
-    static int[] startTimeArr;
-    static int[] endTimeArr;
-    static int maxNumber = 0;
-    static int minStartTime;
-    static int maxEndTime;
+    static numbers[] dp;
+    static int N;
+    static int targetNumber;
 
-    public int solution(String[] lines) {
+    public int solution(int n, int number) {
+        int N = n;
+        targetNumber = number;
+        dp = new numbers[9];
+        dp[1] = new numbers(1);
+        dp[1].numberList.add(N);
 
-        int n = lines.length;
-        minStartTime = Integer.MAX_VALUE;
-        maxEndTime = 0;
+        for(int i = 2; i<=8; i++) {
+            //N을 i 개 사용해서 만들 수 있는 수들을 추가해준다.
+            //N 을 i 개 사용해 만들 수 있는 수는, i-1 과 1의 사칙 연산, .. 1과 i-1 의 사칙연산
+            dp[i] = new numbers(i);
+            for(int j = 1; j < i; j++) {
+                Set<Integer> firstSet = dp[j].numberList;
+                Set<Integer> secondSet = dp[i-j].numberList;
 
-        startTimeArr = new int[n];
-        endTimeArr = new int[n];
-
-        for(int i = 0; i < lines.length; i++) {
-            startTimeArr[i] = getStartTime(lines[i]);
-            minStartTime = Math.min(minStartTime, startTimeArr[i]);
-            endTimeArr[i] = getEndTime(lines[i]);
-            maxEndTime = Math.max(maxEndTime, endTimeArr[i]);
-        }
-
-        for(int i = 0; i < n; i++) {
-            int time = endTimeArr[i];
-            int cnt = 0;
-            for(int j = 0; j < n; j++) {
-                if(time <= startTimeArr[j] && startTimeArr[j] <= time + 999) {
-                    cnt++;
-                }
-                else if(time <= endTimeArr[j] && endTimeArr[j] <= time + 999) {
-                    cnt++;
-                }
-                else if(startTimeArr[j]<=time &&time+999 <= endTimeArr[j]) {
-                    cnt++;
+                for(int firstNumber : firstSet) {
+                    for(int secondNumber : secondSet) {
+                        dp[i].numberList.add(firstNumber + secondNumber);
+                        dp[i].numberList.add(firstNumber * secondNumber);
+                        dp[i].numberList.add(firstNumber - secondNumber);
+                        if(firstNumber != 0 && secondNumber != 0) {
+                            dp[i].numberList.add(firstNumber / secondNumber);
+                        }
+                    }
                 }
             }
-            maxNumber = Math.max(maxNumber, cnt);
+            dp[i].numberList.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+        }
+        for(int i = 1; i <= 8; i++) {
+            if(dp[i].numberList.contains(number)) {
+                return i;
+            }
         }
 
-        return maxNumber;
+        int answer = -1;
+        return answer;
     }
 
-    public static int getStartTime(String log) {
-        StringTokenizer st = new StringTokenizer(log, " ");
-        st.nextToken();
-        String time = st.nextToken();
-        String millSecond = st.nextToken();
-        st = new StringTokenizer(millSecond, "s");
-        float milSec = Float.parseFloat(st.nextToken());
+    public static class numbers {
+        int usedNumber;
+        Set<Integer> numberList;
 
-        st = new StringTokenizer(time, ":");
-        int hour = Integer.parseInt(st.nextToken());
-        int minute = Integer.parseInt(st.nextToken());
-        float second = Float.parseFloat(st.nextToken());
-
-        if(second > milSec) {
-            second = second * 10000 / 10;
-            milSec = milSec * 10000 / 10;
-            return hour*10000000 + minute * 100000 + (int)(second-milSec) + 1;
+        public numbers(int usedNumber) {
+            this.usedNumber = usedNumber;
+            numberList = new HashSet<>();
         }
-        if(minute == 0) {
-            hour = hour-1;
-            minute = 59;
-            second += 60;
-            return hour*10000000 + minute * 100000 + (int)((second-milSec) * 1000f) + 1;
-        }
-        else {
-            minute--;
-            second+= 60;
-            return hour*10000000 + minute * 100000 + (int)((second-milSec) * 1000f) + 1;
-        }
-    }
-
-    public static int getEndTime(String log) {
-        StringTokenizer st = new StringTokenizer(log, " ");
-        st.nextToken();
-        String time = st.nextToken();
-        st = new StringTokenizer(time, ":");
-        int hour = Integer.parseInt(st.nextToken());
-        int minute = Integer.parseInt(st.nextToken());
-        float second = Float.parseFloat(st.nextToken());
-        return hour*10000000 + minute * 100000 + (int)(second*10000 / 10);
     }
 }
