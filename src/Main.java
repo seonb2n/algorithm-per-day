b559430f6cbb03fe278f9cdbabf9ed63df9e8685
seonb2n <626ksb@gmail.com>
2022-03-22 08:25:51 +0900

    프로그래머스 - 입국심사

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public static PriorityQueue<CheckPoint> queue;
-    public static long timeResult;
+    public long solution(int n, int[] times) {
 
-    public static void main(String[] args) {
-        int[] time = {7, 10};
-        solution(6, time);
+        Arrays.sort(times);
+        long left = times[0];  // 1분 부터
+        long right = (long)times[times.length-1]*(long)n;
+
+        while(left < right) {
+            long mid = (left + right) / 2;
+            long number = getNumber(mid, times);
+            if(number >= n) {
+                right = mid;
+            }
+            else {
+                left = mid+1;
+            }
+        }
+        return right;
     }
 
-    public static long solution(int n, int[] times) {
-        queue = new PriorityQueue<>();
-        timeResult = 0;
+    public static long getNumber(long time, int[] times) {
+        long maxNumber = 0;
         for(int i = 0; i < times.length; i++) {
-            queue.offer(new CheckPoint(times[i]));
+            maxNumber += (time /times[i]);
         }
-
-        while(n > 0) {
-            //사람 한 명을 가장 빨리 검색이 끝나는 검문대로 보낸다.
-            CheckPoint minCheckPoint = queue.poll();
-            minCheckPoint.remainedTime += minCheckPoint.time;
-            queue.offer(minCheckPoint);
-            n--;
-        }
-
-        while(!queue.isEmpty()) {
-            timeResult = Math.max(timeResult, queue.poll().remainedTime);
-        }
-
-        return timeResult;
-    }
-
-    public static class CheckPoint implements Comparable<CheckPoint>{
-        long remainedTime;
-        int time;
-
-        public CheckPoint(int time) {
-            this.remainedTime = 0;
-            this.time = time;
-        }
-
-        @Override
-        public int compareTo(CheckPoint c) {
-            long totalTime = this.remainedTime + this.time;
-            long newTotalTime = c.remainedTime + c.time;
-            return totalTime > newTotalTime ? 1 : -1;
-        }
+        return maxNumber;
     }
 }
