e9342e359b1567996d2ebf81f6b6bece60ee3ea4
seonb2n <626ksb@gmail.com>
2022-03-28 15:10:10 +0900

    백준 알고리즘 10217번

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ class Main{
     static int N;
     static int M;
     static int K;
-    static Node[] dist;
-    static List<Vertex>[] adj;
-    static final int INF = 99999;
+    static int[][] dist;
+    static List<Vertex>[] airLineMap;
+    static final int INF = Integer.MAX_VALUE/2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,69 +25,92 @@ class Main{
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            adj = new ArrayList[N+1];
+            airLineMap = new ArrayList[N+1];
 
             for (int j = 0; j < N+1; j++) {
-                adj[j] = new ArrayList<>();
+                airLineMap[j] = new ArrayList<>();
             }
 
             for (int j = 0; j < K; j++) {
                 st = new StringTokenizer(br.readLine(), " ");
                 int startPoint = Integer.parseInt(st.nextToken());
                 int endPoint = Integer.parseInt(st.nextToken());
-                adj[startPoint].add(new Vertex(endPoint, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+                airLineMap[startPoint].add(new Vertex(endPoint, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             }
 
-            djikstra(1);
-            if(dist[N].time != INF) {
-                sb.append(dist[N].time);
-                sb.append("\n");
-            } else {
-                sb.append("Poor KCM");
-                sb.append("\n");
+            dijkstra(1);
+
+            int min = INF;
+
+            for (int j = 1; j <= M; j++) {
+                min = Math.min(min, dist[N][j]);
             }
+
+            if(min == INF) {
+                sb.append("Poor KCM");
+            }
+            else {
+                sb.append(min);
+            }
+            sb.append("\n");
+
         }
+
         System.out.println(sb);
     }
 
     //startPoint 로부터 M 까지 도착하는 최소 소요시간을 구한다.
-    private static void djikstra(int startPoint) {
-        dist = new Node[N+1];
+    private static void dijkstra(int startPoint) {
+        //i번째 지점에 j 만큼의 비용으로 올 수 있는 최소의 시간 값
+        dist = new int[N+1][M+1];
+
         for (int i = 0; i < N+1; i++) {
-            dist[i] = new Node(0, INF);
+            Arrays.fill(dist[i], INF);
         }
 
-        dist[startPoint].time = 0;
+        //1번 공항 초기화
+        dist[startPoint][1] = 0;
 
-        boolean[] isVisited = new boolean[N+1];
-        isVisited[startPoint] = true;
-
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        queue.offer(startPoint);
+        PriorityQueue<Vertex> queue = new PriorityQueue<>();
+        queue.offer(new Vertex(startPoint, 0, 0));
 
         while(!queue.isEmpty()) {
-            int nowNode = queue.poll();
-            for(Vertex vertex : adj[nowNode]) {
-                if(!isVisited[vertex.destination] && dist[vertex.destination].time > dist[nowNode].time + vertex.time && M >= vertex.cost + dist[nowNode].cost) {
-                    dist[vertex.destination].time = dist[nowNode].time + vertex.time;
-                    dist[vertex.destination].cost = dist[nowNode].cost + vertex.cost;
-                    queue.offer(vertex.destination);
-                }
+            Vertex nowVertex = queue.poll();
+            int nowDestination = nowVertex.destination;
+            int nowCost = nowVertex.cost;
+            int nowTime = nowVertex.time;
+
+            if(nowDestination == N) {
+                break;
             }
-            isVisited[nowNode] = true;
+
+            for (Vertex vertex : airLineMap[nowDestination]) {
+                int nextDestination = vertex.destination;
+                int nextCost = nowCost + vertex.cost;
+                int nextTime = nowTime + vertex.time;
+
+                //비용이 M 을 초과하는 경우는 스킵
+                if(nextCost > M) {
+                    continue;
+                }
+
+                //이미 해당 지점에 해당 돈으로 갈 수 있는 시간이 더 작다면
+                if(dist[nextDestination][nextCost] <= nextTime) {
+                    continue;
+                }
+
+                //지금 비용 이상으로 갈 수 있는 지점은 모두 지금 시간을 최소로 하게 한다. (더 비싼 비용으로 더 시간 오래 걸리는 경우를 방지)
+                for (int i = nextCost; i <= M; i++) {
+                    if(dist[nextDestination][i] > nextTime) {
+                        dist[nextDestination][i] = nextTime;
+                    }
+                }
+
+                dist[nextDestination][nextCost] = nextTime;
+                queue.offer(new Vertex(nextDestination, nextCost, nextTime));
+            }
         }
     }
-
-    public static class Node {
-        int cost;
-        int time;
-
-        public Node(int cost, int time) {
-            this.cost = cost;
-            this.time = time;
-        }
-    }
-
 
     public static class Vertex implements Comparable<Vertex>{
         int destination;
@@ -102,7 +125,16 @@ class Main{
 
         @Override
         public int compareTo(Vertex o) {
-            return this.time - o.time;
+            if(this.time < o.time) {
+                return -1;
+            }
+            else if(this.time == o.time) {
+                if(this.cost < o.cost) {
+                    return -1;
+                }
+                return 0;
+            }
+            return 1;
         }
     }
 }
