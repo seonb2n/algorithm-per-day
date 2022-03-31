336fba52dc482d3f3deac256b5c31e02d6e6bf21
seonb2n <626ksb@gmail.com>
2022-03-31 16:05:43 +0900

    백준 알고리즘 1956번

--- src/Main.java
+++ src/Main.java
@@ -5,115 +5,65 @@ import java.util.*;
 
 class Main{
 
-    static int[][] map;
-    static Queue<Node> queue;
-    static int startY = 7;
-    static int startX = 0;
-    static boolean isWay;
-    static int[] move_x = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-    static int[] move_y = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+    static final int INF = Integer.MAX_VALUE / 2;
+    static int V;
+    static int E;
+    static int[][] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        map = new int[8][8];
-        queue = new LinkedList<>();
-        isWay = false;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < 8; i++) {
-            String[] str = br.readLine().split("");
-            for (int k = 0; k < 8; k++) {
-                if(str[k].equals("#")) {
-                    map[i][k] = 1;
-                    queue.add(new Node(i, k));
-                }
-                else {
-                    map[i][k] = 0;
+        dist = new int[V+1][V+1];
+
+        for (int i = 1; i < V+1; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int startPoint = Integer.parseInt(st.nextToken());
+            int endPoint = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            dist[startPoint][endPoint] = cost;
+        }
+
+        //플로이드 와샬 알고리즘을 사용해서 dist 를 각 지점에 대한 최솟값으로 계산한다.
+        //거치는 노드
+        for (int i = 1; i < V+1; i++) {
+            //출발 노드
+            for (int j = 1; j < V+1; j++) {
+                //도착 노드
+                for (int k = 1; k < V+1; k++) {
+                    if(dist[j][k] > dist[j][i] + dist[i][k]) {
+                        dist[j][k] = dist[j][i] + dist[i][k];
+                    }
                 }
             }
         }
 
-        //bfs 로 각 칸이 몇 턴 뒤에 벽으로 변하는지 구해야 함.
-        BFS();
+        //순환한다는 것은 a->b, b->a 가 INF 가 아니라는 것이다.
+        int answer = INF;
 
-        //dfs 로 경로를 탐색한다. 9턴 동안 살아있기만 하면 된다.
-        DFS(1, startY, startX);
+        for (int i = 1; i < V + 1; i++) {
+            for (int j = 1; j < V+1; j++) {
+                if(i != j && dist[i][j] != INF && dist[j][i] != INF) {
+                    answer = Math.min(answer, dist[i][j] + dist[j][i]);
+                }
+            }
+        }
 
-        if(isWay) {
-            System.out.println(1);
+        if(answer != INF) {
+            System.out.println(answer);
         }
         else {
-            System.out.println(0);
+            System.out.println(-1);
         }
-    }
 
-    //각 칸이 몇 턴 뒤에 노드로 변하는지 계산한다.
-    public static void BFS() {
-        int nowTurn = 2;
-
-        while (!queue.isEmpty()) {
-            int nowSize = queue.size();
-            for (int i = 0; i < nowSize; i++) {
-                Node nowNode = queue.poll();
-
-                //지금의 벽이 아래로 한 칸씩 이동한다.
-                int nowY = nowNode.y;
-                int nowX = nowNode.x;
-
-                if((nowY + 1) < 8) {
-                    if(map[nowY+1][nowX] != 0) {
-                        String temp = String.valueOf(map[nowY+1][nowX]);
-                        temp = temp + nowTurn;
-                        map[nowY+1][nowX] = Integer.parseInt(temp);
-                    }
-                    else {
-                        map[nowY+1][nowX] = nowTurn;
-                    }
-                    queue.add(new Node(nowY+1, nowX));
-                }
-            }
-            nowTurn++;
-        }
-    }
-
-    public static void DFS(int nowTurn, int nowY, int nowX) {
-        if(nowTurn == 9) {
-            isWay = true;
-        }
-        else {
-            for (int i = 0; i < 9; i++) {
-                int newY = nowY+move_y[i];
-                int newX = nowX+move_x[i];
-                if(inArea(newY, newX)) {
-                    int[] temp = Arrays.stream(String.valueOf(map[newY][newX]).split("")).mapToInt(Integer::new).toArray();
-                    //nowTurn 이 temp 의 값에 해당 사항이 없을 때만 다음 단계로 탐색을 할 수 있다.
-                    boolean isContain = false;
-                    for (int j = 0; j < temp.length; j++) {
-                        if(temp[j] == nowTurn || temp[j] == nowTurn+1) {
-                            isContain = true;
-                            break;
-                        }
-                    }
-                    if(!isContain) {
-                        DFS(nowTurn+1, newY, newX);
-                    }
-                }
-            }
-        }
-    }
-
-    public static boolean inArea(int y, int x) {
-        return 0<= y && y < 8 && 0 <= x && x < 8;
-    }
-
-    public static class Node {
-        int y;
-        int x;
-
-        public Node(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
     }
 }
