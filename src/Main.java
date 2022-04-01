54581e77f6b9610d84aaf6fe9d493a3826873ad7
seonb2n <626ksb@gmail.com>
2022-04-01 09:51:18 +0900

    백준 알고리즘 16946번

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,14 @@ class Main{
     static int N;
     static int M;
     static int[][] map;
+    static int[][] groupMap;
     static Queue<Node> queue;
     static int count;
-    static boolean[][] isVisited;
     static int[] y_move = {-1, 1, 0, 0};
     static int[] x_move = {0, 0, 1, -1};
     static int[][] result;
+    static HashMap<Integer, Integer> groups;
+    static int groupId = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,9 +25,10 @@ class Main{
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
+        groupMap = new int[N][M];
         result = new int[N][M];
         queue = new LinkedList<>();
-
+        groups = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             String[] str = br.readLine().split("");
@@ -37,33 +40,63 @@ class Main{
             }
         }
 
-        //각각의 벽에 대해서 벽을 부수고 이동할 수 있는 칸의 개수를 DFS 로 탐색한다.
+        //각각의 0에 대해서 인접한 0 끼리 그룹화를 해준다.
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                //해당 지점이 0이고, 아직 그룹화되지 않은 장소라면
+                if(map[i][j] == 0 && groupMap[i][j] == 0) {
+                    groupId++;
+                    count = 0;
+                    BFS(i, j);
+                    groups.put(groupId, count);
+                }
+            }
+        }
+
+        //각각의 벽에 대해서 4방향을 확인하면서 그룹의 개수를 세주면 된다.
         while (!queue.isEmpty()) {
-            count = 1;
-            isVisited = new boolean[N][M];
             Node nowNode = queue.poll();
-            DFS(nowNode.y, nowNode.x);
-            result[nowNode.y][nowNode.x] = count;
+            int nowY = nowNode.y;
+            int nowX = nowNode.x;
+            int nowCount = 1;
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int i = 0; i < 4; i++) {
+                int newY = nowY + y_move[i];
+                int newX = nowX + x_move[i];
+                if(inArea(newY, newX) && map[newY][newX] != 1) {
+                    set.add(groupMap[newY][newX]);
+                }
+            }
+
+            for(Integer i : set) {
+                nowCount += groups.get(i);
+            }
+            result[nowY][nowX] = nowCount % 10;
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 sb.append(result[i][j]);
             }
-            sb.append("\n");
+            if(i != N-1) {
+                sb.append("\n");
+            }
         }
 
         System.out.println(sb);
     }
 
-    public static void DFS(int nowY, int nowX) {
+    //인접한 0을 탐색하며 해당 그룹맵을 최신화 시킨다.
+    public static void BFS(int nowY, int nowX) {
+        groupMap[nowY][nowX] = groupId;
+        count++;
+
         for (int i = 0; i < 4; i++) {
             int newY = nowY + y_move[i];
             int newX = nowX + x_move[i];
-            if(inArea(newY, newX) && !isVisited[newY][newX] && map[newY][newX] == 0) {
-                count++;
-                isVisited[newY][newX] = true;
-                DFS(newY, newX);
+            if(inArea(newY, newX) && groupMap[newY][newX] == 0 && map[newY][newX] == 0) {
+                BFS(newY, newX);
             }
         }
     }
