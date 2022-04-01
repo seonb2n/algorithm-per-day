27e6f653a6a44f79202d570ae3a42fe008a5615e
seonb2n <626ksb@gmail.com>
2022-04-01 11:25:21 +0900

    백준 알고리즘 2098번

--- src/Main.java
+++ src/Main.java
@@ -6,112 +6,71 @@ import java.util.*;
 class Main{
 
     static int N;
-    static int M;
     static int[][] map;
-    static int[][] groupMap;
-    static Queue<Node> queue;
-    static int count;
-    static int[] y_move = {-1, 1, 0, 0};
-    static int[] x_move = {0, 0, 1, -1};
-    static int[][] result;
-    static HashMap<Integer, Integer> groups;
-    static int groupId = 0;
+    static int[][] dp;
+    static final int INF = Integer.MAX_VALUE / 2;
+    static int MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
 
-        map = new int[N][M];
-        groupMap = new int[N][M];
-        result = new int[N][M];
-        queue = new LinkedList<>();
-        groups = new HashMap<>();
+        //비트마스킹을 통해서 방문한 노드를 판별할 것이기 때문에, N 만큼의 자리를 가진 2진수를 만들어준다.
+        MAX_VALUE = 1 << N;
+        //해당 노드(i)에서 노드 방문 상태(j)일 때, 원점으로 돌아가는 최소 비용의 기록
+        dp = new int[N][MAX_VALUE];
+        map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            String[] str = br.readLine().split("");
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(str[j]);
-                if(map[i][j] == 1) {
-                    queue.add(new Node(i, j));
-                }
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.fill(dp[i], INF);
         }
 
-        //각각의 0에 대해서 인접한 0 끼리 그룹화를 해준다.
+        //비트마스크로 방문한 정점을 기록한다.
+        System.out.println(TSP(0, 1));
+    }
+
+    public static int TSP(int nowNode, int visited) {
+        //모든 지점을 다 방문한 경우
+        if(visited == MAX_VALUE-1) {
+            //지금 노드에서 시작 지점(0)까지 가는 경로가 없다면
+            if(map[nowNode][0] == 0) {
+                return INF;
+            }
+            //지금 노드에서 0까지 가는 값을 반환한다.
+            return map[nowNode][0];
+        }
+
+        //이미 지금 점에서 원점으로 돌아가는 비용이 계산되어 있다면 해당 값을 반환
+        if(dp[nowNode][visited] != INF) {
+            return dp[nowNode][visited];
+        }
+
+        //visited 로부터, 방문하지 않은 지점들을 찾아서 방문해줘야 한다.
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                //해당 지점이 0이고, 아직 그룹화되지 않은 장소라면
-                if(map[i][j] == 0 && groupMap[i][j] == 0) {
-                    groupId++;
-                    count = 0;
-                    BFS(i, j);
-                    groups.put(groupId, count);
-                }
+            //각 지점을 비트 연산자로 변환시켜야 한다.
+            int point = 1<<i;
+
+            //visited 와 비교해서 각 지점을 방문했는지 안했는지 따져본다.
+            //visited 에 point 가 들어가 있다면 겹치기 때문에 1이 나온다.
+            //즉, 이미 해당 point 를 방문한 경우이다.
+            if((visited & point) != 0) {
+                continue;
             }
+            //지금 위치에서 탐색하고자 하는 지점까지 길이 없는 경우도 있다.
+            if(map[nowNode][i] == 0) {
+                continue;
+            }
+            //해당 지점을 아직 방문하지 않았고 길도 존재한다면 dp 를 업데이트 해준다.
+            //지금 위치에서 방문하고자 하는 지점까지의 거리 + 방문하고자 하는 지점에서 목표지점까지의 값을 업데이트해준다.
+            dp[nowNode][visited] = Math.min(dp[nowNode][visited], TSP(i, point | visited) + map[nowNode][i]);
         }
+        //TSP 는 지금 지점에서 방문한 지점이 visited 와 같을 떄, 최솟 값을 반환한다.
+        return dp[nowNode][visited];
 
-        //각각의 벽에 대해서 4방향을 확인하면서 그룹의 개수를 세주면 된다.
-        while (!queue.isEmpty()) {
-            Node nowNode = queue.poll();
-            int nowY = nowNode.y;
-            int nowX = nowNode.x;
-            int nowCount = 1;
-            HashSet<Integer> set = new HashSet<>();
-
-            for (int i = 0; i < 4; i++) {
-                int newY = nowY + y_move[i];
-                int newX = nowX + x_move[i];
-                if(inArea(newY, newX) && map[newY][newX] != 1) {
-                    set.add(groupMap[newY][newX]);
-                }
-            }
-
-            for(Integer i : set) {
-                nowCount += groups.get(i);
-            }
-            result[nowY][nowX] = nowCount % 10;
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                sb.append(result[i][j]);
-            }
-            if(i != N-1) {
-                sb.append("\n");
-            }
-        }
-
-        System.out.println(sb);
-    }
-
-    //인접한 0을 탐색하며 해당 그룹맵을 최신화 시킨다.
-    public static void BFS(int nowY, int nowX) {
-        groupMap[nowY][nowX] = groupId;
-        count++;
-
-        for (int i = 0; i < 4; i++) {
-            int newY = nowY + y_move[i];
-            int newX = nowX + x_move[i];
-            if(inArea(newY, newX) && groupMap[newY][newX] == 0 && map[newY][newX] == 0) {
-                BFS(newY, newX);
-            }
-        }
-    }
-
-    public static boolean inArea(int y, int x) {
-        return 0 <= y && y < N && 0 <= x && x < M;
-    }
-
-    public static class Node {
-        int y;
-        int x;
-
-        public Node(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
     }
 }
