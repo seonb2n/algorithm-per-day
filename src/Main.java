bbddfd2d2d8a5fdb73a06fc29e2fc36714429045
seonb2n <626ksb@gmail.com>
2022-04-05 22:37:22 +0900

    백준 알고리즘 17404번

--- src/Main.java
+++ src/Main.java
@@ -5,113 +5,69 @@ import java.util.*;
 
 class Main {
 
-    static int N;
-    static int K;
-    static long motherNumber;
-    static long resultNumber;
-    static char[][] numbers;
     static BufferedReader br;
-    static long[][] dp;
-    static int [][] dpMod;
+    static int N;
+    static int[][] houseCost;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        numbers = new char[N][];
-        for (int i = 0; i < N; i++) {
-            numbers[i] = br.readLine().toCharArray();
-        }
-        K = Integer.parseInt(br.readLine());
+        houseCost = new int[N + 1][3];
 
-        //분모는 N ! 이다.
-        motherNumber = 1;
-        for (int i = 2; i <= N; i++) {
-            motherNumber = motherNumber * i;
+        for (int i = 1; i <= N; i++) {
+            houseCost[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        //dp 를 사용해서 특정 숫자들을 사용했을 때, 해당 숫자가 나눠지는지를 마킹해야 한다.
-        //dp[i][j] 는 비트마스킹 된 i 를 이어붙인 숫자를 k 로 나눴을 때 나머지가 j 인 경우의 수이다.
-        dp = new long[1 << N][K];
-        for (int i = 0; i < (1 << N); i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        //i 번째 까지 집을 칠했을 때, i 번째 집이 j 색깔일 떄 지금까지 칠하는데 든 비용의 최솟 값
+        dp = new int[N + 1][3];
 
-        //나머지 i 에 j 번째 숫자를 더할 때 나오는 나머지를 기록한 dp 이다.
-        dpMod = new int[K][N];
-        for (int i = 0; i < K; i++) {
-            Arrays.fill(dpMod[i], -1);
-        }
+        //첫번째 집의 값을 고정시킨 후 과정을 반복한다.
+        dp[N][0] = Integer.MAX_VALUE;
+        dp[N][1] = Integer.MAX_VALUE;
+        dp[N][2] = Integer.MAX_VALUE;
 
-        resultNumber = DFS(0, 0);
+        for (int i = 0; i < 3; i++) {
+            int firstHouseColor = houseCost[1][i];
+            for (int j = 0; j < 3; j++) {
+                if(j != i) {
+                    dp[2][j] = houseCost[2][j] + firstHouseColor;
+                }
+                else {
+                    dp[2][i] = 1000 * 1000 + 1;
+                }
+            }
 
-        if(resultNumber == 0) {
-            System.out.print("0/1");
-        }
-        else if(motherNumber == resultNumber) {
-            System.out.print("1/1");
-        }
-        else {
-            long gcd = findGCD(motherNumber, resultNumber);
-            motherNumber = motherNumber / gcd;
-            resultNumber = resultNumber / gcd;
+            for (int j = 3; j <= N-1; j++) {
+                dp[j][0] = fillDp(j, 0);
+                dp[j][1] = fillDp(j, 1);
+                dp[j][2] = fillDp(j, 2);
+            }
 
-            System.out.print(resultNumber+"/"+motherNumber);
-        }
-    }
-
-    //최대 공약수를 찾는 메서드
-    private static long findGCD(long motherNumber, long resultNumber) {
-        while(motherNumber % resultNumber != 0 ) {
-            long temp = motherNumber % resultNumber;
-            motherNumber = resultNumber;
-            resultNumber = temp;
-        }
-        return resultNumber;
-    }
-
-    //비트마스킹으로 처리된 방문 상태, 지금 가지고 있는 나머지를 인자로 한다.
-    //지금까지 방문한 곳이 nowStatus 이고 현재 나머지가 rest 일 때, 앞으로 빙문 안한 숫자들을 추가하면 가질 수 있는 나머지의 경우의 수를 반환한다.
-    public static long DFS(int nowStatus , int rest) {
-        //이미 탐색한 지점은 그대로 반환한다.
-        if(dp[nowStatus][rest] != -1) {
-            return dp[nowStatus][rest];
-        }
-
-        //끝까지 모두 방문
-        if(nowStatus == (1 << N) -1) {
-            if (rest == 0) {
-                return 1L;
-            } else {
-                return 0;
+            for (int j = 0; j < 3; j++) {
+                if(j != i) {
+                    dp[N][j] = Math.min(dp[N][j], fillDp(N, j));
+                }
             }
         }
 
-        //지금 까지 방문하지 않은 곳들 중 하나를 방문한다.
-        for (int i = 0; i < N; i++) {
-            //방문한 곳과 방문하고자 하는 곳을 대조해서 확인한다
-            int nextNumber = 1<<i;
-            //다음 숫자를 아직 탐색하지 않았다면 탐색을 진행한다.
-            if((nowStatus & nextNumber) == 0) {
-
-                dp[nowStatus][rest] +=
-                        DFS(nowStatus | nextNumber, getRemainedNumber(rest, i));
-            }
-        }
-        dp[nowStatus][rest]++;
-        return dp[nowStatus][rest];
+        System.out.println(Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]));
     }
 
-    //rest 에다가 숫자들 중 targetNumberId 를 더한 값에 대한 나머지를 반환한다.
-    public static int getRemainedNumber(int rest, int targetNumberIndex) {
-        if(dpMod[rest][targetNumberIndex] != -1) {
-            return dpMod[rest][targetNumberIndex];
+    //지금 집의 색을 칠한 dp 를 채운다.
+    public static int fillDp(int nowHouseIndex, int nowHouseColor) {
+        int lastCost = 0;
+        switch (nowHouseColor) {
+            case 0:
+                lastCost = Math.min(dp[nowHouseIndex - 1][1], dp[nowHouseIndex - 1][2]);
+                break;
+            case 1:
+                lastCost = Math.min(dp[nowHouseIndex - 1][0], dp[nowHouseIndex - 1][2]);
+                break;
+            case 2:
+                lastCost = Math.min(dp[nowHouseIndex - 1][0], dp[nowHouseIndex - 1][1]);
+                break;
         }
-        int temp = rest;
-        for (int i = 0; i < numbers[targetNumberIndex].length; i++) {
-            temp *= 10;
-            temp = (temp + numbers[targetNumberIndex][i] - '0' ) % K;
-        }
-
-        return dpMod[rest][targetNumberIndex] = temp;
+        return lastCost + houseCost[nowHouseIndex][nowHouseColor];
     }
 }
