2e2c87a611b194d617b5a8204b5cde718502f1e8
seonb2n <626ksb@gmail.com>
2022-04-07 15:01:31 +0900

    백준 알고리즘 2482전

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 class Main {
 
     static BufferedReader br;
-    static int groupA;
-    static int groupB;
-    static int groupC;
-    static boolean[][] isVisited;
+    static int N;
+    static int K;
     static int answer;
+    static int[][] dp;
+    static int mod = 1_000_000_003;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        groupA = Integer.parseInt(str[0]);
-        groupB = Integer.parseInt(str[1]);
-        groupC = Integer.parseInt(str[2]);
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
         answer = 0;
 
-        isVisited = new boolean[1501][1501];
-
-        if((groupA + groupB + groupC) % 3 != 0) {
-            System.out.println(0);
-        }
-        else {
-            DFS(groupA, groupB, groupC);
-            System.out.println(answer);
+        dp = new int[N+1][K+1];
+        for (int i = 0; i < N + 1; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
+        for (int i = 0; i <= N; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
+        }
+        Arrays.fill(dp[0], 0);
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; j <= K; j++) {
+                fillDp(i, j);
+            }
+        }
+
+        answer = (fillDp(N-3, K-1) + fillDp(N-1, K)) % mod;
+
+        System.out.println(answer);
     }
 
-    public static void DFS(int a, int b, int c) {
-        if(a == b && b == c) {
-            answer = 1;
-            return;
+    public static int fillDp(int i, int j) {
+        if(dp[i][j] != -1) {
+            return dp[i][j];
         }
-        //세 숫자에 대해서 값을 변조
-        play(a, b, c);
-        play(a, c, b);
-        play(b, c, a);
-    }
 
-    public static void play(int a, int b, int c) {
-        int min = Math.min(a, b);
-        int max = Math.max(a, b);
-
-        max = max - min;
-        min *= 2;
-
-        if(!isVisited[min][max]) {
-            isVisited[min][max] = isVisited[max][min] = true;
-            DFS(max, min, c);
+        if(i == j * 2 - 1) {
+            dp[i][j] = 1;
+            return 1;
         }
+
+        if(i <= j * 2 - 1) {
+            dp[i][j] = 0;
+            return 0;
+        }
+
+        dp[i][j] = (fillDp(i-2, j-1) + fillDp(i-1, j)) % mod;
+
+        return dp[i][j];
     }
 }
