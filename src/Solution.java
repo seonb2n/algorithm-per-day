6ab143ca481adba8a20a5e53d81299b567e4e218
seonb2n <626ksb@gmail.com>
2022-04-09 12:29:16 +0900

    프로그래머스 - 디크스 컨트롤러

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,72 @@
 import java.util.*;
 
 class Solution {
+    PriorityQueue<Job> pq = new PriorityQueue<>();
+    Job[] jobArr;
 
-    public int[] solution(String[] words, String[] queries) {
-        Trie front = new Trie();
-        Trie back = new Trie();
-
-        for (String word : words) {
-            front.insert(word);
-            back.insert(reverse(word));
+    public int solution(int[][] jobs) {
+        jobArr = new Job[jobs.length];
+        for (int i = 0; i < jobs.length; i++) {
+            jobArr[i] = new Job(jobs[i][0], jobs[i][1]);
         }
 
-        return Arrays.stream(queries).mapToInt(
-                query -> query.charAt(0) == '?' ? back.find(reverse(query), 0)
-                        : front.find(query, 0)).toArray();
-    }
+        Arrays.sort(jobArr, new Comparator<Job>() {
+            @Override
+            public int compare(Job o1, Job o2) {
+                return o1.requestTime - o2.requestTime;
+            }
+        });
 
-    public static class Trie {
-        //문자열의 길이를 key 로 하고, 해당 길이를 가진 문자열의 개수
-        Map<Integer, Integer> lenMap = new HashMap<>();
-        Trie[] child = new Trie[26];
+        int answer = 0;
+        int nowTime = 0;
+        int jobCount = 0;
+        //가장 먼저 들어온 job 실행한 상태
 
-        void insert(String str) {
-            //지금 속해 있는 Trie Class
-            Trie node = this;
-            int len   = str.length();
-            node.lenMap.put(len, lenMap.getOrDefault(len, 0) + 1);
-
-            for (char ch : str.toCharArray()) {
-                int idx = ch - 'a';
-                if (node.child[idx] == null) {
-                    node.child[idx] = new Trie();
+        while (jobCount < jobArr.length) {
+            //하나의 작업이 완료되는 시점까지 들어온 모든 요청을 큐에 추가
+            int i = 0;
+            while (i < jobArr.length && jobArr[i].requestTime <= nowTime) {
+                if(!jobArr[i].isDone) {
+                    pq.offer(jobArr[i]);
+                    jobArr[i].isDone = true;
                 }
-                node = node.child[idx];
-                node.lenMap.put(len, node.lenMap.getOrDefault(len, 0) + 1);
+                i++;
+            }
+
+            //큐가 비어있다면 작업이 없는 것
+            if(pq.isEmpty()) {
+                //다음 작업이 들어올 때까지 시간을 증가시킨다.
+                nowTime++;
+            }
+
+            //작업 진행
+            else {
+                Job nowJob = pq.poll();
+                nowTime += nowJob.taskTime;
+                answer += (nowTime - nowJob.requestTime);
+                jobCount++;
             }
         }
 
-        int find(String str, int i) {
-            if(str.charAt(i) == '?') {
-                return lenMap.getOrDefault(str.length(), 0);
-            }
-            int idx = str.charAt(i) - 'a';
-            return child[idx] == null ? 0 : child[idx].find(str, i + 1);
-        }
+        answer = answer / jobArr.length;
+
+        return answer;
     }
 
-    String reverse(String s) {
-        return new StringBuilder(s).reverse().toString();
+    public static class Job implements Comparable<Job>{
+        int requestTime;
+        int taskTime;
+        boolean isDone;
+
+        public Job(int requestTime, int taskTime) {
+            this.requestTime = requestTime;
+            this.taskTime = taskTime;
+            isDone = false;
+        }
+
+        @Override
+        public int compareTo(Job o) {
+            return this.taskTime - o.taskTime;
+        }
     }
 }
