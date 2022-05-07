c0f1286166f4fb1ada435e76e49c0693eb4aee5a
seonb2n <626ksb@gmail.com>
2022-05-07 13:21:13 +0900

    프로그래머스 - 보석 쇼핑

--- src/Solution.java
+++ src/Solution.java
@@ -1,80 +1,37 @@
 import java.util.*;
 
 class Solution {
-    static List<Long> numberList;
-    static List<Long> tempNumberList;
-    static List<Character> expList;
-    static List<Character> tempExpList;
 
     public static void main(String[] args) {
-        String s = "200-300-500-600*40+500+500";
-        solution(s);
+        String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+        solution(gems);
     }
 
-    public static long solution(String expression) {
-        String[] cases = {"+-*", "+*-", "*-+", "*+-", "-*+", "-+*"};
-        numberList = new LinkedList<>();
-        expList = new LinkedList<>();
+    public static int[] solution(String[] gems) {
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
 
-        String[] split = expression.split("[*+-]");
+        int len = Integer.MAX_VALUE, start = 0, idx = 0;
 
-        for (int i = 0; i < split.length; i++) {
-            numberList.add(Long.parseLong(split[i]));
-        }
+        set.addAll(Arrays.asList(gems));
 
-        split = expression.split("[0-9]");
+        for (int i = 0; i < gems.length; i++) {
+            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
+            queue.add(gems[i]);
 
-        for (int i = 0; i < split.length; i++) {
-            if(!split[i].equals(""))
-                expList.add(split[i].charAt(0));
-        }
-
-        long answer = 0;
-
-        for (int i = 0; i < 6; i++) {
-            tempNumberList = new LinkedList<>();
-            tempNumberList.addAll(numberList);
-            tempExpList = new LinkedList<>();
-            tempExpList.addAll(expList);
-            String nowCase = cases[i];
-            calculateWith(nowCase.charAt(0));
-            calculateWith(nowCase.charAt(1));
-            calculateWith(nowCase.charAt(2));
-
-            answer = Math.max(answer, Math.abs(tempNumberList.get(0)));
-        }
-
-        return answer;
-    }
-
-    public static void calculateWith(char exp) {
-
-        int i = 0;
-        while (tempExpList.contains(exp)) {
-            if(tempExpList.get(i) == exp) {
-                Long firstNumber = tempNumberList.get(i);
-                Long secondNumber = tempNumberList.get(i+1);
-                Long result = 0L;
-                switch (exp) {
-                    case '+':
-                        result = firstNumber + secondNumber;
-                        break;
-                    case '-':
-                        result = firstNumber - secondNumber;
-                        break;
-                    case '*':
-                        result = firstNumber * secondNumber;
-                        break;
-                }
-                tempNumberList.add(i, result);
-                tempNumberList.remove(i+1);
-                tempNumberList.remove(i+1);
-                tempExpList.remove(i);
+            while (map.get(queue.peek()) > 1) {
+                map.put(queue.peek(), map.get(queue.poll()) - 1);
+                idx++;
             }
-            else {
-                i++;
+
+            if (map.size() == set.size() && len > (i - idx)) {
+                len = i - idx;
+                start = idx + 1;
             }
         }
+
+        return new int[]{start, start + len};
     }
 
 }
