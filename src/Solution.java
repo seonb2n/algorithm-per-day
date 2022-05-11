fa5cec385c2f00310e94ae5e1f7c68d7f7422510
seonb2n <626ksb@gmail.com>
2022-05-11 16:27:23 +0900

    프로그래머스 - 셔틀버스

--- src/Solution.java
+++ src/Solution.java
@@ -1,47 +1,82 @@
+import java.time.LocalTime;
 import java.util.*;
 
 class Solution {
-    static Set<Character> left;
-    public static void main(String[] args) {
-        String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-        solution(gems);
-    }
 
-    public static int[] solution(String[] gems) {
-        Map<String, Integer> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
+    public static String solution(int n, int t, int m, String[] timetable) {
+        //셔틀은 n 번 t 분 간격으로 옴
+        //마지막 셔틀에는 무조건 타야 한다.
+        LocalTime answer = LocalTime.now();
+        int shuttle = 0;
 
-        int len = Integer.MAX_VALUE, start = 0, idx = 0;
+        List<Person> personList = new ArrayList<>();
 
-        set.addAll(Arrays.asList(gems));
+        for (int i = 0; i < timetable.length; i++) {
+            String time = timetable[i];
+            personList.add(new Person(time));
+        }
 
-        for (int i = 0; i < gems.length; i++) {
-            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
-            queue.add(gems[i]);
+        Collections.sort(personList);
 
-            while (map.get(queue.peek()) > 1) {
-                map.put(queue.peek(), map.get(queue.poll()) - 1);
-                idx++;
+        //시간마다 셔틀을 보낸다.
+        LocalTime nowTime = LocalTime.parse("09:00");
+        while (shuttle < n) {
+            shuttle++;
+            //지금 셔틀이 마지막일 때 셔틀에 탈 수 있는 사람들을 확인한다.
+            if(shuttle == n) {
+                int lastShuttlePerson = 0;
+                int lastPerson = 0;
+                for (int i = 0; i < personList.size(); i++) {
+                    Person p = personList.get(i);
+                    if(!p.isGone && lastShuttlePerson < m && (p.time.isBefore(nowTime)) || p.time.equals(nowTime)) {
+                        p.isGone = true;
+                        lastShuttlePerson++;
+                        lastPerson = i;
+                    }
+                    if(lastShuttlePerson == m) {
+                        break;
+                    }
+                }
+                //마지막 셔틀에 모든 사람이 다 탔다면 콘은 셔틀을 탈 수 있는 시간에 나와야 한다.
+                if(lastShuttlePerson == m) {
+                    LocalTime lastTime = personList.get(lastPerson).time;
+                    answer = lastTime.minusMinutes(1);
+                }
+                else {
+                    answer = nowTime;
+                }
             }
-
-            if (map.size() == set.size() && len > (i - idx)) {
-                len = i - idx;
-                start = idx + 1;
+            else {
+                int nowShuttlePerson = 0;
+                for (int i = 0; i < personList.size(); i++) {
+                    Person p = personList.get(i);
+                    if(!p.isGone && nowShuttlePerson < m && (p.time.isBefore(nowTime)) || p.time.equals(nowTime)) {
+                        p.isGone = true;
+                        nowShuttlePerson++;
+                    }
+                    if(nowShuttlePerson == m) {
+                        break;
+                    }
+                }
+                nowTime = nowTime.plusMinutes(t);
             }
         }
 
-        return new int[]{start, start + len};
+        return answer.toString();
     }
 
-    public static class Book {
-        String serialNumber;
-        String bookTitle;
+    public static class Person implements Comparable<Person>{
+        boolean isGone;
+        LocalTime time;
+
+        public Person(String s) {
+            time = LocalTime.parse(s);
+            isGone = false;
+        }
 
         @Override
-        public boolean equals(Object o) {
-            Book book = (Book) o;
-            return (Objects.equals(((Book) o).serialNumber, this.serialNumber));
+        public int compareTo(Person o) {
+            return time.compareTo(o.time);
         }
     }
 }
