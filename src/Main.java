684c121e2790b3672bf18152548a4ff23941d694
seonb2n <626ksb@gmail.com>
2022-05-24 21:28:45 +0900

    백준 알고리즘 14003번

--- src/Main.java
+++ src/Main.java
@@ -11,75 +11,68 @@ class Main {
     static BufferedReader br;
     static int N;
     static int[] numbers;
-    static int[] numbersIndex;
-    static List<Integer> stack;
-    static int LCS;
-    static int[] answers;
+
+    static List<Integer> result;
+    //numbers 의 각각의 숫자가 몇 번째 index 에 들어갔는지를 기록하는 배열
+    static int[] numberIndex;
+    static Stack<Integer> stack;
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        result = new ArrayList<>();
+        numberIndex = new int[numbers.length];
+        result.add(Integer.MIN_VALUE);
+        stack = new Stack<>();
 
-        numbersIndex = new int[N];
-        Arrays.fill(numbersIndex, -999);
-        stack = new ArrayList<>();
+        //이분탐색을 이용해서 result 를 채워준다.
+        for (int i = 0; i < numbers.length; i++) {
+            int nowNumber = numbers[i];
 
-        for (int i = 0; i < N; i++) {
-            findLCS(i);
-        }
-
-        LCS = stack.size();
-        answers = new int[LCS];
-
-        int size = answers.length;
-        for (int i = N-1; i >= 0; i--) {
-            if(numbersIndex[i] == size-1) {
-                answers[size-1] = numbers[i];
-                size--;
+            //nowNumber 은 result 에 있는 숫자 중에 자기보다 작은 것 다음 위치에 들어간다.
+            int left = 0;
+            int right = result.size()-1;
+            if(nowNumber > result.get(result.size() - 1)) {
+                result.add(nowNumber);
+                numberIndex[i] = result.size() - 1;
+            }
+            else {
+                while(left < right) {
+                    int mid = (left + right) / 2;
+                    if(result.get(mid) >= nowNumber) {
+                        right = mid;
+                    }
+                    else {
+                        left = mid + 1;
+                    }
+                }
+                result.set(right, nowNumber);
+                numberIndex[i] = right;
             }
         }
 
-        System.out.println(LCS);
-        for (int i = 0; i < LCS; i++) {
-            sb.append(answers[i]);
-            sb.append(" ");
-        }
-        System.out.print(sb);
-    }
+        sb.append(result.size()-1);
+        sb.append("\n");
 
-    public static void findLCS(int numberIndex) {
-        int nowNumber = numbers[numberIndex];
-        int stackSize = stack.size();
-        if(stackSize == 0) {
-            stack.add(nowNumber);
-            numbersIndex[numberIndex] = 0;
-        }
-        else if(stack.get(stackSize - 1) < nowNumber) {
-            stack.add(nowNumber);
-            numbersIndex[numberIndex] = stackSize;
-        }
-        else {
-            //스택의 수를 하나씩 역순으로 본다.
-            boolean isAdded = false;
-            for (int i = stackSize-1; i >= 0; i--) {
-                int temp = stack.get(i);
-                //집어 넣고자 하는 수가 temp 보다는 작되, 최대한 앞쪽에 위치해야 한다.
-                if(temp >= nowNumber) {
-                    continue;
-                }
-                stack.remove(i+1);
-                stack.add(i+1, nowNumber);
-                numbersIndex[numberIndex] = i+1;
-                isAdded = true;
+        //resultIndex 에 들어있는 것을 바탕으로 역순으로 가져온다.
+        int startSize = result.size() - 1;
+        for (int i = numberIndex.length - 1; i >= 0; i--) {
+            if(numberIndex[i] == startSize) {
+                stack.push(numbers[i]);
+                startSize--;
+            }
+            if(startSize == 0) {
                 break;
             }
-            if(stack.get(0) > nowNumber && !isAdded) {
-                stack.remove(0);
-                stack.add(0, nowNumber);
-                numbersIndex[numberIndex] = 0;
-            }
         }
+
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+            sb.append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -2,36 +2,19 @@ import java.util.*;
 
 class Solution {
 
+    static Map<Character, List<String>> rule;
+    static Set<Integer> set;
+
     public static void main(String[] args) {
-        String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-        solution(gems);
+        String s = "DAS";
+        System.out.println(s.substring(0, 4));
+        System.out.println(s.indexOf("DA", 10));
+        rule = new HashMap<>();
+        rule.put('0', new ArrayList<>());
+        rule.get('0').add("O");
+
+        System.out.println("ads");
+
+        List<String> rule = new ArrayList<>();
     }
-
-    public static int[] solution(String[] gems) {
-        Map<String, Integer> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
-
-        int len = Integer.MAX_VALUE, start = 0, idx = 0;
-
-        set.addAll(Arrays.asList(gems));
-
-        for (int i = 0; i < gems.length; i++) {
-            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
-            queue.add(gems[i]);
-
-            while (map.get(queue.peek()) > 1) {
-                map.put(queue.peek(), map.get(queue.poll()) - 1);
-                idx++;
-            }
-
-            if (map.size() == set.size() && len > (i - idx)) {
-                len = i - idx;
-                start = idx + 1;
-            }
-        }
-
-        return new int[]{start, start + len};
-    }
-
 }
