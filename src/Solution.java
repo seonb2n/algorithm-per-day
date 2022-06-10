a2e6f33d07a07c8faa9b3a00c8f06478adda9f6d
seonb2n <626ksb@gmail.com>
2022-06-10 21:49:35 +0900

    프로그래머스 - 금과 은 운반

--- src/Solution.java
+++ src/Solution.java
@@ -2,70 +2,48 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 class Solution {
-    static HashSet<String> answerSet = new HashSet<>();
-    static String[] userIds;
-    static String[] bannedIds;
+    public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
 
-    public static void main(String[] args) {
-        String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
-        String[] banned_id = {"*rodo", "*rodo", "******"};
-        solution(user_id, banned_id);
-    }
+        long start = 1L;
+        long end = 4 * 100000000000000L;
 
-    public static int solution(String[] user_id, String[] banned_id) {
-        boolean[] isVisited = new boolean[user_id.length];
-
-        userIds = new String[user_id.length];
-        System.arraycopy(user_id, 0, userIds, 0, user_id.length);
-        bannedIds = new String[banned_id.length];
-        System.arraycopy(banned_id, 0, bannedIds, 0, banned_id.length);
-
-        //dfs 로 banned_id 와 user_id 를 비교하고
-        //비교한 결과를 hashSet 으로 추가한다.
-        StringBuilder sb = new StringBuilder();
-        DFS(isVisited, sb, 0, banned_id.length);
-
-
-        return answerSet.size();
-    }
-
-    public static boolean isSame(String bannedId, String userId) {
-        if (bannedId.length() != userId.length()) {
-            return false;
-        }
-        for (int i = 0; i < bannedId.length(); i++) {
-            if (bannedId.charAt(i) != '*' && (bannedId.charAt(i) != userId.charAt(i))) {
-                return false;
+        //이분탐색으로 만족하는 시간 값을 찾아야 한다.
+        while(start < end) {
+            long mid = (start + end) / 2;
+            if(check(mid, a, b, g, s, w, t)) {
+                end = mid;
+            }
+            else {
+                start = mid + 1;
             }
         }
-        return true;
+
+        return end;
     }
 
-    public static void DFS(boolean[] nowVisit, StringBuilder sb, int nowIndex, int lastIndex) {
-        if (nowIndex == lastIndex) {
-            //중복된 값이 없도록 sb 에 있던 값들을 정렬해줘야 한다.
-            int[] ans = new int[sb.length()];
-            for (int i = 0; i < sb.length(); i++) {
-                ans[i] = Character.getNumericValue(sb.charAt(i));
+    //이분탐색으로 주어진 시간 안에 a, b 를 만족시키는 금과 은을 운반할 수 있는지 확인하는 메서드
+    private boolean check(long mid, int a, int b, int[] g, int[] s, int[] w, int[] t) {
+        //시간 안에 옮길 수 있는 최대 자원 량
+        long total = 0;
+        //시간 안에 금 우선으로 옮길 때 옮길 수 있는 최대 금량
+        long maxGold = 0;
+        //시간 안에 은 우선으로 옮길 때 옮길 수 있는 최대 은량
+        long maxSilver = 0;
+        for (int i = 0; i < g.length; i++) {
+            //총 트럭이 움직일 수 있는 횟수
+            long nowMoveCount = mid / (t[i] * 2L);
+            //편도로 한 번 더 갈 여유가 있으면 +1
+            if(mid % (t[i] * 2L) >= t[i]) {
+                nowMoveCount++;
             }
-            Arrays.sort(ans);
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < ans.length; i++) {
-                result.append(ans[i]);
-            }
-            answerSet.add(result.toString());
-        } else {
-            String nowBannedId = bannedIds[nowIndex];
-            for (int i = 0; i < userIds.length; i++) {
-                if (!nowVisit[i] && isSame(nowBannedId, userIds[i])) {
-                    nowVisit[i] = true;
-                    sb.append(i);
-                    DFS(nowVisit, sb, nowIndex + 1, lastIndex);
-                    nowVisit[i] = false;
-                    sb.deleteCharAt(sb.length() - 1);
-                }
-            }
+            //트럭이 해당 도시에서 운반할 수 있는 최대량
+            long maxCarry = Math.min(nowMoveCount * w[i], g[i] + s[i]);
+            total += maxCarry;
+
+            maxGold += Math.min(maxCarry, g[i]);
+            maxSilver += Math.min(maxCarry, s[i]);
         }
+        // a <= 최대 골드, b <= 최대 은 이면서 a+b <= 시간안에 옮기는 무게라면 return true
+        return a <= maxGold && b <= maxSilver && ((long)a + (long)b) <= total;
     }
-
 }
