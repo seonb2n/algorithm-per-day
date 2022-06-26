3a0456da6565058db94716ebdc14682bef3341c7
seonb2n <626ksb@gmail.com>
2022-06-26 13:46:13 +0900

    백준 알고리즘 - 1019번

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 class Main {
 
     static BufferedReader br;
     static int N;
+    static int[] numbers;
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        numbers = new int[10];
 
-        int[] numbers = new int[10];
+        int startNumber = 1;
+        int endNumber = N;
+        int digit = 1;
 
-        for (int i = 1; i <= N; i++) {
-            String target = String.valueOf(i);
-            for (int j = 0; j < target.length(); j++) {
-                numbers[Integer.parseInt(String.valueOf(target.charAt(j)))]++;
+        while (startNumber <= endNumber) {
+            //끝자리를 9 로 만들 때까지 빼준다.
+            while (endNumber % 10 != 9 && startNumber <= endNumber) {
+                addNumber(endNumber, digit);
+                endNumber--;
             }
+
+            if(endNumber < startNumber) {
+                break;
+            }
+
+            //끝자리를 0으로 만들떄까지 더해준다.
+            while(startNumber % 10 != 0 && startNumber <= endNumber) {
+                addNumber(startNumber, digit);
+                startNumber++;
+            }
+
+            startNumber /= 10;
+            endNumber /= 10;
+
+            int addCount = (endNumber - startNumber + 1) * digit;
+
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] += addCount;
+            }
+
+            digit = digit * 10;
+
         }
 
         for (int i = 0; i < numbers.length; i++) {
@@ -28,5 +54,13 @@ class Main {
         }
 
         System.out.println(sb);
+    }
+
+
+    public static void addNumber(int x, int digit) {
+        while (x > 0) {
+            numbers[x % 10] += digit;
+            x /= 10;
+        }
     }
 }
