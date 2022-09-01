80bb166c47917d6d43bfb2adc37532b2b516d5b2
seonb2n <626ksb@gmail.com>
2022-09-01 20:15:14 +0900

    프로그래머스 문자열 압축

--- src/Solution.java
+++ src/Solution.java
@@ -3,63 +3,43 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        Point2D[] a = new Point2D[7];
-        a[0] = new Point2D(0, 0);
-        a[1] = new Point2D(1, 1);
-        a[2] = new Point2D(2, 2);
-        a[3] = new Point2D(3, 3);
-        a[4] = new Point2D(3, 2);
-        a[5] = new Point2D(4, 2);
-        a[6] = new Point2D(5, 1);
-
-        solution(a);
+        String test = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        System.out.println(solution(test));
     }
 
-    public static int solution(Point2D[] A) {
+    public static int solution(String s) {
+        String origin = s;
+        int stringSize = s.length();
+        int answer = stringSize;
 
-        int result = 0;
-
-        for (int i = 0; i < A.length; i++) {
-            Point2D firstPoint = A[i];
-            for (int j = i+1; j < A.length; j++) {
-                Point2D secondPoint = A[j];
-                for (int k = j+1; k < A.length; k++) {
-                    Point2D thirdPoint = A[k];
-                    if(isCollinear(firstPoint, secondPoint, thirdPoint)) {
-                        result++;
-                        if(result > 100_000_000) {
-                            return -1;
-                        }
-                    }
+        for (int i = 1; i <= stringSize; i++) {
+            //i 크기만큼 압축을 진행한다.
+            String nowPattern = origin.substring(0, i);
+            int nowPatternNumber = 1;
+            int nowAnswer = stringSize;
+            //문자열을 처음부터 끝까지 훓는다.
+            int nowIndex = i;
+            while (nowIndex < stringSize) {
+                String nextPattern = origin.substring(nowIndex, Math.min(nowIndex + i, stringSize));
+                //다음 글자가 지금 패턴과 일치한다면
+                if(nowPattern.equals(nextPattern)) {
+                    nowPatternNumber++;
                 }
+                else {
+                    //일치하지 않는 경우에는 지금 패턴을 업데이트해준다.
+                    nowAnswer = nowAnswer - Math.max(0, (i * nowPatternNumber - (i + String.valueOf(nowPatternNumber).length())));
+                    nowPattern = nextPattern;
+                    nowPatternNumber = 1;
+                }
+                nowIndex = nowIndex + i;
             }
+            //마지막 패턴에 대한 처리도 해줘야 함.
+            nowAnswer = nowAnswer - Math.max(0, (i * nowPatternNumber - (i + String.valueOf(nowPatternNumber).length())));
+            answer = Math.min(answer, nowAnswer);
+
         }
-        return result;
+
+        return answer;
     }
 
-    public static boolean isCollinear(Point2D a, Point2D b, Point2D c) {
-        if(a.y == b.y && b.y == c.y) {
-            return true;
-        }
-        if(a.x == b.x && b.x == c.x) {
-            return true;
-        }
-        if(b.y == a.y || a.y == c.y || b.y == c.y) {
-            return false;
-        }
-        if((b.x - a.x) / (b.y - a.y) == (c.x - b.x) / (c.y - b.y)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static class Point2D {
-        int x;
-        int y;
-
-        public Point2D(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
