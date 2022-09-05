309ef87a892b7ed149316ab313c5d4d5dac0a282
seonb2n <626ksb@gmail.com>
2022-09-05 12:25:47 +0900

    프로그래머스 - 등산코스 정하기

--- src/Solution.java
+++ src/Solution.java
@@ -2,104 +2,138 @@ import java.util.*;
 
 class Solution {
 
-    //alp, cop 를 i,j 만큼 올리기 위해 필요한 최소 cost
-    static int[][] dp;
-    static Queue<Point> pointQueue = new LinkedList<>();
-    static int targetAlp;
-    static int targetCop;
+    static List<Node> nodeList;
+    static PriorityQueue<Point> priorityQueue;
+    static boolean[][] isVisited;
+    static int intensity;
 
-    public static int solution(int alp, int cop, int[][] problems) {
-        List<Problem> problemList = new ArrayList<>();
-        targetAlp = 0;
-        targetCop = 0;
+    public static void main(String[] args) {
+        int[][] paths = {{1, 2, 3}, {2, 3, 5}, {2, 4, 2}, {2, 5, 4}, {3, 4, 4}, {4, 5, 3}, {4, 6, 1}, {5, 6, 1}};
+        int[] gates = {1, 3};
+        int[] summits = {5};
+        solution(6, paths, gates, summits);
+    }
 
-        for (int[] problem : problems) {
-            Problem p = new Problem(problem[0], problem[1], problem[2], problem[3], problem[4]);
-            problemList.add(p);
-            targetAlp = Math.max(targetAlp, p.alpReq);
-            targetCop = Math.max(targetCop, p.copReq);
+    public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        int[] answer = {};
+
+        nodeList = new ArrayList<>();
+
+        //n번째 노드까지 추가해준다.
+        for (int i = 0; i < n + 1; i++) {
+            nodeList.add(new Node());
         }
 
-        //기본 문제 추가
-        problemList.add(new Problem(0, 0, 1, 0, 1));
-        problemList.add(new Problem(0, 0, 0, 1, 1));
+        for (int[] path : paths) {
+            int nodeOne = path[0];
+            int nodeTwo = path[1];
+            int intensity = path[2];
 
-        //이미 alp, cop 가 target 을 넘었으면 0 반환
-        if (alp >= targetAlp && cop >= targetCop) {
-            return 0;
+            nodeList.get(nodeOne).toNode.add(new Point(nodeTwo, intensity));
+            nodeList.get(nodeTwo).toNode.add(new Point(nodeOne, intensity));
         }
 
-        //alp, cop 중 하나라도 target 을 넘는 경우
-        targetAlp = Math.max(targetAlp, alp);
-        targetCop = Math.max(targetCop, cop);
-
-        //dp 초기화
-        dp = new int[targetAlp + 1][targetCop + 1];
-        for (int i = 0; i < targetAlp + 1; i++) {
-            Arrays.fill(dp[i], 1_000_000);
+        for (int gateNumber : gates) {
+            nodeList.get(gateNumber).isGate = true;
         }
 
-        pointQueue.offer(new Point(alp, cop));
-        dp[alp][cop] = 0;
+        for (int summitNumber : summits) {
+            nodeList.get(summitNumber).isSummit = true;
+        }
 
-        while (!pointQueue.isEmpty()) {
-            //현재 alp, cop 를 queue 에서 꺼낸다.
-            Point nowPoint = pointQueue.poll();
-            int nowAlp = nowPoint.alp;
-            int nowCop = nowPoint.cop;
-
-            //현재 alp, cop 로 도달할 수 있는 alp, cop 를 찾고 해당 dp 의 값을 업데이트한다.
-            for (Problem p : problemList) {
-                if (nowAlp >= p.alpReq && nowCop >= p.copReq) {
-                    int nextAlp = nowAlp + p.alpRwd;
-                    int nextCop = nowCop + p.copRwd;
-                    int nextCost = dp[nowAlp][nowCop] + p.cost;
-                    //다음 alp 와 cop 가 범위를 벗어나지 않도록 업데이트한다.
-                    nextAlp = Math.min(targetAlp, nextAlp);
-                    nextCop = Math.min(targetCop, nextCop);
-
-                    //다음 범위가 target 을 넘은 경우에는 그냥 업데이트만 해준다.
-                    if (nextAlp == targetAlp && nextCop == targetCop) {
-                        dp[nextAlp][nextCop] = Math.min(dp[nextAlp][nextCop], nextCost);
-                    }
-
-                    //다음 범위가 업데이트 가능한지를 확인하고 업데이트 된 경우에는 queue 에 추가한다.
-                    else if (dp[nextAlp][nextCop] > nextCost) {
-                        dp[nextAlp][nextCop] = nextCost;
-                        pointQueue.add(new Point(nextAlp, nextCop));
-                    }
+        //BFS 탐색
+        answer = new int[2];
+        answer[0] = Integer.MAX_VALUE - 1;
+        answer[1] = Integer.MAX_VALUE - 1;
+        for (int gate : gates) {
+            for (int summit : summits) {
+                priorityQueue = new PriorityQueue<>();
+                isVisited = new boolean[n + 1][n + 1];
+                intensity = Integer.MAX_VALUE - 1;
+                priorityQueue.offer(new Point(gate, 0));
+                BFS(summit);
+                if (answer[1] > intensity) {
+                    answer[0] = summit;
+                    answer[1] = intensity;
+                } else if (answer[1] == intensity && answer[0] > summit) {
+                    answer[0] = summit;
                 }
             }
         }
 
-        return dp[targetAlp][targetCop];
+        return answer;
     }
 
+    //startPoint 에서 targetPoint 로 가는 최소 intensity 를 구함
+    public static void BFS(int targetPoint) {
+        while (!priorityQueue.isEmpty()) {
+            Point nowPoint = priorityQueue.poll();
+            Node nowNode = nodeList.get(nowPoint.nowNode);
+            int nowIntensity = nowPoint.intensity;
+
+            if (nowIntensity >= intensity) {
+                return;
+            }
+
+            //지금 노드에서 갈 수 있는 위치 탐색
+            for (int i = 0; i < nowNode.toNode.size(); i++) {
+                Point nextPoint = nowNode.toNode.get(i);
+                Node nextNode = nodeList.get(nextPoint.nowNode);
+                int nextIntensity = Math.max(nowIntensity, nextPoint.intensity);
+
+                //다음 노드가 gate 거나 다른 산의 정상이면 스킵
+                if (nextNode.isGate || (nextNode.isSummit && nextPoint.nowNode != targetPoint)) {
+                    continue;
+                }
+
+                //nextIntensity 가 현재까지 정상에 갈 수 있는 intensity 보다 크면 스킵
+                if (nextIntensity >= intensity) {
+                    continue;
+                }
+
+                //nextPoint 가 target인 경우
+                if (nextPoint.nowNode == targetPoint) {
+                    intensity = nextIntensity;
+                }
+
+                //아직 방문하지 않은 노드일 때
+                else if (!isVisited[nowPoint.nowNode][nextPoint.nowNode]) {
+                    priorityQueue.offer(new Point(nextPoint.nowNode, nextIntensity));
+                    isVisited[nowPoint.nowNode][nextPoint.nowNode] = true;
+                    isVisited[nextPoint.nowNode][nowPoint.nowNode] = true;
+                }
+            }
+        }
+    }
 }
 
-class Point {
-    int alp;
-    int cop;
+//현재 위치에 오기까지 얼만큼의 intensity 로 왔는지를 의미함
+class Point implements Comparable<Point> {
+    int nowNode;
+    int intensity;
 
-    public Point(int alp, int cop) {
-        this.alp = alp;
-        this.cop = cop;
+    public Point(int nowNode, int intensity) {
+        this.nowNode = nowNode;
+        this.intensity = intensity;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        return this.intensity - o.intensity;
     }
 }
 
-class Problem {
-    int alpReq;
-    int copReq;
-    int alpRwd;
-    int copRwd;
-    int cost;
+//산의 각 지점
+class Node {
 
-    public Problem(int alpReq, int copReq, int alpRwd, int copRwd, int cost) {
-        this.alpReq = alpReq;
-        this.copReq = copReq;
-        this.alpRwd = alpRwd;
-        this.copRwd = copRwd;
-        this.cost = cost;
+    //현재 노드에서 갈 수 있는 node 와 그 intensity
+    List<Point> toNode;
+    boolean isGate;
+    boolean isSummit;
+
+    public Node() {
+        this.toNode = new ArrayList<>();
+        isGate = false;
+        isSummit = false;
     }
-
 }
