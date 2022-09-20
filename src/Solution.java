2482712fcff726660f37dfca180839e32be0a21a
seonb2n <626ksb@gmail.com>
2022-09-20 20:26:02 +0900

    프로그래머스 - 행렬과 연산(효율성 해결 실패)

--- src/Solution.java
+++ src/Solution.java
@@ -1,123 +1,63 @@
 import java.util.*;
 
 class Solution {
+    static int w;
+    static int h;
 
-    private static List<List<Node>> graph;
-
-    public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int[] path: paths) {
-            int start = path[0];
-            int end = path[1];
-            int weight = path[2];
-
-            //gate 인 경우에는 다른 곳으로만 갈 수 있다.
-            if (isGate(gates, start)) {
-                graph.get(start).add(new Node(end, weight));
-            }
-            else if (isGate(gates, end)) {
-                graph.get(end).add(new Node(start, weight));
-            }
-            //산봉우리인 경우에는 산봉우리로만 올 수 있다.
-            else if (isSummit(summits, start)) {
-                graph.get(end).add(new Node(start, weight));
-            }
-            else if (isSummit(summits, end)) {
-                graph.get(start).add(new Node(end, weight));
-            }
-            else {
-                graph.get(start).add(new Node(end, weight));
-                graph.get(end).add(new Node(start, weight));
-            }
-        }
-
-        //다익스라를 사용해서 각 정상까지 도착하는데 필요한 intensity 를 구함
-        return dijkstra(n, gates, summits);
+    public static void main(String[] args) {
+        int[][] rc = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        String[] ops = {"Rotate ", "ShiftRow"};
+        solution(rc, ops);
     }
 
-    public static int[] dijkstra(int n, int[] gates, int[] summits) {
-        Queue<Node> queue = new LinkedList<>();
-        //i 번 노드까지 가는데 필요한 min intensity
-        int[] intensity = new int[n+1];
+    public static int[][] solution(int[][] rc, String[] operations) {
+        h = rc.length;
+        w = rc[0].length;
 
-        Arrays.fill(intensity, Integer.MAX_VALUE);
+        int[][] answer = rc;
 
-        //모든 출발지점을 queue 에 추가
-        for (int gate: gates) {
-            queue.add(new Node(gate, 0));
-            intensity[gate] = 0;
-        }
-
-        while (!queue.isEmpty()) {
-            Node nowNode = queue.poll();
-
-            //이미 현재 노드의 weight 가 intensity 보다 커지면 해당 케이스 skip
-            if (nowNode.weight > intensity[nowNode.end]) {
-                continue;
+        for (String op : operations) {
+            if (op.equals("Rotate")) {
+                answer = rotate(answer);
             }
-
-            //지금 노드가 방문할 수 있는 노드를 모두 탐색한다.
-            for (int i = 0; i < graph.get(nowNode.end).size(); i++) {
-                Node nextNode = graph.get(nowNode.end).get(i);
-
-                //다음 노드로 가는데 필요한 피로도는, 다음 노드에 기록되어 있는 피로도와 지금 가중치의 비교
-                int nextIntensity = Math.max(intensity[nowNode.end], nextNode.weight);
-
-                //intensity 에 존재하는 값보다 현재의 피로도가 더 작다면 업데이트를 해준다.
-                if (intensity[nextNode.end] > nextIntensity) {
-                    intensity[nextNode.end] = nextIntensity;
-                    queue.add(new Node(nextNode.end, nextIntensity));
-                }
+            else {
+                answer = shiftRow(answer);
             }
         }
-
-        int[] answer = new int[2];
-        answer[0] = Integer.MAX_VALUE - 1;
-        answer[1] = Integer.MAX_VALUE - 1;
-
-        Arrays.sort(summits);
-
-        for (int summit: summits) {
-            if (intensity[summit] < answer[1]) {
-                answer[0] = summit;
-                answer[1] = intensity[summit];
-            }
-        }
-
         return answer;
     }
 
-    public static boolean isGate(int[] gates, int point) {
-        for (int i = 0; i < gates.length; i++) {
-            if (gates[i] == point) {
-                return true;
-            }
+    public static int[][] shiftRow(int[][] rc) {
+        int[] firstLane = rc[rc.length-1].clone();
+
+        for (int i = rc.length-2; i >= 0; i--) {
+            rc[i+1] = rc[i].clone();
         }
-        return false;
+        rc[0] = firstLane;
+        return rc;
     }
 
-    public static boolean isSummit(int[] summits, int point) {
-        for (int i = 0; i < summits.length; i++) {
-            if (summits[i] == point) {
-                return true;
-            }
+    public static int[][] rotate(int[][] rc) {
+        int B = rc[0][w-1];
+        int C = rc[h-1][0];
+        int D = rc[h-1][w-1];
+
+        //윗줄과 밑줄 처리
+        for (int i = w-2; i >= 0; i--) {
+            rc[0][i+1] = rc[0][i];
+            rc[h-1][w-2-i] = rc[h-1][w-2-i+1];
+
         }
-        return false;
+        rc[h-1][w-2] = D;
+
+        //오른쪽 왼쪽 줄 처리
+        for (int i= h-2; i >= 1; i--) {
+            rc[i+1][w-1] = rc[i][w-1];
+            rc[h-2-i][0] = rc[h-2-i+1][0];
+        }
+        rc[1][w-1] = B;
+        rc[h-2][0] = C;
+        return rc;
     }
 }
 
-//산의 각 지점
-class Node {
-
-    int end;
-    int weight;
-
-    public Node(int e, int w) {
-        this.end = e;
-        this.weight = w;
-    }
-}
