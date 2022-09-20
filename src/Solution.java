bb7795148373e7889e6d032c11d8fcab828bbab5
seonb2n <626ksb@gmail.com>
2022-09-20 22:41:54 +0900

    프로그래머스 - 행렬과 연산

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,17 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     static int w;
     static int h;
 
+    static ArrayDeque<Integer> leftList;
+    static ArrayDeque<Integer> rightList;
+    static LinkedList<ArrayDeque<Integer>> bodyList;
+
     public static void main(String[] args) {
-        int[][] rc = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        String[] ops = {"Rotate ", "ShiftRow"};
+        int[][] rc= {{1,2,3},{4,5,6}, {7,8,9}};
+        String[] ops = {"Rotate", "ShiftRow"};
         solution(rc, ops);
     }
 
@@ -14,50 +19,75 @@ class Solution {
         h = rc.length;
         w = rc[0].length;
 
-        int[][] answer = rc;
+        leftList = new ArrayDeque<>();
+        rightList = new ArrayDeque<>();
+        bodyList = new LinkedList<>();
+
+        //w 가 2인 경우에는 bodyList 가 없으므로 다르게 처리해야 한다.
+        if (w == 2) {
+            for (int i = 0; i < h; i++) {
+                leftList.add(rc[i][0]);
+                rightList.add(rc[i][w-1]);
+            }
+        } else {
+            for (int i = 0; i < h; i++) {
+                leftList.add(rc[i][0]);
+                rightList.add(rc[i][w-1]);
+                ArrayDeque<Integer> body = new ArrayDeque<>();
+                for (int j = 1; j < w-1; j++) {
+                    body.add(rc[i][j]);
+                }
+                bodyList.add(body);
+            }
+        }
 
         for (String op : operations) {
             if (op.equals("Rotate")) {
-                answer = rotate(answer);
-            }
-            else {
-                answer = shiftRow(answer);
+                rotate();
+            } else {
+                shiftRow();
             }
         }
+
+        int[][] answer = new int[h][w];
+        for (int i = 0; i < bodyList.size(); i++) {
+            ArrayDeque<Integer> body = bodyList.get(i);
+            int j = 1;
+            while (!body.isEmpty()) {
+                answer[i][j] = body.pollFirst();
+                j++;
+            }
+        }
+        int i = 0;
+        while (!leftList.isEmpty()) {
+            answer[i][0] = leftList.pollFirst();
+            answer[i][w-1] = rightList.pollFirst();
+            i++;
+        }
+
         return answer;
     }
 
-    public static int[][] shiftRow(int[][] rc) {
-        int[] firstLane = rc[rc.length-1].clone();
+    public static void shiftRow() {
+        // leftList, rightList 를 하나씩 밑으로 내린다.
+        leftList.addFirst(leftList.pollLast());
+        rightList.addFirst(rightList.pollLast());
 
-        for (int i = rc.length-2; i >= 0; i--) {
-            rc[i+1] = rc[i].clone();
+        if (!bodyList.isEmpty()) {
+            bodyList.addFirst(bodyList.pollLast());
         }
-        rc[0] = firstLane;
-        return rc;
     }
 
-    public static int[][] rotate(int[][] rc) {
-        int B = rc[0][w-1];
-        int C = rc[h-1][0];
-        int D = rc[h-1][w-1];
-
-        //윗줄과 밑줄 처리
-        for (int i = w-2; i >= 0; i--) {
-            rc[0][i+1] = rc[0][i];
-            rc[h-1][w-2-i] = rc[h-1][w-2-i+1];
-
+    public static void rotate() {
+        if (!bodyList.isEmpty()) {
+            bodyList.getFirst().addFirst(leftList.pollFirst());
+            rightList.addFirst(bodyList.getFirst().pollLast());
+            bodyList.getLast().addLast(rightList.pollLast());
+            leftList.addLast(bodyList.getLast().pollFirst());
+        } else {
+            rightList.addFirst(leftList.pollFirst());
+            leftList.addLast(rightList.pollLast());
         }
-        rc[h-1][w-2] = D;
-
-        //오른쪽 왼쪽 줄 처리
-        for (int i= h-2; i >= 1; i--) {
-            rc[i+1][w-1] = rc[i][w-1];
-            rc[h-2-i][0] = rc[h-2-i+1][0];
-        }
-        rc[1][w-1] = B;
-        rc[h-2][0] = C;
-        return rc;
     }
 }
 
