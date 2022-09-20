76b919100b67a3f23b21d861e6eeab0ef3418600
seonb2n <626ksb@gmail.com>
2022-09-20 12:41:24 +0900

    프로그래머스 - 등산코스 정하기 (메모리 초과 문제 발생)

--- src/Solution.java
+++ src/Solution.java
@@ -5,19 +5,20 @@ class Solution {
     static List<Node> nodeList;
     static PriorityQueue<Point> priorityQueue;
     static boolean[][] isVisited;
-    static int intensity;
+    static int minIntensity;
+    static int[] answer;
 
     public static void main(String[] args) {
-        int[][] paths = {{1, 2, 3}, {2, 3, 5}, {2, 4, 2}, {2, 5, 4}, {3, 4, 4}, {4, 5, 3}, {4, 6, 1}, {5, 6, 1}};
-        int[] gates = {1, 3};
-        int[] summits = {5};
+        int[][] paths = {{1, 4, 4}, {1, 6, 1}, {1, 7, 3}, {2, 5, 2}, {3, 7, 4}, {5, 6, 6}};
+        int[] gates = {1};
+        int[] summits = {2, 3, 4};
         solution(6, paths, gates, summits);
     }
 
     public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = {};
 
         nodeList = new ArrayList<>();
+        minIntensity = 0;
 
         //n번째 노드까지 추가해준다.
         for (int i = 0; i < n + 1; i++) {
@@ -28,6 +29,9 @@ class Solution {
             int nodeOne = path[0];
             int nodeTwo = path[1];
             int intensity = path[2];
+
+            //현재 경로 탐색 중 최대의 피로도를 초기값으로 가진다.
+            minIntensity = Math.max(intensity, minIntensity);
 
             nodeList.get(nodeOne).toNode.add(new Point(nodeTwo, intensity));
             nodeList.get(nodeTwo).toNode.add(new Point(nodeOne, intensity));
@@ -44,63 +48,57 @@ class Solution {
         //BFS 탐색
         answer = new int[2];
         answer[0] = Integer.MAX_VALUE - 1;
-        answer[1] = Integer.MAX_VALUE - 1;
+        answer[1] = minIntensity;
+
         for (int gate : gates) {
-            for (int summit : summits) {
-                priorityQueue = new PriorityQueue<>();
-                isVisited = new boolean[n + 1][n + 1];
-                intensity = Integer.MAX_VALUE - 1;
-                priorityQueue.offer(new Point(gate, 0));
-                BFS(summit);
-                if (answer[1] > intensity) {
-                    answer[0] = summit;
-                    answer[1] = intensity;
-                } else if (answer[1] == intensity && answer[0] > summit) {
-                    answer[0] = summit;
-                }
-            }
+            priorityQueue = new PriorityQueue<>();
+            isVisited = new boolean[n + 1][n + 1];
+            priorityQueue.offer(new Point(gate, 0));
+            BFS();
         }
 
         return answer;
     }
 
-    //startPoint 에서 targetPoint 로 가는 최소 intensity 를 구함
-    public static void BFS(int targetPoint) {
+    //각각의 gate 에서 출발해서 정상에 도착할 때마다 intensity 를 업데이트해주는 BFS
+    public static void BFS() {
         while (!priorityQueue.isEmpty()) {
             Point nowPoint = priorityQueue.poll();
             Node nowNode = nodeList.get(nowPoint.nowNode);
-            int nowIntensity = nowPoint.intensity;
 
-            if (nowIntensity >= intensity) {
+            //만약 지금 intensity 가 answer[1] 을 넘었다면 더 이상 탐색할 필요가 없다.
+            if (nowPoint.intensity > answer[1]) {
                 return;
             }
 
-            //지금 노드에서 갈 수 있는 위치 탐색
-            for (int i = 0; i < nowNode.toNode.size(); i++) {
-                Point nextPoint = nowNode.toNode.get(i);
-                Node nextNode = nodeList.get(nextPoint.nowNode);
-                int nextIntensity = Math.max(nowIntensity, nextPoint.intensity);
-
-                //다음 노드가 gate 거나 다른 산의 정상이면 스킵
-                if (nextNode.isGate || (nextNode.isSummit && nextPoint.nowNode != targetPoint)) {
-                    continue;
+            //만약 지금 위치가 산의 정상이라면 intensity 를 업데이트해준다.
+            //가장 먼저 도착한 산의 정상은 가장 적은 피로도로 도착한 것이다.
+            if (nowNode.isSummit) {
+                if (nowPoint.intensity < answer[1]) {
+                    answer[0] = nowPoint.nowNode;
+                    answer[1] = nowPoint.intensity;
+                } else if (nowPoint.intensity == answer[1]) {
+                    if (answer[0] > nowPoint.nowNode) {
+                        answer[0] = nowPoint.nowNode;
+                    }
                 }
+            } else {
+                // 아직 피로도도 answer[1] 보다 작고 산의 정상도 아니라면 다음 노드를 탐색한다.
+                for (int i = 0; i < nowNode.toNode.size(); i++) {
+                    Point nextPoint = nowNode.toNode.get(i);
 
-                //nextIntensity 가 현재까지 정상에 갈 수 있는 intensity 보다 크면 스킵
-                if (nextIntensity >= intensity) {
-                    continue;
-                }
+                    // 다음 방문할 노드가 gate 인 경우는 skip
+                    if (nodeList.get(nextPoint.nowNode).isGate) {
+                        continue;
+                    }
 
-                //nextPoint 가 target인 경우
-                if (nextPoint.nowNode == targetPoint) {
-                    intensity = nextIntensity;
-                }
-
-                //아직 방문하지 않은 노드일 때
-                else if (!isVisited[nowPoint.nowNode][nextPoint.nowNode]) {
-                    priorityQueue.offer(new Point(nextPoint.nowNode, nextIntensity));
-                    isVisited[nowPoint.nowNode][nextPoint.nowNode] = true;
-                    isVisited[nextPoint.nowNode][nowPoint.nowNode] = true;
+                    // 다음 방문할 노드가 아직 방문 안한 노드라면
+                    if (!isVisited[nowPoint.nowNode][nextPoint.nowNode] && !isVisited[nextPoint.nowNode][nowPoint.nowNode]) {
+                        int nextIntensity = Math.max(nowPoint.intensity, nextPoint.intensity);
+                        priorityQueue.offer(new Point(nextPoint.nowNode, nextIntensity));
+                        isVisited[nowPoint.nowNode][nextPoint.nowNode] = true;
+                        isVisited[nextPoint.nowNode][nowPoint.nowNode] = true;
+                    }
                 }
             }
         }
