25b8541d3a94561bca75994ec11cfaa08c21e307
seonb2n <626ksb@gmail.com>
2022-11-12 12:02:53 +0900

    Codility - DisappearingPairs

--- src/Solution.java
+++ src/Solution.java
@@ -1,88 +1,38 @@
 import java.util.*;
 
+
 class Solution {
 
     /**
-     * A,B 개수만큼 이루어진 배열을 조합해라. 단 3개 이상 같은 문자가 연속할 수 없다.
-     * @param A
-     * @param B
+     * 연속된 AA, BB, CC 를 지우면 된다.
+     * @param S
      * @return
      */
-    static StringBuilder sb = new StringBuilder();
 
-    public String solution(int A, int B) {
-        if (A == 0) {
-            while (B > 0) {
-                sb.append('b');
-                B--;
-            }
-            return sb.toString();
+    Stack<Character> stack = new Stack<>();
+
+    public String solution(String S) {
+        if (S.equals("")) {
+            return "";
         }
 
-        if (B == 0) {
-            while (A > 0) {
-                sb.append('a');
-                A--;
+        stack.push(S.charAt(0));
+        for (int i = 1; i < S.length(); i++) {
+            char now = S.charAt(i);
+            if (!stack.empty() && stack.peek() == now) {
+                stack.pop();
             }
-            return sb.toString();
-        }
-
-        // A 와 B 의 개수를 같게 만들어준다.
-        if (A > B) {
-            while (A != B && A > 0 && B > 0) {
-                sb.append('a');
-                A--;
-                sb.append('a');
-                A--;
-                sb.append('b');
-                B--;
-            }
-            // A B 하나씩 번갈아가면서 더하거나, 남은 한쪽만 더함.
-            while (A != 0 || B != 0) {
-                if (A != 0) {
-                    sb.append('a');
-                    A--;
-                }
-                if (B != 0) {
-                    sb.append('b');
-                    B--;
-                }
+            else {
+                stack.push(now);
             }
         }
-        else if (A < B) {
-            while (A != B && A > 0 && B > 0) {
-                sb.append('b');
-                B--;
-                sb.append('b');
-                B--;
-                sb.append('b');
-                A--;
-            }
-            // A B 하나씩 번갈아가면서 더하거나, 남은 한쪽만 더함.
-            while (A != 0 || B != 0) {
-                if (B != 0) {
-                    sb.append('b');
-                    B--;
-                }
-                if (A != 0) {
-                    sb.append('a');
-                    A--;
-                }
-            }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
-        else {
-            while (A != 0 || B != 0) {
-                if (B != 0) {
-                    sb.append('b');
-                    B--;
-                }
-                if (A != 0) {
-                    sb.append('a');
-                    A--;
-                }
-            }
-        }
-        return sb.toString();
+        return sb.reverse().toString();
     }
+
 }
+
 
