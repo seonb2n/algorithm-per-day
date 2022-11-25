3b61961c9d66e357e814ad516e3fc689637ddd76
seonb2n <626ksb@gmail.com>
2022-11-25 23:00:46 +0900

    프로그래머스 - 억억단을 외우자

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,62 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
+    static int[] memo;
+    static int[] dp;
 
-    /**
-     * 연속된 AA, BB, CC 를 지우면 된다.
-     * @param S
-     * @return
-     */
-
-    Stack<Character> stack = new Stack<>();
-
-    public String solution(String S) {
-        if (S.equals("")) {
-            return "";
-        }
-
-        stack.push(S.charAt(0));
-        for (int i = 1; i < S.length(); i++) {
-            char now = S.charAt(i);
-            if (!stack.empty() && stack.peek() == now) {
-                stack.pop();
-            }
-            else {
-                stack.push(now);
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
+    public static void main(String[] args) {
+        int[] starts = {1, 1_000_000, 5_000_000};
+        solution(4_900_000, starts);
     }
 
+    public static int[] solution(int e, int[] starts) {
+        memo = new int[e+1];
+        dp = new int[e+1];
+        int min = 5_000_000;
+        for (int start : starts) {
+            min = Math.min(min, start);
+        }
+
+        // memo[n] 은 n 부터 e 까지 중의 최댓값이다.
+        // memo[n-1] 은 memo[n] 과 n-1 의 등장 횟수 중 더 큰 녀석이다.
+        e = Math.min(e, 4_324_320);
+        getCount(e);
+        dp[e] = e;
+        for (int i = e; i >= min+1; i--) {
+            getCount(i-1);
+            if (memo[i-1] >= memo[dp[i]]) {
+                dp[i-1] = i-1;
+            }
+            else {
+                dp[i-1] = dp[i];
+            }
+
+        }
+
+        int[] answer = new int[starts.length];
+        for (int i = 0; i < starts.length; i++) {
+            if (e >= 4324320 && starts[i] <= 4324320) {
+                answer[i] = 4324320;
+            }
+            answer[i] = dp[starts[i]];
+        }
+        return answer;
+    }
+
+    static void getCount(int target) {
+        int sqrt = (int) Math.sqrt(target);
+        int n = 2;
+        for (int j = 2; j <= sqrt; j++) {
+            if (target % j == 0) {
+                n+=2;
+            }
+        }
+        if (sqrt * sqrt == target) n--;
+        memo[target] = n;
+    }
 }
 
 
