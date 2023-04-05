b137b8b6cea5fb1197003a31e64c09f0ebe9c487
seonb2n <626ksb@gmail.com>
2023-04-05 22:23:46 +0900

    - 코딩테스트 연습 - 야근 지수

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,40 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
 
-    private static final int[] noAnswer = {-1};
+    public static void main(String[] args) {
+        int[] works = {4, 3, 3};
+        solution(4, works);
+    }
 
-    static int sum = 0;
+    public static long solution(int n, int[] works) {
+        long answer = 0;
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-    public int[] solution(int n, int s) {
-        int[] answer = new int[n];
-
-        answer[0] = s / n;
-        sum += answer[0];
-        if (answer[0] == 0) {
-            return noAnswer;
+        for (int i = 0; i < works.length; i++) {
+            pq.offer(works[i]);
         }
 
-        for (int i = 1; i < n; i++) {
-            answer[i] = s / n;
-            sum += answer[i];
+        // 야근 지수가 가장 큰 놈을 빼줘야 함
+        for (int i = 0; i < n; i++) {
+            Integer a = pq.poll();
+            if (a == 0) {
+                break;
+            }
+            a--;
+            pq.offer(a);
         }
 
-        sum = s - sum;
-
-        // 합이 s 가 될 때까지 뒤에다 더함
-        for (int i = 0; i < sum; i++) {
-            answer[n-1-i] ++;
+        while (!pq.isEmpty()) {
+            int num = pq.poll();
+            answer += (long) num * num;
         }
 
 
         return answer;
     }
+
 }
