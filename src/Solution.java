150a1fa50e5b91876f304b67beba03d34f7e4564
seonb2n <626ksb@gmail.com>
2023-07-18 23:52:20 +0900

    - 프로그래머스 코딩테스트 연습(등굣길)

--- src/Solution.java
+++ src/Solution.java
@@ -2,39 +2,50 @@ import java.util.*;
 
 class Solution {
 
-    public static int solution(int[][] routes) {
-        int answer = 0;
-        Car[] carArr = new Car[routes.length];
-        for (int i = 0; i < routes.length; i++) {
-            carArr[i] = new Car(routes[i][0], routes[i][1]);
+
+    public static void main(String[] args) {
+        int[][] puddles = {{2,2}};
+        solution(4, 3, puddles);
+    }
+
+    static int[][] memo;
+    static boolean[][] notWay;
+
+    public static int solution(int m, int n, int[][] puddles) {
+        memo = new int[m+1][n+1];
+        notWay = new boolean[m+1][n+1];
+
+        for (int i = 0; i < puddles.length; i++) {
+            notWay[puddles[i][0]][puddles[i][1]] = true;
         }
 
-        Arrays.sort(carArr);
+        memo[1][1] = 1;
 
-        int cam = carArr[0].endPoint;
-        answer++;
-        for (Car car : carArr) {
-            if (car.startPoint > cam) {
-                answer++;
-                cam = car.endPoint;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (!notWay[i][j]) {
+                    memo[i][j] = findMemo(i, j);
+                }
             }
         }
 
-        return answer;
+        return memo[m][n];
     }
 
-    static class Car implements Comparable<Car> {
-        int startPoint;
-        int endPoint;
-
-        public Car(int startPoint, int endPoint) {
-            this.startPoint = startPoint;
-            this.endPoint = endPoint;
+    public static int findMemo(int x, int y) {
+        if (x < 1 || y < 1) {
+            return 0;
         }
 
-        @Override
-        public int compareTo(Car o) {
-            return this.endPoint - o.endPoint;
+        if (notWay[x][y]) {
+            return 0;
+        }
+
+        if (memo[x][y] != 0) {
+            return memo[x][y];
+        }
+        else {
+            return (findMemo(x-1, y) + findMemo(x, y-1)) % 1_000_000_007;
         }
     }
 
