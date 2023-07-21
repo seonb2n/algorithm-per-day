04d4d3360819da8b204d9b395c46e265541cf970
seonb2n <626ksb@gmail.com>
2023-07-21 22:37:45 +0900

    - 프로그래머스 코딩테스트 연습(스티커 모으기 2) - 메모이제이션을 사용해서 통과

--- src/Solution.java
+++ src/Solution.java
@@ -2,61 +2,58 @@ import java.util.*;
 
 class Solution {
 
-    static int N = 0;
-    static int max = 0;
-    static int[] STICKERS;
+    static int N;
+
     public int solution(int sticker[]) {
-        int answer = 0;
         N = sticker.length;
-        STICKERS = new int[N];
 
-        if (sticker.length == 1) {
-            return sticker[0];
+        // memo[i] 는 i 칸 까지의 최대 수
+        // memo[i] = Math.max(memo[i-2] + sticker[i], memo[i-3] + sticker[i], memo[i-1]);
+        int[] memo = new int[N];
+        memo[0] = sticker[0];
+        if (N == 1) {
+            return memo[0];
+        }
+        memo[1] = Math.max(memo[0], sticker[1]);
+        if (N == 2) {
+            return memo[1];
+        }
+        memo[2] = Math.max(Math.max(sticker[0], sticker[1]), sticker[2]);
+        if (N == 3) {
+            return memo[2];
         }
 
-        for (int i = 0; i < N; i++) {
-            STICKERS[i] = sticker[i];
+        int max = 0;
+
+        //0 을 무조건 넣는 경우
+        memo[0] = sticker[0];
+        memo[1] = sticker[0];
+        memo[2] = memo[0] + sticker[2];
+        int index = 3;
+        while(index < N-1) {
+            memo[index] = Math.max(Math.max(memo[index-1], memo[index-2] + sticker[index]), memo[index-3] + sticker[index]);
+            index++;
         }
-        //dfs
-        Set<Integer> detached = new HashSet<Integer>();
-        detached.add(0);
-        dfs(detached, STICKERS[0], 0);
-        detached.add(1);
-        if (N > 2) {
-            detached.add(2);
+
+        memo[N-1] = Math.max(memo[N-2], memo[N-3]);
+        max = Math.max(max, memo[N-1]);
+
+        memo = new int[N];
+        //0 을 무조건 넣지 않는 경우
+        memo[0] = 0;
+        memo[1] = sticker[1];
+        memo[2] = Math.max(memo[1], sticker[2]);
+        index = 3;
+        while(index < N-1) {
+            memo[index] = Math.max(Math.max(memo[index-1], memo[index-2] + sticker[index]), memo[index-3] + sticker[index]);
+            index++;
         }
-        dfs(detached, STICKERS[1], 1);
+
+        memo[N-1] = Math.max(memo[N-2], memo[N-3] + sticker[N-1]);
+        max = Math.max(max, memo[N-1]);
 
         return max;
     }
 
-    void dfs(Set<Integer> detached, int nowSum, int lastNode) {
-        if (detached.size() == N) {
-            max = Math.max(max, nowSum);
-        }
-        else {
-            //last node 에서 두칸을 가는 경우
-            int nextNode = lastNode + 2;
-            if (!detached.contains(nextNode) && nextNode < N) {
-                Set<Integer> newSet = new HashSet<>(Set.copyOf(detached));
-                newSet.add(nextNode);
-                if (nextNode + 1 < N) {
-                    newSet.add(nextNode+1);
-                }
-                dfs(newSet, nowSum+STICKERS[nextNode], nextNode);
-            }
-            //last node 에서 세칸을 가는 경우
-            nextNode = lastNode + 3;
-            if (!detached.contains(nextNode) && nextNode < N) {
-                Set<Integer> newSet = new HashSet<>(Set.copyOf(detached));
-                newSet.add(nextNode);
-                newSet.add(nextNode-1);
-                if (nextNode + 1 < N) {
-                    newSet.add(nextNode+1);
-                }
-                dfs(newSet, nowSum+STICKERS[nextNode], nextNode);
-            }
-        }
-    }
 
 }
