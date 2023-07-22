4f29bde499568baf2bd3506c54e00bedcd1cae96
seonb2n <626ksb@gmail.com>
2023-07-22 23:15:24 +0900

    - 프로그래머스 코딩테스트 연습(기지국 설치) - greedy 알고리즘

--- src/Solution.java
+++ src/Solution.java
@@ -2,58 +2,62 @@ import java.util.*;
 
 class Solution {
 
-    static int N;
-
-    public int solution(int sticker[]) {
-        N = sticker.length;
-
-        // memo[i] 는 i 칸 까지의 최대 수
-        // memo[i] = Math.max(memo[i-2] + sticker[i], memo[i-3] + sticker[i], memo[i-1]);
-        int[] memo = new int[N];
-        memo[0] = sticker[0];
-        if (N == 1) {
-            return memo[0];
-        }
-        memo[1] = Math.max(memo[0], sticker[1]);
-        if (N == 2) {
-            return memo[1];
-        }
-        memo[2] = Math.max(Math.max(sticker[0], sticker[1]), sticker[2]);
-        if (N == 3) {
-            return memo[2];
-        }
-
-        int max = 0;
-
-        //0 을 무조건 넣는 경우
-        memo[0] = sticker[0];
-        memo[1] = sticker[0];
-        memo[2] = memo[0] + sticker[2];
-        int index = 3;
-        while(index < N-1) {
-            memo[index] = Math.max(Math.max(memo[index-1], memo[index-2] + sticker[index]), memo[index-3] + sticker[index]);
-            index++;
-        }
-
-        memo[N-1] = Math.max(memo[N-2], memo[N-3]);
-        max = Math.max(max, memo[N-1]);
-
-        memo = new int[N];
-        //0 을 무조건 넣지 않는 경우
-        memo[0] = 0;
-        memo[1] = sticker[1];
-        memo[2] = Math.max(memo[1], sticker[2]);
-        index = 3;
-        while(index < N-1) {
-            memo[index] = Math.max(Math.max(memo[index-1], memo[index-2] + sticker[index]), memo[index-3] + sticker[index]);
-            index++;
-        }
-
-        memo[N-1] = Math.max(memo[N-2], memo[N-3] + sticker[N-1]);
-        max = Math.max(max, memo[N-1]);
-
-        return max;
+    public static void main(String[] args) {
+        int[] stat = {9};
+        solution(16, stat, 2);
     }
 
+    public static int solution(int n, int[] stations, int w) {
+        int answer = 0;
 
+        // n 개의 아파트에 w 만큼의 기지국을 설치해서 범위를 모두 커버해야 한다.
+        // 범위 내 기지국의 개수를 구하는 공식은 거리 / (1+w*2) 이다.
+        // 1 부터 station[1] 까지 필요한 개수
+//        int end = stations[0] - w-1;
+//        if (end == start) answer++;
+//        if (start < end) {
+//            int width = end-start;
+//            answer += (width / (1+w*2));
+//            //나누어 떨어지지 않으면 추가적인 설치가 필요하다.
+//            if (width % (1+w*2) != 0) {
+//                answer++;
+//            }
+//        }
+        int index = 0;
+        int start = 1;
+        int end = 1;
+        while (index < stations.length) {
+            end = stations[index] - w - 1;
+            if (start == end) {
+                answer++;
+                start = stations[index] + w + 1;
+                index++;
+            } else if (start > end) {
+                start = stations[index] + w + 1;
+                index++;
+            } else {
+                int width = end - start + 1;
+                // 해당 지역 내 설치 최소 설치 개수를 더한다.
+                answer += (width / (1 + w * 2));
+                //나누어 떨어지지 않으면 추가적인 설치가 필요하다.
+                if (width % (1 + w * 2) != 0) {
+                    answer++;
+                }
+                start = stations[index] + w + 1;
+                index++;
+            }
+        }
+
+        // 마지막 개수를 확인한다.
+        if (start < n) {
+            int width = n - start + 1;
+            answer += (width / (1 + w * 2));
+            //나누어 떨어지지 않으면 추가적인 설치가 필요하다.
+            if (width % (1 + w * 2) != 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
