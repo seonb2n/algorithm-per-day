cc6aea674b302171afe31f54d84944f7e3433d41
seonb2n <626ksb@gmail.com>
2023-08-28 20:05:02 +0900

    - 프로그래머스 - 징검다리 건너기 - 효율성 실패

--- src/Solution.java
+++ src/Solution.java
@@ -2,47 +2,66 @@ import java.util.*;
 
 class Solution {
 
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        //int[i][j] 는 i 에서 j 로 가는 최소 비용
-        int[][] mapCosts = dijkstra(n, fares);
+    public static int solution(int[] stones, int k) {
+        int answer = 0;
 
-        //s 에서 특정 위치로 이동한다음, 해당 위치에서 a,b 로 이동하는 경우의 최소 값을 구한다.
-        //출발지에서 각각 가는 경우
-        int answer = Integer.MAX_VALUE;
-
-        for (int i = 1; i <= n; i++) {
-            answer = Math.min(answer, mapCosts[s][i] + mapCosts[i][a] + mapCosts[i][b]);
+        Node[] nodes = new Node[stones.length];
+        for (int i = 0; i < stones.length; i++) {
+            nodes[i] = new Node(i, stones[i]);    
         }
-
-        return answer;
-    }
-
-    public static int[][] dijkstra(int n, int[][] graph) {
-        int[][] result = new int[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(result[i], Integer.MAX_VALUE);
-            result[i][i] = 0;
-        }
-
-        for (int[] edge : graph) {
-            int from = edge[0];
-            int to = edge[1];
-            int cost = edge[2];
-            result[from][to] = cost;
-            result[to][from] = cost; // 양방향 이동을 반영
-        }
-
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (result[i][k] != Integer.MAX_VALUE && result[k][j] != Integer.MAX_VALUE) {
-                        result[i][j] = Math.min(result[i][j], result[i][k] + result[k][j]);
-                    }
-                }
+        // stones 를 오름차순으로 정렬함
+        Arrays.sort(nodes);
+        Set<Integer> zeroStoneSet = new HashSet<Integer>();
+        int index = 0;
+        int lastNumber = 0;
+        while (true) {
+            answer += (nodes[index].stoneSize - lastNumber);
+            lastNumber = nodes[index].stoneSize;
+            zeroStoneSet.add(nodes[index].index);
+            index++;
+            // lastNumber 와 동일한 stoneSize를 모두 처리해준다.
+            while (index < stones.length && nodes[index].stoneSize == lastNumber) {
+                zeroStoneSet.add(nodes[index].index);
+                index++;
+            }
+            // 해당 stones 가 0 이 됐을 때, k 에 위배되는지 확인
+            if (containsConsecutive(zeroStoneSet, k)) {
+                break;
             }
         }
+        
+        return answer;
+    }
+    
+    // set 안에 연속된 숫자가 k 개 만큼 존재하는지 확인한다.
+    public static boolean containsConsecutive(Set<Integer> set, int k) {
+        for (int num : set) {
+            boolean isConsecutive = true;
+            for (int i = 1; i < k; i++) {
+                if (!set.contains(num + i)) {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+            if (isConsecutive) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        return result;
+    public static class Node implements Comparable<Node> {
+        int index;
+        int stoneSize;
+
+        public Node(int index, int stoneSize) {
+            this.index = index;
+            this.stoneSize = stoneSize;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.stoneSize - o.stoneSize;
+        }
     }
 }
