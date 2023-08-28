28a4bd8f601f7f23ad1c327d4d58ebbdd8511380
seonb2n <626ksb@gmail.com>
2023-08-28 20:24:51 +0900

    - 프로그래머스 - 징검다리 건너기 - binary search 를 통해서 해결

--- src/Solution.java
+++ src/Solution.java
@@ -5,28 +5,20 @@ class Solution {
     public static int solution(int[] stones, int k) {
         int answer = 0;
 
-        Node[] nodes = new Node[stones.length];
-        for (int i = 0; i < stones.length; i++) {
-            nodes[i] = new Node(i, stones[i]);    
-        }
-        // stones 를 오름차순으로 정렬함
-        Arrays.sort(nodes);
-        Set<Integer> zeroStoneSet = new HashSet<Integer>();
-        int index = 0;
-        int lastNumber = 0;
-        while (true) {
-            answer += (nodes[index].stoneSize - lastNumber);
-            lastNumber = nodes[index].stoneSize;
-            zeroStoneSet.add(nodes[index].index);
-            index++;
-            // lastNumber 와 동일한 stoneSize를 모두 처리해준다.
-            while (index < stones.length && nodes[index].stoneSize == lastNumber) {
-                zeroStoneSet.add(nodes[index].index);
-                index++;
+        // 이진 탐색으로 풀어야 함
+        int min = 1;
+        int max = 200_000_000;
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            // mid 로 강을 건널 수 있으면 min 을 올린다.
+            if (canCrossRiver(stones, k, mid)) {
+                min = mid + 1;
+                // 정답은 강을 건널 수 있는 최대 값
+                answer = Math.max(answer, mid);
             }
-            // 해당 stones 가 0 이 됐을 때, k 에 위배되는지 확인
-            if (containsConsecutive(zeroStoneSet, k)) {
-                break;
+            // 강을 건널 수 없으면 max 를 줄인다.
+            else {
+                max = mid-1;
             }
         }
         
@@ -34,34 +26,24 @@ class Solution {
     }
     
     // set 안에 연속된 숫자가 k 개 만큼 존재하는지 확인한다.
-    public static boolean containsConsecutive(Set<Integer> set, int k) {
-        for (int num : set) {
-            boolean isConsecutive = true;
-            for (int i = 1; i < k; i++) {
-                if (!set.contains(num + i)) {
-                    isConsecutive = false;
-                    break;
-                }
+    public static boolean canCrossRiver(int[] stones, int k, int friendsNum) {
+        int skip = 0;
+
+        for (int stone : stones) {
+            // 못 건너는 돌이다.
+            if (stone < friendsNum) {
+                skip++;
             }
-            if (isConsecutive) {
-                return true;
+            else {
+                skip= 0;
+            }
+
+            if (skip == k) {
+                return false;
             }
         }
-        return false;
+
+        return true;
     }
 
-    public static class Node implements Comparable<Node> {
-        int index;
-        int stoneSize;
-
-        public Node(int index, int stoneSize) {
-            this.index = index;
-            this.stoneSize = stoneSize;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.stoneSize - o.stoneSize;
-        }
-    }
 }
