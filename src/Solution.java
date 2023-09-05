28de4ce98143e34328c6d9742bcfcca6c550f582
seonb2n <626ksb@gmail.com>
2023-09-05 21:16:31 +0900

    - Longest Unique Substring Set  - 그리디 알고리즘으로 해결

--- src/Solution.java
+++ src/Solution.java
@@ -3,51 +3,65 @@ import java.util.*;
 class Solution {
 
     static int maxNum = 0;
-    static List<String> answerList = new ArrayList<String>();
+    static List<String> answerList = new LinkedList<>();
     static Map<String, Alphabet> alphabetIndex = new HashMap<>();
-    static List<String> alphabetSet = new ArrayList<>();
-    public List<String> longestUniqueSubstringSet(String s) {
-        // implementation
-        // s 에서 문자별 위치를 기록한다.
-        String[] al = s.split("");
+    public static void main(String[] args) {
+        String test = "abmowodfsxadejihgepczpc";
+        System.out.println(longestUniqueSubstringSet(test).toString());
 
-        for (int i = 0; i < al.length; i++) {
-            if (!alphabetIndex.containsKey(al[i])) {
-                alphabetIndex.put(al[i], new Alphabet(i, 0));
-                alphabetSet.add(al[i]);
+    }
+
+    static public List<String> longestUniqueSubstringSet(String s) {
+        //등장하는 모든 문자열의 처음과 끝 위치를 기록한다.
+        String[] splited = s.split("");
+        for (int i = 0; i < splited.length; i++) {
+            String now = splited[i];
+            if (!alphabetIndex.containsKey(now)) {
+                alphabetIndex.put(now, new Alphabet(now, i, i));
             }
-            alphabetIndex.get(al[i]).max = i;
+            alphabetIndex.get(now).maxIndex = i;
         }
 
-        // 기록한 위치를 바탕으로 dfs 를 해서 최장 케이스를 반환한다.
-        dfs(0, 0, new ArrayList<>(), s);
+        //앞에서부터 스캔하면서 해당 문자의 처음 - 끝 까지 하나의 단어를 만든다.
+        int nowWordIndex = -1;
+        int nowWordMaxIndex = -1;
+        int i = 0;
+        while (true) {
+            if (i >= splited.length) {
+                break;
+            }
+            String now = splited[i];
+            //다음에 등장하는 문자가 이전 단어에 포함되면 이전 단어에 붙인다
+            if (alphabetIndex.get(now).minIndex < nowWordMaxIndex) {
+                String nowWord = answerList.get(nowWordIndex);
+                nowWord = nowWord + now;
+                answerList.remove(nowWordIndex);
+                answerList.add(nowWord);
+                i++;
+            }
+            //다음에 등장하는 문자 ~ 끝까지 하나의 단어를 만든다.
+            else {
+                String nowWord = s.substring(alphabetIndex.get(now).minIndex, alphabetIndex.get(now).maxIndex+1);
+                nowWordMaxIndex = alphabetIndex.get(now).maxIndex;
+                answerList.add(nowWord);
+                nowWordIndex++;
+                i = alphabetIndex.get(now).maxIndex+1;
+            }
+        }
 
         return answerList;
     }
 
-    void dfs(int nowNum, int nowIndex, List<String> nowAnswerList, String originStr) {
-        String nowAlphabet = alphabetSet.get(nowIndex);
-        String nowStr = originStr.substring(alphabetIndex.get(nowAlphabet).min, alphabetIndex.get(nowAlphabet).max+1);
-        nowAnswerList.add(nowStr);
-        int nextStart = alphabetIndex.get(nowAlphabet).max+1;
-        for (int i = nowIndex; i < alphabetSet.size(); i++) {
-            if (alphabetIndex.get(alphabetSet.get(i)).min >= nextStart) {
-                dfs(nowNum+1, i, nowAnswerList, originStr);
-            }
-        }
-        if (nowNum > maxNum) {
-            nowNum = maxNum;
-            answerList = nowAnswerList;
+    static class Alphabet {
+        String alp;
+        int minIndex;
+        int maxIndex;
+
+        public Alphabet(String alp, int minIndex, int maxIndex) {
+            this.alp = alp;
+            this.minIndex = minIndex;
+            this.maxIndex = maxIndex;
         }
     }
 
-    class Alphabet {
-        int min;
-        int max;
-
-        public Alphabet(int min, int max) {
-            this.min = min;
-            this.max = max;
-        }
-    }
 }
