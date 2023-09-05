b188ab15a8ffeba9867a1ec2a7db9c6afa28257e
seonb2n <626ksb@gmail.com>
2023-09-05 22:18:09 +0900

    - LeetCode  - longest increasing path in a matrix

--- src/Solution.java
+++ src/Solution.java
@@ -3,65 +3,62 @@ import java.util.*;
 class Solution {
 
     static int maxNum = 0;
-    static List<String> answerList = new LinkedList<>();
-    static Map<String, Alphabet> alphabetIndex = new HashMap<>();
+
+    static int[] x_moves = {-1, 0, 1, 0};
+    static int[] y_moves = {0, 1, 0, -1};
+    static int M;
+    static int N;
+    static int[][] dp;
+
     public static void main(String[] args) {
-        String test = "abmowodfsxadejihgepczpc";
-        System.out.println(longestUniqueSubstringSet(test).toString());
-
+        int [][] arr = {{1}};
+        longestIncreasingPath(arr);
     }
 
-    static public List<String> longestUniqueSubstringSet(String s) {
-        //등장하는 모든 문자열의 처음과 끝 위치를 기록한다.
-        String[] splited = s.split("");
-        for (int i = 0; i < splited.length; i++) {
-            String now = splited[i];
-            if (!alphabetIndex.containsKey(now)) {
-                alphabetIndex.put(now, new Alphabet(now, i, i));
-            }
-            alphabetIndex.get(now).maxIndex = i;
+    public static int longestIncreasingPath(int[][] matrix) {
+        M = matrix.length;
+        N = matrix[0].length;
+        dp = new int[M+1][N+1];
+        for (int i = 0; i < M + 1; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        //앞에서부터 스캔하면서 해당 문자의 처음 - 끝 까지 하나의 단어를 만든다.
-        int nowWordIndex = -1;
-        int nowWordMaxIndex = -1;
-        int i = 0;
-        while (true) {
-            if (i >= splited.length) {
-                break;
-            }
-            String now = splited[i];
-            //다음에 등장하는 문자가 이전 단어에 포함되면 이전 단어에 붙인다
-            if (alphabetIndex.get(now).minIndex < nowWordMaxIndex) {
-                String nowWord = answerList.get(nowWordIndex);
-                nowWord = nowWord + now;
-                answerList.remove(nowWordIndex);
-                answerList.add(nowWord);
-                i++;
-            }
-            //다음에 등장하는 문자 ~ 끝까지 하나의 단어를 만든다.
-            else {
-                String nowWord = s.substring(alphabetIndex.get(now).minIndex, alphabetIndex.get(now).maxIndex+1);
-                nowWordMaxIndex = alphabetIndex.get(now).maxIndex;
-                answerList.add(nowWord);
-                nowWordIndex++;
-                i = alphabetIndex.get(now).maxIndex+1;
+        // dp 로 x, y 에서 갈 수 있는 최대한의 길이를 탐색한다.
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                maxNum = Math.max(getDP(i, j, matrix), maxNum);
             }
         }
-
-        return answerList;
+        return maxNum;
     }
 
-    static class Alphabet {
-        String alp;
-        int minIndex;
-        int maxIndex;
-
-        public Alphabet(String alp, int minIndex, int maxIndex) {
-            this.alp = alp;
-            this.minIndex = minIndex;
-            this.maxIndex = maxIndex;
+    static int getDP(int nowX, int nowY, int[][] matrix) {
+        if (dp[nowX][nowY] != -1) {
+            return dp[nowX][nowY];
+        }
+        // 현재 위치에서 4방향으로 갈 수 있는 값이 없으면 1을 반환한다.
+        // 갈 수 있는 값이 존재하면 해당 위치 dp + 1 이 내 값이다.
+        boolean canGoNext = false;
+        int max = 0;
+        for (int i = 0; i < 4; i++) {
+            int nextX = nowX + x_moves[i];
+            int nextY = nowY + y_moves[i];
+            if (isInArea(nextX, nextY) && matrix[nowX][nowY] < matrix[nextX][nextY]) {
+                canGoNext = true;
+                max = Math.max(getDP(nextX, nextY, matrix), max);
+            }
+        }
+        if (!canGoNext) {
+            dp[nowX][nowY] = 1;
+            return 1;
+        }
+        else {
+            dp[nowX][nowY] = max + 1;
+            return dp[nowX][nowY];
         }
     }
 
+    static boolean isInArea(int x, int y) {
+        return 0 <= x && x < M && 0 <= y && y < N;
+    }
 }
