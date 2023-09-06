3d93fc39d40b4f0e5115269ae6955a9aa4fe7b33
seonb2n <626ksb@gmail.com>
2023-09-06 22:27:17 +0900

    - 프로그래머스  - 여행경로

--- src/Solution.java
+++ src/Solution.java
@@ -2,63 +2,45 @@ import java.util.*;
 
 class Solution {
 
-    static int maxNum = 0;
-
-    static int[] x_moves = {-1, 0, 1, 0};
-    static int[] y_moves = {0, 1, 0, -1};
-    static int M;
-    static int N;
-    static int[][] dp;
+    static boolean[] isUsedTicket;
+    static List<String> answerList;
 
     public static void main(String[] args) {
-        int [][] arr = {{1}};
-        longestIncreasingPath(arr);
+        String[][] src = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+        solution(src);
     }
 
-    public static int longestIncreasingPath(int[][] matrix) {
-        M = matrix.length;
-        N = matrix[0].length;
-        dp = new int[M+1][N+1];
-        for (int i = 0; i < M + 1; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+    public static String[] solution(String[][] tickets) {
 
-        // dp 로 x, y 에서 갈 수 있는 최대한의 길이를 탐색한다.
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                maxNum = Math.max(getDP(i, j, matrix), maxNum);
+        isUsedTicket = new boolean[tickets.length];
+
+        answerList = new ArrayList<String>();
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i][0].equals("ICN")) {
+                isUsedTicket[i] = true;
+                String route = "ICN";
+                dfs(tickets[i][1], route + " " + tickets[i][1], 1, tickets);
+                isUsedTicket[i] = false;
             }
         }
-        return maxNum;
+
+        Collections.sort(answerList);
+
+        return answerList.get(0).split(" ");
     }
 
-    static int getDP(int nowX, int nowY, int[][] matrix) {
-        if (dp[nowX][nowY] != -1) {
-            return dp[nowX][nowY];
+    static void dfs(String nowAirport, String nowRoute, int nowCnt, String[][] tickets) {
+        if (nowCnt == tickets.length) {
+            answerList.add(nowRoute);
         }
-        // 현재 위치에서 4방향으로 갈 수 있는 값이 없으면 1을 반환한다.
-        // 갈 수 있는 값이 존재하면 해당 위치 dp + 1 이 내 값이다.
-        boolean canGoNext = false;
-        int max = 0;
-        for (int i = 0; i < 4; i++) {
-            int nextX = nowX + x_moves[i];
-            int nextY = nowY + y_moves[i];
-            if (isInArea(nextX, nextY) && matrix[nowX][nowY] < matrix[nextX][nextY]) {
-                canGoNext = true;
-                max = Math.max(getDP(nextX, nextY, matrix), max);
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i][0].equals(nowAirport) && !isUsedTicket[i]) {
+                isUsedTicket[i] = true;
+                dfs(tickets[i][1], nowRoute + " " + tickets[i][1], nowCnt+1, tickets);
+                isUsedTicket[i] = false;
             }
         }
-        if (!canGoNext) {
-            dp[nowX][nowY] = 1;
-            return 1;
-        }
-        else {
-            dp[nowX][nowY] = max + 1;
-            return dp[nowX][nowY];
-        }
-    }
 
-    static boolean isInArea(int x, int y) {
-        return 0 <= x && x < M && 0 <= y && y < N;
     }
 }
