1afcdb1329cd0a4ad02ae9c7632f0563206c24c3
seonb2n <626ksb@gmail.com>
2023-09-08 22:40:52 +0900

    - 프로그래머스  - dp + bfs 로 해결

--- src/Solution.java
+++ src/Solution.java
@@ -9,17 +9,30 @@ class Solution {
     static int[] dir = {1, 2, 3, 4};
     static int N;
     static boolean[][] isPassed;
-    public int solution(int[][] board) {
+    static int[][] memo;
+
+    public static void main(String[] args) {
+        int[][] board = {{0,0,1,0},{0,0,0,0},{0,1,0,1},{1,0,0,0}};
+        solution(board);
+    }
+
+    public static int solution(int[][] board) {
         N = board.length;
         isPassed = new boolean[N][N];
+        memo = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(memo[i], Integer.MAX_VALUE);
+        }
 
         isPassed[0][0] = true;
+        memo[0][0] = 0;
         DFS(0, 0, -500,0, board);
 
         return minCost;
     }
 
-    void DFS(int nowX, int nowY, int nowCost, int nowDir, int[][] board) {
+    static void DFS(int nowX, int nowY, int nowCost, int nowDir, int[][] board) {
         if (nowX == N-1 && nowY == N-1) {
             minCost = Math.min(minCost, nowCost);
             return;
@@ -31,19 +44,22 @@ class Solution {
             int nextX = nowX + x_moves[i];
             int nextY = nowY + y_moves[i];
             if (inArea(nextX, nextY) && board[nextX][nextY] == 0 && !isPassed[nextX][nextY]) {
-                isPassed[nextX][nextY] = true;
-                if (nowDir != dir[i]) {
-                    DFS(nextX, nextY, nowCost + 600, dir[i], board);
+                if (memo[nextX][nextY] > nowCost) {
+                    memo[nextX][nextY] = nowCost;
+                    isPassed[nextX][nextY] = true;
+                    if (nowDir == dir[i]) {
+                        DFS(nextX, nextY, nowCost + 100, dir[i], board);
+                    }
+                    else {
+                        DFS(nextX, nextY, nowCost + 600, dir[i], board);
+                    }
+                    isPassed[nextX][nextY] = false;
                 }
-                else {
-                    DFS(nextX, nextY, nowCost + 100, dir[i], board);
-                }
-                isPassed[nextX][nextY] = false;
             }
         }
     }
 
-    boolean inArea(int x, int y) {
+    static boolean inArea(int x, int y) {
         return 0 <= x && x < N && 0 <= y && y < N;
     }
 }
