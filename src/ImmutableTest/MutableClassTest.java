3356e1f91da79782c94a7d73b28ff8e22c0a4d89
seonb2n <626ksb@gmail.com>
2023-10-01 10:51:28 +0900

    - immutable class 구현 테스트

--- src/ImmutableTest/MutableClassTest.java
+++ src/ImmutableTest/MutableClassTest.java
@@ -8,9 +8,16 @@ public class MutableClassTest {
         public A(String myField) {
             this.myField = myField;
         }
+
+        void setField(String myField) {
+            this.myField = myField;
+        }
     }
 
-    public static final class B {
+    // final 키워드로 상속을 통한 변경 허용 안함
+    public final static class B {
+
+        // final 키워드로 객체의 변경에 대해 허용하지 않음
         private final A a;
 
         public B(A a) {
@@ -20,7 +27,7 @@ public class MutableClassTest {
 
         // Getter 메서드만 제공
         public A getA() {
-            return a;
+            return new A(this.a.myField);
         }
     }
 
@@ -28,9 +35,10 @@ public class MutableClassTest {
         B b = new B(new A("field"));
         System.out.println(System.identityHashCode(b));
 
-        b.a = new A("field2"); //cannot assign a value to final variable a
+        b.getA().setField("field2"); //cannot assign a value to final variable a
         System.out.println(System.identityHashCode(b));
 
+        System.out.println(b.getA().myField);
     }
 
 }
