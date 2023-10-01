02c56d64a7609e84ce0586706b4ac36202780cd7
seonb2n <626ksb@gmail.com>
2023-10-01 13:33:29 +0900

    - LeetCode word search ii - https://leetcode.com/problems/word-search-ii/description/

--- src/Solution.java
+++ src/Solution.java
@@ -2,56 +2,79 @@ import java.util.*;
 
 class Solution {
 
-    /**
-     * 정렬된 정수 list 에서 중간값을 찾아서 반환해준다
-     * 중간값이 존재하지 않으면 두 중간값의 평균을 반환한다.
-     */
-    class MedianFinder {
+    static int[] x_moves = {-1, 0, 1, 0};
+    static int[] y_moves = {0, 1, 0, -1};
 
-       PriorityQueue<Integer> lessMedian;
-       PriorityQueue<Integer> moreMedian;
-       int size = 0;
+    public List<String> findWords(char[][] board, String[] words) {
+        // 단어로 이루어진 Trie 를 만든다.
+        // board 를 탐색하면서 일치하는 Trie 의 경우가 있는지 탐색한다.
+        TrieNode rootNode = buildTrie(words);
+        List<String> result = new ArrayList<String>();
 
-        public MedianFinder() {
-            this.lessMedian = new PriorityQueue<Integer>(Comparator.reverseOrder());
-            this.moreMedian = new PriorityQueue<Integer>();
-        }
-
-        public void addNum(int num) {
-            // 첫번째인 경우에는 작은쪽에 넣는다.
-            if (size == 0) {
-                lessMedian.add(num);
-                size++;
-                return;
-            }
-            // 현재 개수가 홀수개인 경우에 큰 쪽에 수를 추가해줘야 한다.
-            else if (size % 2 != 0) {
-                if (lessMedian.peek() <= num) {
-                    moreMedian.add(num);
-                }
-                else {
-                    moreMedian.add(lessMedian.poll());
-                    lessMedian.add(num);
-                }
-            }
-            // 현재 개수가 짝수개면 작은 쪽에 수를 추가해줘야 한다.
-            else {
-                if (num <= moreMedian.peek()) {
-                    lessMedian.add(num);
-                }
-                else {
-                    lessMedian.add(moreMedian.poll());
-                    moreMedian.add(num);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                char c = board[i][j];
+                int nodeIndex = c - 'a';
+                if (rootNode.next[nodeIndex] != null) {
+                    board[i][j] = '#';
+                    dfs(i, j, board, rootNode, result);
+                    board[i][j] = c;
                 }
             }
         }
 
-        public double findMedian() {
-            if (size % 2 != 0) {
-                return lessMedian.peek();
-            }
-            return (lessMedian.peek() + moreMedian.peek()) / 2f;
+        return result;
+    }
+
+    void dfs(int nowX, int nowY, char[][] board, TrieNode nowNode, List<String> result) {
+        if (nowNode.word != null) {
+            result.add(nowNode.word);
+            return;
         }
+        for (int i = 0; i < 4; i++) {
+            int nextX = nowX + x_moves[i];
+            int nextY = nowY + y_moves[i];
+            // 탐색이 가능하다면,
+            if (inArea(nextX, nextY, board.length, board[0].length) && board[nextX][nextY] != '#') {
+                // trie 에 다음 노드가 존재하는지 확인한다.
+                int nextIndex = board[nextX][nextY] - 'a';
+                // 다음 노드가 존재한다면
+                if (nowNode.next[nextIndex] != null) {
+                    char c = board[nextX][nextY];
+                    board[nextX][nextY] = '#';
+                    dfs(nextX, nextY, board, nowNode.next[nextIndex], result);
+                    board[nextX][nextY] = c;
+                }
+            }
+        }
+
+    }
+
+    boolean inArea(int x, int y, int N, int M) {
+        return  0 <= x && x < N && 0 <= y && y < M;
+    }
+
+    public TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        TrieNode nowNode;
+        for (String str: words) {
+            nowNode = root;
+            char[] charArr = str.toCharArray();
+            for (int i = 0; i < charArr.length; i++) {
+                int nextIndex = charArr[i] - 'a';
+                if (nowNode.next[nextIndex] == null) {
+                    nowNode.next[nextIndex] = new TrieNode();
+                }
+                nowNode = nowNode.next[nextIndex];
+            }
+            nowNode.word = str;
+        }
+        return root;
+    }
+
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
     }
 
 }
