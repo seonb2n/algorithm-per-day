a4ca94cc4cb2e1b07dd5f4436ba273f4d6555e49
seonb2n <626ksb@gmail.com>
2023-10-08 17:06:42 +0900

    - LeetCode Maximal Square

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,50 @@
 import java.util.*;
 
+//https://leetcode.com/problems/maximal-square/
 class Solution {
+
+    static int[] x_moves = {-1, 0, 1, 0};
+    static int[] y_moves = {0, 1, 0, -1};
+
+    public int maximalSquare(char[][] matrix) {
+
+        int maxSize = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    int nowSize = maxSize + 1;
+                    while (true) {
+                        if (isSquare(matrix, i, j, nowSize)) {
+                            nowSize++;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    maxSize = Math.max(maxSize, nowSize-1);
+                }
+            }
+        }
+
+        return (maxSize) * (maxSize);
+    }
+
+    boolean isSquare(char[][] matrix, int x, int y, int size) {
+        boolean result = true;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!inArea(x+i, y+j, matrix.length, matrix[0].length) || matrix[x+i][y+j] == '0') {
+                    return false;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    boolean inArea(int x, int y, int N, int M) {
+        return 0 <= x && x < N && 0 <= y && y < M;
+    }
 
 }
