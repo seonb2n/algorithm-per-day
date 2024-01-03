93f013e67d9cbd4d15b71ea291cf882fbdc7a27b
seonbin <626ksb@gmail.com>
2024-01-03 19:48:47 +0900

    - LeetCode sliding-window-maximum - https://leetcode.com/problems/sliding-window-maximum/

--- src/Solution.java
+++ src/Solution.java
@@ -1,67 +1,55 @@
 import java.util.*;
 
-// https://leetcode.com/problems/text-justification/
+// https://leetcode.com/problems/sliding-window-maximum/
 class Solution {
 
-    List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> result = new ArrayList<>();
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int[] res = new int[nums.length - k + 1];
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            String nowWord = words[i];
-            if (sb.length() == 0) {
-                sb.append(nowWord);
-            }
-            else if (sb.length() + nowWord.length() < maxWidth) {
-                sb.append(" ").append(nowWord);
-            }
-            else {
-                String input = sb.toString();
-                result.add(convertToMaxString(input, maxWidth));
-                sb = new StringBuilder();
-                sb.append(nowWord);
-            }
+        int cursor = 0;
+
+        while (cursor < k) {
+            pq.add(new Node(nums[cursor], cursor));
+            cursor++;
         }
 
-        if (sb.length() > 0) {
-            int left = maxWidth - sb.length();
-            sb.append(" ".repeat(Math.max(0, left)));
-            result.add(sb.toString());
+        res[0] = pq.peek().value;
+
+        // 하나씩 추가한다.
+        while (cursor < nums.length) {
+            pq.add(new Node(nums[cursor], cursor));
+
+            //pq 에서 최대값을 찾는다.
+            int max = -10001;
+            while (max == -10001 && !pq.isEmpty()) {
+                if (pq.peek().index + k <= cursor) {
+                    pq.poll();
+                }
+                else {
+                    max = pq.peek().value;
+                }
+            }
+            res[cursor - k + 1] = max;
+            cursor++;
         }
-        return result;
+
+        return res;
     }
 
-    String convertToMaxString(String input, int maxWidth) {
-        String[] split = input.split(" ");
-        if (split.length == 1) {
-            int left = maxWidth - split[0].length();
-            return split[0] +
-                    " ".repeat(left);
-        }
-        int spaceNumber = split.length - 1;
-        int left = maxWidth - input.length();
-        int[] leftResult = divideInput(left, spaceNumber);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < split.length-1; i++) {
-            sb.append(split[i]).append(" ").append(" ".repeat(leftResult[i]));
-        }
-        sb.append(split[split.length - 1]);
-        return sb.toString();
-    }
+    class Node implements Comparable<Node> {
 
-    int[] divideInput(int input, int num) {
-        int[] result = new int[num];
-        int quotient = input / num;
-        int remainder = input % num;
+        int value;
+        int index;
 
-        for (int i = 0; i < num; i++) {
-            result[i] = quotient;
-            if (remainder > 0) {
-                result[i]++;
-                remainder--;
-            }
+        public Node(int value, int index) {
+            this.value = value;
+            this.index = index;
         }
 
-        return result;
+        @Override
+        public int compareTo(Node o) {
+            return this.value - o.value;
+        }
     }
 }
