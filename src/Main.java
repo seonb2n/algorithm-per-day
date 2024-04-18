b4d474f84d66e447b3c52c4e712117c9b9177f60
seonb2n <626ksb@gmail.com>
2024-04-18 22:18:19 +0900

    - Emotion

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class Main {
+import static java.util.Collections.swap;
 
-    static BufferedReader br;
-    static BufferedWriter bw;
+public class Main {
 
-    /**
-     https://www.acmicpc.net/problem/28086
-     *
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
 
-        String problem = br.readLine();
-        String[] split = new String[2];
-        String op = null;
-        if (problem.contains("+")) {
-            split = problem.split("\\+");
-            op = "+";
+        Arrays.stream(solution(new int[]{2, 3, 1, 2}, new int[]{3, 1, 2, 1, 4, 1})).forEach(System.out::print);
+
+        System.out.println();
+        Arrays.stream(solution(new int[]{5, 5, 5}, new int[]{1, 2, 1, 2, 3})).forEach(System.out::print);
+
+        System.out.println();
+        Arrays.stream(solution(new int[]{2, 1, 3, 4, 3}, new int[]{2, 2, 2, 2, 5, 5, 5})).forEach(System.out::print);
+    }
+
+    static int[] solution(int[] emotions, int[] orders) {
+        int[] result = new int[orders.length];
+
+        int[] nowEmotions = Arrays.copyOf(emotions, emotions.length);
+
+        for (int i = 0; i < orders.length; i++) {
+            // 물을 주지만, 0 인 경우엔 변동이 없다.
+            int nowOrder = orders[i]-1;
+            if (nowEmotions[nowOrder] != 0) {
+                nowEmotions[nowOrder] = emotions[nowOrder] + 1;
+            }
+            int nowResult = 0;
+            for (int j = 0; j < nowEmotions.length; j++) {
+                if (nowEmotions[j] > 1) {
+                    nowResult++;
+                    nowEmotions[j]--;
+                } else if (nowEmotions[j] == 1) {
+                    nowEmotions[j]--;
+                }
+            }
+            result[i] = nowResult;
         }
-        if (problem.contains("-")) {
-            split = problem.split("-");
-            op = "-";
-        }
-        if (problem.contains("*")) {
-            split = problem.split("\\*");
-            op = "\\*";
-        }
-        if (problem.contains("/")) {
-            split = problem.split("/");
-            op = "/";
-        }
 
-        for (int i = 0; i < split.length; i++) {
-            System.out.println(split[i]);
-        }
-
-        bw.close();
+//        Arrays.stream(result).forEach(System.out::println);
+        return result;
     }
 
 }
