0fa90c575b9b421b3e35c0e161cdfce2b6a01217
seonb2n <626ksb@gmail.com>
2024-05-22 21:21:05 +0900

    - 프로그래머스 - 정수 삼각형

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,51 @@
 import java.util.*;
 
-// https://leetcode.com/problems/bulls-and-cows/
+// https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java
 class Solution {
-    public String getHint(String secret, String guess) {
 
-        int bulls = 0;
-        int cows = 0;
+    static int[][] dp;
 
-        boolean[] isAlreadySecretChecked = new boolean[secret.length()];
-        boolean[] isAlreadyGuessChecked = new boolean[secret.length()];
+    public int solution(int[][] triangle) {
+        // dp[n][m] 은 n 번째 줄의 [m] 번째 수를 포함한 최댓값이다.
+        dp = new int[triangle.length][triangle[triangle.length-1].length];
+        dp[0][0] = triangle[0][0];
 
-        for (int i = 0; i < secret.length(); i++) {
-            if (guess.charAt(i) == secret.charAt(i)) {
-                bulls++;
-                // guess 중에 사용된 애 찾기
-                isAlreadySecretChecked[i] = true;
-                isAlreadyGuessChecked[i] = true;
-            }
+        if (triangle.length == 1) {
+            return dp[0][0];
         }
 
-        for (int i = 0; i < secret.length(); i++) {
-            for (int j = 0; j < guess.length(); j++) {
-                if (!isAlreadySecretChecked[j] && !isAlreadyGuessChecked[i] && secret.charAt(i) == guess.charAt(j)) {
-                    cows++;
-                    isAlreadyGuessChecked[j] = true;
-                    break;
-                }
+        int floorNumber = 1;
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j <= floorNumber; j++) {
+                dp[i][j] = getDp(i, j, triangle[i][j], floorNumber);
             }
+            floorNumber++;
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(bulls).append('A').append(cows).append('B');
-        return sb.toString();
+        // 마지막 층 중 최댓값 찾기
+        int max = 0;
+        for (int i = 0; i < floorNumber; i++) {
+            max = Math.max(max, dp[triangle.length-1][i]);
+        }
+
+        return max;
+    }
+
+    public int getDp(int n, int m, int nowNumber, int nowFloor) {
+        if (dp[n][m] != 0) {
+            return dp[n][m];
+        }
+        else {
+            if (m == 0) {
+                dp[n][m] = dp[n-1][0] + nowNumber;
+            }
+            else if (m == nowFloor) {
+                dp[n][m] = dp[n-1][nowFloor-1] + nowNumber;
+            }
+            else {
+                dp[n][m] = Math.max(dp[n-1][m-1], dp[n-1][m]) + nowNumber;
+            }
+        }
+        return dp[n][m];
     }
 }
