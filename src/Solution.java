7b7b61b712bd703c1d0af21b4e082be0c6a9135c
seonb2n <626ksb@gmail.com>
2024-06-10 22:14:24 +0900

    - 프로그래머스 - 광고 삽입

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,67 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/12904
+// https://school.programmers.co.kr/learn/courses/30/lessons/72414
 class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        int lastTime = convertToSeconds(play_time);
+        int[] perSeconds = new int[lastTime+1];
 
-    public int solution(String s) {
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        for (String log : logs) {
+            String[] times = log.split("-");
+            int startTime = convertToSeconds(times[0]);
+            int endTime = convertToSeconds(times[1]);
+            for (int j = startTime; j <= endTime; j++) {
+                perSeconds[j]++;
             }
         }
-        return s.substring(start, end + 1).length();
+
+        // 0 초부터 광고 계산
+        int maxTime = 0;
+        int maxValue = 0;
+        int nowTime = 1;
+
+        int totalAdvTime = convertToSeconds(adv_time);
+
+        // 0 초부터 광고 마지막 초까지 누적 시척시간 계산
+        for (int i = 0; i <= totalAdvTime; i++) {
+            maxValue += perSeconds[i];
+        }
+
+        int nowValue = maxValue;
+
+        while (nowTime + totalAdvTime <= lastTime) {
+            nowValue = nowValue - perSeconds[nowTime-1] + perSeconds[nowTime + totalAdvTime];
+            if (nowValue > maxValue) {
+                maxValue = nowValue;
+                maxTime = nowTime;
+            }
+            nowTime++;
+        }
+
+        return convertToTimeString(maxTime);
     }
 
-    private static int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return right - left - 1;
+    public static int convertToSeconds(String timeString) {
+        // 시간 문자열을 콜론(:)으로 분리
+        String[] parts = timeString.split(":");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+        int seconds = Integer.parseInt(parts[2]);
+
+        // 시간을 초 단위로 변환
+        int hoursInSeconds = hours * 3600;
+        int minutesInSeconds = minutes * 60;
+
+        return hoursInSeconds + minutesInSeconds + seconds;
+    }
+
+    public static String convertToTimeString(int totalSeconds) {
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 }
