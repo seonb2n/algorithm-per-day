10b55999f48ec71bcbba71cab471b20fd7234c5a
seonb2n <626ksb@gmail.com>
2024-06-17 21:39:51 +0900

    - LeetCode count-pairs-that-form-a-complete-day-ii

--- src/Solution.java
+++ src/Solution.java
@@ -2,64 +2,23 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/72414
+// https://leetcode.com/problems/count-pairs-that-form-a-complete-day-ii/
 class Solution {
-    public String solution(String play_time, String adv_time, String[] logs) {
-        int lastTime = convertToSeconds(play_time);
-        int[] perSeconds = new int[lastTime+1];
+    // 거꾸로, 내 값 이전에 나랑 합칠 수 있는 나머지를 가진 개수를 찾으면 된다.
+    public long countCompleteDayPairs(int[] hours) {
+        int n = hours.length;
+        long cnt = 0;
+        int[] counts = new int[24];
 
-        for (String log : logs) {
-            String[] times = log.split("-");
-            int startTime = convertToSeconds(times[0]);
-            int endTime = convertToSeconds(times[1]);
-            for (int j = startTime; j < endTime; j++) {
-                perSeconds[j]++;
-            }
+        for (int hour : hours) {
+            int num = hour % 24;
+            int complement = (24 - num) % 24;
+
+            cnt += counts[complement];
+
+            counts[num]++;
         }
 
-        // 0 초부터 광고 계산
-        int maxTime = 0;
-        long maxValue = 0;
-
-        int lastAdvTime = convertToSeconds(adv_time);
-
-        // 0 초부터 광고 마지막 초까지 누적 시척시간 계산
-        for (int i = 0; i < lastAdvTime; i++) {
-            maxValue += perSeconds[i];
-        }
-
-        long nowValue = maxValue;
-
-        for (int i = lastAdvTime; i < lastTime; i++) {
-            nowValue = (nowValue + perSeconds[i] - perSeconds[i-lastAdvTime]);
-            if (nowValue > maxValue) {
-                maxValue = nowValue;
-                maxTime = i - lastAdvTime + 1;
-            }
-        }
-
-        return convertToTimeString(maxTime);
-    }
-
-    public static int convertToSeconds(String timeString) {
-        // 시간 문자열을 콜론(:)으로 분리
-        String[] parts = timeString.split(":");
-        int hours = Integer.parseInt(parts[0]);
-        int minutes = Integer.parseInt(parts[1]);
-        int seconds = Integer.parseInt(parts[2]);
-
-        // 시간을 초 단위로 변환
-        int hoursInSeconds = hours * 3600;
-        int minutesInSeconds = minutes * 60;
-
-        return hoursInSeconds + minutesInSeconds + seconds;
-    }
-
-    public static String convertToTimeString(int totalSeconds) {
-        int hours = totalSeconds / 3600;
-        int minutes = (totalSeconds % 3600) / 60;
-        int seconds = totalSeconds % 60;
-
-        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        return cnt;
     }
 }
