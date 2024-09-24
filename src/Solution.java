8ba11c785989a48ad40420c38cbd85a97ca09607
seonb2n <626ksb@gmail.com>
2024-09-24 21:18:47 +0900

    - Leetcode longest substring without repeating character

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,26 @@
 import java.util.*;
 
-// https://leetcode.com/problems/sum-of-beauty-of-all-substrings/
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class Solution {
-
-    public static void main(String[] args) {
-        beautySum("aabcb");
-    }
-
-    static public int beautySum(String s) {
-        int result = 0;
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0 ;
 
         for (int i = 0; i < s.length(); i++) {
-            // 글자 시작 지점이 바뀔 때마다 새롭게 map 생성
-            Map<Character, Integer> alphabetMap = new HashMap<Character, Integer>();
-            for (int j = i; j < s.length(); j++) {
-                // 이번에 새롭게 추가되는 글자
-                char nowChar = s.charAt(j);
-                if (alphabetMap.containsKey(nowChar)) {
-                    int nowNumber = alphabetMap.get(nowChar);
-                    alphabetMap.put(nowChar, nowNumber + 1);
+            Set<Character> characters = new HashSet<>();
+            characters.add((Character) s.charAt(i));
+            int nowIndex = i;
+            for (int j = i+1; j < s.length(); j++) {
+                Character nowChar = (Character) s.charAt(j);
+                if (!characters.contains(nowChar)) {
+                    characters.add(nowChar);
+                    nowIndex = j;
                 } else {
-                    alphabetMap.put(nowChar, 0);
+                    break;
                 }
-
-                // 새롭게 글자가 추가됐으니 max, min 다시 연산
-                int max = 0;
-                int min = Integer.MAX_VALUE - 1;
-                for (Character c : alphabetMap.keySet()) {
-                    max = Math.max(max, alphabetMap.get(c));
-                    min = Math.min(min, alphabetMap.get(c));
-                }
-                result += (max - min);
             }
+            max = Math.max(max, nowIndex - i + 1);
         }
 
-        // 아름다움을 계산함
-        return result;
+        return  max;
     }
 }
