b021c9e9cc4fd2aa8f1f2892f5e974b6dbdd5a2b
seonb2n <626ksb@gmail.com>
2024-10-24 21:57:02 +0900

    - Leetcode add-two-numbers

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,58 @@
 import java.util.*;
 
-//https://leetcode.com/problems/longest-substring-without-repeating-characters/
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0 ;
+//https://leetcode.com/problems/add-two-numbers/
 
-        for (int i = 0; i < s.length(); i++) {
-            Set<Character> characters = new HashSet<>();
-            characters.add((Character) s.charAt(i));
-            int nowIndex = i;
-            for (int j = i+1; j < s.length(); j++) {
-                Character nowChar = (Character) s.charAt(j);
-                if (!characters.contains(nowChar)) {
-                    characters.add(nowChar);
-                    nowIndex = j;
-                } else {
-                    break;
-                }
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode tempA = l1;
+        ListNode tempB = l2;
+
+        ListNode startResult = new ListNode(0);
+        ListNode result = startResult;
+        int nextVal = 0;
+        while (tempA != null || tempB != null) {
+            // 둘다 값 있음
+            if (tempA != null && tempB != null) {
+                result.next = new ListNode((tempA.val + tempB.val + nextVal) % 10);
+                nextVal = (tempA.val + tempB.val + nextVal) / 10;
+                tempA = tempA.next;
+                tempB = tempB.next;
             }
-            max = Math.max(max, nowIndex - i + 1);
+            // A 만 값 있음
+            else if (tempA != null && tempB == null) {
+                result.next = new ListNode((tempA.val + nextVal) % 10);
+                nextVal = (tempA.val + nextVal) / 10;
+                tempA = tempA.next;
+            }
+            // B 만 값 있음
+            else if (tempB != null && tempA == null) {
+                result.next = new ListNode((tempB.val + nextVal) % 10);
+                nextVal = (tempB.val + nextVal) / 10;
+                tempB = tempB.next;
+            }
+            result = result.next;
+        }
+        if (nextVal != 0) {
+            result.next = new ListNode(nextVal);
         }
 
-        return  max;
+        return startResult.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {
+
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
