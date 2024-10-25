24d3c970fbc884577720aecc2dff40bec02fd35e
seonb2n <626ksb@gmail.com>
2024-10-25 20:31:44 +0900

    - Leetcode median of two sorted Arrays

--- src/Solution.java
+++ src/Solution.java
@@ -1,58 +1,29 @@
 import java.util.*;
 
 //https://leetcode.com/problems/add-two-numbers/
-
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode tempA = l1;
-        ListNode tempB = l2;
-
-        ListNode startResult = new ListNode(0);
-        ListNode result = startResult;
-        int nextVal = 0;
-        while (tempA != null || tempB != null) {
-            // 둘다 값 있음
-            if (tempA != null && tempB != null) {
-                result.next = new ListNode((tempA.val + tempB.val + nextVal) % 10);
-                nextVal = (tempA.val + tempB.val + nextVal) / 10;
-                tempA = tempA.next;
-                tempB = tempB.next;
-            }
-            // A 만 값 있음
-            else if (tempA != null && tempB == null) {
-                result.next = new ListNode((tempA.val + nextVal) % 10);
-                nextVal = (tempA.val + nextVal) / 10;
-                tempA = tempA.next;
-            }
-            // B 만 값 있음
-            else if (tempB != null && tempA == null) {
-                result.next = new ListNode((tempB.val + nextVal) % 10);
-                nextVal = (tempB.val + nextVal) / 10;
-                tempB = tempB.next;
-            }
-            result = result.next;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < nums1.length; i++) {
+            pq.offer(nums1[i]);
         }
-        if (nextVal != 0) {
-            result.next = new ListNode(nextVal);
+        for (int i = 0; i < nums2.length; i++) {
+            pq.offer(nums2[i]);
         }
-
-        return startResult.next;
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {
-
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        int length = pq.size();
+        if (length % 2 == 0) {
+            for (int i = 0; i < length / 2-1; i++) {
+                pq.poll();
+            }
+            int first = pq.poll();
+            int second = pq.poll();
+            return ((double)first + (double)second) / 2;
+        }
+        else {
+            for (int i = 0; i < length / 2 - 1; i++) {
+                pq.poll();
+            }
+            return (double) pq.poll();
+        }
     }
 }
