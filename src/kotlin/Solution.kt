bbeeec6c0203759794bd8a8bd713083e686027c3
seonb2n <626ksb@gmail.com>
2024-10-27 18:25:51 +0900

    - string-to-integer-atoi

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,25 +1,42 @@
 package kotlin
 
-import java.lang.StringBuilder
+import kotlin.text.StringBuilder
 
-// https://leetcode.com/problems/reverse-integer/
+// https://leetcode.com/problems/string-to-integer-atoi/
 class Solution {
-    fun reverse(x: Int): Int {
+    fun myAtoi(s: String): Int {
+        var tempS = s.toCharArray()
+        var cursor: Int = 0
+        var isMinus: Boolean = false
         val sb: StringBuilder = StringBuilder()
-        var stringX = x.toString()
 
-        if (x < 0) {
-            stringX = stringX.substring(1)
+        while (cursor < tempS.size && tempS[cursor] == ' ') {
+            cursor++
+        }
+        if (cursor < tempS.size && (tempS[cursor] == '+' || tempS[cursor] == '-')) {
+            sb.append(tempS[cursor])
+            if (tempS[cursor] == '-') {
+                isMinus = true
+            }
+            cursor++
         }
 
-        for (i in stringX.length - 1 downTo 0) {
-            sb.append(stringX[i])
+        if (cursor < tempS.size && tempS[cursor] !in '0'..'9') {
+            return 0
         }
 
-        return try {
-            if (x < 0) sb.toString().toInt() * -1 else sb.toString().toInt()
+        while (cursor < tempS.size && tempS[cursor] in '0'..'9') {
+            sb.append(tempS[cursor])
+            cursor++
+        }
+        if (sb.toString() == "" || sb.toString() == "+" || sb.toString() == "-") {
+            return 0
+        }
+
+        try {
+           return sb.toString().toInt()
         } catch (e: NumberFormatException) {
-            0
+            return if (isMinus) { Int.MIN_VALUE } else { Int.MAX_VALUE }
         }
     }
 }
