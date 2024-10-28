636b128513fee3f8cabdbc9ba9a83fb42ad543fe
seonb2n <626ksb@gmail.com>
2024-10-28 23:16:13 +0900

    - regular-expression-matching

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,41 +2,26 @@ package kotlin
 
 import kotlin.text.StringBuilder
 
-// https://leetcode.com/problems/string-to-integer-atoi/
+// https://leetcode.com/problems/regular-expressions-matching/
 class Solution {
-    fun myAtoi(s: String): Int {
-        var tempS = s.toCharArray()
-        var cursor: Int = 0
-        var isMinus: Boolean = false
-        val sb: StringBuilder = StringBuilder()
+    fun isMatch(text: String, pattern: String): Boolean {
+        return matchRecursively(text, pattern)
+    }
 
-        while (cursor < tempS.size && tempS[cursor] == ' ') {
-            cursor++
-        }
-        if (cursor < tempS.size && (tempS[cursor] == '+' || tempS[cursor] == '-')) {
-            sb.append(tempS[cursor])
-            if (tempS[cursor] == '-') {
-                isMinus = true
-            }
-            cursor++
-        }
+    private fun matchRecursively(text: String, pattern: String): Boolean {
+        if (pattern.isEmpty()) return text.isEmpty()
 
-        if (cursor < tempS.size && tempS[cursor] !in '0'..'9') {
-            return 0
+        // 첫 문자가 매칭되는지 확인
+        val firstMatch = text.isNotEmpty() &&
+                (pattern[0] == text[0] || pattern[0] == '.')
+
+        // '*' 패턴이면 0-n 번 매칭이 가능하다
+        if (pattern.length >= 2 && pattern[1] == '*') {
+            return matchRecursively(text, pattern.substring(2)) || // 0번 매칭
+                    (firstMatch && matchRecursively(text.substring(1), pattern)) // 1번 이상 매칭
         }
 
-        while (cursor < tempS.size && tempS[cursor] in '0'..'9') {
-            sb.append(tempS[cursor])
-            cursor++
-        }
-        if (sb.toString() == "" || sb.toString() == "+" || sb.toString() == "-") {
-            return 0
-        }
-
-        try {
-           return sb.toString().toInt()
-        } catch (e: NumberFormatException) {
-            return if (isMinus) { Int.MIN_VALUE } else { Int.MAX_VALUE }
-        }
+        // 그 다음 문자 매칭
+        return firstMatch && matchRecursively(text.substring(1), pattern.substring(1))
     }
 }
