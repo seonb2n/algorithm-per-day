8e7d92a43209da052ce46b0aee3590ccb36d6637
seonb2n <626ksb@gmail.com>
2024-10-29 22:20:21 +0900

    - container-with-most-water

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,27 +1,24 @@
 package kotlin
 
-import kotlin.text.StringBuilder
+// https://leetcode.com/problems/container-with-most-water/
 
-// https://leetcode.com/problems/regular-expressions-matching/
 class Solution {
-    fun isMatch(text: String, pattern: String): Boolean {
-        return matchRecursively(text, pattern)
-    }
+    fun maxArea(height: IntArray): Int {
+        var left = 0
+        var right = height.size - 1
+        var maxArea = 0
 
-    private fun matchRecursively(text: String, pattern: String): Boolean {
-        if (pattern.isEmpty()) return text.isEmpty()
+        while (left < right) {
+            val currentArea = (right - left) * Math.min(height[left], height[right])
+            maxArea = Math.max(maxArea, currentArea)
 
-        // 첫 문자가 매칭되는지 확인
-        val firstMatch = text.isNotEmpty() &&
-                (pattern[0] == text[0] || pattern[0] == '.')
-
-        // '*' 패턴이면 0-n 번 매칭이 가능하다
-        if (pattern.length >= 2 && pattern[1] == '*') {
-            return matchRecursively(text, pattern.substring(2)) || // 0번 매칭
-                    (firstMatch && matchRecursively(text.substring(1), pattern)) // 1번 이상 매칭
+            if (height[left] < height[right]) {
+                left++
+            } else {
+                right--
+            }
         }
 
-        // 그 다음 문자 매칭
-        return firstMatch && matchRecursively(text.substring(1), pattern.substring(1))
+        return maxArea
     }
 }
