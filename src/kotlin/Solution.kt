f87614c1c8fe4e6f33e4b06dff20f2f573de515f
seonb2n <626ksb@gmail.com>
2024-11-01 21:25:04 +0900

    - leetcode 3sum-closet

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,37 +1,33 @@
 package kotlin
 
-// https://leetcode.com/problems/3sum/
+// https://leetcode.com/problems/3sum-closet/
 class Solution {
-    fun threeSum(nums: IntArray): List<List<Int>> {
-        nums.sort() // 배열을 정렬하여 중복 체크를 쉽게 함
-        val result = mutableSetOf<List<Int>>() // Set을 사용하여 중복 제거
+    fun threeSumClosest(nums: IntArray, target: Int): Int {
+        nums.sort()
+        var result = nums[0] + nums[1] + nums[2]
 
-        for (i in nums.indices) {
-            // 같은 첫 번째 숫자는 건너뛰기
-            if (i > 0 && nums[i] == nums[i-1]) continue
-
-            var left = i + 1
-            var right = nums.size - 1
+        for (i in 0..nums.size-3) {
+            var left = i+1
+            var right = nums.size-1
 
             while (left < right) {
-                val sum = nums[i] + nums[left] + nums[right]
+                val current = nums[i] + nums[left] + nums[right]
+                if (current == target) {
+                    return current
+                }
 
-                when {
-                    sum == 0 -> {
-                        result.add(listOf(nums[i], nums[left], nums[right]))
-                        left++
-                        right--
-
-                        // 중복되는 값 건너뛰기
-                        while (left < right && nums[left] == nums[left-1]) left++
-                        while (left < right && nums[right] == nums[right+1]) right--
-                    }
-                    sum < 0 -> left++
-                    else -> right--
+                if (Math.abs(current - target) < Math.abs(result - target)) {
+                    result = current
+                }
+                if (current > target) {
+                    right--
+                }
+                else if (current < target) {
+                    left++
                 }
             }
         }
 
-        return result.toList()
+        return result
     }
 }
