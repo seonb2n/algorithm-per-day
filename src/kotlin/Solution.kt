9295d9949ff20be0c4ce914ae7713feef3f92809
seonb2n <626ksb@gmail.com>
2024-11-04 20:51:25 +0900

    - leetcode 4sum

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,37 +1,31 @@
 package kotlin
 
-// https://leetcode.com/problems/letter-combinations-of-a-phone-number
+// https://leetcode.com/problems/4sums
 class Solution {
-    fun letterCombinations(digits: String): List<String> {
-        if (digits.isEmpty()) return emptyList()
-        val result = mutableListOf<String>()
-        val map = mapOf(
-            '2' to "abc",
-            '3' to "def",
-            '4' to "ghi",
-            '5' to "jkl",
-            '6' to "mno",
-            '7' to "pqrs",
-            '8' to "tuv",
-            '9' to "wxyz"
-        )
+    fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
+        nums.sort()
+        val result = mutableSetOf<List<Int>>()
 
-        //DFS
-        fun dfs(digits: String, index: Int, sb: StringBuilder) {
-            if (index == digits.length) {
-                result.add(sb.toString())
-                return
-            }
-            val nowStr = map[digits[index]]!!
-            for (i in nowStr.indices) {
-                sb.append(nowStr[i])
-                dfs(digits, index + 1, sb)
-                sb.deleteCharAt(sb.length - 1)
+        for (i in 0 until nums.size - 1) {
+            for (j in i + 1 until nums.size) {
+                var left = j+1
+                var right = nums.size-1
+
+                while (left < right) {
+                    val nowNumber = nums[i].toLong() + nums[j] + nums[left] + nums[right]
+                    if (nowNumber == target.toLong()) {
+                        result.add(listOf(nums[i], nums[j], nums[left], nums[right]))
+                        left++
+                    }
+                    else if (nowNumber < target) {
+                       left++
+                    }
+                    else {
+                        right--
+                    }
+                }
             }
         }
-        val sb: StringBuilder = StringBuilder()
-        dfs(digits, 0, sb)
-
-        return result
+        return result.map { it.toMutableList() }.toList()
     }
 }
