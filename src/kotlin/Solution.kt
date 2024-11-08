a97a0039d000ac79e720c2d84c96d8b1fd74714b
seonb2n <626ksb@gmail.com>
2024-11-08 21:24:15 +0900

    - leetcode generate-paretheses

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,47 +1,23 @@
 package kotlin
 
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
 class Solution {
-    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-        val stack = ArrayDeque<Int>()
 
-        var now = head
-
-        while (now != null) {
-            stack.add(now.`val`)
-            now = now.next
-        }
-
-        var pastNode: ListNode? = ListNode(stack.removeLast())
-        var cursor = 1
-        if (n == 1) {
-            pastNode = null
-        }
-
-        while (stack.isNotEmpty()) {
-            cursor++
-            if (cursor == n) {
-                cursor++
-                continue
-            }
-            val now = ListNode(stack.removeLast())
-            now.next = pastNode
-            pastNode = now
-        }
-
-        return pastNode
+    val result = mutableListOf<String>()
+    fun generateParenthesis(n: Int): List<String> {
+        dfs(n, 0, 0, "")
+        return result
     }
-}
 
-
-data class ListNode(var `val`: Int) {
-    var next: ListNode? = null
+    fun dfs(n: Int, left: Int, right: Int, now: String) {
+        if (now.length == n * 2) {
+            result.add(now)
+            return
+        }
+        if (left < n) {
+            dfs(n, left + 1, right, now + '(')
+        }
+        if (right < n && right < left) {
+            dfs(n, left, right + 1, now + ')')
+        }
+    }
 }
