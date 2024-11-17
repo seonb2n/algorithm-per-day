55d74015edecd151547c91a69d517a8e6a57a705
seonb2n <626ksb@gmail.com>
2024-11-17 16:07:24 +0900

    - leetcode search-in-rotated-sorted-array

--- src/Solution.java
+++ src/Solution.java
@@ -1,55 +1,28 @@
 import java.util.*;
 
+// leetcode search-in-rotated-sorted-array
 class Solution {
-
-    public List<Integer> findSubstring(String s, String[] words) {
-        Map<String, Integer> wordMap = new HashMap<>();
-        for (String word : words) {
-            if (wordMap.containsKey(word)) {
-                wordMap.put(word, wordMap.get(word) + 1);
-            } else {
-                wordMap.put(word, 1);
-            }
-        }
-
-        int length = words[0].length();
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (dfs(s.substring(i), length, wordMap)) {
-                res.add(i);
-            }
-        }
-        return res;
+    public int search(int[] nums, int target) {
+        return divide(nums, target, 0, nums.length - 1);
     }
 
-    public boolean dfs(String s, int length, Map<String, Integer> wordMap) {
-        if (wordMap.isEmpty()) {
-            return true;
-        }
-        if (s.length() < length) {
-            return false;
-        }
-        String now = s.substring(0, length);
-        if (wordMap.containsKey(now)) {
-            if (wordMap.get(now) == 1) {
-                wordMap.remove(now);
+    public int divide(int[] nums, int target, int left, int right) {
+        if (left == right) {
+            if (nums[left] == target) {
+                return left;
             } else {
-                wordMap.put(now, wordMap.get(now) - 1);
-            }
-            if (dfs(s.substring(length), length, wordMap)) {
-                if (wordMap.containsKey(now)) {
-                    wordMap.put(now, wordMap.get(now) + 1);
-                } else {
-                    wordMap.put(now, 1);
-                }
-                return true;
-            };
-            if (wordMap.containsKey(now)) {
-                wordMap.put(now, wordMap.get(now) + 1);
-            } else {
-                wordMap.put(now, 1);
+                return -1;
             }
         }
-        return false;
+        int mid = (left + right) / 2;
+        var foundLeft = divide(nums, target, left, mid);
+        if (foundLeft != -1) {
+            return foundLeft;
+        }
+        var foundRight = divide(nums, target, mid + 1, right);
+        if (foundRight != -1) {
+            return foundRight;
+        }
+        return -1;
     }
 }
