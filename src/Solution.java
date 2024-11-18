aa049819690b420be5d9527eb0a22533c96a2133
seonb2n <626ksb@gmail.com>
2024-11-18 20:34:44 +0900

    - leetcode  https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,44 @@
 import java.util.*;
 
-// leetcode search-in-rotated-sorted-array
+// leetcode find-first-and-last-position-of-element-in-sorted-array
 class Solution {
-    public int search(int[] nums, int target) {
-        return divide(nums, target, 0, nums.length - 1);
+    static int min;
+    static int max;
+
+    public int[] searchRange(int[] nums, int target) {
+        min = 100_001;
+        max = -1;
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        divide(nums, target, 0, nums.length - 1);
+
+        if (min == 100_001) {
+            min = -1;
+        }
+
+        return new int[]{min, max};
     }
 
-    public int divide(int[] nums, int target, int left, int right) {
+    public void divide(int[] nums, int target, int left, int right) {
         if (left == right) {
             if (nums[left] == target) {
-                return left;
+                min = Math.min(min, left);
+                max = Math.max(max, right);
             } else {
-                return -1;
+                return;
             }
+        } else {
+            if (nums[left] == target) {
+                min = Math.min(min, nums[left]);
+            }
+            if (nums[right] == target) {
+                max = Math.max(max, nums[right]);
+            }
+
+            int mid = (left + right) / 2;
+            divide(nums, target, left, mid);
+            divide(nums, target, mid + 1, right);
         }
-        int mid = (left + right) / 2;
-        var foundLeft = divide(nums, target, left, mid);
-        if (foundLeft != -1) {
-            return foundLeft;
-        }
-        var foundRight = divide(nums, target, mid + 1, right);
-        if (foundRight != -1) {
-            return foundRight;
-        }
-        return -1;
     }
 }
