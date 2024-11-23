e9f40eede90b2ff263fb7a160244d316abf8ec0d
seonb2n <626ksb@gmail.com>
2024-11-23 19:57:11 +0900

    - leetcode https://leetcode.com/problems/sudoku-solver
    - 가장 제약이 많은 곳부터 dfs 로 탐색 beats 43.76

--- src/Solution.java
+++ src/Solution.java
@@ -7,34 +7,68 @@ class Solution {
     }
 
     private boolean solve(char[][] board) {
-        // board를 순회하면서 빈 칸 찾기
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                // 빈 칸을 찾으면
-                if (board[row][col] == '.') {
-                    // 1부터 9까지 숫자를 시도
-                    for (char num = '1'; num <= '9'; num++) {
-                        // 현재 위치에 num을 놓을 수 있는지 검사
-                        if (isValid(board, row, col, num)) {
-                            // 가능하다면 숫자를 놓고
-                            board[row][col] = num;
+        Cell nextCell = findBestCell(board);
 
-                            // 다음 빈 칸으로 재귀 호출
-                            if (solve(board)) {
-                                return true;
-                            } else {
-                                // 실패하면 백트래킹
-                                board[row][col] = '.';
-                            }
-                        }
-                    }
-                    // 1-9 중 어떤 숫자도 놓을 수 없다면 실패
-                    return false;
+        if (nextCell == null) {
+            return true;
+        }
+
+        // board를 순회하면서 빈 칸 찾기
+        // 빈 칸을 찾으면
+        // 1부터 9까지 숫자를 시도
+        for (char num = '1'; num <= '9'; num++) {
+            // 현재 위치에 num을 놓을 수 있는지 검사
+            if (isValid(board, nextCell.row, nextCell.col, num)) {
+                // 가능하다면 숫자를 놓고
+                board[nextCell.row][nextCell.col] = num;
+
+                // 다음 빈 칸으로 재귀 호출
+                if (solve(board)) {
+                    return true;
+                } else {
+                    // 실패하면 백트래킹
+                    board[nextCell.row][nextCell.col] = '.';
                 }
             }
         }
-        // 모든 칸이 채워졌다면 성공
-        return true;
+        // 1-9 중 어떤 숫자도 놓을 수 없다면 실패
+        return false;
+    }
+
+    private Cell findBestCell(char[][] board) {
+        Cell bestCell = null;
+        int minPossibilities = 10; // 9보다 큰 값으로 초기화
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    int count = countPossibleNumbers(board, row, col);
+
+                    // 가능한 숫자가 더 적은 셀을 찾았다면 업데이트
+                    if (count < minPossibilities) {
+                        minPossibilities = count;
+                        bestCell = new Cell(row, col, count);
+
+                        // 최적화: 가능한 숫자가 1개뿐인 셀을 찾았다면 즉시 반환
+                        if (count == 1) {
+                            return bestCell;
+                        }
+                    }
+                }
+            }
+        }
+
+        return bestCell;
+    }
+
+    private int countPossibleNumbers(char[][] board, int row, int col) {
+        int count = 0;
+        for (char num = '1'; num <= '9'; num++) {
+            if (isValid(board, row, col, num)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     private boolean isValid(char[][] board, int row, int col, char num) {
@@ -64,5 +98,17 @@ class Solution {
         }
 
         return true;
+    }
+}
+
+class Cell {
+    int row;
+    int col;
+    int possibleCount;
+
+    Cell(int row, int col, int possibleCount) {
+        this.row = row;
+        this.col = col;
+        this.possibleCount = possibleCount;
     }
 }
