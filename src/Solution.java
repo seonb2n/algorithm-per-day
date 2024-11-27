da52a2bc2087c2f58753f9a75d7fd3d536b29e4b
seonb2n <626ksb@gmail.com>
2024-11-27 21:40:56 +0900

    - leetcode https://leetcode.com/problems/multiply-strings
    -beats 98%
    - 두 반복문 하나로 합치기
    - LinkedList 말고 배열 사용하기

--- src/Solution.java
+++ src/Solution.java
@@ -9,43 +9,36 @@ class Solution {
 
         List<Integer> res = new LinkedList<>();
         for (int i = num2.length() - 1; i >= 0; i--) {
-            Queue<Integer> queue = new LinkedList<>();
-            for (int j = 0; j < num2.length() - i - 1; j++) {
-                queue.add(0);
-            }
+            int cursor = num2.length() - i - 1;  // queue의 시작 위치
             int now = num2.charAt(i) - '0';
-            // num1 을 1개씩 돌면서 now 랑 곱하기
             int prev = 0;
+
+            // 초기 0들 처리
+            for (int j = 0; j < cursor; j++) {
+                if (res.size() - 1 < j) {
+                    res.add(0);
+                }
+            }
+
+            // num1과 곱하면서 바로 결과에 더하기
             for (int j = num1.length() - 1; j >= 0; j--) {
-                char target =  num1.charAt(j);
-                int nowInt = (target - '0') * (now);
-                nowInt += prev;
+                int nowInt = (num1.charAt(j) - '0') * now + prev;
                 prev = nowInt / 10;
-                queue.add(nowInt % 10);
-            }
-            if (prev != 0) {
-                queue.add(prev);
-            }
-            // queue 를 하나씩 꺼내서 res 에 더한다.
-            int prevNum = 0;
-            int cursor = 0;
-            while (!queue.isEmpty()) {
-                int nowInt = queue.poll();
-                nowInt += prevNum;
+                nowInt %= 10;
+
                 if (res.size() - 1 < cursor) {
-                    prevNum = nowInt / 10;
-                    nowInt = nowInt % 10;
                     res.add(nowInt);
                 } else {
                     nowInt += res.get(cursor);
-                    prevNum = nowInt / 10;
+                    prev += nowInt / 10;
                     nowInt %= 10;
                     res.set(cursor, nowInt);
                 }
                 cursor++;
             }
-            if (prevNum != 0) {
-                res.add(prevNum);
+
+            if (prev != 0) {
+                res.add(prev);
             }
         }
 
