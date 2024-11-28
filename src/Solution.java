93686532a6c16ff9c6c33e48b1df7259c9308581
seonb2n <626ksb@gmail.com>
2024-11-28 22:48:00 +0900

    - leetcode https://leetcode.com/problems/maximal-square - 시간 초과

--- src/Solution.java
+++ src/Solution.java
@@ -1,51 +1,35 @@
-import java.util.*;
+// https://leetcode.com/problems/maximal-square
 
-// https://leetcode.com/problems/multiply-strings/
 class Solution {
-    public String multiply(String num1, String num2) {
-        if (num2.equals("0") || num1.equals("0")) {
-            return "0";
-        }
-
-        List<Integer> res = new LinkedList<>();
-        for (int i = num2.length() - 1; i >= 0; i--) {
-            int cursor = num2.length() - i - 1;  // queue의 시작 위치
-            int now = num2.charAt(i) - '0';
-            int prev = 0;
-
-            // 초기 0들 처리
-            for (int j = 0; j < cursor; j++) {
-                if (res.size() - 1 < j) {
-                    res.add(0);
+    public int maximalSquare(char[][] matrix) {
+        int res = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == '1') {
+                    int now = findMaxSquare(matrix, i, j, 2);
+                    res = Math.max(res, now);
                 }
             }
+        }
+        return res * res;
+    }
 
-            // num1과 곱하면서 바로 결과에 더하기
-            for (int j = num1.length() - 1; j >= 0; j--) {
-                int nowInt = (num1.charAt(j) - '0') * now + prev;
-                prev = nowInt / 10;
-                nowInt %= 10;
+    int findMaxSquare(char[][] matrix, int startRow, int startCol, int nowSize) {
+        // 먼저 범위 체크
+        if (startRow + nowSize > matrix.length || startCol + nowSize > matrix[0].length) {
+            return nowSize - 1;
+        }
 
-                if (res.size() - 1 < cursor) {
-                    res.add(nowInt);
-                } else {
-                    nowInt += res.get(cursor);
-                    prev += nowInt / 10;
-                    nowInt %= 10;
-                    res.set(cursor, nowInt);
+        // 정사각형 영역 검사
+        for (int i = startRow; i < startRow + nowSize; i++) {
+            for (int j = startCol; j < startCol + nowSize; j++) {
+                if (matrix[i][j] == '0') {
+                    return nowSize - 1;
                 }
-                cursor++;
-            }
-
-            if (prev != 0) {
-                res.add(prev);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = res.size() - 1; i >= 0; i--) {
-            sb.append(res.get(i));
-        }
-        return sb.toString();
+        // 다음 크기의 정사각형 검사
+        return findMaxSquare(matrix, startRow, startCol, nowSize + 1);
     }
 }
