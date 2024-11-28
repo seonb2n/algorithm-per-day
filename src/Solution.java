05bdab4b52216b5baaadd331c09fccd647d2bd63
seonb2n <626ksb@gmail.com>
2024-11-28 23:04:12 +0900

    - leetcode https://leetcode.com/problems/maximal-square - dp - beats 91.65

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,18 @@
 // https://leetcode.com/problems/maximal-square
-
+// dp?
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int res = 0;
+        int[][] dp = new int[matrix.length + 2][matrix[0].length + 2];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == '1') {
-                    int now = findMaxSquare(matrix, i, j, 2);
-                    res = Math.max(res, now);
+                    dp[i+1][j+1] = Math.min(Math.min(dp[i][j], dp[i][j + 1]), dp[i + 1][j]) + 1;
+                    res = Math.max(res, dp[i + 1][j + 1]);
                 }
             }
         }
         return res * res;
     }
 
-    int findMaxSquare(char[][] matrix, int startRow, int startCol, int nowSize) {
-        // 먼저 범위 체크
-        if (startRow + nowSize > matrix.length || startCol + nowSize > matrix[0].length) {
-            return nowSize - 1;
-        }
-
-        // 정사각형 영역 검사
-        for (int i = startRow; i < startRow + nowSize; i++) {
-            for (int j = startCol; j < startCol + nowSize; j++) {
-                if (matrix[i][j] == '0') {
-                    return nowSize - 1;
-                }
-            }
-        }
-
-        // 다음 크기의 정사각형 검사
-        return findMaxSquare(matrix, startRow, startCol, nowSize + 1);
-    }
 }
