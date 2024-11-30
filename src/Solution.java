7dd72704df78ec523f2cd10c7e7a8b621ae0d0e2
seonb2n <626ksb@gmail.com>
2024-11-30 23:56:26 +0900

    - leetcode https://leetcode.com/problems/flatten-nested-list-iterator - beats 100%

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,49 @@
-// https://leetcode.com/problems/maximal-square
-// dp?
-class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int res = 0;
-        int[][] dp = new int[matrix.length + 2][matrix[0].length + 2];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == '1') {
-                    dp[i+1][j+1] = Math.min(Math.min(dp[i][j], dp[i][j + 1]), dp[i + 1][j]) + 1;
-                    res = Math.max(res, dp[i + 1][j + 1]);
-                }
-            }
+// https://leetcode.com/problems/flatten-nested-list-iterator
+
+
+import java.util.*;
+
+public class NestedIterator implements Iterator<Integer> {
+    Queue<Integer> queue = new LinkedList<>();
+
+    // 생성자
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for (NestedInteger i : nestedList) {
+            addQueue(i);
         }
-        return res * res;
     }
 
+    void addQueue(NestedInteger nestedInteger) {
+        if (nestedInteger.isInteger()) {
+            queue.add(nestedInteger.getInteger());
+        } else {
+            for (NestedInteger nestedIntegerInList : nestedInteger.getList()) {
+                addQueue(nestedIntegerInList);
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        // hasNext()가 true를 반환한 경우에만 호출된다고 가정
+        return queue.poll();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+}
+
+interface NestedInteger {
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return empty list if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
 }
