c95edea7e90e762ad6937db2ef0325ec0c33c91b
seonb2n <626ksb@gmail.com>
2024-12-03 22:48:49 +0900

    - leetcode https://leetcode.com/problems/permutations-ii - beats 67%

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,11 @@
-// https://leetcode.com/problems/permutations
+// https://leetcode.com/problems/permutations=ii
 
 import java.util.*;
 
 
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
         dfs(nums, visited, new ArrayList<>(), res);
@@ -17,13 +18,21 @@ class Solution {
             return;
         }
 
+
+        int prev = -11;
+
         for (int i = 0; i < nums.length; i++) {
-            if (!visited[i]) {
+            if (visited[i] || prev == nums[i]) {
+                continue;
+            }
+
+            if (!visited[i] && prev != nums[i]) {
                 visited[i] = true;
                 current.add(nums[i]);
                 dfs(nums, visited, current, res);
                 current.remove(current.size() - 1);
                 visited[i] = false;
+                prev = nums[i];
             }
         }
     }
