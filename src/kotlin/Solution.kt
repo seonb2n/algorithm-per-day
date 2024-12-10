93c07eb2c08b67504b093372be881b231d5e6829
seonb2n <626ksb@gmail.com>
2024-12-10 22:41:32 +0900

    - leetcode - https://leetcode.com/problems/rotate-image/ - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,59 +1,16 @@
 package kotlin
 
-// https://leetcode.com/problems/decode-string/
-// failed tc "3[z]2[2[y]pq4[2[jk]e1[f]]]ef"
 class Solution {
-    fun decodeString(s: String): String {
-        val sb = StringBuilder()
-        var index = 0
-        while (index < s.length) {
-            if (s[index].isDigit()) {
-                index = dfs(sb, index, s)
-            }
-            else if (s[index] in 'a'..'z') {
-                sb.append(s[index])
-                index++
-            }
-        }
-
-        return sb.toString()
-    }
-
-    fun dfs(sb: StringBuilder, startIndex: Int, s: String): Int {
-        var index = startIndex
-
-        while (index < s.length) {
-            if (s[index].isDigit()) {
-                // 숫자 파싱
-                val number = StringBuilder()
-                while (index < s.length && s[index].isDigit()) {
-                    number.append(s[index])
-                    index++
-                }
-
-                // '[' 건너뛰기
-                index++ // skip '['
-
-                // 재귀적으로 내부 문자열 처리
-                val innerContent = StringBuilder()
-                index = dfs(innerContent, index, s)
-
-                index++ // skip ']'
-
-                // 반복 처리
-                val repeatCount = number.toString().toInt()
-                repeat(repeatCount) {
-                    sb.append(innerContent)
-                }
-            } else if (s[index] == ']') {
-                return index
-            } else {
-                // 일반 문자는 직접 추가
-                sb.append(s[index])
-                index++
+    fun rotate(matrix: Array<IntArray>) {
+        val n = matrix.size
+        for (i in 0 until (n + 1) / 2) {
+            for (j in 0 until n / 2) {
+                val temp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = temp
             }
         }
-
-        return index
     }
 }
