658b6151bebd1234985af0b4242c8ffce04ad22d
seonb2n <626ksb@gmail.com>
2024-12-16 20:12:28 +0900

    - leetcode - https://leetcode.com/problems/rotate-image/ - beats 18.97%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,16 +1,43 @@
 package kotlin
 
+import java.util.*
+
+// https://leetcode.com/problems/range-sum-of-bst/
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
 class Solution {
-    fun rotate(matrix: Array<IntArray>) {
-        val n = matrix.size
-        for (i in 0 until (n + 1) / 2) {
-            for (j in 0 until n / 2) {
-                val temp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = temp
+    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+        var result = 0
+        val queue = LinkedList<TreeNode>()
+        queue.offer(root)
+        // root 부터 BFS 로 탐색
+        while (queue.isNotEmpty()) {
+            val now = queue.poll()
+            // 숫자가 low 부터 high 사이면 결과에 삽입
+            if (now.`val` in low..high) {
+                result += now.`val`
+            }
+            if (now.left != null) {
+                queue.offer(now.left)
+            }
+            if (now.right != null) {
+                queue.offer(now.right)
             }
         }
+
+        return result
     }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
 }
