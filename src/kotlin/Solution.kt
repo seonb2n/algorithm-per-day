0a6af69af5e134edfbc472c90c1b1b3be9591e5d
seonb2n <626ksb@gmail.com>
2024-12-16 20:21:55 +0900

    - leetcode - https://leetcode.com/problems/rotate-image/ - beats 24.14%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -21,14 +21,18 @@ class Solution {
         // root 부터 BFS 로 탐색
         while (queue.isNotEmpty()) {
             val now = queue.poll()
+
             // 숫자가 low 부터 high 사이면 결과에 삽입
             if (now.`val` in low..high) {
                 result += now.`val`
             }
-            if (now.left != null) {
+
+            // 현재 값이 low 보다 작으면, 왼쪽 서브트리도 모두 low 보다 작아서 탐색할 필요 x
+            if (now.`val` > low && now.left != null) {
                 queue.offer(now.left)
             }
-            if (now.right != null) {
+            // 현재 값이 high 보다 크면, 오른쪽 서브트리도 모두 high 보다 커서 탐색할 필요 x
+            if (now.`val` < high && now.right != null) {
                 queue.offer(now.right)
             }
         }
