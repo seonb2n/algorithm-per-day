e50cf57cf8ab6e8f33b1a16569026e52d95c2007
seonb2n <626ksb@gmail.com>
2024-12-18 20:48:00 +0900

    - leetcode - https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree - beats 76

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,40 +4,41 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.abs
 
-// https://leetcode.com/problems/asteroid-collision/
+// https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 class Solution {
-    fun asteroidCollision(asteroids: IntArray): IntArray {
-        val stack = ArrayDeque<Int>()
-        for (asteroid in asteroids) {
-            if (asteroid > 0) {
-                stack.add(asteroid)
-                continue
-            }
+    val map : MutableMap<Int, MutableList<Node>> = HashMap()
+    fun verticalTraversal(root: TreeNode?): List<List<Int>> {
+        if (root == null) return emptyList()
+        // DFS 로 각 node 별 x,y 값을 계산한다.
+        dfs(root, 0, 0)
 
-            var broken = false
-            while (stack.isNotEmpty() && stack.last() > 0) {
-                // 그게 아니면 하나씩 꺼내서 처리
-                // 현재 꺼가 크면 stack 에 있는거 제거
-                if (stack.last() < abs(asteroid)) {
-                    stack.removeLast()
-                }
-                // 둘이 같으면 부서짐 처리
-                else if (stack.last() == abs(asteroid)) {
-                    stack.removeLast()
-                    broken = true
-                    break
-                }
-                // 스택의 내용이 크면 끝
-                else {
-                    broken = true
-                    break
-                }
-            }
-            // 왼쪽으로 가는 소행성이 stack 에 있는 소행성을 다 부쉈다면
-            if (!broken) {
-                stack.add(asteroid)
-            }
-        }
-        return stack.toIntArray()
+        // map 에서 key 값이 작은 순서대로 꺼내서, List<List<Int>> 를 만든다.
+       return map.toSortedMap()
+           .values.map { nodes -> nodes.sorted().map { it.value } }.toMutableList()
     }
+
+    fun dfs(nowNode: TreeNode, x: Int, y: Int) {
+        map.getOrPut(y) { mutableListOf() }.add(Node(x, nowNode.`val`))
+        if (nowNode.left != null) {
+            dfs(nowNode.left!!, x + 1, y - 1)
+        }
+        if (nowNode.right != null) {
+            dfs(nowNode.right!!, x + 1, y + 1)
+        }
+    }
+}
+
+data class Node(val x: Int, val value: Int): Comparable<Node> {
+    override fun compareTo(other: Node): Int {
+        // x 값이 다르면 x 기준 오름차순
+        val xCompare = x.compareTo(other.x)
+
+        // x 값이 같으면 value 기준으로 오름차순
+        return if (xCompare != 0) xCompare else value.compareTo(other.value)
+    }
+}
+
+class TreeNode(var `val`: Int) {
+         var left: TreeNode? = null
+         var right: TreeNode? = null
 }
