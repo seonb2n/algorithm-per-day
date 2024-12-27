1c4af170a4f89501f14ddfccfa3b5938eb32ad66
seonb2n <626ksb@gmail.com>
2024-12-27 22:47:04 +0900

    - leetcode - daily-question 2024-12-26 - beats 76.6

--- src/Solution.java
+++ src/Solution.java
@@ -4,21 +4,35 @@ import java.util.*;
 
 // https://leetcode.com/problems/target-sum/?envType=daily-question&envId=2024-12-26
 class Solution {
-    int count = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        // nums 로부터 dfs 로 target 탐색
-        dfs(nums, 0, 0, target);
-        return count;
-    }
-
-    public void dfs(int[] nums, int index, int nowValue, int target) {
-        if (index == nums.length) {
-            if (nowValue == target) {
-                count++;
-            }
-            return;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
-        dfs(nums, index+1, nowValue + nums[index], target);
-        dfs(nums, index+1, nowValue - nums[index], target);
+
+        // 불가능한 경우 early return
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+
+        // dp[i][j]: i번째 인덱스까지 고려했을 때 합이 j가 되는 경우의 수
+        int[][] dp = new int[nums.length][2 * sum + 1];
+
+        // 초기값 설정
+        // sum 을 중간값으로 사용
+        dp[0][sum + nums[0]] += 1;
+        dp[0][sum - nums[0]] += 1;
+
+        // dp 테이블 채우기
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = -sum; j <= sum; j++) {
+                if (dp[i-1][j + sum] > 0) {
+                    dp[i][j + sum + nums[i]] += dp[i-1][j + sum];
+                    dp[i][j + sum - nums[i]] += dp[i-1][j + sum];
+                }
+            }
+        }
+
+        return target > sum ? 0 : dp[nums.length-1][sum + target];
     }
 }
