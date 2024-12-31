92405c9e1238989f4296268f0e2af0a01472c13d
seonb2n <626ksb@gmail.com>
2024-12-31 20:58:04 +0900

    - leetcode - https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/ - beats 74.6

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,55 @@
 
 import java.util.*;
 
-// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+// https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
 class Solution {
-    public int minSteps(String s, String t) {
-        int count = 0;
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        // 각 숫자별 갯수를 셈
+        // 숫자가 적은 순서대로 제거해야 함
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        Arrays.sort(arr);
 
-        int[] alp = new int[26];
-
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            alp[(int)(c - 'a')]++;
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            char c= t.charAt(i);
-            int index = (int)(c - 'a');
-            if (alp[index] > 0) {
-                alp[index]--;
+        Node n = new Node(arr[0], 1);
+        for (int i = 1; i < arr.length; i++) {
+            int now = arr[i];
+            if (n.number == now) {
+                n.count++;
+            } else {
+                q.offer(n);
+                n = new Node(arr[i], 1);
             }
         }
+        q.offer(n);
 
-        for (int i = 0; i < 26; i++) {
-            count += alp[i];
+        int cnt = 0;
+        int rest = k;
+        while(!q.isEmpty()) {
+            Node next = q.poll();
+            if (rest == 0) {
+                cnt++;
+            }
+            else if (next.count <= rest) {
+                rest -= next.count;
+            } else {
+                rest = 0;
+                cnt++;
+            }
         }
+        return cnt;
+    }
+}
 
-        return count;
+class Node implements Comparable<Node> {
+    int number;
+    int count;
+
+    public Node(int n, int c) {
+        this.number = n;
+        this.count = c;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.count - o.count;
     }
 }
