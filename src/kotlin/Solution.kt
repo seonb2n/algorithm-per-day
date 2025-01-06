8f5ef04981d7de0dd3437d4cfae8750691d77ac3
seonb2n <626ksb@gmail.com>
2025-01-06 21:10:59 +0900

    - leetcode - https://leetcode.com/problems/jump-game-ii/submissions/1499500697/ - beats 29.7

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,37 +4,24 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.abs
 
-// https://leetcode.com/problems/shifting-letters-ii/?envType=daily-question&envId=2025-01-05
+// https://leetcode.com/problems/jump-game-ii/
 class Solution {
-    fun shiftingLetters(s: String, shifts: Array<IntArray>): String {
-        // shifts 배열을 돌면서 s 의 각 자리별로 얼마나 shift 되야하는지 연산한다.
-        // 누적합을 사용하면 시간을 단축할 수 있다.
-        val diff = IntArray(s.length + 1)
-
-        for (shift in shifts) {
-            val value = if (shift[2] == 1) 1 else -1
-            diff[shift[0]] += value
-            diff[shift[1] + 1] -= value
+    fun jump(nums: IntArray): Int {
+        // dp[n] = n 번에 도달할 수 있는 최소 점프
+        val dp = IntArray(nums.size) { Int.MAX_VALUE }
+        dp[0] = 0
+        if (nums.size == 1) {
+            return 0
         }
 
-        val sum = IntArray(s.length) { 0 }
-        sum[0] = diff[0]
-        for (i in 1 until s.length) {
-           sum[i] = sum[i - 1] + diff[i]
-        }
-
-        // shift
-        val sb = StringBuilder()
-        for (i in 0..s.length-1) {
-            var now = s[i].code - 'a'.code
-            val shift = sum[i]
-            if (shift >= 0) {
-                now = (now + shift % 26) % 26
-            } else {
-                now = (now + (shift % 26) + 26) % 26
+        for (i in nums.indices) {
+            val now = nums[i]
+            for (j in 1 ..now) {
+                if (i + j < nums.size) {
+                    dp[i+j] = Math.min(dp[i+j], dp[i] + 1)
+                }
             }
-            sb.append(('a'.code + now).toChar())
         }
-        return sb.toString()
+        return dp[nums.size - 1]
     }
 }
