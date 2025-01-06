19132220087c2c312585f40cfde51d7f1f83abeb
seonb2n <626ksb@gmail.com>
2025-01-06 21:27:54 +0900

    - leetcode - https://leetcode.com/problems/jump-game-ii/submissions/1499500697/ - greedy - beats 95.6

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -7,21 +7,23 @@ import kotlin.math.abs
 // https://leetcode.com/problems/jump-game-ii/
 class Solution {
     fun jump(nums: IntArray): Int {
-        // dp[n] = n 번에 도달할 수 있는 최소 점프
-        val dp = IntArray(nums.size) { Int.MAX_VALUE }
-        dp[0] = 0
-        if (nums.size == 1) {
-            return 0
-        }
+        if (nums.size <= 1) return 0
 
-        for (i in nums.indices) {
-            val now = nums[i]
-            for (j in 1 ..now) {
-                if (i + j < nums.size) {
-                    dp[i+j] = Math.min(dp[i+j], dp[i] + 1)
-                }
+        var curMax = 0  // 현재 점프로 갈 수 있는 최대 거리
+        var nextMax = 0 // 다음 점프로 갈 수 있는 최대 거리
+        var jumps = 0
+
+        for (i in 0 until nums.size - 1) {
+            nextMax = maxOf(nextMax, i + nums[i])
+
+            if (i == curMax) {
+                jumps++
+                curMax = nextMax
+
+                if (curMax >= nums.size - 1) break
             }
         }
-        return dp[nums.size - 1]
+
+        return jumps
     }
 }
