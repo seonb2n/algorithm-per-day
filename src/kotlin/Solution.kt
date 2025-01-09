f965691a82527376128780208e16a98b4c0cb905
seonb2n <626ksb@gmail.com>
2025-01-09 21:51:05 +0900

    - leetcode - https://leetcode.com/problems/counting-words-with-a-given-prefix/?envType=daily-question&envId=2025-01-09 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,26 +4,21 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.abs
 
-// https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/?envType=daily-question&envId=2025-01-08
+//  https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/?envType=daily-question&envId=2025-01-08i
 class Solution {
-    fun countPrefixSuffixPairs(words: Array<String>): Int {
-        var count = 0
-        for (i in 0 until words.size) {
-            val now = words[i]
-            for (j in i + 1 until words.size) {
-                if (isPrefixAndSuffix(now, words[j])) {
-                    count++
-                }
+    fun prefixCount(words: Array<String>, pref: String): Int {
+        var result = 0
+
+        for (word in words) {
+            if (word.length < pref.length) {
+                continue
+            }
+            val prefix = word.substring(0, pref.length)
+            if (prefix == pref) {
+                result++
             }
         }
-        return count
-    }
 
-    // st1 이 str2 의 prefix 이자 suffix
-    fun isPrefixAndSuffix(str1: String, str2: String): Boolean {
-        if (str1.length > str2.length) return false
-        val prefix = str2.substring(0, str1.length)
-        val suffix = str2.substring(str2.length - str1.length)
-        return str1 == prefix && str1 == suffix
+        return result
     }
 }
