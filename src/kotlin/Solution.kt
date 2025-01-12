0858290827b8f69643148a4c2fad150d8d128104
seonb2n <626ksb@gmail.com>
2025-01-12 10:41:59 +0900

    - leetcode - https://leetcode.com/problems/construct-k-palindrome-strings/submissions/1505618883/?envType=daily-question&envId=2025-01-11 - beats 80%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,44 +4,26 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.abs
 
-//https://leetcode.com/problems/word-subsets/?envType=daily-question&envId=2025-01-10
+//https://leetcode.com/problems/construct-k-palindrome-strings/?envType=daily-question&envId=2025-01-11
 class Solution {
-    fun wordSubsets(words1: Array<String>, words2: Array<String>): List<String> {
-        // words2 의 모든 str 이 words1 의 subset 인지 판별
-        val result: MutableList<String> = mutableListOf()
+    fun canConstruct(s: String, k: Int): Boolean {
+        val n = s.length
+        if (n == k) return true
+        if (n < k) return false
 
-        // words2 의 모든 문자열을 모은 통합 카운터를 만들 수 있다.
-        val maxCounter = IntArray(26)
-        for (word in words2) {
-            val counter = IntArray(26)
-            for (char in word) {
-                val now = char - 'a'
-                counter[now]++
-                if (counter[now] > maxCounter[now]) {
-                    maxCounter[now] = counter[now]
-                }
-            }
+        val count = IntArray(26)
+
+        // 문자 개수 카운트
+        for (c in s) {
+            count[c - 'a']++
         }
 
-        for (word in words1) {
-            val chars = word.toCharArray()
-            val counter = IntArray(26)
-            for (char in chars) {
-                counter[char - 'a']++
-            }
-            // words2 의 글자들이 maxCounter 에 모두 존재해야 한다.
-            var flag = true
-            for (i in 0..25) {
-                if (maxCounter[i] > counter[i]) {
-                    flag = false
-                    break
-                }
-            }
-            if (flag) {
-                result.add(word)
-            }
-
+        // 홀수 개수 문자 카운트
+        var oddCount = 0
+        for (i in 0..25) {
+            if (count[i] % 2 != 0) oddCount++
         }
-        return result
+
+        return oddCount <= k
     }
 }
