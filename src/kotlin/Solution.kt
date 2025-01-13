dad260854ca484f445c766963cae000a9e7d1807
seonb2n <626ksb@gmail.com>
2025-01-13 20:53:14 +0900

    - leetcode - https://leetcode.com/problems/minimum-length-of-string-after-operations/?envType=daily-question&envId=2025-01-13

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,28 +2,33 @@ package kotlin
 
 import java.util.*
 import kotlin.collections.ArrayDeque
+import kotlin.collections.ArrayList
 import kotlin.math.abs
 
-//https://leetcode.com/problems/construct-k-palindrome-strings/?envType=daily-question&envId=2025-01-11
+// https://leetcode.com/problems/minimum-length-of-string-after-operations/?envType=daily-question&envId=2025-01-13
 class Solution {
-    fun canConstruct(s: String, k: Int): Boolean {
-        val n = s.length
-        if (n == k) return true
-        if (n < k) return false
+    fun minimumLength(s: String): Int {
+        // s 를 기준으로 left 에 1글자, right 에 1글자가 존재해야 함
+        val map: MutableMap<Char, MutableList<Int>> = mutableMapOf()
+        val arr = s.toCharArray()
+        for (c in 0..s.length-1) {
+            val now = s[c]
+            if (!map.containsKey(now)) {
+                map[now] = mutableListOf(c)
+            } else {
+                map[now]!!.add(c)
+            }
 
-        val count = IntArray(26)
-
-        // 문자 개수 카운트
-        for (c in s) {
-            count[c - 'a']++
+            // 3개가 쌓이면 제거
+            if (map[now]!!.size == 3) {
+                val target = map[s[c]]!!
+                arr[target[0]] = '.'
+                arr[target[2]] = '.'
+                target.removeAt(0)
+                target.removeAt(1)
+            }
         }
 
-        // 홀수 개수 문자 카운트
-        var oddCount = 0
-        for (i in 0..25) {
-            if (count[i] % 2 != 0) oddCount++
-        }
-
-        return oddCount <= k
+        return arr.filter { it != '.' }.size
     }
 }
