f6a3d6de45d63d7ad11c9fb5da94a8e15f2ce691
seonb2n <626ksb@gmail.com>
2025-01-14 22:00:45 +0900

    - leetcode - https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/?envType=daily-question&envId=2025-01-14 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -8,32 +8,26 @@ import kotlin.math.abs
 // https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/?envType=daily-question&envId=2025-01-14
 class Solution {
     fun findThePrefixCommonArray(A: IntArray, B: IntArray): IntArray {
-        val res = IntArray(A.size + 1)
-        for (i in A.indices) {
-            val a = A[i]
-            res[a] = Math.max(res[a], i)
+        val n = A.size
+        val result = IntArray(n)
+        // 각 숫자가 등장한 횟수를 저장
+        val count = IntArray(n + 1)
+        var commonCount = 0
 
-            val b = B[i]
-            res[b] = Math.max(res[b], i)
-        }
-        res.sort()
-        // 각 숫자를 압축
-        val ans = IntArray(A.size)
-        var origin = 0
-        for (i in 1 until A.size + 1) {
-            val now = res[i]
-            if (origin == now) {
-                ans[now]++
-            } else {
-                // origin 부터 now 는 모두 ans[origin] 으로 채운다
-                for (j in origin+1 .. now) {
-                    ans[j] = ans[origin]
-                }
-                origin = now
-                ans[now]++
-            }
+        for (i in 0 until n) {
+            // A[i]의 등장 횟수를 증가
+            count[A[i]]++
+            // A[i]가 B에서도 이미 나왔다면 공통 요소 추가
+            if (count[A[i]] == 2) commonCount++
+
+            // B[i]의 등장 횟수를 증가
+            count[B[i]]++
+            // B[i]가 A에서도 이미 나왔다면 공통 요소 추가
+            if (count[B[i]] == 2) commonCount++
+
+            result[i] = commonCount
         }
 
-        return ans
+        return result
     }
 }
