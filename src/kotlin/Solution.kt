2f09030ebce9b59478949e273eaeda52ff772c70
seonb2n <626ksb@gmail.com>
2025-01-16 21:03:45 +0900

    - leetcode - https://leetcode.com/problems/binary-tree-level-order-traversal/description/ - beats 54.46%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,33 +4,39 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.collections.ArrayList
 import kotlin.math.abs
+import kotlin.math.floor
 
 // https://leetcode.com/problems/minimize-xor/?envType=daily-question&envId=2025-01-15
 class Solution {
-    fun minimizeXor(num1: Int, num2: Int): Int {
-        val target = Integer.bitCount(num2)
-
-        var current = 0
-        var result = 0
-
-        // current xor num1 의 최솟값을 찾아야 함
-        for (i in 31 downTo 0) {
-            val bit = (num1 and (1 shl i))
-
-            when {
-                // num1의 해당 위치가 1이고, 아직 1을 더 넣을 수 있으면
-                bit != 0 && current < target -> {
-                    result = result or (1 shl i)
-                    current++
-                }
-                // 남은 1을 채워야 하는 경우
-                current < target && i < (target - current) -> {
-                    result = result or (1 shl i)
-                    current++
-                }
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        val queue = LinkedList<Node>()
+        if (root == null) {
+            return emptyList()
+        }
+        queue.offer(Node(root, 1))
+        val result = ArrayList<ArrayList<Int>>()
+        while (!queue.isEmpty()) {
+            val now = queue.poll()
+            if (result.size < now.floor) {
+                result.add(ArrayList())
+            }
+            result.get(now.floor-1).add(now.treeNode.`val`)
+            if (now.treeNode.left != null) {
+                queue.offer(Node(now.treeNode.left!!, now.floor + 1))
+            }
+            if (now.treeNode.right != null) {
+                queue.offer(Node(now.treeNode.right!!, now.floor + 1))
             }
         }
-
         return result
     }
+}
+
+class Node(val treeNode: TreeNode, val floor: Int)
+
+class TreeNode(var `val`: Int) {
+
+    var left: TreeNode? = null
+
+    var right: TreeNode? = null
 }
