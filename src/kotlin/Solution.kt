64e1b02dd3c160592af0e3897156aa057b67a902
seonb2n <626ksb@gmail.com>
2025-01-18 21:47:55 +0900

    - leetcode - https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/?envType=daily-question&envId=2025-01-18 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,42 +1,52 @@
 package kotlin
 
-import java.util.*
-import kotlin.collections.ArrayDeque
-import kotlin.collections.ArrayList
-import kotlin.math.abs
-import kotlin.math.floor
-
-// https://leetcode.com/problems/minimize-xor/?envType=daily-question&envId=2025-01-15
+// https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/?envType=daily-question&envId=2025-01-18
 class Solution {
-    fun levelOrder(root: TreeNode?): List<List<Int>> {
-        val queue = LinkedList<Node>()
-        if (root == null) {
-            return emptyList()
+    fun minCost(grid: Array<IntArray>): Int {
+        val m = grid.size
+        val n = grid[0].size
+        // 해당 셀에 도달하기 위한 최소 비용
+        val dp = Array(m) { Array(n) { Int.MAX_VALUE } }
+
+        fun dfs(i: Int, j: Int, cost: Int) {
+            // 범위를 벗어나거나 현재 비용이 더 큰 경우 중단
+            if (i !in 0 until m
+                || j !in 0 until n
+                || cost >= dp[i][j]) return
+
+
+            dp[i][j] = cost
+
+            // 현재 셀의 방향에 따른 다음 위치 계산
+            // 1. 현재 방향 대로 이동
+            when (grid[i][j]) {
+                1 -> dfs(i, j + 1, cost)     // 오른쪽
+                2 -> dfs(i, j - 1, cost)     // 왼쪽
+                3 -> dfs(i + 1, j, cost)     // 아래
+                4 -> dfs(i - 1, j, cost)     // 위
+            }
+            // 2. 아래로 이동 (비용 1)
+            if (i + 1 < m && grid[i][j] != 3) {
+                dfs(i + 1, j, cost + 1)
+            }
+
+            // 3. 오른쪽으로 이동 (비용 1)
+            if (j + 1 < n && grid[i][j] != 1) {
+                dfs(i, j + 1, cost + 1)
+            }
+
+            // 4. 위로 이동 (비용 1)
+            if (i - 1 in 0 until m && grid[i][j] != 4) {
+                dfs(i-1, j, cost + 1)
+            }
+
+            // 5. 왼쪽으로 이동 (비용 1)
+            if (j - 1 in 0 until n && grid[i][j] != 2) {
+                dfs(i, j - 1, cost + 1)
+            }
         }
-        queue.offer(Node(root, 1))
-        val result = ArrayList<ArrayList<Int>>()
-        while (!queue.isEmpty()) {
-            val now = queue.poll()
-            if (result.size < now.floor) {
-                result.add(ArrayList())
-            }
-            result.get(now.floor-1).add(now.treeNode.`val`)
-            if (now.treeNode.left != null) {
-                queue.offer(Node(now.treeNode.left!!, now.floor + 1))
-            }
-            if (now.treeNode.right != null) {
-                queue.offer(Node(now.treeNode.right!!, now.floor + 1))
-            }
-        }
-        return result
+        dfs(0, 0, 0)
+
+        return dp[m-1][n-1]
     }
-}
-
-class Node(val treeNode: TreeNode, val floor: Int)
-
-class TreeNode(var `val`: Int) {
-
-    var left: TreeNode? = null
-
-    var right: TreeNode? = null
 }
