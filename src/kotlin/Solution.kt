3966062b20204239767a9b74b55c9eb216161c28
seonb2n <626ksb@gmail.com>
2025-01-19 11:45:35 +0900

    - leetcode - https://leetcode.com/problems/trapping-rain-water-ii/submissions/1513097362/?envType=daily-question&envId=2025-01-19 - beats 90%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,52 +1,69 @@
 package kotlin
 
-// https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/?envType=daily-question&envId=2025-01-18
+import java.util.PriorityQueue
+import kotlin.math.max
+
+// https://leetcode.com/problems/trapping-rain-water-ii/?envType=daily-question&envId=2025-01-19
 class Solution {
-    fun minCost(grid: Array<IntArray>): Int {
-        val m = grid.size
-        val n = grid[0].size
-        // 해당 셀에 도달하기 위한 최소 비용
-        val dp = Array(m) { Array(n) { Int.MAX_VALUE } }
+    fun trapRainWater(heightMap: Array<IntArray>): Int {
+        var result = 0
 
-        fun dfs(i: Int, j: Int, cost: Int) {
-            // 범위를 벗어나거나 현재 비용이 더 큰 경우 중단
-            if (i !in 0 until m
-                || j !in 0 until n
-                || cost >= dp[i][j]) return
+        val n = heightMap.size
+        val m = heightMap[0].size
+        val isChecked = Array(n) { BooleanArray(m) }
+
+        // 높이가 낮은 셀부터 연산
+        val pq = PriorityQueue<Cell>(compareBy { it.height })
+
+        // 모서리셀을 추가
+        for (i in 0 until n) {
+            pq.offer(Cell(i, 0, heightMap[i][0]))
+            pq.offer(Cell(i, m-1, heightMap[i][m-1]))
+            isChecked[i][0] = true
+            isChecked[i][m-1] = true
+        }
+        for (j in 1 until m-1) {
+            pq.offer(Cell(0, j, heightMap[0][j]))
+            pq.offer(Cell(n-1, j, heightMap[n-1][j]))
+            isChecked[0][j] = true
+            isChecked[n-1][j] = true
+        }
 
 
-            dp[i][j] = cost
+        fun inRange(i: Int, j: Int): Boolean {
+            return i in 0 until n && j in 0 until m
+        }
 
-            // 현재 셀의 방향에 따른 다음 위치 계산
-            // 1. 현재 방향 대로 이동
-            when (grid[i][j]) {
-                1 -> dfs(i, j + 1, cost)     // 오른쪽
-                2 -> dfs(i, j - 1, cost)     // 왼쪽
-                3 -> dfs(i + 1, j, cost)     // 아래
-                4 -> dfs(i - 1, j, cost)     // 위
-            }
-            // 2. 아래로 이동 (비용 1)
-            if (i + 1 < m && grid[i][j] != 3) {
-                dfs(i + 1, j, cost + 1)
-            }
+        // 이동할 수 있는 4방향
+        val dirs = arrayOf(intArrayOf(-1,0), intArrayOf(1,0), intArrayOf(0,-1), intArrayOf(0,1))
+        // 현재까지 탐색된 셀에서 가장 높은 담장
+        var maxHeight = 0
 
-            // 3. 오른쪽으로 이동 (비용 1)
-            if (j + 1 < n && grid[i][j] != 1) {
-                dfs(i, j + 1, cost + 1)
-            }
+        // bfs 로 각 셀마다 순회하면서 물이 채워질 수 있는 양을 계산한다.
+        while (pq.isNotEmpty()) {
+            val cell = pq.poll()
+            maxHeight = maxOf(maxHeight, cell.height)
 
-            // 4. 위로 이동 (비용 1)
-            if (i - 1 in 0 until m && grid[i][j] != 4) {
-                dfs(i-1, j, cost + 1)
-            }
+            // 4 방향 탐색
+            for (dir in dirs) {
+                val newRow = cell.row + dir[0]
+                val newCol = cell.col + dir[1]
 
-            // 5. 왼쪽으로 이동 (비용 1)
-            if (j - 1 in 0 until n && grid[i][j] != 2) {
-                dfs(i, j - 1, cost + 1)
+                if (inRange(newRow, newCol) && !isChecked[newRow][newCol]) {
+                    isChecked[newRow][newCol] = true
+
+                    // 이번에 탐색된 cell 이 주변 담장보다 작다면, 물이 채워질 수 있다.
+                    if (heightMap[newRow][newCol] < maxHeight) {
+                        result += (maxHeight - heightMap[newRow][newCol])
+                    }
+
+                    pq.offer(Cell(newRow, newCol, heightMap[newRow][newCol]))
+                }
             }
         }
-        dfs(0, 0, 0)
 
-        return dp[m-1][n-1]
+        return result
     }
+
+    data class Cell(val row: Int, val col: Int, val height: Int)
 }
