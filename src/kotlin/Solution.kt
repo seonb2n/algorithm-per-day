7afe4e83081be5712fed0c52947492b66e1a3929
seonb2n <626ksb@gmail.com>
2025-01-21 22:59:52 +0900

    - leetcode - https://leetcode.com/problems/grid-game/submissions/1515778565/?envType=daily-question&envId=2025-01-21 - beats 66.67%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,67 +3,30 @@ package kotlin
 import java.util.PriorityQueue
 import kotlin.math.max
 
-// https://leetcode.com/problems/trapping-rain-water-ii/?envType=daily-question&envId=2025-01-19
+// https://leetcode.com/problems/grid-game/?envType=daily-question&envId=2025-01-21
 class Solution {
-    fun trapRainWater(heightMap: Array<IntArray>): Int {
-        var result = 0
+    fun gridGame(grid: Array<IntArray>): Long {
+        val n = grid[0].size
+        // i 번째까지의 합
+        val topSum = LongArray(n + 1)
+        val bottomSum = LongArray(n + 1)
 
-        val n = heightMap.size
-        val m = heightMap[0].size
-        val isChecked = Array(n) { BooleanArray(m) }
-
-        // 높이가 낮은 셀부터 연산
-        val pq = PriorityQueue<Cell>(compareBy { it.height })
-
-        // 모서리셀을 추가
+        // 1번 블록부터 시작
         for (i in 0 until n) {
-            pq.offer(Cell(i, 0, heightMap[i][0]))
-            pq.offer(Cell(i, m-1, heightMap[i][m-1]))
-            isChecked[i][0] = true
-            isChecked[i][m-1] = true
-        }
-        for (j in 1 until m-1) {
-            pq.offer(Cell(0, j, heightMap[0][j]))
-            pq.offer(Cell(n-1, j, heightMap[n-1][j]))
-            isChecked[0][j] = true
-            isChecked[n-1][j] = true
+            topSum[i+1] = topSum[i] + grid[0][i].toLong()
+            bottomSum[i+1] = bottomSum[i] + grid[1][i].toLong()
         }
 
+        // first robot 의 이동경로 = 위의 이동 + 아래 이동
+        // second robot 의 이동 경로 = first robot 이 내려간 지점 이후의 상단 남은 값, 내려간 지점 이전의 하단 값 중 최대
+        var result = Long.MAX_VALUE
 
-        fun inRange(i: Int, j: Int): Boolean {
-            return i in 0 until n && j in 0 until m
-        }
-
-        // 이동할 수 있는 4방향
-        val dirs = arrayOf(intArrayOf(-1,0), intArrayOf(1,0), intArrayOf(0,-1), intArrayOf(0,1))
-        // 현재까지 탐색된 셀에서 가장 높은 담장
-        var maxHeight = 0
-
-        // bfs 로 각 셀마다 순회하면서 물이 채워질 수 있는 양을 계산한다.
-        while (pq.isNotEmpty()) {
-            val cell = pq.poll()
-            maxHeight = maxOf(maxHeight, cell.height)
-
-            // 4 방향 탐색
-            for (dir in dirs) {
-                val newRow = cell.row + dir[0]
-                val newCol = cell.col + dir[1]
-
-                if (inRange(newRow, newCol) && !isChecked[newRow][newCol]) {
-                    isChecked[newRow][newCol] = true
-
-                    // 이번에 탐색된 cell 이 주변 담장보다 작다면, 물이 채워질 수 있다.
-                    if (heightMap[newRow][newCol] < maxHeight) {
-                        result += (maxHeight - heightMap[newRow][newCol])
-                    }
-
-                    pq.offer(Cell(newRow, newCol, heightMap[newRow][newCol]))
-                }
-            }
+        for (i in 1 until n+1) {
+            // i 번째 위치에서 first robot 이 내려갔다면
+            val secondRobot = (topSum[n] - topSum[i]).coerceAtLeast(bottomSum[i - 1])
+            result = minOf(result, secondRobot)
         }
 
         return result
     }
-
-    data class Cell(val row: Int, val col: Int, val height: Int)
 }
