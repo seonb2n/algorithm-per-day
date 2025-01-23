c3c71da127bccc8cd6ba872c14f2ab4e74632b45
seonb2n <626ksb@gmail.com>
2025-01-23 20:03:47 +0900

    - leetcode - https://leetcode.com/problems/count-servers-that-communicate/?envType=daily-question&envId=2025-01-23 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,30 +3,40 @@ package kotlin
 import java.util.PriorityQueue
 import kotlin.math.max
 
-// https://leetcode.com/problems/grid-game/?envType=daily-question&envId=2025-01-21
+// https://leetcode.com/problems/count-servers-that-communicate/?envType=daily-question&envId=2025-01-23
 class Solution {
-    fun gridGame(grid: Array<IntArray>): Long {
-        val n = grid[0].size
-        // i 번째까지의 합
-        val topSum = LongArray(n + 1)
-        val bottomSum = LongArray(n + 1)
+    fun countServers(grid: Array<IntArray>): Int {
+        // line 과 column 에 컴퓨터가 2개 이상인지 체크한다.
+        val x = IntArray(grid.size) { 0 }
+        val y = IntArray(grid.size) { 0 }
 
-        // 1번 블록부터 시작
-        for (i in 0 until n) {
-            topSum[i+1] = topSum[i] + grid[0][i].toLong()
-            bottomSum[i+1] = bottomSum[i] + grid[1][i].toLong()
+        val nodeList = mutableListOf<Node>()
+
+        for (i in grid.indices) {
+            var counter = 0
+            for (j in grid[0].indices) {
+                if (grid[i][j] == 1) {
+                    nodeList.add(Node(i, j))
+                    counter++
+                    x[i]++
+                    y[j]++
+                }
+            }
         }
 
-        // first robot 의 이동경로 = 위의 이동 + 아래 이동
-        // second robot 의 이동 경로 = first robot 이 내려간 지점 이후의 상단 남은 값, 내려간 지점 이전의 하단 값 중 최대
-        var result = Long.MAX_VALUE
-
-        for (i in 1 until n+1) {
-            // i 번째 위치에서 first robot 이 내려갔다면
-            val secondRobot = (topSum[n] - topSum[i]).coerceAtLeast(bottomSum[i - 1])
-            result = minOf(result, secondRobot)
+        // computer 의 위치를 저장하고, 라인과 컬럼을 확인한다.
+        var result = 0
+        for (node in nodeList) {
+            if (x[node.x] >= 2 || y[node.y] >= 2) {
+                result++
+            }
         }
 
         return result
     }
 }
+
+data class Node(
+    val x: Int,
+    val y: Int
+)
