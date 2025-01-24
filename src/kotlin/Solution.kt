bd7c029098a30bbad44b7a17495858b709574ea1
seonb2n <626ksb@gmail.com>
2025-01-24 23:54:40 +0900

    - leetcode - https://leetcode.com/problems/find-eventual-safe-states/?envType=daily-question&envId=2025-01-24 - beats 78%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,40 +3,28 @@ package kotlin
 import java.util.PriorityQueue
 import kotlin.math.max
 
-// https://leetcode.com/problems/count-servers-that-communicate/?envType=daily-question&envId=2025-01-23
+// https://leetcode.com/problems/count-servers-that-communicate/?envType=daily-question&envId=2025-01-2
 class Solution {
-    fun countServers(grid: Array<IntArray>): Int {
-        // line 과 column 에 컴퓨터가 2개 이상인지 체크한다.
-        val x = IntArray(grid.size) { 0 }
-        val y = IntArray(grid.size) { 0 }
+    fun eventualSafeNodes(graph: Array<IntArray>): List<Int> {
+        val visited = BooleanArray(graph.size)
+        val inPath = BooleanArray(graph.size)
+        val safe = BooleanArray(graph.size)
 
-        val nodeList = mutableListOf<Node>()
+        fun dfs(node: Int): Boolean {
+            if (visited[node]) return safe[node]
+            visited[node] = true
+            inPath[node] = true
 
-        for (i in grid.indices) {
-            var counter = 0
-            for (j in grid[0].indices) {
-                if (grid[i][j] == 1) {
-                    nodeList.add(Node(i, j))
-                    counter++
-                    x[i]++
-                    y[j]++
+            for (next in graph[node]) {
+                if (inPath[next] || !dfs(next)) {
+                    return false
                 }
             }
+            inPath[node] = false
+            safe[node] = true
+            return true
         }
 
-        // computer 의 위치를 저장하고, 라인과 컬럼을 확인한다.
-        var result = 0
-        for (node in nodeList) {
-            if (x[node.x] >= 2 || y[node.y] >= 2) {
-                result++
-            }
-        }
-
-        return result
+        return graph.indices.filter { dfs(it) }
     }
 }
-
-data class Node(
-    val x: Int,
-    val y: Int
-)
