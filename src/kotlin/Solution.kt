17e0ef91a476ce8ac3562d5a47ed043c11248324
seonb2n <626ksb@gmail.com>
2025-01-26 11:00:17 +0900

    - leetcode - https://leetcode.com/problems/find-eventual-safe-states/?envType=daily-question&envId=2025-01-26 - dfs 시간복잡도 실패

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,28 +3,39 @@ package kotlin
 import java.util.PriorityQueue
 import kotlin.math.max
 
-// https://leetcode.com/problems/count-servers-that-communicate/?envType=daily-question&envId=2025-01-2
+// https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/?envType=daily-question&envId=2025-01-26
 class Solution {
-    fun eventualSafeNodes(graph: Array<IntArray>): List<Int> {
-        val visited = BooleanArray(graph.size)
-        val inPath = BooleanArray(graph.size)
-        val safe = BooleanArray(graph.size)
 
-        fun dfs(node: Int): Boolean {
-            if (visited[node]) return safe[node]
-            visited[node] = true
-            inPath[node] = true
 
-            for (next in graph[node]) {
-                if (inPath[next] || !dfs(next)) {
-                    return false
+    fun maximumInvitations(favorite: IntArray): Int {
+        var max = 0
+        val n = favorite.size
+        val visited = BooleanArray(n)
+
+        // dfs
+        fun dfs(list: MutableList<Int>) {
+            if (list.size > 1 && isValid(list, favorite)) {
+                max = maxOf(max, list.size)
+            }
+
+            // 다음 직원 탐색
+            for (j in 0 until n) {
+                if (!visited[j]) {
+                    visited[j] = true
+                    list.add(j)
+                    dfs(list)
+                    list.removeAt(list.size - 1)
+                    visited[j] = false
                 }
             }
-            inPath[node] = false
-            safe[node] = true
-            return true
         }
 
-        return graph.indices.filter { dfs(it) }
+        for (i in 0 until n) {
+            visited[i] = true
+            dfs(mutableListOf(i))
+            visited[i] = false
+        }
+
+        return max
     }
 }
