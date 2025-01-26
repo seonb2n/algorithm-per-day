afd230c4787ac96842c8f7ade70ad58b36e10cc8
seonb2n <626ksb@gmail.com>
2025-01-26 11:34:31 +0900

    - leetcode - https://leetcode.com/problems/find-eventual-safe-states/?envType=daily-question&envId=2025-01-26 - bfs

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,37 +5,61 @@ import kotlin.math.max
 
 // https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/?envType=daily-question&envId=2025-01-26
 class Solution {
+    private fun bfs(start: Int, visited: MutableSet<Int>, graph: List<List<Int>>): Int {
+        val queue = ArrayDeque<Pair<Int, Int>>()
+        queue.add(start to 0)
+        var maxDepth = 0
 
+        while(queue.isNotEmpty()) {
+            val (node, depth) = queue.removeFirst()
+            for(next in graph[node]) {
+                if(next in visited) continue
+                visited.add(next)
+                queue.add(next to depth + 1)
+                maxDepth = maxOf(maxDepth, depth + 1)
+            }
+        }
+        return maxDepth
+    }
 
     fun maximumInvitations(favorite: IntArray): Int {
-        var max = 0
         val n = favorite.size
+        val reverseGraph = List(n) { mutableListOf<Int>() }
+        favorite.forEachIndexed { person, fav ->
+            reverseGraph[fav].add(person)
+        }
+
+        var maxCycle = 0
+        var twoWayCycleSum = 0
         val visited = BooleanArray(n)
 
-        // dfs
-        fun dfs(list: MutableList<Int>) {
-            if (list.size > 1 && isValid(list, favorite)) {
-                max = maxOf(max, list.size)
-            }
+        for(start in 0 until n) {
+            if(visited[start]) continue
 
-            // 다음 직원 탐색
-            for (j in 0 until n) {
-                if (!visited[j]) {
-                    visited[j] = true
-                    list.add(j)
-                    dfs(list)
-                    list.removeAt(list.size - 1)
-                    visited[j] = false
+            val cycleNodes = mutableMapOf<Int, Int>()
+            var curr = start
+            var depth = 0
+
+            while(!visited[curr]) {
+                visited[curr] = true
+                cycleNodes[curr] = depth++
+                val next = favorite[curr]
+
+                if(next in cycleNodes) {
+                    val cycleLen = depth - cycleNodes[next]!!
+                    if(cycleLen == 2) {
+                        val visitedSet = hashSetOf(curr, next)
+                        twoWayCycleSum += 2 + bfs(next, visitedSet, reverseGraph) +
+                                bfs(curr, visitedSet, reverseGraph)
+                    } else {
+                        maxCycle = maxOf(maxCycle, cycleLen)
+                    }
+                    break
                 }
+                curr = next
             }
         }
 
-        for (i in 0 until n) {
-            visited[i] = true
-            dfs(mutableListOf(i))
-            visited[i] = false
-        }
-
-        return max
+        return maxOf(maxCycle, twoWayCycleSum)
     }
 }
