5365aadbe22722ff6e103f4f552f1b5d756bb8d8
seonb2n <626ksb@gmail.com>
2025-02-02 11:00:02 +0900

    - leetcode - https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/?envType=daily-question&envId=2025-02-02 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,30 +1,20 @@
 package kotlin
 
-// https://leetcode.com/problems/special-array-i/?envType=daily-question&envId=2025-02-01
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/?envType=daily-question&envId=2025-02-02
 class Solution {
-    fun isArraySpecial(nums: IntArray): Boolean {
-        // nums 를 순회하면서 짝수, 홀수가 반복
+    fun check(nums: IntArray): Boolean {
+        var rotations = 0
 
-        var isOdd = nums[0] % 2 != 0
+        for (i in nums.indices) {
+            if (nums[i] > nums[(i + 1) % nums.size]) {
+                rotations++
+            }
 
-        for (i in 1 until nums.size) {
-            val now = nums[i]
-            // 현재 숫자는 짝수가 되어야 함
-            if (isOdd) {
-                if (now % 2 == 0) {
-                    isOdd = false
-                } else {
-                    return false
-                }
-                // 현재 숫자는 홀수가 되어야 함
-            } else {
-                if (now % 2 != 0) {
-                    isOdd = true
-                } else {
-                    return false
-                }
+            if (rotations > 1) {
+                return false
             }
         }
+
         return true
     }
 }
