deb7cf8db55e47fea8de8eea41f7b75fafb325a3
seonb2n <626ksb@gmail.com>
2025-02-06 21:25:09 +0900

    - leetcode - https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/?envType=daily-question&envId=2025-02-05 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,41 +1,34 @@
 package kotlin
 
-// https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/?envType=daily-question&envId=2025-02-05
+// https://leetcode.com/problems/tuple-with-same-product/description/?envType=daily-question&envId=2025-02-06
 class Solution {
-    fun areAlmostEqual(s1: String, s2: String): Boolean {
-        if (s1.equals(s2)) {
-            return true
+    fun tupleSameProduct(nums: IntArray): Int {
+        // 임의의 두 수의 곱을 저장
+        val multipleMap = HashMap<Int, MutableList<Pair<Int, Int>>>()
+
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                multipleMap.computeIfAbsent(nums[i] * nums[j]) { mutableListOf() }.add(Pair(i, j))
+            }
         }
 
-        var firstDifferent = -1
-        var secondDifferent = -1
+        // 계산
+        var count = 0
+        for (i in multipleMap.keys) {
+            val pairs = multipleMap[i]
 
-        var isSwap = false
+            for (j in pairs!!.indices) {
+                for (k in j + 1 until pairs.size) {
+                    val (a, b) = pairs[j]
+                    val (c, d) = pairs[k]
 
-        for (i in 0 until s1.length) {
-            // 아직 swap 을 안했다면 값을 업데이트
-            if (s1[i] != s2[i]) {
-                if (!isSwap) {
-                    if (firstDifferent == -1) {
-                        firstDifferent = i
-                    } else {
-                        secondDifferent = i
+                    if (a != c && a != d && b != c && b != d) {
+                        count++
                     }
-                // 이미 swap 을 했는데 또 다른 값이 나오면
-                } else {
-                    return false
-                }
-            }
-            // swap 을 안하고 값이 다르면 변경
-            if (!isSwap && firstDifferent != -1 && secondDifferent != -1) {
-                if (s1[firstDifferent] == s2[secondDifferent] && s1[secondDifferent] == s2[firstDifferent]) {
-                    isSwap = true
-                    // swap 해서 같아지지 않는다면
-                } else {
-                    return false
                 }
             }
         }
-        return isSwap
+
+        return count * 8
     }
 }
