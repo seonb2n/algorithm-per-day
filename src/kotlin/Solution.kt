c9aa4aed3ab799826a705c83c7cd2b2d31f2c267
seonb2n <626ksb@gmail.com>
2025-02-07 23:45:13 +0900

    - leetcode - https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls/?envType=daily-question&envId=2025-02-07 - beats 67%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,34 +1,37 @@
 package kotlin
 
-// https://leetcode.com/problems/tuple-with-same-product/description/?envType=daily-question&envId=2025-02-06
+// https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls/?envType=daily-question&envId=2025-02-07
 class Solution {
-    fun tupleSameProduct(nums: IntArray): Int {
-        // 임의의 두 수의 곱을 저장
-        val multipleMap = HashMap<Int, MutableList<Pair<Int, Int>>>()
+    fun queryResults(limit: Int, queries: Array<IntArray>): IntArray {
+        val colorCounter = mutableMapOf<Int, Int>() // 각 색상의 등장 횟수
+        val ballColors = mutableMapOf<Int, Int>() // 각 공의 현재 색상
+        val result = IntArray(queries.size)
 
-        for (i in nums.indices) {
-            for (j in i + 1 until nums.size) {
-                multipleMap.computeIfAbsent(nums[i] * nums[j]) { mutableListOf() }.add(Pair(i, j))
-            }
-        }
+        for (i in queries.indices) {
+            val ball = queries[i][0]
+            val newColor = queries[i][1]
 
-        // 계산
-        var count = 0
-        for (i in multipleMap.keys) {
-            val pairs = multipleMap[i]
-
-            for (j in pairs!!.indices) {
-                for (k in j + 1 until pairs.size) {
-                    val (a, b) = pairs[j]
-                    val (c, d) = pairs[k]
-
-                    if (a != c && a != d && b != c && b != d) {
-                        count++
-                    }
+            val currentColor = ballColors[ball]
+            if (currentColor != null && currentColor != newColor) {
+                // 기존 색상 카운트 감소
+                val count = colorCounter[currentColor]!! - 1
+                if (count == 0) {
+                    colorCounter.remove(currentColor)
+                } else {
+                    colorCounter[currentColor] = count
                 }
+                // 새 색상 추가
+                colorCounter[newColor] = colorCounter.getOrDefault(newColor, 0) + 1
+                ballColors[ball] = newColor
+            } else if (currentColor == null) {
+                // 처음 칠하는 경우
+                colorCounter[newColor] = colorCounter.getOrDefault(newColor, 0) + 1
+                ballColors[ball] = newColor
             }
-        }
+            // currentColor == newColor인 경우는 아무 작업도 필요 없음
 
-        return count * 8
+            result[i] = colorCounter.size
+        }
+        return result
     }
 }
