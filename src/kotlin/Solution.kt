27f345a7fdfa9c36a71745164dde013178f5d05b
seonb2n <626ksb@gmail.com>
2025-02-10 23:19:33 +0900

    - leetcode - https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-10 - beats 95%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,16 +5,16 @@ import java.util.*
 // https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-10
 class Solution {
     fun clearDigits(s: String): String {
-        val charStack = Stack<Char>()
+        val sb = StringBuilder()
         for (c in s.toCharArray()) {
             if (c in '0'..'9') {
-                charStack.pop()
+                sb.deleteAt(sb.length - 1)
             }
             else {
-                charStack.add(c)
+                sb.append(c)
             }
         }
 
-        return charStack.joinToString("")
+        return sb.toString()
     }
 }
