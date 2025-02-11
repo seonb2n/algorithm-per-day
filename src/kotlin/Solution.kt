d80b46aac58655964cabee208bd0bbada2525a4b
seonb2n <626ksb@gmail.com>
2025-02-11 22:44:30 +0900

    - leetcode - https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-11 - beats 33%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,19 +2,16 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-10
+// https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-11
 class Solution {
-    fun clearDigits(s: String): String {
+    fun removeOccurrences(s: String, part: String): String {
         val sb = StringBuilder()
-        for (c in s.toCharArray()) {
-            if (c in '0'..'9') {
-                sb.deleteAt(sb.length - 1)
-            }
-            else {
-                sb.append(c)
+        for (c in s) {
+            sb.append(c)
+            if (sb.contains(part)) {
+                sb.delete(sb.length - part.length, sb.length)
             }
         }
-
         return sb.toString()
     }
 }
