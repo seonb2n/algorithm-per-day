3921c7569acbc1c83f1af8ab86da83c15b7d3207
seonb2n <626ksb@gmail.com>
2025-02-11 22:49:13 +0900

    - leetcode - https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-11 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,13 +5,15 @@ import java.util.*
 // https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-11
 class Solution {
     fun removeOccurrences(s: String, part: String): String {
-        val sb = StringBuilder()
-        for (c in s) {
-            sb.append(c)
-            if (sb.contains(part)) {
-                sb.delete(sb.length - part.length, sb.length)
-            }
+        val sb = StringBuilder(s)
+        val partLength = part.length
+
+        var index = sb.indexOf(part)
+        while (index != -1) {
+            sb.delete(index, index + partLength)
+            index = sb.indexOf(part)
         }
+
         return sb.toString()
     }
 }
