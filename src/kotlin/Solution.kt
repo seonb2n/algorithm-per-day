44ef4500b5503b4773b2dc892937c50fc5c50d63
seonb2n <626ksb@gmail.com>
2025-02-12 23:02:31 +0900

    - leetcode - https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/?envType=daily-question&envId=2025-02-12 - beats 54.5%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,18 +2,39 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/clear-digits/?envType=daily-question&envId=2025-02-11
+// https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/?envType=daily-question&envId=2025-02-12
 class Solution {
-    fun removeOccurrences(s: String, part: String): String {
-        val sb = StringBuilder(s)
-        val partLength = part.length
-
-        var index = sb.indexOf(part)
-        while (index != -1) {
-            sb.delete(index, index + partLength)
-            index = sb.indexOf(part)
+    fun maximumSum(nums: IntArray): Int {
+        fun sumDigit(digit: Int): Int {
+            val s = digit.toString()
+            var result = 0
+            for (c in s) {
+                result += c - '0'
+            }
+            return result
         }
 
-        return sb.toString()
+        val maxMap = mutableMapOf<Int, Pair<Int, Int>>()
+        for (d in nums) {
+            val now = sumDigit(d)
+            if (maxMap.containsKey(now)) {
+                val (a, b) = maxMap[now]!!
+                when {
+                    d > a -> maxMap[now] = Pair(d, a)
+                    d > b -> maxMap[now] = Pair(a, d)
+                }
+            } else {
+                maxMap[now] = Pair(d, 0)
+            }
+        }
+
+        var res = -1
+        for (key in maxMap.keys) {
+            val (a, b) = maxMap[key]!!
+            if (a != 0 && b != 0) {
+                res = maxOf(res, a + b)
+            }
+        }
+        return res
     }
 }
