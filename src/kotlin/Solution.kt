58c868e88916ad4236f661e44d012238738ea57e
seonb2n <626ksb@gmail.com>
2025-02-20 19:08:03 +0900

    - leetcode - https://leetcode.com/problems/find-unique-binary-string/?envType=daily-question&envId=2025-02-20 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -6,17 +6,15 @@ import kotlin.collections.ArrayList
 // https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2025-02-20
 class Solution {
     fun findDifferentBinaryString(nums: Array<String>): String {
-        val sorted = nums.sorted()
-        var min = 0
-        for (num in sorted) {
-            val now = num.toInt(2)
-            if (min == now) {
-                min++
-            }
-            else if (min < now) {
-                return min.toString(2).padStart(nums.size, '0')
+        // 칸토어의 대각선 논법 활용
+        val result = StringBuilder()
+        for (i in nums.indices) {
+            if (nums[i][i] == '0') {
+                result.append('1')
+            } else {
+                result.append('0')
             }
         }
-        return min.toString(2).padStart(nums.size, '0')
+        return result.toString()
     }
 }
