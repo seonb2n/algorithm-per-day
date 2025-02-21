522d3ec0d028bc6d8d6dd35d80efd5c60c123b08
seonb2n <626ksb@gmail.com>
2025-02-21 22:17:47 +0900

    - leetcode - https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/?envType=daily-question&envId=2025-02-21 - beats 80%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,18 +3,35 @@ package kotlin
 import java.util.*
 import kotlin.collections.ArrayList
 
-// https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2025-02-20
-class Solution {
-    fun findDifferentBinaryString(nums: Array<String>): String {
-        // 칸토어의 대각선 논법 활용
-        val result = StringBuilder()
-        for (i in nums.indices) {
-            if (nums[i][i] == '0') {
-                result.append('1')
-            } else {
-                result.append('0')
-            }
+// https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/?envType=daily-question&envId=2025-02-21
+class FindElements(root: TreeNode?) {
+    private val foundSet = HashSet<Int>()
+    init {
+        if (root != null) {
+            root.`val` = 0
+            clean(root)
         }
-        return result.toString()
     }
+
+    private fun clean(now: TreeNode) {
+        foundSet.add(now.`val`)
+
+        if (now.left != null) {
+            now.left!!.`val` = now.`val` * 2 + 1
+            clean(now.left!!)
+        }
+        if (now.right != null) {
+            now.right!!.`val` = now.`val` * 2 + 2
+            clean(now.right!!)
+        }
+    }
+
+    fun find(target: Int): Boolean {
+        return foundSet.contains(target)
+    }
+
 }
+class TreeNode(var `val`: Int) {
+         var left: TreeNode? = null
+         var right: TreeNode? = null
+    }
