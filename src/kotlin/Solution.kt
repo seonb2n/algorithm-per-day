042ed3d82f4e7779ce8917abfa9b8b8dd87fa594
seonb2n <626ksb@gmail.com>
2025-02-22 20:33:04 +0900

    - leetcode - https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/description/?envType=daily-question&envId=2025-02-22 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,35 +3,48 @@ package kotlin
 import java.util.*
 import kotlin.collections.ArrayList
 
-// https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/?envType=daily-question&envId=2025-02-21
-class FindElements(root: TreeNode?) {
-    private val foundSet = HashSet<Int>()
-    init {
-        if (root != null) {
-            root.`val` = 0
-            clean(root)
-        }
+// https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/description/?envType=daily-question&envId=2025-02-22
+class Solution {
+    var index = 0
+
+    fun recoverFromPreorder(traversal: String): TreeNode? {
+        return dfs(traversal, 0)
     }
 
-    private fun clean(now: TreeNode) {
-        foundSet.add(now.`val`)
+    private fun dfs(traversal: String, depth: Int): TreeNode? {
+        var current = 0
+        var i = index
 
-        if (now.left != null) {
-            now.left!!.`val` = now.`val` * 2 + 1
-            clean(now.left!!)
+        while (i < traversal.length && traversal[i] == '-') {
+            current++
+            i++
         }
-        if (now.right != null) {
-            now.right!!.`val` = now.`val` * 2 + 2
-            clean(now.right!!)
+
+        if (current != depth) return null
+
+        // 숫자 값을 파싱
+        var num = 0
+        while (i < traversal.length && traversal[i].isDigit()) {
+            num = num * 10 + (traversal[i] - '0')
+            i++
         }
-    }
 
-    fun find(target: Int): Boolean {
-        return foundSet.contains(target)
-    }
+        // 현재 인덱스 업데이트
+        index = i
 
+        // 새 노드 생성
+        val node = TreeNode(num)
+
+        // 왼쪽 자식 탐색
+        node.left = dfs(traversal, depth + 1)
+
+        // 오른쪽 자식 탐색
+        node.right = dfs(traversal, depth + 1)
+
+        return node
+    }
 }
 class TreeNode(var `val`: Int) {
          var left: TreeNode? = null
          var right: TreeNode? = null
-    }
+}
