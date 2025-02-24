b7a19dc54c3c2721016b40a800ef364960bfd230
seonb2n <626ksb@gmail.com>
2025-02-24 23:05:49 +0900

    - leetcode - https://leetcode.com/problems/most-profitable-path-in-a-tree/?envType=daily-question&envId=2025-02-25 - beats 33 %

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,56 +3,26 @@ package kotlin
 import java.util.*
 import kotlin.collections.ArrayList
 
-// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/?envType=daily-question&envId=2025-02-23
+// https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/?envType=daily-question&envId=2025-02-25
 class Solution {
-    fun constructFromPrePost(preorder: IntArray, postorder: IntArray): TreeNode? {
+    fun numOfSubarrays(arr: IntArray): Int {
+        val MOD = 1_000_000_007
+        var result = 0
+        var sum = 0L
+        var evenCount = 1 // 빈 부분 배열은 짝수 합(0) 처리
+        var oddCount = 0
 
-        if (preorder.isEmpty() || postorder.isEmpty()) return null
-        return construct(preorder, postorder, 0, preorder.size - 1, 0, postorder.size - 1)
-    }
-
-    private fun construct(
-        preorder: IntArray, postorder: IntArray,
-        preStart: Int, preEnd: Int, postStart: Int, postEnd: Int
-    ): TreeNode? {
-        // 유효하지 않은 범위
-        if (preStart > preEnd || postStart > postEnd) return null
-
-        // 단일 노드
-        if (preStart == preEnd) {
-            return TreeNode(preorder[preStart])
+        for (i in arr.indices) {
+            sum += arr[i]
+            // 현재의 누적합이 짝수라면, 지금까지 존재하던 홀수 누적합의 개수를 더함
+            if (sum % 2 == 0L) {
+                result = (result + oddCount) % MOD
+                evenCount++
+            } else {
+                result = (result + evenCount) % MOD
+                oddCount++
+            }
         }
-
-        // 현재 서브트리의 루트 노드
-        val root = TreeNode(preorder[preStart])
-
-        // 왼쪽 서브트리의 루트
-        val leftRoot = preorder[preStart + 1]
-
-        // 후위 순위에서 왼쪽 서브트리 루트 찾기
-        var postLeftRoot = postStart
-        while (postorder[postLeftRoot] != leftRoot) {
-            postLeftRoot++
-        }
-
-        // 왼쪽 서브트리의 크기 계산
-        val leftSubTreeSize = postLeftRoot - postStart + 1
-
-        // 왼쪽 서브트리 구성
-        root.left = construct(preorder, postorder,
-            preStart + 1, preStart + leftSubTreeSize,
-            postStart, postLeftRoot)
-
-        // 오른쪽 서브트리 구성
-        root.right = construct(preorder, postorder,
-            preStart + leftSubTreeSize + 1, preEnd, postLeftRoot + 1, postEnd - 1
-            )
-
-        return root
+        return result
     }
-}
-
-class TreeNode(var `val`: Int) {
-     var left: TreeNode? = null
-     var right: TreeNode? = null
 }
