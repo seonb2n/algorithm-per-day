65bb2a72bde86f73aaaf32b05f1ca17d2c9b924f
seonb2n <626ksb@gmail.com>
2025-03-05 21:51:15 +0900

    - leetcode - https://leetcode.com/problems/count-total-number-of-colored-cells/?envType=daily-question&envId=2025-03-05 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,22 +5,9 @@ import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.math.min
 
-// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/?envType=daily-question&envId=2025-03-04
+// https://leetcode.com/problems/count-total-number-of-colored-cells/?envType=daily-question&envId=2025-03-05
 class Solution {
-    fun checkPowersOfThree(n: Int): Boolean {
-        return checkAndPass(n)
-    }
-
-    fun checkAndPass(now: Int): Boolean {
-        if (now == 3 || now == 1) {
-            return true
-        }
-        if (now % 3 == 0) {
-            return checkAndPass(now / 3)
-        }
-        else if ((now - 1) % 3 == 0) {
-            return checkAndPass((now - 1) / 3)
-        }
-        return false
+    fun coloredCells(n: Int): Long {
+        return n.toLong() * n * 2 - 2 * n + 1
     }
 }
