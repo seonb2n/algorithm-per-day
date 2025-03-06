a2760df55883ca17b16afa122084ad086eff0fe1
seonb2n <626ksb@gmail.com>
2025-03-06 22:47:23 +0900

    - leetcode - https://leetcode.com/problems/find-missing-and-repeated-values/?envType=daily-question&envId=2025-03-06 - beats 19%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,9 +5,30 @@ import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.math.min
 
-// https://leetcode.com/problems/count-total-number-of-colored-cells/?envType=daily-question&envId=2025-03-05
+// https://leetcode.com/problems/find-missing-and-repeated-values/?envType=daily-question&envId=2025-03-06
 class Solution {
-    fun coloredCells(n: Int): Long {
-        return n.toLong() * n * 2 - 2 * n + 1
+    fun findMissingAndRepeatedValues(grid: Array<IntArray>): IntArray {
+        val n = grid.size
+        val n2 = n * n
+        val set = mutableSetOf<Int>()
+
+        var twiceNumber = -1
+
+        for (i in 1..n2) {
+            set.add(i)
+        }
+
+        for (arr in grid) {
+            for (i in 0..n-1) {
+                val now = arr[i]
+                if (set.contains(now)) {
+                    set.remove(now)
+                } else {
+                    twiceNumber = now
+                }
+            }
+        }
+
+        return intArrayOf(twiceNumber, set.first())
     }
 }
