864dcd7f92a349f2e154d91ed0484a9bb62083a9
seonb2n <626ksb@gmail.com>
2025-03-13 22:39:47 +0900

    - leetcode - https://leetcode.com/problems/zero-array-transformation-ii/?envType=daily-question&envId=2025-03-13 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,41 +5,37 @@ import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.math.min
 
-// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/?envType=daily-question&envId=2025-03-11
+// https://leetcode.com/problems/zero-array-transformation-ii/submissions/1572507944/
 class Solution {
-    fun numberOfSubstrings(s: String): Int {
-        val n = s.length
-        var result = 0
-        val a_list = mutableListOf<Int>()
-        val b_list = mutableListOf<Int>()
-        val c_list = mutableListOf<Int>()
+    fun minZeroArray(nums: IntArray, queries: Array<IntArray>): Int {
+        val n = nums.size
+        var sum = 0
+        var k = 0
+        // 차분 배열
+        val cnt = IntArray(n + 1)
 
-        for (i in 0..n-1) {
-            when(s[i]) {
-                'a' -> a_list.add(i)
-                'b' -> b_list.add(i)
-                'c' -> c_list.add(i)
+        for (i in 0 until n) {
+            // 현재 값과, 현재 위치에 대한 업데이트 결과 값(차분 배열의 값 + 누적합)
+            while (sum + cnt[i] < nums[i]) {
+                // 끝에 도달했다면 0 만들기 불가
+                if (k == queries.size) return -1
+
+                val l = queries[k][0]
+                val r = queries[k][1]
+                val now = queries[k][2]
+                k++
+
+                // 이미 지나가서 업데이트 필요 없음
+                if (r < i) continue
+
+                // 차분 배열 업데이트
+                cnt[maxOf(l, i)] += now
+                cnt[r + 1] -= now
             }
+            // 누적합 업데이트
+            sum += cnt[i]
         }
 
-        if (a_list.size == 0 || b_list.size == 0 || c_list.size == 0) {
-            return 0
-        }
-
-        var aCursor = 0
-        var bCursor = 0
-        var cCursor = 0
-        while (aCursor < a_list.size && bCursor < b_list.size && cCursor < c_list.size) {
-            val max = maxOf(a_list[aCursor], b_list[bCursor], c_list[cCursor])
-            val min = minOf(a_list[aCursor], b_list[bCursor], c_list[cCursor])
-            result += (n - max)
-
-            if (min == a_list[aCursor]) { aCursor++ }
-            if (min == b_list[bCursor]) { bCursor++ }
-            if (min == c_list[cCursor]) { cCursor++ }
-        }
-
-
-        return result
+        return k
     }
 }
