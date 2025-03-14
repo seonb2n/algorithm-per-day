098edcb5d588c58941ebff1bdd13596a738e8bc3
seonb2n <626ksb@gmail.com>
2025-03-14 22:53:21 +0900

    - leetcode - https://leetcode.com/problems/maximum-candies-allocated-to-k-children/?envType=daily-question&envId=2025-03-14 - beats 25%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,37 +5,44 @@ import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.math.min
 
-// https://leetcode.com/problems/zero-array-transformation-ii/submissions/1572507944/
+// https://leetcode.com/problems/maximum-candies-allocated-to-k-children/?envType=daily-question&envId=2025-03-14
 class Solution {
-    fun minZeroArray(nums: IntArray, queries: Array<IntArray>): Int {
-        val n = nums.size
-        var sum = 0
-        var k = 0
-        // 차분 배열
-        val cnt = IntArray(n + 1)
+    fun maximumCandies(candies: IntArray, k: Long): Int {
+        // candies 를 나누어서 k 명의 children 이 가질 수 있는 최대 개수를 찾아야 한다.
 
-        for (i in 0 until n) {
-            // 현재 값과, 현재 위치에 대한 업데이트 결과 값(차분 배열의 값 + 누적합)
-            while (sum + cnt[i] < nums[i]) {
-                // 끝에 도달했다면 0 만들기 불가
-                if (k == queries.size) return -1
-
-                val l = queries[k][0]
-                val r = queries[k][1]
-                val now = queries[k][2]
-                k++
-
-                // 이미 지나가서 업데이트 필요 없음
-                if (r < i) continue
-
-                // 차분 배열 업데이트
-                cnt[maxOf(l, i)] += now
-                cnt[r + 1] -= now
-            }
-            // 누적합 업데이트
-            sum += cnt[i]
+        var sum = 0L
+        var right = 0
+        for (candy in candies) {
+            sum += candy
+            right = maxOf(right, candy)
+        }
+        if (sum < k) {
+            return 0
         }
 
-        return k
+        var left = 1
+
+        // 이진탐색으로 checkDividable 가능한 최대 c 탐색
+        while (left <= right) {
+            val mid = (left + right) / 2
+            if (checkDividable(mid, candies, k)) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+        return right
+    }
+
+    private fun checkDividable(c: Int, candies: IntArray, k: Long): Boolean {
+        var pile = 0L
+        for (candy in candies) {
+            pile += candy / c
+            if (pile >= k) {
+                break
+            }
+        }
+        return pile >= k
     }
 }
