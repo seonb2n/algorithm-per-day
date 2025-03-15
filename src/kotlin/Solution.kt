4464ca58a4efc49a4db3ae3c1afa9e252a6fd936
seonb2n <626ksb@gmail.com>
2025-03-15 10:45:21 +0900

    - leetcode - https://leetcode.com/problems/house-robber-iv/?envType=daily-question&envId=2025-03-15 - beats -%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,44 +5,40 @@ import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.math.min
 
-// https://leetcode.com/problems/maximum-candies-allocated-to-k-children/?envType=daily-question&envId=2025-03-14
+// https://leetcode.com/problems/house-robber-iv/?envType=daily-question&envId=2025-03-15
 class Solution {
-    fun maximumCandies(candies: IntArray, k: Long): Int {
-        // candies 를 나누어서 k 명의 children 이 가질 수 있는 최대 개수를 찾아야 한다.
+    fun minCapability(nums: IntArray, k: Int): Int {
+        // 이진 탐색
 
-        var sum = 0L
-        var right = 0
-        for (candy in candies) {
-            sum += candy
-            right = maxOf(right, candy)
-        }
-        if (sum < k) {
-            return 0
+        fun checkCapa(capa: Int): Boolean {
+            var count = 0
+            var i = 0
+
+            while (i < nums.size) {
+                if (nums[i] <= capa) {
+                    count++
+                    i += 2
+                } else {
+                    i++
+                }
+            }
+            return count >= k
         }
 
         var left = 1
+        var right = nums.maxOrNull() ?: Int.MAX_VALUE
 
-        // 이진탐색으로 checkDividable 가능한 최대 c 탐색
-        while (left <= right) {
+        while (left < right) {
             val mid = (left + right) / 2
-            if (checkDividable(mid, candies, k)) {
+
+            if (checkCapa(mid)) {
+                right = mid
+            }
+            else {
                 left = mid + 1
-            } else {
-                right = mid - 1
             }
         }
-
-        return right
+        return left
     }
 
-    private fun checkDividable(c: Int, candies: IntArray, k: Long): Boolean {
-        var pile = 0L
-        for (candy in candies) {
-            pile += candy / c
-            if (pile >= k) {
-                break
-            }
-        }
-        return pile >= k
-    }
 }
