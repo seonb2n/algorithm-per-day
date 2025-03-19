373b2af27804a3197c33b563eb14909a03b0462d
seonb2n <626ksb@gmail.com>
2025-03-19 20:04:51 +0900

    - leetcode - https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/?envType=daily-question&envId=2025-03-19 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,31 +5,27 @@ import kotlin.math.floor
 import kotlin.math.sqrt
 
 
-// https://leetcode.com/problems/longest-nice-subarray/submissions/1577940019/?envType=daily-question&envId=2025-03-18
-
+// https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/submissions/1578972065/?envType=daily-question&envId=2025-03-19
 class Solution {
-    fun longestNiceSubarray(nums: IntArray): Int {
-        // 연속된 배열을 찾음
-        // 슬라이딩 윈도우 방식
-
-        // 새로운 비트 탐색
-        // 새로운 비트가 기존과 겹치면, 안겹치는 조건을 만족할 때까지 앞에 인자를 제거
-
-        var max = 1
-        var start = 0
-        var bitUsed = 0
+    fun minOperations(nums: IntArray): Int {
+        // greedy 앞에서부터 0 을 발견하면 flip 처리하면 된다.
         val n = nums.size
+        var counter = 0
 
-        for (end in 0 until n) {
-            while ((bitUsed and nums[end]) != 0) {
-                bitUsed -= nums[start]
-                start++
+        for (i in 0 until n - 2) {
+            if (nums[i] == 0) {
+                nums[i] = 1 - nums[i]
+                nums[i + 1] = 1 - nums[i + 1]
+                nums[i + 2] = 1 - nums[i + 2]
+                counter++
             }
-
-            bitUsed = bitUsed or nums[end]
-            max = maxOf(max, end - start + 1)
         }
 
-        return max
+        // 마지막 2 개의 원소가 1인지
+        if (nums[n-2] == 0 || nums[n-1] == 0) {
+            return -1
+        }
+
+        return counter
     }
 }
