467b0a45c19825765a4784ce6d8b6360fdf988c6
seonb2n <626ksb@gmail.com>
2025-03-20 21:50:29 +0900

    - leetcode - https://leetcode.com/problems/insert-delete-getrandom-o1/ - beats 98%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,31 +1,41 @@
 package kotlin
 
 import java.util.*
+import kotlin.collections.HashSet
 import kotlin.math.floor
 import kotlin.math.sqrt
 
 
-// https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/submissions/1578972065/?envType=daily-question&envId=2025-03-19
-class Solution {
-    fun minOperations(nums: IntArray): Int {
-        // greedy 앞에서부터 0 을 발견하면 flip 처리하면 된다.
-        val n = nums.size
-        var counter = 0
+// https://leetcode.com/problems/insert-delete-getrandom-o1/
+class RandomizedSet() {
+    val list = ArrayList<Int>()
+    val map = HashMap<Int, Int>()
 
-        for (i in 0 until n - 2) {
-            if (nums[i] == 0) {
-                nums[i] = 1 - nums[i]
-                nums[i + 1] = 1 - nums[i + 1]
-                nums[i + 2] = 1 - nums[i + 2]
-                counter++
-            }
-        }
-
-        // 마지막 2 개의 원소가 1인지
-        if (nums[n-2] == 0 || nums[n-1] == 0) {
-            return -1
-        }
-
-        return counter
+    private fun search(`val`: Int): Boolean {
+        return map.containsKey(`val`)
     }
+
+    fun insert(`val`: Int): Boolean {
+        if (search(`val`)) return false
+        list.add(`val`)
+        map[`val`] = list.size - 1
+        return true
+    }
+
+    fun remove(`val`: Int): Boolean {
+        if (!search(`val`)) return false
+        val index = map[`val`]!!
+        val lastElement = list[list.size - 1]
+        list[index] = lastElement
+        map[lastElement] = index
+        list.removeAt(list.size - 1)
+        map.remove(`val`)
+        return true
+    }
+
+    fun getRandom(): Int {
+        if (list.isEmpty()) throw NoSuchElementException("Set is empty")
+        return list[Random.nextInt(list.size)]
+    }
+
 }
