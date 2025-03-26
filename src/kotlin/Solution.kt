9c547ef75a19c155a606107cd651bedafccab53d
seonb2n <626ksb@gmail.com>
2025-03-26 22:03:08 +0900

    - leetcode - https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/description/?envType=daily-question&envId=2025-03-26 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,46 +1,35 @@
 package kotlin
 
 import java.util.*
+import kotlin.math.abs
 
-// https://leetcode.com/problems/count-days-without-meetings/?envType=daily-question&envId=2025-03-24
+// https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/?envType=daily-question&envId=2025-03-26
 class Solution {
-    fun countDays(days: Int, meetings: Array<IntArray>): Int {
-        // 누적합
-        val events = mutableListOf<Pair<Int, Int>>()
-
-        for (meet in meetings) {
-            val start = meet[0]
-            val end = meet[1]
-
-            events.add(Pair(start, 1))
-            if (end < days) {
-                events.add(Pair(end + 1, -1))
+    fun minOperations(grid: Array<IntArray>, x: Int): Int {
+        // 모든 숫자를 x 로 나눈 나머지가 같아야 한다.
+        val left = grid[0][0] % x
+        val n = grid.size
+        val m = grid[0].size
+        val flat = IntArray(n * m)
+        var last = 0
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+               if (grid[i][j] % x != left) {
+                   return -1
+               }
+                flat[last] = grid[i][j]
+                last++
             }
         }
 
-        events.sortBy { it.first }
+        // 정렬하고, 가운데 숫자에 맞추기 위해서 연산이 얼마나 필요한지 계산한다.
+        flat.sort()
 
-        var sum = 0
+        val target = flat[last / 2]
         var result = 0
-        var lastDay = 1
-
-        for (event in events) {
-            val currentDay = event.first
-
-            // 현재 이벤트 이전의 날짜 동안 미팅이 없는지 확인
-            if (sum == 0) {
-                result += (currentDay - lastDay)
-            }
-
-            sum += event.second
-            lastDay = currentDay
+        for (i in 0 until last) {
+            result += abs(target - flat[i]) / x
         }
-
-        // 마지막 이벤트부터 days까지 확인
-        if (sum == 0 && lastDay <= days) {
-            result += (days + 1 - lastDay)
-        }
-
         return result
     }
 }
