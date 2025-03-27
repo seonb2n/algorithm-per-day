8ad99e503eed31b5664961cc2929eefd3c040245
seonb2n <626ksb@gmail.com>
2025-03-27 19:53:14 +0900

    - leetcode - https://leetcode.com/problems/minimum-index-of-a-valid-split/description/?envType=daily-question&envId=2025-03-27 - beats 33%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,33 +3,27 @@ package kotlin
 import java.util.*
 import kotlin.math.abs
 
-// https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/?envType=daily-question&envId=2025-03-26
+// https://leetcode.com/problems/minimum-index-of-a-valid-split/description/?envType=daily-question&envId=2025-03-27
 class Solution {
-    fun minOperations(grid: Array<IntArray>, x: Int): Int {
-        // 모든 숫자를 x 로 나눈 나머지가 같아야 한다.
-        val left = grid[0][0] % x
-        val n = grid.size
-        val m = grid[0].size
-        val flat = IntArray(n * m)
-        var last = 0
+    fun minimumIndex(nums: List<Int>): Int {
+        // dominent element 탐색
+        val numberMap = nums.groupBy { it }.mapValues { it.value.size }
+        val dominent = numberMap.maxByOrNull { it.value }?.key!!
+        val count = numberMap.maxByOrNull { it.value }?.value!!
+
+        val n = nums.size
+
+        var now = 0
         for (i in 0 until n) {
-            for (j in 0 until m) {
-               if (grid[i][j] % x != left) {
-                   return -1
-               }
-                flat[last] = grid[i][j]
-                last++
+            if (nums[i] == dominent) {
+                now++
+            }
+
+            if (i + 1 < now * 2  && n-i-1 < (count - now) * 2) {
+                return i
             }
         }
 
-        // 정렬하고, 가운데 숫자에 맞추기 위해서 연산이 얼마나 필요한지 계산한다.
-        flat.sort()
-
-        val target = flat[last / 2]
-        var result = 0
-        for (i in 0 until last) {
-            result += abs(target - flat[i]) / x
-        }
-        return result
+        return -1
     }
 }
