c4cc427b9440ffef2e4b1795f8013d2e6f857a00
seonb2n <626ksb@gmail.com>
2025-03-30 19:54:30 +0900

    - leetcode - https://leetcode.com/problems/remove-k-digits/ - beats 82%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,41 +5,35 @@ import java.util.*
 // https://leetcode.com/problems/remove-k-digits/
 class Solution {
     fun removeKdigits(num: String, k: Int): String {
-        // greedy
         if (num.length <= k) {
             return "0"
         }
-        val linkedList = LinkedList<Char>()
+        val deque = ArrayDeque<Char>()
         var remain = k
 
         for (digit in num) {
-            // 현재 숫자보다 큰 숫자가 있으면 제거
-            while (remain > 0 && linkedList.isNotEmpty() && linkedList.last > digit) {
-                linkedList.removeLast()
+            // 현재 숫자보다 큰 숫자가 있으면 제거 (그리디)
+            while (remain > 0 && deque.isNotEmpty() && deque.last() > digit) {
+                deque.removeLast()
                 remain--
             }
-            linkedList.add(digit)
+            deque.addLast(digit)
         }
 
-        // 제거할 숫자가 남았으면 뒤부터 제거(오름차순으로 들어있으면 큰 수부터 제거해야한다)
-        while (remain > 0 && linkedList.isNotEmpty()) {
-            linkedList.removeLast()
-            remain--
+        // 아직 제거할 숫자가 남아있다면 뒤에서부터 제거
+        repeat(remain) {
+            if (deque.isNotEmpty()) {
+                deque.removeLast()
+            }
+        }
+
+        while (deque.isNotEmpty() && deque.first() == '0') {
+            deque.removeFirst()
         }
 
         val sb = StringBuilder()
-        // 앞에서부터 0 제거
-        while( linkedList.size > 0 && linkedList[0] == '0') {
-            linkedList.removeFirst()
-        }
-        if (linkedList.size == 0) {
-            return "0"
-        }
+        deque.forEach { sb.append(it) }
 
-        for (i in linkedList.indices) {
-            sb.append(linkedList[i])
-        }
-
-        return sb.toString()
+        return if (sb.isEmpty()) "0" else sb.toString()
     }
 }
