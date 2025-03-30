a96c3ae08be6eef922fac62bd3d2068ed7052e85
seonb2n <626ksb@gmail.com>
2025-03-30 16:09:45 +0900

    - leetcode - https://leetcode.com/problems/partition-labels/?envType=daily-question&envId=2025-03-30 - go - beats 100%

--- src/go/Solution.go
+++ src/go/Solution.go
@@ -1,56 +1,27 @@
 package main
 
-// https://leetcode.com/problems/maximum-number-of-points-from-grid-queries/submissions/1589662417/?envType=daily-question&envId=2025-03-28
-// 4방향 이동을 위한 방향 벡터
-var directions = [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 상, 하, 좌, 우
+// https://leetcode.com/problems/partition-labels/submissions/1590841099/?envType=daily-question&envId=2025-03-30
+func partitionLabels(s string) []int {
+	result := []int{}
+	lastMap := make(map[rune]int)
 
-type Cell struct {
-	row, col int
-}
-
-func maxPoints(grid [][]int, queries []int) []int {
-	m, n := len(grid), len(grid[0])
-	k := len(queries)
-	answer := make([]int, k)
-
-	// 쿼리마다 BFS 수행
-	for i, query := range queries {
-		visited := make(map[Cell]bool)
-
-		queue := []Cell{{0, 0}}
-
-		if grid[0][0] >= query {
-			answer[i] = 0
-			continue
-		}
-
-		visited[Cell{0, 0}] = true
-		points := 1
-
-		for len(queue) > 0 {
-			current := queue[0]
-			queue = queue[1:] // 큐에서 제거
-
-			for _, dir := range directions {
-				newRow, newCol := current.row+dir[0], current.col+dir[1]
-
-				// 범위 내에 있음
-				if newRow >= 0 && newRow < m && newCol >= 0 && newCol < n {
-					// 새 셀의 값이 쿼리보다 작다면
-					if grid[newRow][newCol] < query {
-						newCell := Cell{newRow, newCol}
-
-						if !visited[newCell] {
-							visited[newCell] = true
-							points++
-							queue = append(queue, newCell)
-						}
-					}
-				}
-			}
-		}
-		answer[i] = points
+	for i, char := range s {
+		lastMap[char] = i
 	}
 
-	return answer
+	start := 0
+	end := 0
+
+	for i, char := range s {
+		if lastMap[char] > end {
+			end = lastMap[char]
+		}
+
+		if i == end {
+			result = append(result, end-start+1)
+			start = end + 1
+		}
+	}
+
+	return result
 }
