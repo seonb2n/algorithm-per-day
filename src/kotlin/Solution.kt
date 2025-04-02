0b40c75a459c177e1910a462abb9374eba77ff82
seonb2n <626ksb@gmail.com>
2025-04-02 22:58:53 +0900

    - leetcode - https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/?envType=daily-question&envId=2025-04-02 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,28 +1,22 @@
 package kotlin
 
-import java.util.*
-
 // https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/?envType=daily-question&envId=2025-04-02
 class Solution {
     fun maximumTripletValue(nums: IntArray): Long {
         // 단순 구현
         val n = nums.size
-        var max = 0L
-        // 시작 구간별 최댓값 구해두기
-        val start = IntArray(n)
-        start[n-1] = nums[n-1]
-        for (i in n-2 downTo 0) {
-            start[i] = maxOf(start[i + 1], nums[i])
+        // i 까지의 최댓값
+        var maxNum = 0L
+        // i 까지의 최대 차이
+        var maxDiff = 0L
+        var result = 0L
+
+        for (i in 0 until n) {
+            result = maxOf(result, maxDiff * nums[i])
+            maxDiff = maxOf(maxDiff, maxNum - nums[i])
+            maxNum = maxOf(maxNum, nums[i].toLong())
         }
 
-        for (i in 0 until n-2) {
-            for (j in i+1 until n-1) {
-                if (nums[i].toLong() - nums[j] < 0) {
-                    continue
-                }
-                max = maxOf(max, (nums[i].toLong() - nums[j]) * start[j+1])
-            }
-        }
-        return max
+        return result
     }
 }
