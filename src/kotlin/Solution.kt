9e1c272cc10c6ad56e1adfa79026d73f4ec41e41
seonb2n <626ksb@gmail.com>
2025-04-04 23:11:53 +0900

    - leetcode - https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/submissions/1596613336/?envType=daily-question&envId=2025-04-04 - beats 75%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,22 +1,38 @@
 package kotlin
 
-// https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/description/?envType=daily-question&envId=2025-04-03
+// https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/?envType=daily-question&envId=2025-04-04
 class Solution {
-    fun maximumTripletValue(nums: IntArray): Long {
-        // 단순 구현
-        val n = nums.size
-        // i 까지의 최댓값
-        var maxNum = 0L
-        // i 까지의 최대 차이
-        var maxDiff = 0L
-        var result = 0L
+    fun lcaDeepestLeaves(root: TreeNode?): TreeNode? {
+        // dfs
+        fun dfs(node: TreeNode?): Result {
+            if (node == null) return Result(null, 0)
 
-        for (i in 0 until n) {
-            result = maxOf(result, maxDiff * nums[i])
-            maxDiff = maxOf(maxDiff, maxNum - nums[i])
-            maxNum = maxOf(maxNum, nums[i].toLong())
+            // 리프노드
+            if (node.left == null && node.right == null) return Result(node, 1)
+
+            val leftResult = dfs(node.left)
+            val rightResult = dfs(node.right)
+
+            // 왼쪽과 오른쪽의 깊이가 같으면 현재 노드가 LCA
+            if (leftResult.depth == rightResult.depth) {
+                return Result(node, leftResult.depth + 1)
+            }
+
+            // 다르면 더 깊은쪽을 반환
+            return if (leftResult.depth > rightResult.depth) {
+                Result(leftResult.node, leftResult.depth + 1)
+            } else {
+                Result(rightResult.node, rightResult.depth + 1)
+            }
         }
 
-        return result
+        return dfs(root).node
     }
+
+    data class Result(val node: TreeNode?, val depth: Int)
 }
+
+class TreeNode(var `val`: Int) {
+         var left: TreeNode? = null
+         var right: TreeNode? = null
+    }
