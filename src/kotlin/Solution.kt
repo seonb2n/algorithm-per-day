4b0d12be4ffae6d03110ef2d6c6c7bb73ce18668
seonb2n <626ksb@gmail.com>
2025-04-06 11:53:44 +0900

    - leetcode - https://leetcode.com/problems/largest-divisible-subset/?envType=daily-question&envId=2025-04-06 - dp - beats 89%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,35 +3,40 @@ package kotlin
 // https://leetcode.com/problems/largest-divisible-subset/?envType=daily-question&envId=2025-04-06
 class Solution {
     fun largestDivisibleSubset(nums: IntArray): List<Int> {
-        // 백트래킹
+        //dp
+        if (nums.isEmpty()) return emptyList()
         val n = nums.size
-        var result = mutableListOf<Int>()
-
         nums.sort()
 
-        fun backtrack(now: MutableList<Int>, index: Int) {
-            if (now.size > result.size) {
-                result.clear()
-                result.addAll(now)
-            }
-            if (n - index <= result.size) {
-                return
-            }
-            var last = 1
-            if (now.size != 0) {
-                last = now.last()
-            }
-            for (i in index until n) {
-                if (nums.isEmpty() || nums[i] % last == 0) {
-                    now.add(nums[i])
-                    backtrack(now, i + 1)
-                    now.removeLast()
+        // dp[i]는 nums[i]를 마지막 원소로 하는 최대 부분집합의 크기
+        val dp = IntArray(n) { 1 }
+        // prev[i]는 nums[i]를 마지막 원소로 하는 최대 부분집합에서 이전 원소의 인덱스
+        val prev = IntArray(n) { -1 }
+
+        var maxIdx = 0
+        for (i in 1 until n) {
+            for (j in 0 until i) {
+                // nums[i]가 nums[j]로 나누어 떨어지고, 기존 dp[i]보다 dp[j]+1이 크면 업데이트
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
                 }
+            }
+
+            // 최대 크기의 부분집합 갱신
+            if (dp[i] > dp[maxIdx]) {
+                maxIdx = i
             }
         }
 
-        backtrack(mutableListOf(), 0)
+        // 최대 부분집합 재구성
+        val result = mutableListOf<Int>()
+        var currIdx = maxIdx
+        while (currIdx >= 0) {
+            result.add(nums[currIdx])
+            currIdx = prev[currIdx]
+        }
 
-        return result
+        return result.reversed()
     }
 }
