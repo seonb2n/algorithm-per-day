52ffbfe3d5e961173ced956d69c303f813d07289
seonb2n <626ksb@gmail.com>
2025-04-09 20:14:51 +0900

    - leetcode - https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/?envType=daily-question&envId=2025-04-09 - beats 30%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,32 +1,20 @@
 package kotlin
 
-// https://leetcode.com/problems/partition-equal-subset-sum/submissions/1599549077/?envType=daily-question&envId=2025-04-07
+
+// https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/?envType=daily-question&envId=2025-04-09
 class Solution {
-    fun canPartition(nums: IntArray): Boolean {
-        val sum = nums.sum()
-
-        // 합이 홀수이면 두 부분으로 동일하게 나눌 수 없음
-        if (sum % 2 != 0) {
-            return false
+    fun minOperations(nums: IntArray, k: Int): Int {
+        // nums 의 요소 중에 k 보다 작은게 1개라도 있으면 -1 반환
+        val steps = nums.toSet()
+        if (steps.any { it < k }) {
+            return -1
         }
-
-        val target = sum / 2
-        val n = nums.size
-
-        // DP 접근법: dp[i]는 합이 i가 가능한지 여부
-        val dp = BooleanArray(target + 1) { false }
-        dp[0] = true
-
-        for (num in nums) {
-            // 역순으로 순회하여 중복 계산 방지
-            for (j in target downTo num) {
-                // j-num 합이 가능하다면, j 합도 가능함
-                if (dp[j - num]) {
-                    dp[j] = true
-                }
-            }
+        var result = 0
+        if (!steps.contains(k)) {
+            result++
         }
+        result += steps.count() -1
 
-        return dp[target]
+        return result
     }
 }
