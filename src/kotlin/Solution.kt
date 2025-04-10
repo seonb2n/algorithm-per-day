5ad368847a36283bb4625b658c0bf69fce99a689
seonb2n <626ksb@gmail.com>
2025-04-10 22:30:49 +0900

    - leetcode - https://leetcode.com/problems/count-pairs-that-form-a-complete-day-ii/ - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,30 +1,21 @@
 package kotlin
 
 
-// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+// https://leetcode.com/problems/count-pairs-that-form-a-complete-day-ii/
 class Solution {
-    fun sumNumbers(root: TreeNode?): Int {
-        //dfs
-        var result = 0
+    fun countCompleteDayPairs(hours: IntArray): Long {
+        // set 구성
+        val extras = IntArray(24)
 
-        if (root == null) {
-            return 0
+        for (hour in hours) {
+            extras[hour % 24]++
         }
 
-        fun dfs(now: Int, node: TreeNode) {
-            val next = (now * 10 + node.`val`)
-            if (node.left == null && node.right == null) {
-                result += next
-            }
-            if (node.left != null) {
-                dfs(next, node.left)
-            }
-            if (node.right != null) {
-                dfs(next, node.right)
-            }
+        var result = extras[0].toLong() * (extras[0] - 1) / 2
+        result += extras[12].toLong() * (extras[12] - 1) / 2
+        for (i in 1 until 12) {
+            result += (extras[i].toLong() * extras[24-i])
         }
-
-        dfs(0, root)
 
         return result
     }
