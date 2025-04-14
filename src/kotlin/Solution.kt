3e3bb9a5f65cb9eb2bf7599638318fb52b9db0c5
seonb2n <626ksb@gmail.com>
2025-04-14 22:24:39 +0900

    - leetcode - https://leetcode.com/problems/count-good-triplets/?envType=daily-question&envId=2025-04-14 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,21 +1,27 @@
 package kotlin
 
+import kotlin.math.abs
 
-// https://leetcode.com/problems/count-good-numbers/submissions/1605208137/?envType=daily-question&envId=2025-04-13
+
+// https://leetcode.com/problems/count-good-triplets/?envType=daily-question&envId=2025-04-14
 class Solution {
-    fun countGoodNumbers(n: Long): Int {
-        val modulo = 1_000_000_007L
-        val even = (n + 1) / 2
-        val odd = n / 2
+    fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
+        var result = 0
+        val n = arr.size
 
-        val result = pow(5L, even, modulo) * pow(4L, odd, modulo) % modulo
-        return result.toInt()
-    }
+        for (i in 0 until n) {
+            for (j in i + 1 until n) {
+                if (abs(arr[i] - arr[j]) > a) {
+                    continue
+                }
+                for (k in j + 1 until n) {
+                    if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
+                        result++
+                    }
+                }
+            }
+        }
 
-    fun pow(a: Long, b:Long, m: Long): Long {
-        if (b == 0L) return 1L
-        val half = pow(a, b / 2, m)
-        val result = half * half % m
-        return if (b % 2 == 0L) result else (result * a) % m
+        return result
     }
 }
