8661290bd56c3610e50cc5fcf356e007fd4168ff
seonb2n <626ksb@gmail.com>
2025-04-17 22:27:27 +0900

    - leetcode - https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/?envType=daily-question&envId=2025-04-17 - beats 60%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,28 +2,16 @@ package kotlin
 
 import kotlin.math.abs
 
-// https://leetcode.com/problems/count-the-number-of-good-subarrays/?envType=daily-question&envId=2025-04-16
+// https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/?envType=daily-question&envId=2025-04-17
 class Solution {
-    fun countGood(nums: IntArray, k: Int): Long {
-        // 슬라이딩 윈도
-        var result = 0L
-        var left = 0
-        var pairCount = 0L
-        val pairMap = mutableMapOf<Int, Int>()
-
-        for (right in nums.indices) {
-            val currentNum = nums[right]
-            val currentPair = pairMap.getOrDefault(currentNum, 0)
-            pairCount += currentPair
-            pairMap[currentNum] = currentPair + 1
-
-            while (pairCount >= k && left <= right) {
-                result += (nums.size - right)
-                val leftNum = nums[left]
-                val leftFreq = pairMap[leftNum]!!
-                pairMap[leftNum] = leftFreq - 1
-                pairCount -= (leftFreq - 1) // 제거되는 페어 수
-                left++
+    fun countPairs(nums: IntArray, k: Int): Int {
+        var result = 0
+        val n = nums.size
+        for (i in 0 until n) {
+            for (j in i + 1 until n) {
+                if (nums[i] == nums[j] && i * j % k == 0) {
+                    result++
+                }
             }
         }
 
