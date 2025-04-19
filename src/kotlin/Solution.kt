bed7964254228b8316ec074f42d2ea4db9f0482b
seonb2n <626ksb@gmail.com>
2025-04-19 13:43:06 +0900

    - leetcode - https://leetcode.com/problems/count-the-number-of-fair-pairs/?envType=daily-question&envId=2025-04-19 - beats 90%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,19 +2,32 @@ package kotlin
 
 import kotlin.math.abs
 
-// https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/?envType=daily-question&envId=2025-04-17
+// https://leetcode.com/problems/count-the-number-of-fair-pairs/?envType=daily-question&envId=2025-04-19
 class Solution {
-    fun countPairs(nums: IntArray, k: Int): Int {
-        var result = 0
-        val n = nums.size
-        for (i in 0 until n) {
-            for (j in i + 1 until n) {
-                if (nums[i] == nums[j] && i * j % k == 0) {
-                    result++
+    fun countFairPairs(nums: IntArray, lower: Int, upper: Int): Long {
+        var result = 0L
+        nums.sort()
+
+        // nums[i] + nums[j] <= value인 페어의 수를 세는 함수
+        fun countLessThanOrEqual(nums: IntArray, value: Int): Long {
+            var count = 0L
+            var left = 0
+            var right = nums.size - 1
+
+            while (left < right) {
+                if (nums[left] + nums[right] <= value) {
+                    // left와 right 사이의 모든 j에 대해 (left, j) 쌍이 조건을 만족함
+                    count += (right - left).toLong()
+                    left++
+                } else {
+                    // 합이 너무 큼, right를 줄여야 함
+                    right--
                 }
             }
+
+            return count
         }
 
-        return result
+        return countLessThanOrEqual(nums, upper) - countLessThanOrEqual(nums, lower - 1)
     }
 }
