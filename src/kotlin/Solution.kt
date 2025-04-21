a5063ddffd9369334d00f018ec036ae013d14c07
seonb2n <626ksb@gmail.com>
2025-04-21 22:29:25 +0900

    - leetcode - https://leetcode.com/problems/count-the-hidden-sequences/description/?envType=daily-question&envId=2025-04-21 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,39 +2,25 @@ package kotlin
 
 import kotlin.math.abs
 
-// https://leetcode.com/problems/count-and-say/?envType=daily-question&envId=2025-04-18
+// https://leetcode.com/problems/count-the-hidden-sequences/description/?envType=daily-question&envId=2025-04-21
 class Solution {
-    fun countAndSay(n: Int): String {
-        if (n == 1) return "1"
-
-        fun compress(input: String): String {
-            var cursor = 0
-            val len = input.length
-            val sb = StringBuilder()
-            var now = input[cursor]
-            var count = 1
-            cursor++
-            while (cursor < len) {
-                val next = input[cursor]
-                if (now != next) {
-                    sb.append(count)
-                    sb.append(now)
-                    now = next
-                    count = 1
-                } else {
-                    count++
-                }
-                cursor++
-            }
-            sb.append(count)
-            sb.append(now)
-            return sb.toString()
+    fun numberOfArrays(differences: IntArray, lower: Int, upper: Int): Int {
+        var min = 0
+        var max = 0
+        var now = 0
+        for (d in differences) {
+            now += d
+            min = minOf(min, now)
+            max = maxOf(max, now)
         }
 
-        var result = "11"
-        for (i in 2 until n) {
-            result = compress(result)
+        var result = 0
+        val minGap = lower - min
+        val maxGap = upper - max
+        if (minGap <= maxGap) {
+            result += (maxGap - minGap + 1)
         }
+
         return result
     }
 }
