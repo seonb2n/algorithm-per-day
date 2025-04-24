ef7177d10a35716db68c3de47c95952d62b1994f
seonb2n <626ksb@gmail.com>
2025-04-24 20:55:08 +0900

    - leetcode - https://leetcode.com/problems/count-complete-subarrays-in-an-array/?envType=daily-question&envId=2025-04-24 - beats 33%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,36 +3,29 @@ package kotlin
 import java.util.PriorityQueue
 import kotlin.math.abs
 
-// https://leetcode.com/problems/count-largest-group/?envType=daily-question&envId=2025-04-23
+// https://leetcode.com/problems/count-complete-subarrays-in-an-array/?envType=daily-question&envId=2025-04-24
 class Solution {
-    fun countLargestGroup(n: Int): Int {
-        val digitMap = mutableMapOf<Int, Int>()
-        fun numberToDigitSum(n: Int): Int {
-            var result = 0
-            var now = n
-            while (now > 0) {
-                result += now % 10
-                now = now / 10
-            }
-            return result
-        }
-
-        for (i in 1..n) {
-            val now = numberToDigitSum(i)
-            val count = digitMap.getOrDefault(now, 0)
-            digitMap[now] = count + 1
-        }
-
+    fun countCompleteSubarrays(nums: IntArray): Int {
+        // two point
+        val distinctSet = nums.toSet()
         var result = 0
-        var max = 0
-        for (key in digitMap.keys) {
-            if (digitMap[key]!! > max) {
-                max = digitMap[key]!!
-                result = 1
-            } else if (digitMap[key]!! == max) {
-                result++
+        val n = nums.size
+        for (i in 0 until n) {
+            val nowSet = mutableSetOf<Int>()
+            nowSet.add(nums[i])
+            if (nowSet.size == distinctSet.size) {
+                result += (n - i)
+                continue
+            }
+            for (j in i + 1 until n) {
+                nowSet.add(nums[j])
+                if (nowSet.size == distinctSet.size) {
+                    result += (n - j)
+                    break
+                }
             }
         }
+
         return result
     }
 }
