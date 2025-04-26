bd15baba38e0cc87b99c124020257ce4dc2bdcb6
seonb2n <626ksb@gmail.com>
2025-04-26 12:33:25 +0900

    - leetcode - https://leetcode.com/problems/count-subarrays-with-fixed-bounds/?envType=daily-question&envId=2025-04-26 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,31 +1,41 @@
 package kotlin
 
-import java.util.PriorityQueue
-import kotlin.math.abs
 
-// https://leetcode.com/problems/count-complete-subarrays-in-an-array/?envType=daily-question&envId=2025-04-24
+// https://leetcode.com/problems/count-subarrays-with-fixed-bounds/?envType=daily-question&envId=2025-04-26
 class Solution {
-    fun countCompleteSubarrays(nums: IntArray): Int {
-        // two point
-        val distinctSet = nums.toSet()
-        var result = 0
+    fun countSubarrays(nums: IntArray, minK: Int, maxK: Int): Long {
+        // sliding window
         val n = nums.size
-        for (i in 0 until n) {
-            val nowSet = mutableSetOf<Int>()
-            nowSet.add(nums[i])
-            if (nowSet.size == distinctSet.size) {
-                result += (n - i)
-                continue
+        var result = 0L
+        var left = 0
+        var minKPos = -1
+        var maxKPos = -1
+        var invalidPos = -1 // minK 보다 작거나 max 보다 큰 값
+
+        for (right in 0 until n) {
+            if (nums[right] == minK) {
+                minKPos = right
             }
-            for (j in i + 1 until n) {
-                nowSet.add(nums[j])
-                if (nowSet.size == distinctSet.size) {
-                    result += (n - j)
-                    break
-                }
+            if (nums[right] == maxK) {
+                maxKPos = right
+            }
+            if (nums[right] < minK || nums[right] > maxK) {
+                invalidPos = right
+            }
+
+            // 윈도우 유효하지 않을 떄 left 를 이동시킨다.
+            while (left <= right && invalidPos >= left) {
+                left = invalidPos + 1
+                if (minKPos < left) minKPos = -1
+                if (maxKPos < left) maxKPos = -1
+                invalidPos = -1
+            }
+
+            if (minKPos >= left && maxKPos >= left) {
+                val minPos = minOf(minKPos, maxKPos)
+                result += (minPos - left + 1).toLong()
             }
         }
-
         return result
     }
 }
