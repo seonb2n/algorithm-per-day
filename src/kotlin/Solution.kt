86fd93426824dbf66a9013a04f97e29d49014361
seonb2n <626ksb@gmail.com>
2025-04-27 09:46:01 +0900

    - leetcode - https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/?envType=daily-question&envId=2025-04-27 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,41 +1,18 @@
 package kotlin
 
 
-// https://leetcode.com/problems/count-subarrays-with-fixed-bounds/?envType=daily-question&envId=2025-04-26
+// https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/?envType=daily-question&envId=2025-04-27
 class Solution {
-    fun countSubarrays(nums: IntArray, minK: Int, maxK: Int): Long {
-        // sliding window
+    fun countSubarrays(nums: IntArray): Int {
+        var result = 0
         val n = nums.size
-        var result = 0L
-        var left = 0
-        var minKPos = -1
-        var maxKPos = -1
-        var invalidPos = -1 // minK 보다 작거나 max 보다 큰 값
 
-        for (right in 0 until n) {
-            if (nums[right] == minK) {
-                minKPos = right
-            }
-            if (nums[right] == maxK) {
-                maxKPos = right
-            }
-            if (nums[right] < minK || nums[right] > maxK) {
-                invalidPos = right
-            }
-
-            // 윈도우 유효하지 않을 떄 left 를 이동시킨다.
-            while (left <= right && invalidPos >= left) {
-                left = invalidPos + 1
-                if (minKPos < left) minKPos = -1
-                if (maxKPos < left) maxKPos = -1
-                invalidPos = -1
-            }
-
-            if (minKPos >= left && maxKPos >= left) {
-                val minPos = minOf(minKPos, maxKPos)
-                result += (minPos - left + 1).toLong()
+        for (i in 0 until n-2) {
+            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+                result++
             }
         }
+
         return result
     }
 }
