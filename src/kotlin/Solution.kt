ea5bd47542a9e54546c714aabc4a366b16760d49
seonb2n <626ksb@gmail.com>
2025-04-28 21:32:59 +0900

    - leetcode - https://leetcode.com/problems/count-subarrays-with-score-less-than-k/?envType=daily-question&envId=2025-04-28 - beats 88%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,35 +3,23 @@ package kotlin
 // https://leetcode.com/problems/count-subarrays-with-score-less-than-k/?envType=daily-question&envId=2025-04-28
 class Solution {
     fun countSubarrays(nums: IntArray, k: Long): Long {
-        // 누적합으로 sum 구해두기
         val n = nums.size
-        val sums = LongArray(n)
-        sums[0] = nums[0].toLong()
-        for (i in 1 until n) {
-            sums[i] = sums[i - 1] + nums[i].toLong()
-        }
+        var left = 0
+        var sum = 0L
         var result = 0L
-        if (sums[0] < k) {
-            result++
-        }
-        for (i in n-1 downTo 1) {
-            val now = sums[i] * (i + 1)
-            if (now < k) {
-                result += i.toLong()
-                break
-            }
-        }
 
-        for (i in 1 until n) {
-            for (j in n-1 downTo i) {
-                val now = (sums[j] - sums[i-1]) * (j - i + 1)
-                if (now < k) {
-                    result += (j - i + 1).toLong()
-                    break
-                }
-            }
-        }
+        for (right in 0 until n) {
+            sum += nums[right].toLong()
 
+            // 현재 윈도우의 점수 계산
+            while (left <= right && sum * (right - left + 1) >= k) {
+                sum -= nums[left].toLong()
+                left++
+            }
+
+            // 조건에 부합
+            result += (right - left + 1)
+        }
 
 
         return result
