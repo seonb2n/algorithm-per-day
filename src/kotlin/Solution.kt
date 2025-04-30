1fed0d60cf42aa45db51d6eb638569a84b6bb793
seonb2n <626ksb@gmail.com>
2025-04-30 20:08:01 +0900

    - leetcode - https://leetcode.com/problems/find-numbers-with-even-number-of-digits/?envType=daily-question&envId=2025-04-30 - beats 21%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,26 +1,13 @@
 package kotlin
 
-// https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/?envType=daily-question&envId=2025-04-29
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/?envType=daily-question&envId=2025-04-30
 class Solution {
-    fun countSubarrays(nums: IntArray, k: Int): Long {
-        val n = nums.size
-        var left = 0
-        var counter = 0
-        var result = 0L
-
-        val max = nums.maxOrNull() ?: return 0L
-
-        for (right in 0 until n) {
-            if (nums[right] == max) {
-                counter++
-            }
-
-            while (counter >= k) {
-                result += (n - right).toLong()
-                if (nums[left] == max) {
-                    counter--
-                }
-                left++
+    fun findNumbers(nums: IntArray): Int {
+        var result = 0
+        for (n in nums) {
+            val str = n.toString()
+            if (str.length % 2 == 0) {
+                result++
             }
         }
 
