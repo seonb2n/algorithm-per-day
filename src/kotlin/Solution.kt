cd7d75bbfb4de3df465834c35279f91897936cdd
seonb2n <626ksb@gmail.com>
2025-05-02 12:54:00 +0900

    - leetcode - https://leetcode.com/problems/push-dominoes/submissions/1623293834/?envType=daily-question&envId=2025-05-02 - beats 34%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,87 +2,51 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign/?envType=daily-question&envId=2025-05-01 }
+
+// https://leetcode.com/problems/push-dominoes/submissions/1623293834/?envType=daily-question&envId=2025-05-02
 class Solution {
-    fun maxTaskAssign(tasks: IntArray, workers: IntArray, pills: Int, strength: Int): Int {
-        // 작업과 작업자를 오름차순으로 정렬합니다.
-        tasks.sort()
-        workers.sort()
+    fun pushDominoes(dominoes: String): String {
+        val n = dominoes.length
+        val forces = IntArray(n) // 각 도미노에 가해지는 힘 (양수: 오른쪽, 음수: 왼쪽)
+        val result = dominoes.toCharArray()
 
-        val n = tasks.size
-        val m = workers.size
-
-        // k개의 작업을 완료할 수 있는지 확인하는 함수
-        fun canDoKTasks(k: Int): Boolean {
-            if (k == 0) return true
-            if (k > m) return false
-
-            // 사용 가능한 작업자를 관리하기 위한 멀티셋(TreeMap 사용)
-            val availableWorkers = TreeMap<Int, Int>()
-            for (workerStrength in workers) {
-                availableWorkers.put(workerStrength, availableWorkers.getOrDefault(workerStrength, 0) + 1)
-            }
-
-            var pillsLeft = pills // 남은 알약 수
-
-            for (i in k - 1 downTo 0) {
-                val taskStrength = tasks[i]
-                var assigned = false
-
-                // 1. 알약 없이 작업을 수행할 수 있는 가장 약한 작업자 찾기
-                val strongestWorkerKey = availableWorkers.lastKey()
-
-                if (strongestWorkerKey != null && strongestWorkerKey >= taskStrength) {
-                    val count = availableWorkers.get(strongestWorkerKey)!!
-                    if (count == 1) {
-                        availableWorkers.remove(strongestWorkerKey)
-                    } else {
-                        availableWorkers.put(strongestWorkerKey, count - 1)
-                    }
-                    assigned = true
-                }
-
-                // 2. 알약 없이 할 수 없으면, 알약을 사용해서 할 수 있는지 확인
-                if (!assigned && pillsLeft > 0) {
-                    val requiredStrength = taskStrength - strength
-                    val workerKeyWithPill = availableWorkers.ceilingKey(requiredStrength)
-
-                    if (workerKeyWithPill != null) {
-                        val count = availableWorkers.get(workerKeyWithPill)!!
-                        if (count == 1) {
-                            availableWorkers.remove(workerKeyWithPill)
-                        } else {
-                            availableWorkers.put(workerKeyWithPill, count - 1)
-                        }
-                        pillsLeft--
-                        assigned = true
-                    }
-                }
-
-                if (!assigned) {
-                    return false
-                }
-            }
-
-            return true
-        }
-
-
-        // 이진 탐색으로 최대 k 찾기
-        var left = 0
-        var right = minOf(n, m)
-        var result = 0 // 완료 가능한 최대 작업 수
-
-        while (left <= right) {
-            val mid = (left + right) / 2
-            if (canDoKTasks(mid)) {
-                result = mid
-                left = mid + 1
+        // 1. 오른쪽으로 가는 힘('R') 전파
+        var force = 0
+        for (i in 0 until n) {
+            if (dominoes[i] == 'R') {
+                force = n // 최대 힘으로 설정
+            } else if (dominoes[i] == 'L') {
+                force = 0 // 'L'은 오른쪽 힘을 차단
             } else {
-                right = mid - 1
+                force = maxOf(0, force - 1) // 힘은 감소하지 않음, '.'에서는 유지
+            }
+            forces[i] += force
+        }
+
+        // 2. 왼쪽으로 가는 힘('L') 전파
+        force = 0
+        for (i in n - 1 downTo 0) {
+            if (dominoes[i] == 'L') {
+                force = n // 최대 힘으로 설정
+            } else if (dominoes[i] == 'R') {
+                force = 0 // 'R'은 왼쪽 힘을 차단
+            } else {
+                force = maxOf(0, force - 1) // 힘은 감소하지 않음
+            }
+            forces[i] -= force
+        }
+
+        // 3. 최종 상태 결정
+        for (i in 0 until n) {
+            if (forces[i] > 0) {
+                result[i] = 'R'
+            } else if (forces[i] < 0) {
+                result[i] = 'L'
+            } else {
+                result[i] = '.'
             }
         }
 
-        return result
+        return result.joinToString("")
     }
 }
