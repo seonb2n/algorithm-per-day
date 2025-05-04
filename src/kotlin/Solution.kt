84d3f620ca9fc9b9e1e69cf0f68056a344e79844
seonb2n <626ksb@gmail.com>
2025-05-04 20:41:47 +0900

    - leetcode - https://leetcode.com/problems/number-of-equivalent-domino-pairs/?envType=daily-question&envId=2025-05-04 - beats 63%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,51 +2,17 @@ package kotlin
 
 import java.util.*
 
-
-// https://leetcode.com/problems/push-dominoes/submissions/1623293834/?envType=daily-question&envId=2025-05-02
+// https://leetcode.com/problems/number-of-equivalent-domino-pairs/?envType=daily-question&envId=2025-05-04
 class Solution {
-    fun pushDominoes(dominoes: String): String {
-        val n = dominoes.length
-        val forces = IntArray(n) // 각 도미노에 가해지는 힘 (양수: 오른쪽, 음수: 왼쪽)
-        val result = dominoes.toCharArray()
-
-        // 1. 오른쪽으로 가는 힘('R') 전파
-        var force = 0
-        for (i in 0 until n) {
-            if (dominoes[i] == 'R') {
-                force = n // 최대 힘으로 설정
-            } else if (dominoes[i] == 'L') {
-                force = 0 // 'L'은 오른쪽 힘을 차단
-            } else {
-                force = maxOf(0, force - 1) // 힘은 감소하지 않음, '.'에서는 유지
-            }
-            forces[i] += force
+    fun numEquivDominoPairs(dominoes: Array<IntArray>): Int {
+        val pairMap = mutableMapOf<Pair<Int, Int>, Int>()
+        var result = 0
+        for (d in dominoes) {
+            val now = if(d[0] > d[1]) Pair(d[1], d[0]) else Pair(d[0], d[1])
+            val exist = pairMap.getOrDefault(now, 0)
+            result += exist
+            pairMap[now] = exist + 1
         }
-
-        // 2. 왼쪽으로 가는 힘('L') 전파
-        force = 0
-        for (i in n - 1 downTo 0) {
-            if (dominoes[i] == 'L') {
-                force = n // 최대 힘으로 설정
-            } else if (dominoes[i] == 'R') {
-                force = 0 // 'R'은 왼쪽 힘을 차단
-            } else {
-                force = maxOf(0, force - 1) // 힘은 감소하지 않음
-            }
-            forces[i] -= force
-        }
-
-        // 3. 최종 상태 결정
-        for (i in 0 until n) {
-            if (forces[i] > 0) {
-                result[i] = 'R'
-            } else if (forces[i] < 0) {
-                result[i] = 'L'
-            } else {
-                result[i] = '.'
-            }
-        }
-
-        return result.joinToString("")
+        return result
     }
 }
