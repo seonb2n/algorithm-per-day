32aaaa6eea40afeeb041fa511b01258dd2a52141
seonb2n <626ksb@gmail.com>
2025-05-05 11:31:14 +0900

    - leetcode - https://leetcode.com/problems/domino-and-tromino-tiling/description/?envType=daily-question&envId=2025-05-05 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,17 +2,25 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/number-of-equivalent-domino-pairs/?envType=daily-question&envId=2025-05-04
+// https://leetcode.com/problems/domino-and-tromino-tiling/description/?envType=daily-question&envId=2025-05-05
 class Solution {
-    fun numEquivDominoPairs(dominoes: Array<IntArray>): Int {
-        val pairMap = mutableMapOf<Pair<Int, Int>, Int>()
-        var result = 0
-        for (d in dominoes) {
-            val now = if(d[0] > d[1]) Pair(d[1], d[0]) else Pair(d[0], d[1])
-            val exist = pairMap.getOrDefault(now, 0)
-            result += exist
-            pairMap[now] = exist + 1
+    fun numTilings(n: Int): Int {
+        if (n == 1) return 1
+        if (n == 2) return 2
+
+        val modulo = 1_000_000_007 // 10^9 + 7
+
+        // Long 타입 사용 (정수 오버플로우 방지)
+        val dp = LongArray(1001)
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+
+        for (i in 4..n) {
+            // 올바른 점화식 적용 (Long 타입으로 계산)
+            dp[i] = (dp[i-1] * 2 + dp[i-3]) % modulo
         }
-        return result
+
+        return dp[n].toInt()
     }
 }
