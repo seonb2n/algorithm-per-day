8f226f8cb4b8576e834eb42ea342177fda0a32e2
seonb2n <626ksb@gmail.com>
2025-05-07 22:25:08 +0900

    - leetcode - https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/?envType=daily-question&envId=2025-05-07 - beats 80%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,19 +1,54 @@
 package kotlin
 
 import java.util.*
+import kotlin.collections.ArrayDeque
 
-// https://leetcode.com/problems/domino-and-tromino-tiling/description/?envType=daily-question&envId=2025-05-05
-// https://leetcode.com/problems/build-array-from-permutation/?envType=daily-question&envId=2025-05-06
+
+// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/?envType=daily-question&envId=2025-05-07
 class Solution {
-    fun buildArray(nums: IntArray): IntArray {
-        val n = nums.size
-        val result = IntArray(n)
+    fun minTimeToReach(moveTime: Array<IntArray>): Int {
+        var result = Int.MAX_VALUE
+        val n = moveTime.size
+        val m = moveTime[0].size
 
-        for (i in nums.indices) {
-            val now = nums[nums[i]]
-            result[i] = now
+        // 각 방에 도달가능한 최소 시간
+        val minTime = Array(n) { IntArray(m) { Int.MAX_VALUE } }
+
+        val dir = arrayOf(
+            intArrayOf(1, 0),
+            intArrayOf(-1, 0),
+            intArrayOf(0, 1),
+            intArrayOf(0, -1)
+        )
+
+        val deque = ArrayDeque<Triple<Int, Int, Int>>()
+        deque.add(Triple(0, 0, 0))
+
+        while (deque.isNotEmpty()) {
+            val now = deque.removeFirst()
+
+            if (now.first == n - 1 && now.second == m - 1) {
+                result = minOf(result, now.third)
+            }
+
+            for (d in dir) {
+                val nextI = d[0] + now.first
+                val nextJ = d[1] + now.second
+
+                if (0 <= nextI && nextI < n && 0 <= nextJ && nextJ < m) {
+                    var nextTime = now.third + 1
+
+                    if (nextTime <= moveTime[nextI][nextJ]) {
+                        nextTime = moveTime[nextI][nextJ] + 1
+                    }
+                    if (nextTime < minTime[nextI][nextJ]) {
+                        minTime[nextI][nextJ] = nextTime
+                        println("${nextI}:${nextJ}:${nextTime}")
+                        deque.add(Triple(nextI, nextJ, nextTime))
+                    }
+                }
+            }
         }
-
-        return result
+        return -1
     }
 }
