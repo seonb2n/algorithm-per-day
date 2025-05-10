f2caec85661150ec892afc093b988e44cdc3f504
seonb2n <626ksb@gmail.com>
2025-05-10 11:04:03 +0900

    - leetcode - https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/?envType=daily-question&envId=2025-05-10 - beats -%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,63 +4,59 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
-// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-ii/?envType=daily-question&envId=2025-05-08
+// https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/?envType=daily-question&envId=2025-05-10
 class Solution {
-    fun minTimeToReach(moveTime: Array<IntArray>): Int {
-        var result = Int.MAX_VALUE
-        val n = moveTime.size
-        val m = moveTime[0].size
-
-        // 각 방에 도달가능한 최소 시간
-        val minTime = Array(n) { IntArray(m) { Int.MAX_VALUE } }
-
-        val dir = arrayOf(
-            intArrayOf(1, 0),
-            intArrayOf(-1, 0),
-            intArrayOf(0, 1),
-            intArrayOf(0, -1)
-        )
-
-        val deque = ArrayDeque<Node>()
-        deque.add(Node(0, 0, 0, false))
-
-        while (deque.isNotEmpty()) {
-            val now = deque.removeFirst()
-
-            if (now.i == n - 1 && now.j == m - 1) {
-               result = minOf(result, now.time)
-                continue
+    fun minSum(nums1: IntArray, nums2: IntArray): Long {
+        // 0 이 없는 nums 가 기준이 되어야 한다.
+        // 0 이 있는 nums 는 최솟값이 정해져야 한다.
+        var nums1Sum = 0L
+        var nums1ZeroCount = 0
+        for (n in nums1) {
+            if (n == 0) {
+                nums1ZeroCount++
             }
-
-            if (now.time > minTime[now.i][now.j]) continue
-
-            for (d in dir) {
-                val nextI = d[0] + now.i
-                val nextJ = d[1] + now.j
-
-                if (0 <= nextI && nextI < n && 0 <= nextJ && nextJ < m) {
-                    var nextTime = now.time + 1
-
-                    if (nextTime <= moveTime[nextI][nextJ]) {
-                        nextTime = moveTime[nextI][nextJ] + 1
-                    }
-                    if (now.odd) {
-                        nextTime++
-                    }
-                    if (nextTime < minTime[nextI][nextJ]) {
-                        minTime[nextI][nextJ] = nextTime
-                        deque.add(Node(nextI, nextJ, nextTime, !now.odd))
-                    }
-                }
-            }
+            nums1Sum += n
         }
-        return result
-    }
 
-    data class Node(
-        val i: Int,
-        val j: Int,
-        val time: Int,
-        val odd: Boolean,
-    )
+        var nums2Sum = 0L
+        var nums2ZeroCount = 0
+        for (n in nums2) {
+            if (n == 0) {
+                nums2ZeroCount++
+            }
+            nums2Sum += n
+        }
+
+        if (nums1ZeroCount ==0 && nums2ZeroCount == 0 && nums1Sum == nums2Sum) {
+            return nums1Sum
+        }
+
+        if (nums1ZeroCount == 0 && nums2ZeroCount == 0) {
+            return -1
+        }
+
+        // 둘 모두 zero 가 있으면
+        if (nums1ZeroCount > 0 && nums2ZeroCount > 0) {
+            val nums1Min = nums1ZeroCount + nums1Sum
+            val nums2Min = nums2ZeroCount + nums2Sum
+            return maxOf(nums1Min, nums2Min)
+        }
+        // 1만 zero 가 있으면
+        if (nums1ZeroCount > 0) {
+            val nums1Min = nums1ZeroCount + nums1Sum
+            if (nums1Min > nums2Sum) {
+                return -1
+            }
+            return nums2Sum
+        }
+        if (nums2ZeroCount > 0) {
+            val nums2Min = nums2ZeroCount + nums2Sum
+            if (nums2Min > nums1Sum) {
+                return -1
+            }
+            return nums1Sum
+        }
+
+        return -1
+    }
 }
