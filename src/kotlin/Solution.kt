5d152f94b51ce6b111b1166ad4bff2ce51c0dce3
seonb2n <626ksb@gmail.com>
2025-05-15 20:35:45 +0900

    - leetcode - https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/?envType=daily-question&envId=2025-05-15 - beats 50%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,75 +5,26 @@ import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
 
+// https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/?envType=daily-question&envId=2025-05-15
 class Solution {
-    fun lengthAfterTransformations(s: String, t: Int, nums: List<Int>): Int {
-        val modulo = 1_000_000_007
-        val vector = IntArray(26)
-        for (c in s) {
-            val now = c - 'a'
-            vector[now]++
-        }
-
-        // 변환행렬
-        // 연산 후, j 가 변환 후 i 로 몇개 나오는지
-        val matrix = Array(26) { LongArray(26) }
-        for (i in 0 until 26) {
-            val num = nums[i]
-            for (j in 1..num) {
-                val nextChar = (i + j) % 26
-                matrix[nextChar][i] = 1L
-            }
-        }
-
-        // 행렬 거듭제곱 계산
-        val resultMatrix = matrixPow(matrix, t, modulo)
-
-        var result = 0L
-        for (i in 0 until 26) {
-            for (j in 0 until 26) {
-                result = (result + resultMatrix[i][j] * vector[j]) % modulo
-            }
-        }
-        return result.toInt()
-    }
-
-    // 행렬 곱셈
-    private fun multiplyMatrix(a: Array<LongArray>, b: Array<LongArray>, modulo: Int): Array<LongArray> {
-        val n = a.size
-        val result = Array(n) { LongArray(n) }
-
+    fun getLongestSubsequence(words: Array<String>, groups: IntArray): List<String> {
+        val n = words.size
+        var result: List<String> = emptyList()
+        // greedy
         for (i in 0 until n) {
-            for (j in 0 until n) {
-                for (k in 0 until n) {
-                    result[i][j] = (result[i][j] + a[i][k] * b[k][j]) % modulo
-                }
+            val now = mutableListOf<String>()
+            var last = groups[i]
+            now.add(words[i])
+            for (j in i + 1 until n) {
+               if (groups[j] != last) {
+                   now.add(words[j])
+                   last = groups[j]
+               }
+            }
+            if (result.size < now.size) {
+                result = now
             }
         }
-
-        return result
-    }
-
-    private fun matrixPow(matrix: Array<LongArray>, power: Int, modulo: Int): Array<LongArray> {
-        val n = matrix.size
-        var result = Array(n) { LongArray(n) }
-
-        // 단위 행렬로 초기화
-        for (i in 0 until n) {
-            result[i][i] = 1
-        }
-
-        var base = matrix
-        var exp = power
-
-        while (exp > 0) {
-            if (exp % 2 == 1) {
-                result = multiplyMatrix(result, base, modulo)
-            }
-
-            base = multiplyMatrix(base, base, modulo)
-            exp /= 2
-        }
-
         return result
     }
 }
