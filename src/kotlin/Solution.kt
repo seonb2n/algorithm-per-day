c847f6e1ebc09135aa85f3f1151b063c47f0fef4
seonb2n <626ksb@gmail.com>
2025-05-19 19:01:04 +0900

    - leetcode - https://leetcode.com/problems/type-of-triangle/?envType=daily-question&envId=2025-05-19 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,36 +4,21 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
-
-// https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2025-05-17
+// https://leetcode.com/problems/type-of-triangle/?envType=daily-question&envId=2025-05-19
 class Solution {
-    fun sortColors(nums: IntArray): Unit {
-        val counter = IntArray(3)
-
-        for (n in nums) {
-            if (n == 0) {
-                counter[0]++
-            }
-            else if (n == 1) {
-                counter[1]++
-            }
-            else if (n == 2) {
-                counter[2]++
-            }
+    fun triangleType(nums: IntArray): String {
+        // equilateral
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral"
         }
-        for (i in nums.indices) {
-            if (counter[0] > 0) {
-                nums[i] = 0
-                counter[0]--
-            }
-            else if (counter[1] > 0) {
-                nums[i] = 1
-                counter[1]--
-            }
-            else if (counter[2] > 0) {
-                nums[i] = 2
-                counter[2]--
-            }
+        if ((nums[0] == nums[1] && nums[0] + nums[1] > nums[2])
+            || (nums[1] == nums[2] && nums[1] + nums[2] > nums[0])
+            || (nums[0] == nums[2] && nums[0] + nums[2] > nums[1])) {
+            return "isosceles"
         }
+        if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) {
+            return "scalene"
+        }
+        return "none"
     }
 }
