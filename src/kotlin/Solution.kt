52c84beaea1d8e173b15756433bd3af415419d4d
seonb2n <626ksb@gmail.com>
2025-05-20 20:25:23 +0900

    - leetcode - https://leetcode.com/problems/zero-array-transformation-i/?envType=daily-question&envId=2025-05-20 - beats 41%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,21 +4,27 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
-// https://leetcode.com/problems/type-of-triangle/?envType=daily-question&envId=2025-05-19
+// https://leetcode.com/problems/zero-array-transformation-i/?envType=daily-question&envId=2025-05-20
 class Solution {
-    fun triangleType(nums: IntArray): String {
-        // equilateral
-        if (nums[0] == nums[1] && nums[1] == nums[2]) {
-            return "equilateral"
+    fun isZeroArray(nums: IntArray, queries: Array<IntArray>): Boolean {
+        val sums = IntArray(nums.size + 1)
+        for (q in queries) {
+            sums[q[0]]--
+            sums[q[1] + 1]++
         }
-        if ((nums[0] == nums[1] && nums[0] + nums[1] > nums[2])
-            || (nums[1] == nums[2] && nums[1] + nums[2] > nums[0])
-            || (nums[0] == nums[2] && nums[0] + nums[2] > nums[1])) {
-            return "isosceles"
+
+        // 누적합 구하기
+        val total = IntArray(nums.size + 1)
+        total[0] = sums[0]
+        for (i in 1..nums.size) {
+            total[i] = sums[i] + total[i - 1]
         }
-        if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) {
-            return "scalene"
+
+        for (i in nums.indices) {
+            if (nums[i] + total[i] > 0) {
+                return false
+            }
         }
-        return "none"
+        return true
     }
 }
