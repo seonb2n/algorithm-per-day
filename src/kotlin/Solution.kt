781a982a0e81858daceb5131fcd9a487041edb61
seonb2n <626ksb@gmail.com>
2025-05-22 20:35:28 +0900

    - leetcode - https://leetcode.com/problems/zero-array-transformation-iii/?envType=daily-question&envId=2025-05-22 - time limit

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,29 +4,68 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
-// https://leetcode.com/problems/set-matrix-zeroes/?envType=daily-question&envId=2025-05-21
+// https://leetcode.com/problems/zero-array-transformation-iii/?envType=daily-question&envId=2025-05-22
 class Solution {
-    fun setZeroes(matrix: Array<IntArray>): Unit {
-        val m = matrix.size
-        val n = matrix[0].size
-        val mArray = IntArray(m)
-        val nArray = IntArray(n)
+    fun maxRemoval(nums: IntArray, queries: Array<IntArray>): Int {
+        val n = nums.size
+        val queryGroups = Array(n) { mutableListOf<Query>() }
 
-        for (i in 0 until m) {
-            for (j in 0 until n) {
-                if (matrix[i][j] == 0) {
-                    mArray[i] = -1
-                    nArray[j] = -1
+        for (i in queries.indices) {
+            val start = queries[i][0]
+            if (start < n) {
+                queryGroups[start].add(Query(queries[i][0], queries[i][1], i))
+            }
+        }
+
+        // 현재 사용 가능한 쿼리들 (끝점이 먼 순서대로)
+        val availableQueries = PriorityQueue<Query>()
+        var used = 0
+
+        for (pos in 0 until n) {
+            availableQueries.addAll(queryGroups[pos])
+
+            val validQueries = mutableListOf<Query>()
+            while (availableQueries.isNotEmpty()) {
+                val query = availableQueries.poll()
+                if (query.end >= pos) {
+                    validQueries.add(query)
                 }
             }
-        }
+            availableQueries.addAll(validQueries)
 
-        for (i in 0 until m) {
-            for (j in 0 until n) {
-               if (mArray[i] == -1 || nArray[j] == -1) {
-                   matrix[i][j] = 0
-               }
+            // 현재 위치에서 필요한 만큼 쿼리 사용
+            while (nums[pos] > 0) {
+                if (availableQueries.isEmpty()) {
+                    return -1
+                }
+
+                val q = availableQueries.poll()
+                // 쿼리 적용
+                for (i in q.start..q.end) {
+                    if (i < n) {
+                        nums[i]--
+                    }
+                }
+                used++
             }
         }
+
+        for (num in nums) {
+            if (num > 0) {
+                return -1
+            }
+        }
+
+        return queries.size - used
+    }
+}
+
+class Query(
+    val start: Int,
+    val end: Int,
+    val index: Int = -1
+) : Comparable<Query> {
+    override fun compareTo(other: Query): Int {
+        return other.end.compareTo(this.end)
     }
 }
