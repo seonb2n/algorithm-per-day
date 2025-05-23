c48eb0d950ab162c725dbb7238c7d2fda37b5848
seonb2n <626ksb@gmail.com>
2025-05-23 19:09:34 +0900

    - leetcode - https://leetcode.com/problems/find-the-maximum-sum-of-node-values/?envType=daily-question&envId=2025-05-23 - time limit

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -4,68 +4,52 @@ import java.util.*
 import kotlin.collections.ArrayDeque
 import kotlin.math.min
 
-// https://leetcode.com/problems/zero-array-transformation-iii/?envType=daily-question&envId=2025-05-22
+//https://leetcode.com/problems/find-the-maximum-sum-of-node-values/?envType=daily-question&envId=2025-05-23
 class Solution {
-    fun maxRemoval(nums: IntArray, queries: Array<IntArray>): Int {
+    fun maximumValueSum(nums: IntArray, k: Int, edges: Array<IntArray>): Long {
         val n = nums.size
-        val queryGroups = Array(n) { mutableListOf<Query>() }
+        val graph = Array(n) { mutableListOf<Int>() }
 
-        for (i in queries.indices) {
-            val start = queries[i][0]
-            if (start < n) {
-                queryGroups[start].add(Query(queries[i][0], queries[i][1], i))
-            }
+        for ((i, edge) in edges.withIndex()) {
+            graph[edge[0]].add(i)
+            graph[edge[1]].add(i)
         }
 
-        // 현재 사용 가능한 쿼리들 (끝점이 먼 순서대로)
-        val availableQueries = PriorityQueue<Query>()
-        var used = 0
+        val used = BooleanArray(edges.size)
+        var maxSum = 0L
 
-        for (pos in 0 until n) {
-            availableQueries.addAll(queryGroups[pos])
-
-            val validQueries = mutableListOf<Query>()
-            while (availableQueries.isNotEmpty()) {
-                val query = availableQueries.poll()
-                if (query.end >= pos) {
-                    validQueries.add(query)
+        fun dfs(edgeChoices: BooleanArray) {
+            val xorCount = IntArray(n)
+            for (i in edgeChoices.indices) {
+                if (edgeChoices[i]) {
+                    xorCount[edges[i][0]]++
+                    xorCount[edges[i][1]]++
                 }
             }
-            availableQueries.addAll(validQueries)
 
-            // 현재 위치에서 필요한 만큼 쿼리 사용
-            while (nums[pos] > 0) {
-                if (availableQueries.isEmpty()) {
-                    return -1
-                }
-
-                val q = availableQueries.poll()
-                // 쿼리 적용
-                for (i in q.start..q.end) {
-                    if (i < n) {
-                        nums[i]--
-                    }
-                }
-                used++
+            // 최종 값들의 합 계산
+            var sum = 0L
+            for (i in 0 until n) {
+                val finalValue = if (xorCount[i] % 2 == 1) nums[i] xor k else nums[i]
+                sum += finalValue
             }
+            maxSum = maxOf(maxSum, sum)
         }
 
-        for (num in nums) {
-            if (num > 0) {
-                return -1
+        fun backtrack(edgeIdx: Int, choices: BooleanArray) {
+            if (edgeIdx == edges.size) {
+                dfs(choices)
+                return
             }
+
+            backtrack(edgeIdx + 1, choices)
+
+            choices[edgeIdx] = true
+            backtrack(edgeIdx + 1, choices)
+            choices[edgeIdx] = false
         }
 
-        return queries.size - used
-    }
-}
-
-class Query(
-    val start: Int,
-    val end: Int,
-    val index: Int = -1
-) : Comparable<Query> {
-    override fun compareTo(other: Query): Int {
-        return other.end.compareTo(this.end)
+        backtrack(0, BooleanArray(edges.size))
+        return maxSum
     }
 }
