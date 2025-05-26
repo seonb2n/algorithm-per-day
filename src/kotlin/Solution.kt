f4b16c34334d8650122735824846d49a79653377
seonb2n <626ksb@gmail.com>
2025-05-26 21:20:46 +0900

    - leetcode - https://leetcode.com/problems/largest-color-value-in-a-directed-graph/?envType=daily-question&envId=2025-05-26 - beats 50%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,41 +2,65 @@ package kotlin
 
 import java.util.*
 import kotlin.collections.ArrayDeque
+import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/?envType=daily-question&envId=2025-05-25
+// https://leetcode.com/problems/largest-color-value-in-a-directed-graph/?envType=daily-question&envId=2025-05-26
 class Solution {
-    fun longestPalindrome(words: Array<String>): Int {
-        // 각 단어의 빈도를 저장하는 맵
-        val wordCount = mutableMapOf<String, Int>()
-
-        for (word in words) {
-            wordCount[word] = wordCount.getOrDefault(word, 0) + 1
+    fun largestPathValue(colors: String, edges: Array<IntArray>): Int {
+        val n = colors.length
+        val graph = Array(n) { mutableListOf<Int>() }
+        // 진입 차수
+        val inDegree = IntArray(n)
+        for (edge in edges) {
+            graph[edge[0]].add(edge[1])
+            inDegree[edge[1]]++
         }
 
-        var pairs = 0
-        var centerUsed = false
-        val processed = mutableSetOf<String>()
+        // dp[i][j] = i 번 노드에서 j 번째 색상의 최대 ㄱ수
+        val dp = Array(n) { IntArray(26) }
+        val queue: Queue<Int> = LinkedList()
+        // 처리된 노드 수
+        var processed = 0
 
-        for ((word, count) in wordCount) {
-            if (processed.contains(word)) continue
-
-            val reversed = word.reversed()
-
-            if (word == reversed) {
-                pairs += count / 2
-                if (count % 2 == 1 && !centerUsed) {
-                    centerUsed = true
-                }
-            } else if (wordCount.containsKey(reversed)) {
-                val pairCount = minOf(count, wordCount[reversed]!!)
-                pairs += pairCount
-                processed.add(reversed)
+        // 진입 차수가 0 인 노드를 queue 추가
+        for (i in 0 until n) {
+            if (inDegree[i] == 0) {
+                queue.offer(i)
+                // 해당 위치는 자기 색상을 갖고 있음
+                dp[i][colors[i] - 'a'] = 1
             }
-
-            processed.add(word)
         }
 
-        return pairs * 4 + if (centerUsed) 2 else 0
+        while (queue.isNotEmpty()) {
+            val current = queue.poll()
+            processed++
+            for (next in graph[current]) {
+                for (c in 0 until 26) {
+                    val color = 'a' + c
+                    val addValue = if (colors[next] == color) 1 else 0
+                    // 다음 색상 업데이트
+                    dp[next][c] = maxOf(dp[next][c], dp[current][c] + addValue)
+                }
+
+                inDegree[next]--
+                if (inDegree[next] == 0) {
+                    queue.offer(next)
+                }
+            }
+        }
+
+        // 처리되지 않은 노드가 있다면 사이클이 존재
+        if (processed != n) {
+            return -1
+        }
+
+        var maxValue = 0
+        for (i in 0 until n) {
+            for (c in 0 until 26) {
+                maxValue = maxOf(maxValue, dp[i][c])
+            }
+        }
+        return maxValue
     }
 }
