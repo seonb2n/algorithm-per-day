1ab59c871de5d47e3672bbe24172f8b04fb4c92c
seonb2n <626ksb@gmail.com>
2025-05-31 10:23:26 +0900

    - leetcode - https://leetcode.com/problems/find-closest-node-to-given-two-nodes/?envType=daily-question&envId=2025-05-30 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,13 +5,44 @@ import kotlin.collections.ArrayDeque
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/?envType=daily-question&envId=2025-05-27
+// https://leetcode.com/problems/find-closest-node-to-given-two-nodes/?envType=daily-question&envId=2025-05-30
 class Solution {
-    fun differenceOfSums(n: Int, m: Int): Int {
-        val totalSum = n * (n + 1) / 2
-        val k = n / m  // m의 배수 개수
-        val multiplesSum = m * k * (k + 1) / 2
+    fun closestMeetingNode(edges: IntArray, node1: Int, node2: Int): Int {
+        val n = edges.size
 
-        return totalSum - 2 * multiplesSum
+        fun getDist(start: Int): IntArray {
+            val dist = IntArray(n) { -1 }
+            val visited = BooleanArray(n)
+
+            var current = start
+            var cost = 0
+
+            while (current != -1 && !visited[current]) {
+                visited[current] = true
+                dist[current] = cost
+                current = edges[current]
+                cost++
+            }
+
+            return dist
+        }
+
+        val dist1 = getDist(node1)
+        val dist2 = getDist(node2)
+
+        var result = -1
+        var minMaxDist = Int.MAX_VALUE
+
+        for (i in 0 until n) {
+            if (dist1[i] != -1 && dist2[i] != -1) {  // 둘 다 도달 가능
+                val maxDist = maxOf(dist1[i], dist2[i])
+                if (maxDist < minMaxDist) {
+                    minMaxDist = maxDist
+                    result = i
+                }
+            }
+        }
+
+        return result
     }
 }
