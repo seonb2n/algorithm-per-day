5e04025948e5a6b7a10f9928e7a1edf0ac6e3e69
seonb2n <626ksb@gmail.com>
2025-06-06 13:09:54 +0900

    - leetcode - https://leetcode.com/problems/lexicographically-smallest-equivalent-string/?envType=daily-question&envId=2025-06-05 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,29 +2,51 @@ package kotlin
 
 import java.util.*
 import kotlin.collections.ArrayDeque
+import kotlin.collections.HashMap
 import kotlin.math.max
 import kotlin.math.min
 
-
-// https://leetcode.com/problems/find-the-lexicographically-largest-string-from-the-box-i/?envType=daily-question&envId=2025-06-04
+// https://leetcode.com/problems/lexicographically-smallest-equivalent-string/?envType=daily-question&envId=2025-06-05
 class Solution {
-    fun answerString(word: String, numFriends: Int): String {
-        val n = word.length
-        if (numFriends == 1) return word
-        var result = ""
-        val maxLength = n - numFriends + 1
+    fun smallestEquivalentString(s1: String, s2: String, baseStr: String): String {
+        // union find
+        // 각 문자의 부모
+        val parent = IntArray(26) { it }
 
-        for (i in 0 until n) {
-            // 가능한 최대 길이로 확인
-            val endIndex = minOf(n, i + maxLength)
-            if (endIndex > i) {
-                val subString = word.substring(i, endIndex)
-                if (subString > result) {
-                    result = subString
+        // 문자가 속한 그룹 찾기
+        fun find(x: Int): Int {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x])
+            }
+            return parent[x]
+        }
+
+        // 그룹 합치기
+        fun union(x: Int, y: Int) {
+            val rootX = find(x)
+            val rootY = find(y)
+            if (rootX != rootY) {
+                if (rootX < rootY) {
+                    parent[rootY] = rootX
+                } else {
+                    parent[rootX] = rootY
                 }
             }
         }
 
-        return result
+        val n = s1.length
+        for (i in 0 until n) {
+            val char1 = s1[i] - 'a'
+            val char2 = s2[i] - 'a'
+            union(char1, char2)
+        }
+
+        // baseStr
+        val result = StringBuilder()
+        for (c in baseStr) {
+            val root = find(c - 'a')
+            result.append('a' + root)
+        }
+        return result.toString()
     }
 }
