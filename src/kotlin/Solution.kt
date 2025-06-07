97c362f447e816e4d5e845bb2a0430936e01d0d4
seonb2n <626ksb@gmail.com>
2025-06-07 09:55:30 +0900

    - leetcode - https://leetcode.com/problems/lexicographically-minimum-string-after-removing-stars/?envType=daily-question&envId=2025-06-07 - beats 50%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -6,40 +6,30 @@ import kotlin.collections.HashMap
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/?envType=daily-question&envId=2025-06-06
+// https://leetcode.com/problems/lexicographically-minimum-string-after-removing-stars/?envType=daily-question&envId=2025-06-07
 class Solution {
-    fun robotWithString(s: String): String {
-        val stack = Stack<Char>()
-        val freq = IntArray(26)
-
-        for (c in s) {
-            freq[c - 'a']++
+    fun clearStars(s: String): String {
+        val pq = PriorityQueue<Pair<Char, Int>>() { a, b ->
+            if (a.first != b.first) a.first.compareTo(b.first)
+            else b.second.compareTo(a.second)
         }
 
+        val toRemove = mutableSetOf<Int>()
+
+        for (i in s.indices) {
+            if (s[i] == '*') {
+               val removed = pq.poll()
+                toRemove.add(removed.second)
+                toRemove.add(i)
+            } else {
+                pq.add(Pair(s[i], i))
+            }
+        }
         val sb = StringBuilder()
-
-        fun smallestChar(): Char {
-            for (i in freq.indices) {
-                if (freq[i] > 0) {
-                    return 'a' + i
-                }
+        for (i in s.indices) {
+            if (i !in toRemove) {
+                sb.append(s[i])
             }
-            return 'a'
-        }
-
-        for (c in s) {
-            stack.push(c)
-            freq[c - 'a']--
-
-
-            // 현재 stack top 이 앞으로 나올 가장 작은 문자보다 작으면 지금 pop
-            while (!stack.empty() && stack.peek() <= smallestChar()) {
-                sb.append(stack.pop())
-            }
-        }
-        // 남은 마지막 추가
-        while (!stack.empty()) {
-            sb.append(stack.pop())
         }
 
         return sb.toString()
