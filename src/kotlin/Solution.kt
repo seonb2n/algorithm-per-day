8d81e366ea175edbcbc5e0626cb89d426dbc11de
seonb2n <626ksb@gmail.com>
2025-06-11 21:38:40 +0900

    - leetcode - https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/?envType=daily-question&envId=2025-06-10 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -6,43 +6,32 @@ import kotlin.collections.HashMap
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/?envType=daily-question&envId=2025-06-09
+// https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/?envType=daily-question&envId=2025-06-10
 class Solution {
-    fun findKthNumber(n: Int, k: Int): Int {
-        val result = mutableListOf<Int>()
+    fun maxDifference(s: String): Int {
+        val freq = IntArray(26)
+        for (c in s) {
+            freq[c - 'a']++
+        }
 
-
-        fun dfs(now: Int) {
-            result.add(now)
-            if (result.size > k) {
-                return
-            }
-            val next = now * 10
-            if (next <= n) {
-                dfs(next)
-            }
-            // 자릿수가 바뀌기 전까지 더함
-            if (now % 10 == 0)  {
-                val limit = (now / 10 + 1) * 10
-                for (i in 1 until 10) {
-                    val plus = now + i
-                    if (plus > limit) {
-                        break
+        var max = 0
+        var min = Int.MAX_VALUE
+        for (i in freq.indices) {
+            val now = freq[i]
+            if (now != 0) {
+                if (now % 2 == 0) {
+                    if (now < min) {
+                        min = now
                     }
-                    if (plus <= n) {
-                        dfs(plus)
-                    } else {
-                        break
+                }
+                else {
+                    if (now > max) {
+                        max = now
                     }
                 }
             }
-        }
 
-        for (i in 1 until 10) {
-            dfs(i)
         }
-
-        return result.get(k-1)
+        return max - min
     }
 }
-
