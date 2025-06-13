4b193978c592e6d00b743be864a94fde13ed7d84
seonb2n <626ksb@gmail.com>
2025-06-13 22:43:12 +0900

    - leetcode - https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/?envType=daily-question&envId=2025-06-13 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -8,16 +8,45 @@ import kotlin.math.max
 import kotlin.math.min
 
 
-// https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/?envType=daily-question&envId=2025-06-12
+
+// https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/?envType=daily-question&envId=2025-06-13
 class Solution {
-    fun maxAdjacentDistance(nums: IntArray): Int {
-        var max = 0
+    fun minimizeMax(nums: IntArray, p: Int): Int {
         val n = nums.size
-        max = abs( nums[n-1] - nums[0])
-        for (i in 0 until n) {
-            max = maxOf(max, abs(nums[i] - nums[i + 1]))
+
+        nums.sort()
+        // 주어진 min 으로 p 개의 쌍을 만들 수 있는지 체크
+        var left = 0
+        var right = nums[n - 1] - nums[0]
+
+        fun canFormPairs(mid: Int): Boolean {
+            var pairs = 0
+            var i = 0
+            while (i < n - 1) {
+                if (nums[i + 1] - nums[i] <= mid) {
+                    pairs++
+                    i += 2
+                    if (pairs == p) {
+                        return true
+                    }
+                }
+                else {
+                    i++
+                }
+            }
+            return pairs >= p
         }
 
-        return max
+        while (left < right) {
+            val mid = (left + right) / 2
+            // mid 개 만들 수 있는지
+            if (canFormPairs(mid)) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return left
     }
 }
