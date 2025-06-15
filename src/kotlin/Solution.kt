deb7b9ffeef916d6aabc999b2b94e7c01a07f284
seonb2n <626ksb@gmail.com>
2025-06-15 10:16:16 +0900

    - leetcode - https://leetcode.com/problems/max-difference-you-can-get-from-changing-an-integer/?envType=daily-question&envId=2025-06-15 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -7,9 +7,10 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/?envType=daily-question&envId=2025-06-14
+
+// https://leetcode.com/problems/max-difference-you-can-get-from-changing-an-integer/?envType=daily-question&envId=2025-06-15
 class Solution {
-    fun minMaxDifference(num: Int): Int {
+    fun maxDiff(num: Int): Int {
         val target = num.toString()
         // 앞에서부터 9 가 아닌 숫자 remap
         val max = StringBuilder()
@@ -26,31 +27,40 @@ class Solution {
                 break
             }
         }
-        // max 가 비었다면 전체 9 로 구성됐으니 길이만큼 8 로 치환
         if (max.isEmpty()) {
             for (i in target.indices) {
                 max.append("9")
             }
         }
 
-        // 앞에서부터 0 이 아닌 숫자 remap
         val min = StringBuilder()
-        for (i in target.indices) {
-            val now = target[i]
-            if (now != '0') {
-                for (c in target) {
-                    if (c == now) {
-                        min.append("0")
-                    } else {
-                        min.append(c)
+
+        // 제일 앞자리가 1인 경우
+        if (target[0] == '1') {
+            var replaced = false
+            for (i in target.indices) {
+                val now = target[i]
+                if (!replaced && now != '0' && now != '1') {
+                    // 이 숫자를 0으로 바꾸기로 결정
+                    for (c in target) {
+                        min.append(if (c == now) '0' else c)
                     }
+                    replaced = true
+                    break
                 }
-                break
+            }
+            // 만약 1과 0만 있는 경우 (예: 1010) 그대로 유지
+            if (!replaced) {
+                min.append(target)
+            }
+        } else {
+            // 제일 앞자리가 1이 아닌 경우 - 첫 번째 자리를 1로 바꿈
+            val firstDigit = target[0]
+            for (c in target) {
+                min.append(if (c == firstDigit) '1' else c)
             }
         }
 
-        println(max.toString().toInt())
-        println(min.toString().toInt())
         return max.toString().toInt() - min.toString().toInt()
     }
 }
