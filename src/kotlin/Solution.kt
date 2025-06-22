10653c7dcad161713244a8cf7311b7c2ed1a7f23
seonb2n <626ksb@gmail.com>
2025-06-22 12:12:39 +0900

    - leetcode - https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/?envType=daily-question&envId=2025-06-22 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -7,35 +7,25 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/?envType=daily-question&envId=2025-06-21
+// https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/?envType=daily-question&envId=2025-06-22
 class Solution {
-    fun minimumDeletions(word: String, k: Int): Int {
-        val freq = IntArray(26)
-
-        for (c in word) {
-            freq[c - 'a']++
-        }
-
-        val frequencies = freq.filter { it > 0 }.sorted()
-        if (frequencies.size <= 1) return 0
-
-        var minDelete = Int.MAX_VALUE
-
-        // 빈도를 최소화하는 경우
-        for (minF in frequencies) {
-            // 현재 허용하는 최대 maxFreq
-            val maxFreq = minF + k
-            var deletions = 0
-            for (fe in frequencies) {
-                when {
-                    fe < minF -> deletions += fe // 전체 삭제
-                    fe > maxFreq -> deletions += fe - maxFreq // 초과 삭제
-                }
+    fun divideString(s: String, k: Int, fill: Char): Array<String> {
+        val result = ArrayList<String>()
+        val sb = StringBuilder()
+        for (c in s) {
+            if (sb.length == k) {
+                result.add(sb.toString())
+                sb.clear()
             }
-
-            minDelete = minOf(minDelete, deletions)
+            sb.append(c)
+        }
+        if (sb.isNotEmpty()) {
+            while (sb.length < k) {
+                sb.append(fill)
+            }
+            result.add(sb.toString())
         }
 
-        return minDelete
+        return result.toTypedArray()
     }
 }
