8328551f8987aaebec3e75aa3aa08b40d39e78dd
seonb2n <626ksb@gmail.com>
2025-07-06 13:56:13 +0900

    - leetcode - https://leetcode.com/problems/finding-pairs-with-a-certain-sum/?envType=daily-question&envId=2025-07-06 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -7,23 +7,37 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/?envType=daily-question&envId=2025-07-03
-class Solution {
-    fun findLucky(arr: IntArray): Int {
-        val counter = IntArray(501)
+// https://leetcode.com/problems/finding-pairs-with-a-certain-sum/?envType=daily-question&envId=2025-07-06
+class FindSumPairs(val nums1: IntArray, val nums2: IntArray) {
 
-        for (num in arr) {
-            if (num > 0 && num <= 500) {
-                counter[num]++
-            }
+    val numMap = HashMap<Int, Int>()
+    init {
+        for (num in nums2) {
+            numMap[num] = numMap.getOrDefault(num, 0) + 1
         }
-
-        for (i in counter.indices.reversed()) {
-            if (counter[i] != 0 && counter[i] == i) {
-                return i
-            }
-        }
-
-        return -1
     }
+
+    fun add(index: Int, `val`: Int) {
+        val origin = nums2[index]
+        numMap[origin] = numMap[origin]!! - 1
+
+        if (numMap[origin] == 0) {
+            numMap.remove(origin)
+        }
+
+        val next = origin + `val`
+        nums2[index] = next
+        numMap[next] = numMap.getOrDefault(next, 0)+ 1
+    }
+
+    fun count(tot: Int): Int {
+        var result = 0
+        for (num in nums1) {
+            if (num < tot) {
+                result += numMap.getOrDefault(tot - num, 0)
+            }
+        }
+        return result
+    }
+
 }
