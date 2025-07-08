8e5adf19b1db718a0d1dbc482d4673bc7f93ae0c
seonb2n <626ksb@gmail.com>
2025-07-08 22:48:00 +0900

    - leetcode - https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/?envType=daily-question&envId=2025-07-07 - time limit

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -7,37 +7,21 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-// https://leetcode.com/problems/finding-pairs-with-a-certain-sum/?envType=daily-question&envId=2025-07-06
-class FindSumPairs(val nums1: IntArray, val nums2: IntArray) {
+// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/?envType=daily-question&envId=2025-07-07
+class Solution {
+    fun maxEvents(events: Array<IntArray>): Int {
+        events.sortBy { it[1] }
+        val usedDay = mutableSetOf<Int>()
 
-    val numMap = HashMap<Int, Int>()
-    init {
-        for (num in nums2) {
-            numMap[num] = numMap.getOrDefault(num, 0) + 1
-        }
-    }
-
-    fun add(index: Int, `val`: Int) {
-        val origin = nums2[index]
-        numMap[origin] = numMap[origin]!! - 1
-
-        if (numMap[origin] == 0) {
-            numMap.remove(origin)
-        }
-
-        val next = origin + `val`
-        nums2[index] = next
-        numMap[next] = numMap.getOrDefault(next, 0)+ 1
-    }
-
-    fun count(tot: Int): Int {
-        var result = 0
-        for (num in nums1) {
-            if (num < tot) {
-                result += numMap.getOrDefault(tot - num, 0)
+        for (e in events) {
+            for (day in e[0]..e[1]) {
+                if (day !in usedDay) {
+                    usedDay.add(day)
+                    break
+                }
             }
         }
-        return result
-    }
 
+        return usedDay.size
+    }
 }
