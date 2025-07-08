26aa7b94c4fe0a5e01851f557434e41f069a3fb3
seonb2n <626ksb@gmail.com>
2025-07-08 22:53:03 +0900

    - leetcode - https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/?envType=daily-question&envId=2025-07-07 - time limit

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -10,18 +10,29 @@ import kotlin.math.min
 // https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/?envType=daily-question&envId=2025-07-07
 class Solution {
     fun maxEvents(events: Array<IntArray>): Int {
+        // 종료일 기준으로 정렬
         events.sortBy { it[1] }
-        val usedDay = mutableSetOf<Int>()
 
-        for (e in events) {
-            for (day in e[0]..e[1]) {
-                if (day !in usedDay) {
-                    usedDay.add(day)
-                    break
-                }
+        val maxDay = events.maxOf { it[1] }
+        val availableEvents = mutableListOf<IntArray>()
+        var eventIndex = 0
+        var count = 0
+
+        for (day in 1..maxDay) {
+            while (eventIndex < events.size && events[eventIndex][0] <= day) {
+                availableEvents.add(events[eventIndex])
+                eventIndex++
+            }
+
+            availableEvents.removeAll { it[1] < day }
+
+            if (availableEvents.isNotEmpty()) {
+                val selectedEvent = availableEvents.minByOrNull { it[1] }!!
+                availableEvents.remove(selectedEvent)
+                count++
             }
         }
 
-        return usedDay.size
+        return count
     }
 }
