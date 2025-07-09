9726c26ebf21ff2c594a8919a9fb02978e2f21d0
seonb2n <626ksb@gmail.com>
2025-07-09 20:22:32 +0900

    [wip] - leetcode - https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-i/?envType=daily-question&envId=2025-07-09 - -

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,41 +1,68 @@
 package kotlin
 
 import java.util.*
-import kotlin.collections.ArrayDeque
-import kotlin.collections.HashMap
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 
-// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/?envType=daily-question&envId=2025-07-07
+// https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-i/?envType=daily-question&envId=2025-07-09
 class Solution {
-    fun maxEvents(events: Array<IntArray>): Int {
-        // 종료일 기준으로 정렬
-        events.sortBy { it[0] }
-        val n = events.size
-        val maxDay = events.maxOf { it[1] }
-
-        var result = 0
-        var index = 0
-        val pq = PriorityQueue<Int>()
-        for (i in 1 until maxDay+1) {
-            // 오늘 시작하는 이벤트 추가
-            while (index < n && events[index][0] <= i) {
-                pq.add(events[index][1])
-                index++
-            }
-
-            // 이미 종료된 이벤트 제거
-            while (pq.isNotEmpty() && pq.peek() < i) {
-                pq.poll()
-            }
-
-            if (pq.isNotEmpty()) {
-                pq.poll()
-                result++
-            }
+    fun maxFreeTime(eventTime: Int, k: Int, startTime: IntArray, endTime: IntArray): Int {
+        val n = startTime.size
+        // 갭별 크기
+        val gaps = mutableListOf<Int>()
+        if (startTime[0] > 0) {
+            gaps.add(startTime[0])
+        }
+        gaps.add(startTime[0])
+        for (i in 0 until n-1) {
+            gaps.add(startTime[i + 1] - endTime[i])
+        }
+        if (eventTime > endTime[n - 1]) {
+            gaps.add(eventTime - endTime[n-1])
         }
 
-        return result
+        // 미팅별 지속시간
+        val meetingDuration = IntArray(n)
+        for (i in 0 until n) {
+            meetingDuration[i] = endTime[i] - startTime[i]
+        }
+
+        var maxFreeTime = gaps.maxOf { it }
+        // K + 1 개의 연속된 gap 을 합쳐봄
+        // 마지막 시작점은 gaps.size - k - 1
+        for (i in 0..gaps.size - k - 1) {
+            // 해당 k 개의 미팅을 이동시킬 공간이 존재하는지 확인
+            // 0 ~ i-1 번째 gap 과 i + k + 1 ~ gaps.size-1 gap 에 공간이 존재하는지 확인
+            val nowEvents = meetingDuration.sliceArray(i until i+k)
+            val pq = PriorityQueue<Int>(Collections.reverseOrder())
+            pq.addAll(nowEvents.toList())
+
+            // 왼쪽 gap들에서 배치 시도
+            for (j in 0 until i) {
+                if (pq.isEmpty()) {
+                    break
+                }
+                if (gaps[j] >= pq.peek()) {
+                    pq.poll()
+                }
+            }
+
+            // 오른쪽 gap들에서 배치 시도
+            for (j in i + k until gaps.size) {
+                if (pq.isEmpty()) {
+                    break
+                }
+                if (gaps[j] >= pq.peek()) {
+                    pq.poll()
+                }
+            }
+
+            if (pq.isEmpty()) {
+                var gapSum = 0
+                for (j in i until i + k + 1) {
+                    gapSum += gaps[j]
+                }
+                maxFreeTime = maxOf(maxFreeTime, gapSum)
+            }
+        }
+        return maxFreeTime
     }
 }
