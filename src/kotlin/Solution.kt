740f46292396715b4a5116df55348d1ba1bac660
seonb2n <626ksb@gmail.com>
2025-07-13 13:28:59 +0900

    - leetcode - https://leetcode.com/problems/maximum-matching-of-players-with-trainers/?envType=daily-question&envId=2025-07-13 - beats 88%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -5,26 +5,24 @@ import java.util.*
 // https://leetcode.com/problems/maximum-matching-of-players-with-trainers/?envType=daily-question&envId=2025-07-13
 class Solution {
     fun matchPlayersAndTrainers(players: IntArray, trainers: IntArray): Int {
-        val playerQueue = PriorityQueue<Int>()
-        for (player in players) {
-            playerQueue.add(player)
-        }
-
-        val trainerQueue = PriorityQueue<Int>()
-        for (trainer in trainers) {
-            trainerQueue.add(trainer)
-        }
+        players.sort()
+        trainers.sort()
 
         var result = 0
+        var pCursor = 0
+        var tCursor = 0
 
-        while (playerQueue.isNotEmpty() && trainerQueue.isNotEmpty()) {
-            val player = playerQueue.peek()
-            val trainer = trainerQueue.poll()
-            if (player <= trainer) {
-                playerQueue.poll()
+        while (pCursor < players.size && tCursor < trainers.size) {
+            if (players[pCursor] <= trainers[tCursor]) {
                 result++
+                pCursor++
+                tCursor++
+            } else {
+                tCursor++
             }
         }
+
+
 
         return result
     }
