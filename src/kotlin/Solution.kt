d1d7c6f0ab44ca84752022e740df839cacc07f55
seonb2n <626ksb@gmail.com>
2025-07-17 19:53:00 +0900

    - leetcode - https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-ii/?envType=daily-question&envId=2025-07-17 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,29 +2,31 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/?envType=daily-question&envId=2025-07-16
+
+// https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-ii/?envType=daily-question&envId=2025-07-17
 class Solution {
-    fun maximumLength(nums: IntArray): Int {
-        val n = nums.size
+    fun maximumLength(nums: IntArray, k: Int): Int {
 
-        // 패턴 1: 모든 인접한 합이 짝수 (같은 패리티끼리)
-        val evenCount = nums.count { it % 2 == 0 }
-        val oddCount = n - evenCount
-        val pattern1Length = maxOf(evenCount, oddCount)
-
-        // 패턴 2: 모든 인접한 합이 홀수 (다른 패리티끼리)
-        // maxEndingWith[0] = 짝수로 끝나는 최대 길이
-        // maxEndingWith[1] = 홀수로 끝나는 최대 길이
-        val maxEndingWith = intArrayOf(0, 0)
+        // dp[i][j] = i 를 만족하면서 k 로 끝나는 최대 길이
+        val dp = Array(k) { IntArray(k) }
 
         for (num in nums) {
-            val parity = num % 2
-            // 현재 원소와 다른 패리티로 끝나는 subsequence에 추가
-            maxEndingWith[parity] = maxEndingWith[1 - parity] + 1
+            val remainder = num % k
+
+            for (target in 0 until k) {
+                // 이전 원소의 나머지 (prev + remain) % k == target
+                val prevRemainder = (target - remainder + k) % k
+                dp[target][remainder] = maxOf(dp[target][remainder], dp[target][prevRemainder] + 1)
+            }
         }
 
-        val pattern2Length = maxOf(maxEndingWith[0], maxEndingWith[1])
+        var max = 0
 
-        return maxOf(pattern1Length, pattern2Length)
+        for (i in 0 until k) {
+            for (j in 0 until k) {
+                max = maxOf(max, dp[i][j])
+            }
+        }
+        return max
     }
 }
