f061b3ff097d58d22810d5cb8d98049adc500297
seonb2n <626ksb@gmail.com>
2025-07-26 13:22:11 +0900

    - leetcode - https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/?envType=daily-question&envId=2025-07-25 - 60%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,55 +3,24 @@ package kotlin
 import java.util.*
 import kotlin.math.abs
 
-
-// https://leetcode.com/problems/maximum-score-from-removing-substrings/?envType=daily-question&envId=2025-07-23
+// https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/?envType=daily-question&envId=2025-07-25
 class Solution {
-    fun maximumGain(s: String, x: Int, y: Int): Int {
-        // greedy
+    fun maxSum(nums: IntArray): Int {
+        val set= mutableSetOf<Int>()
+        val deleted = PriorityQueue<Int>(reverseOrder())
+
         var result = 0
-
-        fun removeAB(input: String): String {
-            val stack = Stack<Char>()
-            for (c in input) {
-                if (stack.isNotEmpty() && stack.peek() == 'a' && c == 'b') {
-                    stack.pop()
-                    result += x
-                } else {
-                    stack.push(c)
-                }
+        for (num in nums) {
+            if (num > 0 && !set.contains(num)) {
+                result += num
+                set.add(num)
+            } else {
+                deleted.add(num)
             }
-
-            val sb = StringBuilder()
-            while (stack.isNotEmpty()) {
-                sb.append(stack.pop())
-            }
-
-            return sb.toString().reversed()
         }
 
-        fun removeBA(input: String): String {
-            val stack = Stack<Char>()
-            for (c in input) {
-                if (stack.isNotEmpty() && stack.peek() == 'b' && c == 'a') {
-                    stack.pop()
-                    result += y
-                } else {
-                    stack.push(c)
-                }
-            }
-
-            val sb = StringBuilder()
-            while (stack.isNotEmpty()) {
-                sb.append(stack.pop())
-            }
-
-            return sb.toString().reversed()
-        }
-
-        if (x > y) {
-            removeBA(removeAB(s))
-        } else {
-            removeAB(removeBA(s))
+        if (result == 0) {
+            result += deleted.peek()
         }
 
         return result
