8d092cf6b6087e470c3ce0831d01322b01e28f4c
seonb2n <626ksb@gmail.com>
2025-07-27 12:19:38 +0900

    - leetcode - https://leetcode.com/problems/count-hills-and-valleys-in-an-array/?envType=daily-question&envId=2025-07-27 - 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,26 +3,36 @@ package kotlin
 import java.util.*
 import kotlin.math.abs
 
-// https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/?envType=daily-question&envId=2025-07-25
+// https://leetcode.com/problems/count-hills-and-valleys-in-an-array/?envType=daily-question&envId=2025-07-27
 class Solution {
-    fun maxSum(nums: IntArray): Int {
-        val set= mutableSetOf<Int>()
-        val deleted = PriorityQueue<Int>(reverseOrder())
+    fun countHillValley(nums: IntArray): Int {
+        val stack = Stack<Int>()
+        stack.push(nums[0])
 
-        var result = 0
-        for (num in nums) {
-            if (num > 0 && !set.contains(num)) {
-                result += num
-                set.add(num)
-            } else {
-                deleted.add(num)
+        for (i in 1 until nums.size) {
+            if (stack.peek() != nums[i]) {
+                stack.push(nums[i])
             }
         }
 
-        if (result == 0) {
-            result += deleted.peek()
-        }
+        var result = 0
 
+        for (i in stack.indices) {
+            if (i == 0) {
+                continue
+            }
+            if (i == stack.lastIndex) {
+                continue
+            }
+            val left = stack.get(i-1)
+            val right = stack.get(i+1)
+            val now = stack.get(i)
+            if (left < now && now > right) {
+                result++
+            } else if (left > now && now < right) {
+                result++
+            }
+        }
         return result
     }
 }
