c7a2b4a51f4ae66262f9538cc341f9ae7ca5f98f
seonb2n <626ksb@gmail.com>
2025-07-28 19:55:35 +0900

    - leetcode - https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/?envType=daily-question&envId=2025-07-28 - 75%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,36 +3,27 @@ package kotlin
 import java.util.*
 import kotlin.math.abs
 
-// https://leetcode.com/problems/count-hills-and-valleys-in-an-array/?envType=daily-question&envId=2025-07-27
+// https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/?envType=daily-question&envId=2025-07-28
 class Solution {
-    fun countHillValley(nums: IntArray): Int {
-        val stack = Stack<Int>()
-        stack.push(nums[0])
-
-        for (i in 1 until nums.size) {
-            if (stack.peek() != nums[i]) {
-                stack.push(nums[i])
-            }
+    fun countMaxOrSubsets(nums: IntArray): Int {
+        // or 의 maximum 은 전체의 or
+        var maxOrValue = 0
+        for (num in nums) {
+            maxOrValue = maxOrValue or num
         }
 
-        var result = 0
+        // backtrack
+        return backtrack(nums, 0, 0, maxOrValue)
+    }
 
-        for (i in stack.indices) {
-            if (i == 0) {
-                continue
-            }
-            if (i == stack.lastIndex) {
-                continue
-            }
-            val left = stack.get(i-1)
-            val right = stack.get(i+1)
-            val now = stack.get(i)
-            if (left < now && now > right) {
-                result++
-            } else if (left > now && now < right) {
-                result++
-            }
+    fun backtrack(nums: IntArray, index: Int, current: Int, max: Int): Int {
+        if (index == nums.size) {
+            return if (current == max) 1 else 0
         }
-        return result
+        // 현재를 제외하고 달성이 가능한 경우의 수
+        val countWithout = backtrack(nums, index + 1, current, max)
+        // 현재를 포함해서 달성이 가능한 경우의 수
+        val countWith = backtrack(nums, index + 1, current or nums[index], max)
+        return countWithout + countWith
     }
 }
