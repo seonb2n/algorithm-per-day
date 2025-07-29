bbf42204050dc789e554b97802dbd955b4fb1241
seonb2n <626ksb@gmail.com>
2025-07-29 20:50:47 +0900

    - leetcode - https://leetcode.com/problems/smallest-subarrays-with-maximum-bitwise-or/?envType=daily-question&envId=2025-07-29 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -9,20 +9,27 @@ class Solution {
         val n = nums.size
         val result = IntArray(n)
 
-        fun backtrack(now: Int, maxLen: Int, nowIndex: Int, start: Int): Int {
-            if (nowIndex == n) return maxLen
+        // 각 비트 위치별로 마지막으로 1이 등장한 인덱스를 추적
+        val lastBitIndex = IntArray(32) { -1 }
 
-            val next = now or nums[nowIndex]
-            if (now < next) {
-                val nextMaxLen = nowIndex - start + 1
-                return backtrack(next, nextMaxLen, nowIndex + 1, start)
-            } else {
-                return backtrack(now, maxLen, nowIndex + 1, start)
+        // 뒤에서부터 처리
+        for (i in n - 1 downTo 0) {
+            // 현재 숫자의 각 비트가 1인 위치들을 업데이트
+            for (bit in 0 until 32) {
+                if ((nums[i] shr bit) and 1 == 1) {
+                    lastBitIndex[bit] = i
+                }
             }
-        }
 
-        for (i in 0 until n) {
-            result[i] = backtrack(nums[i], 1, i + 1, i)
+            // 현재 위치에서 만들 수 있는 최대 OR을 위해 필요한 최대 거리
+            var maxDistance = 1
+            for (bit in 0 until 32) {
+                if (lastBitIndex[bit] != -1) {
+                    maxDistance = maxOf(maxDistance, lastBitIndex[bit] - i + 1)
+                }
+            }
+
+            result[i] = maxDistance
         }
 
         return result
