634422bcd9c3dbd0bb7d816a1af05d1c3e80d256
seonb2n <626ksb@gmail.com>
2025-08-01 21:21:56 +0900

    - leetcode - https://leetcode.com/problems/pascals-triangle/description/?envType=daily-question&envId=2025-08-01 - beats 80%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,27 +3,22 @@ package kotlin
 import java.util.*
 import kotlin.math.abs
 
-// https://leetcode.com/problems/bitwise-ors-of-subarrays/?envType=daily-question&envId=2025-07-31
+// https://leetcode.com/problems/pascals-triangle/description/?envType=daily-question&envId=2025-08-01
 class Solution {
-    fun subarrayBitwiseORs(arr: IntArray): Int {
-        val result = mutableSetOf<Int>()
-        var currentOrs = mutableSetOf<Int>()
+    fun generate(numRows: Int): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+        result.add(listOf(1))
 
-        for (n in arr) {
-            val nextOrs = mutableSetOf<Int>()
-
-            // 1. 새로운 subarray 시작: [n]
-            nextOrs.add(n)
-
-            // 2. 기존 subarray들을 n으로 확장
-            for (prev in currentOrs) {
-                nextOrs.add(prev or n)
+        for (i in 1 until numRows) {
+            val list = mutableListOf<Int>()
+            list.add(1)
+            for (j in 1 until i) {
+                list.add(result[i-1][j-1] + result[i-1][j])
             }
-
-            currentOrs = nextOrs
-            result.addAll(currentOrs)
+            list.add(1)
+            result.add(list)
         }
 
-        return result.size
+        return result
     }
 }
