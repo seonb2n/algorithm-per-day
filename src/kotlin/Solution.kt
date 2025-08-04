a46d1c018d5a7896e70283bcea5c6094d15d3188
seonb2n <626ksb@gmail.com>
2025-08-04 20:12:47 +0900

    - leetcode - https://leetcode.com/problems/fruit-into-baskets/submissions/1722863572/?envType=daily-question&envId=2025-08-04 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -1,67 +1,46 @@
 package kotlin
 
-import java.util.*
-import kotlin.math.abs
-
-// https://leetcode.com/problems/rearranging-fruits/?envType=daily-question&envId=2025-08-02
+// https://leetcode.com/problems/fruit-into-baskets/submissions/1722863572/?envType=daily-question&envId=2025-08-04
 class Solution {
-    fun minCost(basket1: IntArray, basket2: IntArray): Long {
-        // 정렬해서 같으려면 쌍이 맞아야 함
-        val totalCount = mutableMapOf<Int, Int>()
-        val count1 = mutableMapOf<Int, Int>()
-        val count2 = mutableMapOf<Int, Int>()
+    fun totalFruit(fruits: IntArray): Int {
+        if (fruits.isEmpty()) return 0
 
-        for (i in 0 until basket1.size) {
-            totalCount[basket1[i]] = totalCount.getOrDefault(basket1[i], 0) + 1
-            totalCount[basket2[i]] = totalCount.getOrDefault(basket2[i], 0) + 1
+        var max = 1
+        var last = fruits[0]        // 가장 최근에 본 과일
+        var secondLast = -1         // 그 이전 과일
+        var lastCount = 1           // 가장 최근 과일이 연속으로 나온 횟수
+        var curr = 1                // 현재 유효한 윈도우 길이
 
-            count1[basket1[i]] = count1.getOrDefault(basket1[i], 0) + 1
-            count2[basket2[i]] = count2.getOrDefault(basket2[i], 0) + 1
-        }
+        for (i in 1 until fruits.size) {
+            val fruit = fruits[i]
 
-        for (c in totalCount.values) {
-            if (c % 2 == 1) {
-                return -1L
-            }
-        }
-
-        // basket1 에서 옮길 과일
-        val target1 = mutableListOf<Int>()
-        // basket2 에서 옮길 과일
-        val target2 = mutableListOf<Int>()
-
-        for (f in totalCount.keys) {
-            val targetCount = totalCount[f]!! / 2
-            val current1 = count1.getOrDefault(f, 0)
-            val current2 = count2.getOrDefault(f, 0)
-
-            if (current1 > targetCount) {
-                for (i in 0 until current1 - targetCount) {
-                    target1.add(f)
+            when (fruit) {
+                last -> {
+                    // 같은 과일이 계속 나오는 경우
+                    lastCount++
+                    curr++
                 }
-            } else if (current2 > targetCount) {
-                for (i in 0 until current2 - targetCount) {
-                    target2.add(f)
+                secondLast -> {
+                    // 이전 과일이 다시 나오는 경우
+                    curr++
+                    // last와 secondLast를 교체
+                    secondLast = last
+                    last = fruit
+                    lastCount = 1
+                }
+                else -> {
+                    // 완전히 새로운 과일 등장 (3번째 타입)
+                    // 윈도우를 lastCount + 1로 리셋
+                    curr = lastCount + 1
+                    secondLast = last
+                    last = fruit
+                    lastCount = 1
                 }
             }
+
+            max = maxOf(max, curr)
         }
 
-        target1.sort()
-        target2.sortDescending()
-
-        var result = 0L
-        val minFruit = (basket1 + basket2).minOrNull() ?: Int.MAX_VALUE
-
-        for (i in target1.indices) {
-            val fruit1 = target1[i]
-            val fruit2 = target2[i]
-
-            val directCost = minOf(fruit1, fruit2).toLong()
-            val minCost = 2L * minFruit
-
-            result += minOf(directCost, minCost)
-        }
-
-        return result
+        return max
     }
 }
