346bce9b0d160c8263d3ba0c0e48ccc6218682aa
seonb2n <626ksb@gmail.com>
2025-08-06 21:51:08 +0900

    - leetcode - https://leetcode.com/problems/fruits-into-baskets-iii/?envType=daily-question&envId=2025-08-06 - time limit exceeded

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,24 +2,33 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/fruits-into-baskets-ii/?envType=daily-question&envId=2025-08-05
+// https://leetcode.com/problems/fruits-into-baskets-iii/?envType=daily-question&envId=2025-08-06
 class Solution {
     fun numOfUnplacedFruits(fruits: IntArray, baskets: IntArray): Int {
+        val capacityMap = TreeMap<Int, PriorityQueue<Int>>()
+
+        for (i in baskets.indices) {
+            capacityMap.computeIfAbsent(baskets[i]) { PriorityQueue<Int>() }.offer(i)
+        }
+
         var counter = 0
 
-        val n = baskets.size
-        val isUsed = BooleanArray(n)
+        for (f in fruits) {
+            val possible = capacityMap.tailMap(fruit)
 
-        for (fruit in fruits) {
-            var placed = false
-            for (i in baskets.indices) {
-                if (fruit <= baskets[i] && !isUsed[i]) {
-                    isUsed[i] = true
-                    placed = true
-                    break
+            var minIndex = Int.MAX_VALUE
+            var min = -1
+
+            for ((capacity, indices) in possible) {
+                if (indices.isNotEmpty() && indices.peek() < minIndex) {
+                    minIndex = indices.peek()
+                    min = capacity
                 }
             }
-            if (!placed) {
+
+            if (min < Int.MAX_VALUE) {
+                capacityMap[min]!!.poll()
+            } else {
                 counter++
             }
         }
