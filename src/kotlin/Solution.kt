be20a5bfaa091797e4a454c796a45546e664cff7
seonb2n <626ksb@gmail.com>
2025-08-07 20:18:03 +0900

    - leetcode - https://leetcode.com/problems/powx-n/ - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -2,16 +2,25 @@ package kotlin
 
 import java.util.*
 
-// https://leetcode.com/problems/group-anagrams/
+// https://leetcode.com/problems/powx-n/
 class Solution {
-    fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        val map = mutableMapOf<String, MutableList<String>>()
+    fun myPow(x: Double, n: Int): Double {
+        fun pow(base: Double, exp: Long): Double {
+            if (exp == 0L) return 1.0
+            if (exp == 1L) return base
+            if (exp == -1L) return 1.0 / base
 
-        for (s in strs) {
-            val now = s.toCharArray().sorted().joinToString("")
-            map.getOrPut(now) { mutableListOf() }.add(s)
+            val half = pow(base, exp / 2)
+
+            return if (exp % 2 == 0L) {
+                half * half
+            } else if (exp > 0) {
+                half * half * base
+            } else {
+                half * half / base
+            }
         }
 
-        return map.values.toList()
+        return pow(x, n.toLong())
     }
 }
