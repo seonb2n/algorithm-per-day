d3da3cc30c544946269e007ca3c045602ad1b36c
seonb2n <626ksb@gmail.com>
2025-08-09 09:12:33 +0900

    - leetcode - https://leetcode.com/problems/soup-servings/?envType=daily-question&envId=2025-08-09 - beats 10%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,31 +3,13 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/soup-servings/?envType=daily-question&envId=2025-08-08
+// https://leetcode.com/problems/soup-servings/?envType=daily-question&envId=2025-08-09
 class Solution {
-    fun soupServings(n: Int): Double {
-        if (n > 5000) return 1.0
+    fun isPowerOfTwo(n: Int): Boolean {
+        if (n == 1) return true
+        if (n < 0) return false
 
-        val units = ceil(n.toDouble() / 25).toInt()
-        val cache = Array(units + 1) { DoubleArray(units + 1) { -1.0 } }
+        return Integer.toBinaryString(n).toCharArray().count { it == '1' } == 1
 
-        fun calcProb(a: Int, b: Int): Double {
-            if (a <= 0 && b <= 0) return 0.5
-            if (a <= 0) return 1.0
-            if (b <= 0) return 0.0
-
-            if (cache[a][b] != -1.0) return cache[a][b]
-
-            val prob = 0.25 * (
-                    calcProb(a - 4, b) + calcProb(a - 3, b - 1) + calcProb(a - 2, b - 2) + calcProb(a - 1, b - 3)
-                    )
-            cache[a][b] = prob
-            return prob
-        }
-
-        return calcProb(units, units)
     }
 }
-
-
-
