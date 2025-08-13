9df92917c994b31314b27644952588966acdc3a3
seonb2n <626ksb@gmail.com>
2025-08-13 20:45:59 +0900

    - leetcode - https://leetcode.com/problems/power-of-three/?envType=daily-question&envId=2025-08-13 - beats 92%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,53 +3,18 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/ways-to-express-an-integer-as-sum-of-powers/?envType=daily-question&envId=2025-08-12
+// https://leetcode.com/problems/power-of-three/?envType=daily-question&envId=2025-08-13
 class Solution {
-    fun numberOfWays(n: Int, x: Int): Int {
-        val MOD = 1000000007
-        var k = 2
-
-        val xMap = mutableMapOf<Int, Int>()
-        xMap[1] = 1
-        while (true) {
-            var temp = k
-            for (i in 1 until x) {
-                temp *= k
-            }
-            xMap[k] = temp
-
-            if (temp > n) {
-                break
+    fun isPowerOfThree(n: Int): Boolean {
+        if (n <= 0) return false
+        var target = n
+        while (target > 1) {
+            if (target % 3 == 0) {
+                target /= 3
             } else {
-                k++
+                return false
             }
         }
-
-        // dp[i][j] = j번째 수까지 사용해서 합 i를 만드는 방법의 수
-        val dp = Array(n + 1) { IntArray(k + 1) { -1 } }
-
-        for (j in 0 until k) {
-            dp[0][j] = 1
-        }
-
-        fun findDp(i: Int, j: Int): Int {
-            if (i < 0 || j < 0) return 0
-            if (i == 0) return 1
-
-            if (dp[i][j] != -1) return dp[i][j]
-
-            // j번째 수를 사용하지 않는 경우
-            dp[i][j] = findDp(i, j - 1)
-
-            // j번째 수를 사용하는 경우
-            xMap[j]?.let { power ->
-                if (i >= power) {
-                    dp[i][j] = (dp[i][j] + findDp(i - power, j - 1)) % MOD
-                }
-            }
-            return dp[i][j]
-        }
-
-        return findDp(n, k - 1)
+        return true
     }
 }
