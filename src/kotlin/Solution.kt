f68010cc1e7521225038d975575bb822a3d2cf84
seonb2n <626ksb@gmail.com>
2025-08-14 19:14:58 +0900

    - leetcode - https://leetcode.com/problems/largest-3-same-digit-number-in-string/?envType=daily-question&envId=2025-08-14 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,10 +3,34 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/power-of-three/?envType=daily-question&envId=2025-08-13
+// https://leetcode.com/problems/largest-3-same-digit-number-in-string/?envType=daily-question&envId=2025-08-14
 class Solution {
-    fun isPowerOfThree(n: Int): Boolean {
-        if (n <= 0) return false
-        return 1162261467 % n == 0
+    fun largestGoodInteger(num: String): String {
+        var counter = 0
+        var now = 'a'
+        var max = ""
+
+        for (c in num) {
+            if (now == c) {
+                counter++
+            } else {
+                now = c
+                counter = 1
+            }
+
+            if (counter == 3) {
+                if (max == "") {
+                    max = "$c$c$c"
+                } else {
+                    val temp = "$c$c$c"
+                    if (max.toInt() < temp.toInt()) {
+                        max = temp
+                    }
+                }
+                counter = 0
+            }
+        }
+
+        return max
     }
 }
