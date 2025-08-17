a87df1f69f4663677ff5e0124fff06a921b6e17b
seonb2n <626ksb@gmail.com>
2025-08-17 12:47:04 +0900

    - leetcode - https://leetcode.com/problems/new-21-game/?envType=daily-question&envId=2025-08-17 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,21 +3,36 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/maximum-69-number/?envType=daily-question&envId=2025-08-16
+// https://leetcode.com/problems/new-21-game/?envType=daily-question&envId=2025-08-17
 class Solution {
-    fun maximum69Number (num: Int): Int {
-        val str = num.toString()
-        val sb = StringBuilder()
-        var isChanged = false
-        for (c in str) {
-            if (c == '6' && !isChanged) {
-                sb.append('9')
-                isChanged = true
-            } else {
-                sb.append(c)
+    fun new21Game(n: Int, k: Int, maxPts: Int): Double {
+        // 특별한 경우들
+        if (k == 0) return 1.0
+        if (n >= k + maxPts - 1) return 1.0
+
+        // dp[i] = 정확히 i점을 가질 확률
+        val dp = DoubleArray(k + maxPts)
+        dp[0] = 1.0
+
+        var windowSum = 1.0
+
+        for (i in 1 until k + maxPts) {
+            dp[i] = windowSum / maxPts
+            if (i < k) {
+                windowSum += dp[i]
+            }
+
+            val leftBound = i - maxPts
+            if (leftBound >= 0 && leftBound < k) {
+                windowSum -= dp[leftBound]
             }
         }
 
-        return sb.toString().toInt()
+        var result = 0.0
+        for (i in k..min(n, k + maxPts - 1)) {
+            result += dp[i]
+        }
+
+        return result
     }
 }
