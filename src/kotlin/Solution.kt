cfb566154478ae5038e6a5ebe18744fc72d2e9f2
seonb2n <626ksb@gmail.com>
2025-08-18 19:58:47 +0900

    - leetcode - https://leetcode.com/problems/maximum-subarray/ - beats 10%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,36 +3,25 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/new-21-game/?envType=daily-question&envId=2025-08-17
+// https://leetcode.com/problems/maximum-subarray/
 class Solution {
-    fun new21Game(n: Int, k: Int, maxPts: Int): Double {
-        // 특별한 경우들
-        if (k == 0) return 1.0
-        if (n >= k + maxPts - 1) return 1.0
-
-        // dp[i] = 정확히 i점을 가질 확률
-        val dp = DoubleArray(k + maxPts)
-        dp[0] = 1.0
-
-        var windowSum = 1.0
-
-        for (i in 1 until k + maxPts) {
-            dp[i] = windowSum / maxPts
-            if (i < k) {
-                windowSum += dp[i]
-            }
-
-            val leftBound = i - maxPts
-            if (leftBound >= 0 && leftBound < k) {
-                windowSum -= dp[leftBound]
-            }
+    fun maxSubArray(nums: IntArray): Int {
+        if (nums.size == 1) {
+            return nums[0]
         }
 
-        var result = 0.0
-        for (i in k..min(n, k + maxPts - 1)) {
-            result += dp[i]
+        var sum = 0
+        var lastSum = 0
+        var min = 0
+        var max = nums.maxOrNull()!!
+
+        for (n in nums) {
+            lastSum = sum
+            sum += n
+            min = minOf(min, lastSum)
+            max = maxOf(max, sum - min)
         }
 
-        return result
+        return max
     }
 }
