7495753223a21b9cd6010dc57c1b424a9f4a3ecc
seonb2n <626ksb@gmail.com>
2025-08-20 21:52:06 +0900

    - leetcode - https://leetcode.com/problems/count-square-submatrices-with-all-ones/?envType=daily-question&envId=2025-08-20 - beats 8%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,25 +3,44 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/maximum-subarray/
+// https://leetcode.com/problems/count-square-submatrices-with-all-ones/?envType=daily-question&envId=2025-08-20
 class Solution {
-    fun maxSubArray(nums: IntArray): Int {
-        if (nums.size == 1) {
-            return nums[0]
+    fun countSquares(matrix: Array<IntArray>): Int {
+        val n = matrix.size
+        val m = matrix[0].size
+        val min = minOf(n, m)
+
+        var result = 0
+
+        fun checkSize(j: Int, k: Int, size: Int): Boolean {
+            if (j+size > n || k + size > m) return false
+
+            for (a in j until j+size) {
+                for (b in k until k+size) {
+                    if (matrix[a][b] == 0) {
+                        return false
+                    }
+                }
+            }
+            return true
         }
 
-        var sum = 0
-        var lastSum = 0
-        var min = 0
-        var max = nums.maxOrNull()!!
+        for (i in 1 until min + 1) {
+            // 크기가 i 인 square 찾기
+            for (j in 0 until n) {
+                for (k in 0 until m) {
+                    val start = matrix[j][k]
+                    if (start == 0) {
+                        continue
+                    }
+                    if (checkSize(j, k, i)) {
+                        result++
+                    }
 
-        for (n in nums) {
-            lastSum = sum
-            sum += n
-            min = minOf(min, lastSum)
-            max = maxOf(max, sum - min)
+                }
+            }
         }
 
-        return max
+        return result
     }
 }
