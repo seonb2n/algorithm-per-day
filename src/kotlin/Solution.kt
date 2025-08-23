7a42dcfaa21b62447860303ddbc7bb8e0057c227
seonb2n <626ksb@gmail.com>
2025-08-23 19:38:23 +0900

    - leetcode - https://leetcode.com/problems/find-the-minimum-area-to-cover-all-ones-i/submissions/1745276447/?envType=daily-question&envId=2025-08-22 - beats 50%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,42 +3,28 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/count-submatrices-with-all-ones/?envType=daily-question&envId=2025-08-21
+// https://leetcode.com/problems/find-the-minimum-area-to-cover-all-ones-i/submissions/1745276447/?envType=daily-question&envId=2025-08-22
 class Solution {
-    fun numSubmat(mat: Array<IntArray>): Int {
-        val n = mat.size
-        val m = mat[0].size
-        // dp[i][j] = 오른쪽 아래 모서리가 i,j 인 모든 1 로 이루어진 직사각형
-        val dp = Array(n) { IntArray(m) }
+    fun minimumArea(grid: Array<IntArray>): Int {
+        var minRow = Int.MAX_VALUE
+        var maxRow = Int.MIN_VALUE
+        var minCol = Int.MAX_VALUE
+        var maxCol = Int.MIN_VALUE
 
-        // i,j 까지 세로로 연속된 1의 개수
-        val height = Array(n) { IntArray(m) }
-        var result = 0
+        val n = grid.size
+        val m = grid[0].size
 
         for (i in 0 until n) {
             for (j in 0 until m) {
-                if (mat[i][j] == 0) {
-                    height[i][j] = 0
-                    dp[i][j] = 0
-                } else {
-                    height[i][j] = if (i == 0) 1 else height[i-1][j] + 1
-
-                    dp[i][j] = height[i][j] // 세로 * 1 짜리 직사각형들
-
-                    var minHeight = height[i][j]
-
-                    // 왼쪽으로 이동하면서 직사각형 추가
-                    for (k in j-1 downTo 0) {
-                        if (mat[i][k] == 0) break
-                        minHeight = minOf(minHeight, height[i][k])
-                        dp[i][j] += minHeight
-                    }
-
-                    result += dp[i][j]
+                if (grid[i][j] == 1) {
+                    minRow = minOf(minRow, i)
+                    maxRow = maxOf(maxRow, i)
+                    minCol = minOf(minCol, j)
+                    maxCol = maxOf(maxCol, j)
                 }
             }
         }
 
-        return result
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1)
     }
 }
