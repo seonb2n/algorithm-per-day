a5d8b75f350b2407a7237411d1af5f54483103a5
seonb2n <626ksb@gmail.com>
2025-08-24 13:00:57 +0900

    - leetcode - https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/?envType=daily-question&envId=2025-08-24 - beats 50%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -6,47 +6,25 @@ import kotlin.math.ceil
 // https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/?envType=daily-question&envId=2025-08-24
 class Solution {
     fun longestSubarray(nums: IntArray): Int {
-        var isZero = false
-        val sums = mutableListOf<Int>()
-        for (n in nums) {
-            if (n == 0) {
-                isZero = true
-                sums.add(n)
-            } else {
-                if (sums.size > 0) {
-                    val last = sums.last()
-                    if (last > 0) {
-                        sums.removeLast()
-                        sums.add(last + 1)
-                    } else {
-                        sums.add(1)
-                    }
-                } else {
-                    sums.add(1)
+        var left = 0
+        var zeroCount = 0
+        var maxLength = 0
+
+        for (right in nums.indices) {
+            if (nums[right] == 0) {
+                zeroCount++
+            }
+
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--
                 }
-
+                left++
             }
+
+            maxLength = maxOf(maxLength, right - left)
         }
 
-        if (!isZero) {
-            return nums.size - 1
-        }
-
-        var max = sums[0]
-        if (sums.size == 2) {
-            max = sums[0] + sums[1]
-        }
-
-        for (i in 1 until sums.size-1) {
-            if (sums[i] == 0) {
-                val now = sums[i-1] + sums[i] + sums[i+1]
-                max = maxOf(max, now)
-            } else {
-                max = maxOf(max, sums[i])
-            }
-        }
-
-        return max
-
+        return maxLength
     }
 }
