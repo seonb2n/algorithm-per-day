7446a59cf8d5a8d7be435ea5791acfa8e385efb6
seonb2n <626ksb@gmail.com>
2025-08-28 22:49:46 +0900

    - leetcode - https://leetcode.com/problems/sort-matrix-by-diagonals/?envType=daily-question&envId=2025-08-28 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,23 +3,55 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/?envType=daily-question&envId=2025-08-26
+// https://leetcode.com/problems/sort-matrix-by-diagonals/?envType=daily-question&envId=2025-08-28
 class Solution {
-    fun areaOfMaxDiagonal(dimensions: Array<IntArray>): Int {
-        var max = 0
-        var longest = 0
+    fun sortMatrix(grid: Array<IntArray>): Array<IntArray> {
+        val n = grid.size
+        val result = Array<IntArray>(n) { IntArray(n) }
 
-        for (d in dimensions) {
-            val now = d[0] * d[0] + d[1] * d[1]
-            if (now > longest) {
-                longest = now
-                max = d[0] * d[1]
+        // 오름차순
+        for (i in n-1 downTo 1) {
+            val list = mutableListOf<Int>()
+            var x = 0
+            var y = i
+            while (x < n && y < n) {
+                list.add(grid[x][y])
+                x += 1
+                y += 1
             }
-            else if (now == longest) {
-                max = maxOf(d[0] * d[1], max)
+            list.sort()
+            x = 0
+            y = i
+            var index = 0
+            while (x < n && y < n) {
+                result[x][y] = list[index]
+                x += 1
+                y += 1
+                index++
             }
         }
 
-        return max
+        // 내림차순
+        for (i in 0 until n) {
+            val list = mutableListOf<Int>()
+            var x = i
+            var y = 0
+            while (x < n && y < n) {
+                list.add(grid[x][y])
+                x += 1
+                y += 1
+            }
+            list.sortDescending()
+            x = i
+            y = 0
+            var index = 0
+            while (x < n && y < n) {
+                result[x][y] = list[index]
+                x += 1
+                y += 1
+                index++
+            }
+        }
+        return result
     }
 }
