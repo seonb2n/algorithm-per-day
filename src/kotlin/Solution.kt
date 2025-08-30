f4693e3ace3dc021de6ed4cca4eecc4b830be43d
seonb2n <626ksb@gmail.com>
2025-08-30 13:32:30 +0900

    - leetcode - https://leetcode.com/problems/valid-sudoku/?envType=daily-question&envId=2025-08-30 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,55 +3,40 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/sort-matrix-by-diagonals/?envType=daily-question&envId=2025-08-28
+// https://leetcode.com/problems/valid-sudoku/?envType=daily-question&envId=2025-08-30
 class Solution {
-    fun sortMatrix(grid: Array<IntArray>): Array<IntArray> {
-        val n = grid.size
-        val result = Array<IntArray>(n) { IntArray(n) }
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val rowValid = Array(9) { BooleanArray(10) }
+        val columnValid = Array(9) { BooleanArray(10) }
+        val cellValid = Array(9) { BooleanArray(10) }
 
-        // 오름차순
-        for (i in n-1 downTo 1) {
-            val list = mutableListOf<Int>()
-            var x = 0
-            var y = i
-            while (x < n && y < n) {
-                list.add(grid[x][y])
-                x += 1
-                y += 1
-            }
-            list.sort()
-            x = 0
-            y = i
-            var index = 0
-            while (x < n && y < n) {
-                result[x][y] = list[index]
-                x += 1
-                y += 1
-                index++
+        fun getCellNumber(i: Int, j: Int): Int {
+            if (i < 3 && j < 3) return 0
+            if (i < 3 && j < 6) return 1
+            if (i < 3 && j < 9) return 2
+            if (i < 6 && j < 3) return 3
+            if (i < 6 && j < 6) return 4
+            if (i < 6 && j < 9) return 5
+            if (i < 9 && j < 3) return 6
+            if (i < 9 && j < 6) return 7
+            if (i < 9 && j < 9) return 8
+            return 0
+        }
+
+        for (i in 0 until 9) {
+            for (j in 0 until 9) {
+                if (board[i][j] != '.') {
+                    val num = board[i][j] - '0'
+                    if (rowValid[i][num] || columnValid[j][num] || cellValid[getCellNumber(i, j)][num]) {
+                        return false
+                    }
+                    rowValid[i][num] = true
+                    columnValid[j][num] = true
+                    cellValid[getCellNumber(i, j)][num] = true
+                }
             }
         }
 
-        // 내림차순
-        for (i in 0 until n) {
-            val list = mutableListOf<Int>()
-            var x = i
-            var y = 0
-            while (x < n && y < n) {
-                list.add(grid[x][y])
-                x += 1
-                y += 1
-            }
-            list.sortDescending()
-            x = i
-            y = 0
-            var index = 0
-            while (x < n && y < n) {
-                result[x][y] = list[index]
-                x += 1
-                y += 1
-                index++
-            }
-        }
-        return result
+        return true
     }
 }
