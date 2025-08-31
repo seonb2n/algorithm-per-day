2f166a6b4aaf18b0bb996acfd1a176e6aa636c5d
seonb2n <626ksb@gmail.com>
2025-08-31 11:58:13 +0900

    - leetcode - https://leetcode.com/problems/sudoku-solver/?envType=daily-question&envId=2025-08-31 - beats 78%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,36 +3,58 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/valid-sudoku/?envType=daily-question&envId=2025-08-30
+// https://leetcode.com/problems/sudoku-solver/?envType=daily-question&envId=2025-08-31
 class Solution {
-    fun isValidSudoku(board: Array<CharArray>): Boolean {
-        val rowValid = Array(9) { BooleanArray(10) }
-        val columnValid = Array(9) { BooleanArray(10) }
-        val cellValid = Array(9) { BooleanArray(10) }
+    fun solveSudoku(board: Array<CharArray>): Unit {
+        solve(board)
+    }
 
-        fun getCellNumber(i: Int, j: Int): Int {
-            if (i < 3 && j < 3) return 0
-            if (i < 3 && j < 6) return 1
-            if (i < 3 && j < 9) return 2
-            if (i < 6 && j < 3) return 3
-            if (i < 6 && j < 6) return 4
-            if (i < 6 && j < 9) return 5
-            if (i < 9 && j < 3) return 6
-            if (i < 9 && j < 6) return 7
-            if (i < 9 && j < 9) return 8
-            return 0
+    private fun solve(board: Array<CharArray>): Boolean {
+        for (row in 0..8) {
+            for (col in 0..8) {
+                if (board[row][col] == '.') {
+                    // 1부터 9까지 숫자 시도
+                    for (num in '1'..'9') {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num
+
+                            if (solve(board)) {
+                                return true
+                            }
+
+                            board[row][col] = '.'
+                        }
+                    }
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun isValid(board: Array<CharArray>, row: Int, col: Int, num: Char): Boolean {
+        // 1. 같은 행에 중복 숫자가 있는지 확인
+        for (i in 0..8) {
+            if (board[row][i] == num) {
+                return false
+            }
         }
 
-        for (i in 0 until 9) {
-            for (j in 0 until 9) {
-                if (board[i][j] != '.') {
-                    val num = board[i][j] - '0'
-                    if (rowValid[i][num] || columnValid[j][num] || cellValid[getCellNumber(i, j)][num]) {
-                        return false
-                    }
-                    rowValid[i][num] = true
-                    columnValid[j][num] = true
-                    cellValid[getCellNumber(i, j)][num] = true
+        // 2. 같은 열에 중복 숫자가 있는지 확인
+        for (i in 0..8) {
+            if (board[i][col] == num) {
+                return false
+            }
+        }
+
+        // 3. 같은 3x3 박스에 중복 숫자가 있는지 확인
+        val boxRow = (row / 3) * 3
+        val boxCol = (col / 3) * 3
+
+        for (i in boxRow until boxRow + 3) {
+            for (j in boxCol until boxCol + 3) {
+                if (board[i][j] == num) {
+                    return false
                 }
             }
         }
