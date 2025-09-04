7c00bd11274878e00e6f4f2daa65c3edc95945d9
seonb2n <626ksb@gmail.com>
2025-09-04 18:02:34 +0900

    - leetcode - https://leetcode.com/problems/find-closest-person/?envType=daily-question&envId=2025-09-04 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,34 +3,15 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/find-the-number-of-ways-to-place-people-ii/?envType=daily-question&envId=2025-09-03
+// https://leetcode.com/problems/find-closest-person/?envType=daily-question&envId=2025-09-04
 class Solution {
-    fun numberOfPairs(points: Array<IntArray>): Int {
-        val n = points.size
-        if (n < 2) return 0
+    fun findClosest(x: Int, y: Int, z: Int): Int {
+        val first = abs(x - z)
+        val second = abs(y - z)
 
-        val sorted = points.sortedWith(compareBy<IntArray> { it[0] }.thenByDescending { it[1] })
-
-        var count = 0
-
-        for (i in 0 until n) {
-            val pointA = sorted[i] // upperLeft
-            var xMin = pointA[0]-1
-            var xMax = Int.MAX_VALUE
-            var yMin = Int.MIN_VALUE
-            var yMax = pointA[1]+1
-
-            for (j in i + 1 until n) {
-                val pointB = sorted[j]
-                if (xMin < pointB[0] && pointB[0] < xMax
-                    && yMin < pointB[1] && pointB[1] < yMax) {
-                    count++
-                    xMin = pointB[0]
-                    yMin = pointB[1]
-                }
-            }
-        }
-
-        return count
+        if (first == second) return 0
+        if (first > second) return 2
+        if (first < second) return 1
+        return 0
     }
 }
