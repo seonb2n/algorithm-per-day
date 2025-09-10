69b1eb6b53900224e7d24a75c1d6a5151dada488
seonb2n <626ksb@gmail.com>
2025-09-10 17:54:56 +0900

    - leetcode - https://leetcode.com/problems/minimum-number-of-people-to-teach/submissions/1765842232/?envType=daily-question&envId=2025-09-10 - beats 25%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,28 +3,41 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/?envType=daily-question&envId=2025-09-08
+// https://leetcode.com/problems/minimum-number-of-people-to-teach/submissions/1765842232/?envType=daily-question&envId=2025-09-10
 class Solution {
-    fun getNoZeroIntegers(n: Int): IntArray {
-        val result = IntArray(2)
-        result[0] = 1
-        result[1] = n - 1
+    fun minimumTeachings(n: Int, languages: Array<IntArray>, friendships: Array<IntArray>): Int {
+        val userLanguages = languages.map { it.toSet() }
 
+        val cannotCommunicate = mutableListOf<Pair<Int, Int>>()
 
-        fun checkHasZero(a: Int): Boolean {
-            return a.toString().contains("0")
-        }
+        for (f in friendships) {
+            val user1 = f[0] - 1
+            val user2 = f[1] - 1
 
-        while(result[0] <= result[1]) {
-            if (!checkHasZero(result[0]) && !checkHasZero(result[1])) {
-                break
-            } else {
-                result[0]++
-                result[1]--
+            if (userLanguages[user1].intersect(userLanguages[user2]).isEmpty()) {
+                cannotCommunicate.add(Pair(user1, user2))
             }
         }
 
+        var result = Int.MAX_VALUE
 
-        return result
+        for (lang in 1..n) {
+            // 언어의 가르침이 필요한 사용자
+            val needToTeach = mutableSetOf<Int>()
+
+            for ((u1, u2) in cannotCommunicate) {
+                if (lang !in userLanguages[u1] && lang !in userLanguages[u2]) {
+                    needToTeach.add(u1)
+                    needToTeach.add(u2)
+                } else if (lang !in userLanguages[u1]) {
+                    needToTeach.add(u1)
+                } else if (lang !in userLanguages[u2]) {
+                    needToTeach.add(u2)
+                }
+            }
+
+            result = minOf(result, needToTeach.size)
+        }
+        return if (result == Int.MAX_VALUE) 0 else result
     }
 }
