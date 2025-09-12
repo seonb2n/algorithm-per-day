6077991ea15d690b6b1d581b455d61fffeba38ce
seonb2n <626ksb@gmail.com>
2025-09-12 19:18:07 +0900

    - leetcode - https://leetcode.com/problems/vowels-game-in-a-string/?envType=daily-question&envId=2025-09-12 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,34 +3,17 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/sort-vowels-in-a-string/?envType=daily-question&envId=2025-09-11
+// https://leetcode.com/problems/vowels-game-in-a-string/?envType=daily-question&envId=2025-09-12
 class Solution {
-    fun sortVowels(s: String): String {
-        val vowels = setOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
-
-        val vowelList = mutableListOf<Char>()
-        for (char in s) {
-            if (char in vowels) {
-                vowelList.add(char)
+    fun doesAliceWin(s: String): Boolean {
+        val vowels = setOf('a', 'e', 'i', 'o', 'u')
+        var counter = 0
+        for (c in s) {
+            if (vowels.contains(c)) {
+                counter++
             }
         }
 
-        vowelList.sort()
-
-        val sb = StringBuilder()
-        var vowelIndex = 0
-
-        for (char in s) {
-            if (char in vowels) {
-                // vowel인 경우 정렬된 리스트에서 순서대로 가져옴
-                sb.append(vowelList[vowelIndex])
-                vowelIndex++
-            } else {
-                // consonant인 경우 원래 문자 그대로 유지
-                sb.append(char)
-            }
-        }
-
-        return sb.toString()
+        return counter > 0
     }
 }
