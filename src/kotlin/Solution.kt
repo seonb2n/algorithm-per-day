c879ae75c44e1f6dd26bade6bac4ea4b44eaa8e5
seonb2n <626ksb@gmail.com>
2025-09-13 13:27:10 +0900

    - leetcode - https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/?envType=daily-question&envId=2025-09-13 - beats 30%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,17 +3,29 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/vowels-game-in-a-string/?envType=daily-question&envId=2025-09-12
+// https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/?envType=daily-question&envId=2025-09-13
 class Solution {
-    fun doesAliceWin(s: String): Boolean {
-        val vowels = setOf('a', 'e', 'i', 'o', 'u')
-        var counter = 0
+    fun maxFreqSum(s: String): Int {
+        val counter = mutableMapOf<Char, Int>()
+
         for (c in s) {
-            if (vowels.contains(c)) {
-                counter++
+            val now = counter.getOrDefault(c, 0)
+            counter[c] = now + 1
+        }
+
+        val vowles = setOf('a', 'e', 'i', 'o', 'u')
+
+        var vMax = 0
+        var cMax = 0
+
+        for (key in counter.keys) {
+            if (vowles.contains(key)) {
+                vMax = maxOf(vMax, counter[key]!!)
+            } else {
+                cMax = maxOf(cMax, counter[key]!!)
             }
         }
 
-        return counter > 0
+        return vMax + cMax
     }
 }
