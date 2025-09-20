f18b9f52c20fc0b02bbebd04194db7086d321b62
seonb2n <626ksb@gmail.com>
2025-09-20 14:55:34 +0900

    - leetcode - https://leetcode.com/problems/design-spreadsheet/?envType=daily-question&envId=2025-09-19 - beats 100%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,64 +3,29 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/design-task-manager/?envType=daily-question&envId=2025-09-18
-class TaskManager(tasks: List<List<Int>>) {
-    // 전역 우선순위 큐 (priority, taskId) - 최대 힙
-    private val globalQueue = PriorityQueue<Pair<Int, Int>> { a, b ->
-        if (a.first != b.first) b.first.compareTo(a.first) // 우선순위 내림차순
-        else b.second.compareTo(a.second) // taskId 내림차순
+// https://leetcode.com/problems/design-spreadsheet/?envType=daily-question&envId=2025-09-19
+class Spreadsheet(rows: Int) {
+
+    val cellMap: MutableMap<String, Int> = mutableMapOf<String, Int>()
+
+    fun setCell(cell: String, value: Int) {
+        cellMap[cell] = value
     }
 
-    private val taskMap = mutableMapOf<Int, Pair<Int, Int>>()
-
-    init {
-        for (task in tasks) {
-            val (userId, taskId, priority) = task
-            add(userId, taskId, priority)
-        }
+    fun resetCell(cell: String) {
+        cellMap[cell] = 0
     }
 
-    fun add(userId: Int, taskId: Int, priority: Int) {
-        globalQueue.offer(Pair(priority, taskId))
+    fun getValue(formula: String): Int {
+        val exp = formula.substring(1)
+        val nums = exp.split("+")
 
-        taskMap[taskId] = Pair(userId, priority)
+        val first = if (nums[0].contains(Regex(".*[A-Z].*"))) cellMap.getOrDefault(nums[0], 0) else nums[0].toInt()
+        val second = if (nums[1].contains(Regex(".*[A-Z].*"))) cellMap.getOrDefault(nums[1], 0) else nums[1].toInt()
+        return first + second
     }
 
-    fun edit(taskId: Int, newPriority: Int) {
-        val (userId, _) = taskMap[taskId]!!
-        taskMap[taskId] = Pair(userId, newPriority)
-        // 새로운 우선순위로 전역 큐에 추가 (기존 항목은 lazy deletion으로 처리)
-        globalQueue.offer(Pair(newPriority, taskId))
-    }
-
-    fun rmv(taskId: Int) {
-        taskMap.remove(taskId)
-    }
-
-    fun execTop(): Int {
-        // 전역 큐의 최상단이 유효한 작업이 될 때까지 정리
-        while (globalQueue.isNotEmpty()) {
-            val (priority, taskId) = globalQueue.peek()
-
-            // 맵에 존재하고 우선순위가 일치하는지 확인
-            val mapEntry = taskMap[taskId]
-            if (mapEntry != null && mapEntry.second == priority) {
-                // 유효한 작업 발견 - 실행
-                globalQueue.poll()
-                val userId = mapEntry.first
-                taskMap.remove(taskId)
-                return userId
-            } else {
-                // 무효한 작업이므로 큐에서 제거
-                globalQueue.poll()
-            }
-        }
-
-        // 실행할 작업이 없는 경우
-        return -1
-    }
 }
-
 /**
  * Your TaskManager object will be instantiated and called as such:
  * var obj = TaskManager(tasks)
