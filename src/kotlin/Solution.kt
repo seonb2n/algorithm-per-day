be4ab4000dff57af72897bf5a0fb27a0b1eed3ea
seonb2n <626ksb@gmail.com>
2025-09-22 20:52:17 +0900

    - leetcode - https://leetcode.com/problems/count-elements-with-maximum-frequency/submissions/1779030919/?envType=daily-question&envId=2025-09-22 - beats 67%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,108 +3,27 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/implement-router/?envType=daily-question&envId=2025-09-20
-class Router(memoryLimit: Int) {
+// https://leetcode.com/problems/count-elements-with-maximum-frequency/submissions/1779030919/?envType=daily-question&envId=2025-09-22
+class Solution {
+    fun maxFrequencyElements(nums: IntArray): Int {
+        val numberMap = mutableMapOf<Int, Int>()
 
-    private val packetSet = mutableSetOf<Packet>()
-    private val packetQueue = LinkedList<Packet>()
-    private val limit = memoryLimit
-
-    private val destinationLists = mutableMapOf<Int, MutableList<Packet>>()
-
-    fun addPacket(source: Int, destination: Int, timestamp: Int): Boolean {
-        val next = Packet(source, destination, timestamp)
-
-        if (packetSet.contains(next)) {
-            return false
+        for (n in nums) {
+            numberMap[n] = numberMap.getOrDefault(n, 0) + 1
         }
 
-        if (packetQueue.size >= limit) {
-            val oldest = packetQueue.poll()
-            removeFromDestinationList(oldest)
-            packetSet.remove(oldest)
-        }
-
-        packetQueue.add(next)
-        packetSet.add(next)
-        addToDestinationList(next)
-        return true
-    }
-
-    fun forwardPacket(): IntArray {
-        val packet = packetQueue.poll()
-        if (packet == null) {
-            return intArrayOf()
-        }
-
-        packetSet.remove(packet)
-        removeFromDestinationList(packet)
-        return intArrayOf(packet.source, packet.destination, packet.timestamp)
-    }
-
-
-    fun getCount(destination: Int, startTime: Int, endTime: Int): Int {
-        val packetList = destinationLists[destination] ?: return 0
-
-        val startIndex = findFirstGreaterOrEqual(packetList, startTime)
-        if (startIndex >= packetList.size) return 0
-
-        val endIndex = findLastLessOrEqual(packetList, endTime)
-        if (endIndex < 0) return 0
-
-        return if (endIndex >= startIndex) endIndex - startIndex + 1 else 0
-    }
-
-    private fun addToDestinationList(packet: Packet) {
-        val list = destinationLists.computeIfAbsent(packet.destination) { mutableListOf() }
-        list.add(packet)
-    }
-
-    private fun removeFromDestinationList(packet: Packet) {
-        val list = destinationLists[packet.destination] ?: return
-        list.remove(packet)
-
-        if (list.isEmpty()) {
-            destinationLists.remove(packet.destination)
-        }
-    }
-
-    private fun findFirstGreaterOrEqual(list: List<Packet>, target: Int): Int {
-        var left = 0
-        var right = list.size
-
-        while (left < right) {
-            val mid = (left + right) / 2
-            if (list[mid].timestamp >= target) {
-                right = mid
-            } else {
-                left = mid + 1
+        var max = 0
+        var counter = 0
+        for (key in numberMap.keys) {
+            if (numberMap[key]!! > max) {
+                counter = numberMap[key]!!
+                max = numberMap[key]!!
+            }
+            else if (numberMap[key]!! == max) {
+                counter += numberMap[key]!!
             }
         }
 
-        return left
+        return counter
     }
-
-    private fun findLastLessOrEqual(list: List<Packet>, target: Int): Int {
-        var left = -1
-        var right = list.size - 1
-
-        while (left < right) {
-            val mid = (left + right + 1) / 2
-            if (list[mid].timestamp <= target) {
-                left = mid
-            } else {
-                right = mid - 1
-            }
-        }
-
-        return left
-    }
-
-
-    private data class Packet(
-        val source: Int,
-        val destination: Int,
-        val timestamp: Int
-    )
 }
