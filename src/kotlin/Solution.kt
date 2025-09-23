b82800b9f1a5b0f102bb6df9c6f4ae5b6210c6e2
seonb2n <626ksb@gmail.com>
2025-09-23 20:14:21 +0900

    - leetcode - https://leetcode.com/problems/compare-version-numbers/description/?envType=daily-question&envId=2025-09-23 - beats 77%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,27 +3,39 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/count-elements-with-maximum-frequency/submissions/1779030919/?envType=daily-question&envId=2025-09-22
+// https://leetcode.com/problems/compare-version-numbers/description/?envType=daily-question&envId=2025-09-23
 class Solution {
-    fun maxFrequencyElements(nums: IntArray): Int {
-        val numberMap = mutableMapOf<Int, Int>()
+    fun compareVersion(version1: String, version2: String): Int {
+        val first = version1.split(".")
+        val second = version2.split(".")
 
-        for (n in nums) {
-            numberMap[n] = numberMap.getOrDefault(n, 0) + 1
+        fun compare(index: Int): Int {
+            if (index >= first.size && index >= second.size) {
+                return 0
+            }
+            else if (index >= first.size && index < second.size) {
+                if (second[index].toInt() > 0) {
+                    return -1
+                } else {
+                    return compare(index + 1)
+                }
+            }
+            else if (index >= second.size && index < first.size) {
+                if (first[index].toInt() > 0) {
+                    return 1
+                } else {
+                    return compare(index + 1)
+                }
+            }
+            else {
+                if (first[index].toInt() == second[index].toInt()) {
+                    return compare(index + 1)
+                } else {
+                    return if (first[index].toInt() > second[index].toInt()) 1 else -1
+                }
+            }
         }
 
-        var max = 0
-        var counter = 0
-        for (key in numberMap.keys) {
-            if (numberMap[key]!! > max) {
-                counter = numberMap[key]!!
-                max = numberMap[key]!!
-            }
-            else if (numberMap[key]!! == max) {
-                counter += numberMap[key]!!
-            }
-        }
-
-        return counter
+        return compare(0)
     }
 }
