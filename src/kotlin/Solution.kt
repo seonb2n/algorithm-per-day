3dfc8112502f2e33959432b3bdf0f2fad4dfbc42
Seonbin KIM <626ksb@gmail.com>
2025-09-26 15:09:18 +0900

    Update Solution.kt
    - https://leetcode.com/problems/valid-triangle-number/
    - beats 60%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,39 +3,25 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/compare-version-numbers/description/?envType=daily-question&envId=2025-09-23
+// https://leetcode.com/problems/valid-triangle-number/
 class Solution {
-    fun compareVersion(version1: String, version2: String): Int {
-        val first = version1.split(".")
-        val second = version2.split(".")
+    fun triangleNumber(nums: IntArray): Int {
+        var counter = 0
+        nums.sort()
+        for (i in 2 until nums.size) {
+            val c = nums[i]
+            var left = 0
+            var right = i-1
 
-        fun compare(index: Int): Int {
-            if (index >= first.size && index >= second.size) {
-                return 0
-            }
-            else if (index >= first.size && index < second.size) {
-                if (second[index].toInt() > 0) {
-                    return -1
+            while (left < right) {
+                if (nums[left] + nums[right] > c) {
+                    counter += (right - left)
+                    right--
                 } else {
-                    return compare(index + 1)
-                }
-            }
-            else if (index >= second.size && index < first.size) {
-                if (first[index].toInt() > 0) {
-                    return 1
-                } else {
-                    return compare(index + 1)
-                }
-            }
-            else {
-                if (first[index].toInt() == second[index].toInt()) {
-                    return compare(index + 1)
-                } else {
-                    return if (first[index].toInt() > second[index].toInt()) 1 else -1
+                    left++
                 }
             }
         }
-
-        return compare(0)
+        return counter
     }
 }
