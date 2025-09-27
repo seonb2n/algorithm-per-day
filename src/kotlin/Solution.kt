78098728099d74be32a3ee99340d76b00f7be09c
seonb2n <626ksb@gmail.com>
2025-09-27 19:07:51 +0900

    - leetcode - https://leetcode.com/problems/largest-triangle-area/submissions/1784079192/?envType=daily-question&envId=2025-09-27 - beats 77%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,29 +3,25 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/triangle/description/?envType=daily-question&envId=2025-09-25
+// https://leetcode.com/problems/largest-triangle-area/submissions/1784079192/?envType=daily-question&envId=2025-09-27ㅑ
 class Solution {
-    fun minimumTotal(triangle: List<List<Int>>): Int {
-        val n = triangle.size
-        // dp[i][j] = i층의 j번째 위치까지 오는 최소 경로 합
-        val dp = Array(n) { i -> IntArray(triangle[i].size) { Int.MAX_VALUE } }
-        
-        // 초기값: 맨 위 꼭짓점
-        dp[0][0] = triangle[0][0]
-        
-        for (i in 1 until n) {
-            for (j in triangle[i].indices) {
-                // 왼쪽 위에서 오는 경우 (i-1, j-1)
-                if (j - 1 >= 0) {
-                    dp[i][j] = minOf(dp[i][j], dp[i-1][j-1] + triangle[i][j])
-                }
-                // 바로 위에서 오는 경우 (i-1, j)
-                if (j < triangle[i-1].size) {
-                    dp[i][j] = minOf(dp[i][j], dp[i-1][j] + triangle[i][j])
+    fun largestTriangleArea(points: Array<IntArray>): Double {
+        var maxArea = 0.0
+        val n = points.size
+
+        for (i in 0 until n) {
+            for (j in i + 1 until n) {
+                for (k in j + 1 until n) {
+                    val (x1, y1) = points[i]
+                    val (x2, y2) = points[j]
+                    val (x3, y3) = points[k]
+
+                    val area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+                    maxArea = max(maxArea, area)
                 }
             }
         }
-        
-        return dp[n-1].min()!!
+
+        return maxArea
     }
 }
