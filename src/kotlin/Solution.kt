64f9accc71ed7d820a7b237541d0a32a1e43547b
seonb2n <626ksb@gmail.com>
2025-11-15 11:49:19 +0900

    - leetcode - https://leetcode.com/problems/final-value-of-variable-after-performing-operations/?envType=daily-question&envId=2025-10-20 - beats 50%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,17 +3,30 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/final-value-of-variable-after-performing-operations/?envType=daily-question&envId=2025-10-20
+// https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i/?envType=daily-question&envId=2025-11-04
 class Solution {
-    fun finalValueAfterOperations(operations: Array<String>): Int {
-        var result = 0
-        for (opr in operations) {
-            when(opr) {
-                "++X" -> result++
-                "X++" -> result++
-                "X--" -> result--
-                "--X" -> result--
+    fun findXSum(nums: IntArray, k: Int, x: Int): IntArray {
+        val len = nums.size - k + 1
+        val result = IntArray(len)
+
+        for (i in 0 until len) {
+            val map = mutableMapOf<Int, Int>()
+
+            for (j in i until i + k) {
+                map[nums[j]] = map.getOrDefault(nums[j], 0) + 1
             }
+
+            val sorted = map.entries.sortedWith(
+                compareByDescending<Map.Entry<Int, Int>> { it.value }
+                    .thenByDescending { it.key }
+            )
+
+            var now = 0
+            for (entry in sorted.take(x)) {
+                now += entry.key * entry.value
+            }
+
+            result[i] = now
         }
 
         return result
