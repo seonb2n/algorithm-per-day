fb3a1c2a518b496336b586cb0a507895533b2b81
seonb2n <626ksb@gmail.com>
2025-11-30 12:21:36 +0900

    - leetcode - https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/submissions/1842959774/?envType=daily-question&envId=2025-11-29 - beats 60%

--- src/kotlin/Solution.kt
+++ src/kotlin/Solution.kt
@@ -3,32 +3,12 @@ package kotlin
 import java.util.*
 import kotlin.math.ceil
 
-// https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/?envType=daily-question&envId=2025-11-15
+// https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/?envType=daily-question&envId=2025-11-29
 class Solution {
-    fun numberOfSubstrings(s: String): Int {
-        val n = s.length
-        var count = 0
+    fun minOperations(nums: IntArray, k: Int): Int {
+        val sum = nums.sum()
 
-        // 0의 개수를 고정하고 탐색
-        for (i in 0 until n) {
-            var zeros = 0
-            var ones = 0
-
-            for (j in i until n) {
-                if (s[j] == '0') {
-                    zeros++
-                    // 0이 너무 많아지면 조기 종료
-                    if (zeros * zeros > n) break
-                } else {
-                    ones++
-                }
-
-                if (ones >= zeros * zeros) {
-                    count++
-                }
-            }
-        }
-
-        return count
+        val rest = sum % k
+        return rest
     }
 }
